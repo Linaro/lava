955431f992421f6b335fde8e1dcd53b9df3a5cbc
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2010-06-28 18:17:02 +0200

    Add support for checking code coverage

--- test.py
+++ test.py
@@ -3,12 +3,35 @@
 Helper script for runnint launch_control tests
 """
 
+import sys
 import unittest
 
 if __name__ == "__main__":
-    # XXX: Hack, this is related to the fact that unittest uses
-    # __import__() and does not use fromlist=[''] to import packages
-    # correctly. Fortunately (?) python imports sub-packages when the
-    # package itself is already imported. This is ugly but it works
-    import launch_control.tests
-    unittest.main(defaultTest='launch_control.tests.test_suite')
+    try:
+        import coverage
+    except ImportError:
+        coverage = None
+    else:
+        # Start coverage check before importing from mocker, to get all of it.
+        coverage.erase()
+        coverage.start()
+    try:
+        # XXX: Hack, this is related to the fact that unittest uses
+        # __import__() and does not use fromlist=[''] to import packages
+        # correctly. Fortunately (?) python imports sub-packages when the
+        # package itself is already imported. This is ugly but it works
+        import launch_control.tests
+        unittest.main(defaultTest='launch_control.tests.test_suite')
+    finally:
+        if coverage and len(sys.argv) == 1:
+            coverage.stop()
+            from launch_control.tests import app_modules
+            mod_paths = [module.replace(".", "/") + ".py" for module in app_modules()]
+            coverage.report(mod_paths)
+        elif len(sys.argv) != 1:
+            # don't about not running code coverage tests when we're
+            # testing specific thing
+            pass
+        else:
+            print "WARNING: No coverage test performed"
+            print "Please install python-coverage package"
