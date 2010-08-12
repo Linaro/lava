e44d8f347e5d7f64de454e67f9f4ecf7f71dc763
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2010-08-12 11:40:59 +0200

    Minor cleanup in PluggableJSONDecoder:  - replace all commented-out print statements with debug logging messages  - use "unmarshall" instead of "awake"

--- launch_control/utils/json/decoder.py
+++ launch_control/utils/json/decoder.py
@@ -5,6 +5,8 @@ from . import (
         ISimpleJSONType,
         mod_json)
 
+import logging
+
 class PluggableJSONDecoder(mod_json.JSONDecoder):
     """
     JSON decoder with special support for ISimpleJSONType and
@@ -23,6 +25,7 @@ class PluggableJSONDecoder(mod_json.JSONDecoder):
         self._registry = registry
         self._type_expr = type_expr
         self._class_hint = class_hint
+        self.logger = logging.getLogger("PluggableJSONDecoder")
         super(PluggableJSONDecoder, self).__init__(
                 object_hook = self._object_hook, **kwargs)
 
@@ -48,41 +51,44 @@ class PluggableJSONDecoder(mod_json.JSONDecoder):
     def raw_decode(self, s, **kw):
         obj, end = super(PluggableJSONDecoder, self).raw_decode(s, **kw)
         if self._type_expr:
-            obj = self._awaken(obj, self._type_expr)
+            obj = self._unmarshall(obj, self._type_expr)
         return obj, end
 
-    def _awaken_dict(self, json_doc, type_expr):
+    def _unmarshall_dict(self, json_doc, type_expr):
         """
-        Awaken a dictionary.
+        Unmarshall a JSON object to a python dictionary of objects
         """
         if not isinstance(json_doc, dict):
-            raise ValueError("Awakened object is not a dictionary")
-        #print "Waking up a dictionary with types: %r" % (type_expr,)
+            raise ValueError("unmarshalled object is not a dictionary")
+        self.logger.debug("Unmarshalling a dictionary with types: %r",
+                type_expr)
         for key_name, value_type in type_expr.iteritems():
             if key_name in json_doc:
-                #print "Translating element %r with type %r" % (key_name, value_type)
-                value = self._awaken(json_doc[key_name], value_type)
-                #print "Translated element %r to value %r" % (key_name, value)
+                self.logger.debug("Translating element %r with type %r",
+                        key_name, value_type)
+                value = self._unmarshall(json_doc[key_name], value_type)
+                self.logger.debug("Translated element %r to value %r",
+                        key_name, value)
                 json_doc[key_name] = value
         return json_doc
 
-    def _awaken_list(self, json_doc, type_expr):
+    def _unmarshall_list(self, json_doc, type_expr):
         """
-        Awaken a list
+        Unmarshall a list of python objects from a JSON list
         """
         if not isinstance(json_doc, list):
-            raise ValueError("Awakened object is not a list")
+            raise ValueError("unmarshalled object is not a list")
         cls = type_expr[0]
-        #print "Waking up a list of %s" % (cls,)
-        return [self._awaken(item, cls) for item in json_doc]
+        self.logger.debug("Unmarshalling a list of %s", cls)
+        return [self._unmarshall(item, cls) for item in json_doc]
 
-    def _awaken_object(self, json_doc, type_expr):
-        #print "Waking up object based on type: %r" % (type_expr,)
+    def _unmarshall_object(self, json_doc, type_expr):
+        self.logger.debug("Unmarshalling object based on type: %r", type_expr)
         cls = type_expr
         # Find a proxy class if possible
         if cls in self._registry.proxies:
             proxy_cls = self._registry.proxies[cls]
-            #print "Remapped type expression to %r" % (proxy_cls,)
+            self.logger.debug("Remapped type expression to %r", proxy_cls)
         else:
             proxy_cls = cls
         # The class we're working with _must_ be one of the supported
@@ -90,9 +96,11 @@ class PluggableJSONDecoder(mod_json.JSONDecoder):
         if not issubclass(proxy_cls, (IFundamentalJSONType,
             ISimpleJSONType, IComplexJSONType)):
             raise TypeError("Incorrect type expression: %r" % (proxy_cls,))
-        #print "Waking up a instance of %r using %r" % (cls, proxy_cls)
+        self.logger.debug("Unmarshalling a instance of %r using %r",
+                cls, proxy_cls)
         if issubclass(proxy_cls, IComplexJSONType):
-            #print "Translating attributes for object of class %r" % (proxy_cls,)
+            self.logger.debug("Translating attributes for object of "
+                    "class %r", proxy_cls)
             if not isinstance(json_doc, dict):
                 raise ValueError("When waking up %r via %r the JSON "
                         "document was not a dictionary" % (cls,
@@ -101,26 +109,29 @@ class PluggableJSONDecoder(mod_json.JSONDecoder):
             new_json_doc = {}
             for attr_name, value in json_doc.iteritems():
                 if attr_name in types:
-                    #print "Translating attribute %r with type %r" % (attr_name, types[attr_name])
-                    value = self._awaken(value, types[attr_name])
-                    #print "Translated attribute %r to value %r" % (attr_name, value)
+                    self.logger.debug("Translating attribute %r with type %r", 
+                            attr_name, types[attr_name])
+                    value = self._unmarshall(value, types[attr_name])
+                    self.logger.debug("Translated attribute %r to value %r", 
+                            attr_name, value)
                 new_json_doc[attr_name] = value
             json_doc = new_json_doc
-        #print "Attempting to instantiate %r from %r" % (cls, json_doc)
+        self.logger.debug("Attempting to instantiate %r from %r", cls,
+                json_doc)
         obj = proxy_cls.from_json(json_doc)
         if not isinstance(obj, cls):
             raise TypeError("Object instantiated using %r is not of"
                     " expected type %r" % (proxy_cls, cls))
-        #print "Instantiated %r" % (obj,)
+        self.logger.debug("Instantiated %r", obj)
         return obj
 
-    def _awaken(self, json_doc, type_expr):
+    def _unmarshall(self, json_doc, type_expr):
         """
-        Awaken objects from their JSON representation.
+        Unmarshall objects from their JSON representation.
         """
         if isinstance(type_expr, list):
-            return self._awaken_list(json_doc, type_expr)
+            return self._unmarshall_list(json_doc, type_expr)
         if isinstance(type_expr, dict):
-            return self._awaken_dict(json_doc, type_expr)
+            return self._unmarshall_dict(json_doc, type_expr)
         else:
-            return self._awaken_object(json_doc, type_expr)
+            return self._unmarshall_object(json_doc, type_expr)
