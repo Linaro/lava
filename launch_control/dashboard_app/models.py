29eb2ed32103f647c2954aeb4307d1b09bb15d7a
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2010-08-26 19:27:06 +0200

    Add NamedAttribute and HardwareDevice models, unit tests and admin interface

--- launch_control/dashboard_app/models.py
+++ launch_control/dashboard_app/models.py
@@ -2,6 +2,8 @@
 Database models of the Dashboard application
 """
 
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import ugettext as _
 from django.utils.translation import ungettext
@@ -37,3 +39,62 @@ class SoftwarePackage(models.Model):
 
     class Meta:
         unique_together = (('name', 'version'))
+
+
+class NamedAttribute(models.Model):
+    """
+    Model for adding generic named attributes
+    to arbitrary other model instances.
+
+    Example:
+        class Foo(Model):
+            attributes = generic.GenericRelation(NamedAttribute)
+    """
+
+    name = models.CharField(
+            help_text = _help_max_length(32),
+            max_length = 32)
+
+    value = models.CharField(
+            help_text = _help_max_length(256),
+            max_length = 256)
+
+    # Content type plumbing
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+
+    def __unicode__(self):
+        return _(u"Attribute {name}: {value}").format(
+                name = self.name,
+                value = self.value)
+
+    class Meta:
+        unique_together = (('object_id', 'name'))
+
+
+class HardwareDevice(models.Model):
+    """
+    Model for hardware devices
+
+    All devices are simplified into an instance of pre-defined class
+    with arbitrary key-value attributes.
+    """
+    device_type = models.CharField(
+            verbose_name = _(u"Device Type"),
+            help_text = _(u"One of pre-defined device types"),
+            max_length = 32,
+            choices = (
+                (u"device.cpu", _(u"CPU")),
+                (u"device.mem", _(u"Memory")),
+                (u"device.usb", _(u"USB device")),
+                (u"device.pci", _(u"PCI device")),
+                (u"device.board", _(u"Board/Motherboard"))))
+
+    description = models.CharField(
+            verbose_name = _(u"Description"),
+            help_text = (_(u"Human readable device summary.")
+                + " " + _help_max_length(256)),
+            max_length = 256)
+
+    attributes = generic.GenericRelation(NamedAttribute)
--- launch_control/dashboard_app/tests.py
+++ launch_control/dashboard_app/tests.py
@@ -2,11 +2,13 @@
 Unit tests of the Dashboard application
 """
 
-from django.test import TestCase
+from django.contrib.contenttypes import generic
 from django.db import IntegrityError
+from django.test import TestCase
 
 from launch_control.utils.call_helper import ObjectFactoryMixIn
 from launch_control.dashboard_app.models import (
+        HardwareDevice,
         SoftwarePackage,
         )
 
@@ -28,3 +30,26 @@ class SoftwarePackageTestCase(TestCase, ObjectFactoryMixIn):
         pkg1.save()
         pkg2 = self.make(SoftwarePackage)
         self.assertRaises(IntegrityError, pkg2.save)
+
+
+class HardwarePackageTestCase(TestCase, ObjectFactoryMixIn):
+
+    class Dummy:
+        class HardwareDevice:
+            device_type = 'device.cpu'
+            description = 'some cpu'
+    
+    def test_creation_1(self):
+        dummy, hw_device = self.make_and_get_dummy(HardwareDevice)
+        self.assertEqual(hw_device.device_type, dummy.device_type)
+        self.assertEqual(hw_device.description, dummy.description)
+
+    def test_attributes(self):
+        hw_device = self.make(HardwareDevice)
+        hw_device.save()
+        hw_device.attributes.create(name="connection-bus", value="usb")
+        self.assertEqual(hw_device.attributes.count(), 1)
+        attr = hw_device.attributes.get()
+        self.assertEqual(attr.name, "connection-bus")
+        self.assertEqual(attr.value, "usb")
+
