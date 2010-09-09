5af3213a9ee6d1449da09254186eed519858894a
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2010-09-09 18:31:13 +0200

    Add views bundle_stream_list() and bundle_stream_detail()

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -2,13 +2,15 @@
 Views for the Dashboard application
 """
 
-# Original inspiration from:
-# Brendan W. McAdams <brendan.mcadams@thewintergrp.com>
-from django.conf import settings
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import (
+        get_object_or_404,
+        render_to_response,
+        )
+from django.views.generic import list_detail
 
 from dashboard_app.dispatcher import DjangoXMLRPCDispatcher
+from dashboard_app.models import (Bundle, BundleStream)
 from dashboard_app.xmlrpc import DashboardAPI
 
 
@@ -25,6 +27,8 @@ def _get_dashboard_dispatcher():
 DashboardDispatcher = _get_dashboard_dispatcher()
 
 
+# Original inspiration from:
+# Brendan W. McAdams <brendan.mcadams@thewintergrp.com>
 def xml_rpc_handler(request, dispatcher):
     """
     XML-RPC handler.
@@ -53,3 +57,42 @@ def xml_rpc_handler(request, dispatcher):
 
 def dashboard_xml_rpc_handler(request):
     return xml_rpc_handler(request, DashboardDispatcher)
+
+
+def bundle_stream_list(request):
+    """
+    List of bundle streams.
+
+    The list is paginated and dynamically depends on the currently
+    logged in user.
+    """
+    bundle_streams = BundleStream.get_allowed_for_user(request.user).order_by('pathname')
+    return list_detail.object_list(request,
+            paginate_by = 25,
+            queryset = bundle_streams,
+            template_name = 'dashboard_app/bundle_stream_list.html',
+            template_object_name = 'bundle_stream',
+            )
+
+
+def bundle_stream_detail(request, pathname):
+    """
+    List of bundle streams.
+
+    The list is paginated and dynamically depends on the currently
+    logged in user.
+    """
+    bundle_stream = get_object_or_404(BundleStream, pathname=pathname)
+    if bundle_stream.can_access(request.user):
+        return list_detail.object_detail(request,
+                queryset = BundleStream.objects.all(),
+                slug_field = 'pathname',
+                slug = pathname,
+                template_name = 'dashboard_app/bundle_stream_detail.html',
+                template_object_name = 'bundle_stream',
+                )
+    else:
+        resp = render_to_response("403.html",
+                context_instance=RequestContext(request))
+        resp.status_code = 403
+        return resp
