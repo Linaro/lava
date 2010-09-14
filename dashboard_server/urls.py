c03aa5e6a2457ddf7a2cebfbb026ed483cb81e43
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2010-09-14 12:56:45 +0200

    Add django.contrib.databrowse support

--- dashboard_server/urls.py
+++ dashboard_server/urls.py
@@ -1,13 +1,33 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.contrib import databrowse
 from django.views.generic.simple import direct_to_template
 
 from dashboard_app.views import dashboard_xml_rpc_handler
+from dashboard_app.models import (
+    Bundle,
+    BundleStream,
+    HardwareDevice,
+    NamedAttribute,
+    SoftwarePackage,
+    Test,
+    TestCase,
+    TestRun
+)
 
+# Register our models with data browser
+databrowse.site.register(Bundle)
+databrowse.site.register(BundleStream)
+databrowse.site.register(HardwareDevice)
+databrowse.site.register(NamedAttribute)
+databrowse.site.register(SoftwarePackage)
+databrowse.site.register(Test)
+databrowse.site.register(TestCase)
+databrowse.site.register(TestRun)
 
+# Enable admin stuff
 admin.autodiscover()
-
 
 urlpatterns = patterns('',
     url(r'^$', direct_to_template,
@@ -18,6 +38,7 @@ urlpatterns = patterns('',
         kwargs={'template': 'about_alpha.html'}),
     url(r'^xml-rpc/', dashboard_xml_rpc_handler,
         name='xml-rpc-handler'),
+    url(r'^databrowse/(.*)', databrowse.site.root),
     (r'^admin/', include(admin.site.urls)),
     )
 
