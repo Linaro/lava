90a7c9777b14db8680be10ade1ae6120c95a6417
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2010-09-14 12:35:09 +0200

    Fix typo in error message

--- dashboard_app/fixtures.py
+++ dashboard_app/fixtures.py
@@ -92,7 +92,7 @@ def created_bundles(spec):
     for pathname, content_filename, content in spec:
         pathname_parts = pathname.split('/')
         if len(pathname_parts) < 3:
-            raise ValueError("Pathname to short: %r" % pathname)
+            raise ValueError("Pathname too short: %r" % pathname)
         if pathname_parts[0] != '':
             raise ValueError("Pathname must be absolute: %r" % pathname)
         if pathname_parts[1] == 'anonymous':
@@ -102,7 +102,7 @@ def created_bundles(spec):
             correct_length = 2
         elif pathname_parts[1] == 'personal':
             if len(pathname_parts) < 4:
-                raise ValueError("Pathname to short: %r" % pathname)
+                raise ValueError("Pathname too short: %r" % pathname)
             user = User.objects.create(username=pathname_parts[2])
             user.save()
             group = None
@@ -110,7 +110,7 @@ def created_bundles(spec):
             correct_length = 3
         elif pathname_parts[1] == 'team':
             if len(pathname_parts) < 4:
-                raise ValueError("Pathname to short: %r" % pathname)
+                raise ValueError("Pathname too short: %r" % pathname)
             user = None
             group = Group.objects.create(name=pathname_parts[2])
             group.save()
