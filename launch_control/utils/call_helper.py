ab9427392be08f71bd750eb56a8e06579c0cec12
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2010-09-22 21:31:02 +0200

    Change file headers to refer to version 3 of the license, not 3 or any later version

--- launch_control/utils/call_helper.py
+++ launch_control/utils/call_helper.py
@@ -6,9 +6,8 @@
 # This file is part of Launch Control.
 #
 # Launch Control is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation
 #
 # Launch Control is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
