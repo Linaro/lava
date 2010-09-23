511f5c0688e592caf71595e5448266b30e263f0c
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2010-09-23 02:16:21 +0200

    Fix tests after URL changes

--- dashboard_app/tests.py
+++ dashboard_app/tests.py
@@ -706,6 +706,8 @@ class DjangoXMLRPCDispatcherFaultCodeTests(DjangoXMLRPCDispatcherTestCase):
 
 class DashboardAPITestCase(TestCase):
 
+    urls = 'dashboard_app.test_urls'
+
     def setUp(self):
         super(DashboardAPITestCase, self).setUp()
         self.client = Client()
@@ -1131,7 +1133,15 @@ class DjangoTestCaseWithScenarios(TestCase):
         stream = BundleStream.objects.create(slug='')
 
 
-class BundleStreamListViewAnonymousTest(TestCase):
+class DashboardViewTestCase(TestCase):
+    """
+    Helper class that ensures dashboard views are mapped in URLs the way
+    we expect, regardless of actual deployment.
+    """
+    urls = 'dashboard_app.test_urls'
+
+
+class BundleStreamListViewAnonymousTest(DashboardViewTestCase):
 
     _USER = "user"
     _GROUP = "group"
@@ -1204,7 +1214,7 @@ class BundleStreamListViewAuthorizedTest(BundleStreamListViewAnonymousTest):
         self.client.login_user(self.user)
 
 
-class BundleStreamDetailViewAnonymousTest(TestCase):
+class BundleStreamDetailViewAnonymousTest(DashboardViewTestCase):
 
     _USER = "user"
     _GROUP = "group"
@@ -1265,7 +1275,6 @@ class ModelWithAttachments(models.Model):
 class AttachmentTestCase(TestCase):
     _CONTENT = "text"
     _FILENAME = "filename"
-
 
     def setUp(self):
         self.obj = ModelWithAttachments.objects.create()
