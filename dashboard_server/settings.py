4dd26d5b389d62cc356a0449174299b7a2353d08
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2010-09-29 14:09:32 +0300

    Rework configuration so that it's easier to get started

--- dashboard_server/settings.py
+++ dashboard_server/settings.py
@@ -17,42 +17,22 @@
 # along with Launch Control.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Settings module suitable for development
+Dashboard server settings module.
 """
 
 # CONFIGURATION
 # =============
 #
-# To configure the server create local_settings.py and change the
-# following line from `CONFIGURED = False' to `CONFIGURED = True'.
+# To configure the server create local_settings.py and add any django
+# configuration options you care about. Please look at
+# local_settings.py.example to get started.
 #
-# Look at default_settings.py for explanation on what can be changed.
-#
-# When this is False a very simple configuration is created that allows
-# you to run the server directly from the development environment.
-try:
-    from local_settings import CONFIGURED
-except ImportError:
-    CONFIGURED = False
-
 # DO NOT CHANGE SETTINGS BELOW
 # ============================
+
 from default_settings import *
 
-if not CONFIGURED:
-    DATABASE_ENGINE = 'sqlite3'
-    DATABASE_NAME = os.path.join(BASE_DIR, 'database.db')
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-    MEDIA_URL = '/site_media/'
-    DEBUG = True
-    TEMPLATE_DEBUG = DEBUG
-    ADMINS = ()
-    SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-    try:
-        # You still might want this to configure email and
-        # administration stuff. I do this for development 
-        from local_settings import *
-    except ImportError:
-        pass
-else:
+try:
     from local_settings import *
+except ImportError:
+    from development_settings import *
