589e8a045e98e68e2400e733a3bf2329867157ec
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2010-10-07 17:18:35 +0200

    Prepare for test splitup into separate modules.
    This step is fairly simple, all tests are moved to a tests package and __init__
    ensures that all old behaviour is kept as is. Test urls are renamed to
    'tests.urls' test utils were likewise moved to tests.utils.
        
    There was one required change, tests.tests has one model that is needed for
    testing and this model previously belonged to the 'dashboard_app' application.
    After moving tests to a dedicated python package I had to update 'app_label' to
    make django test runner instantiate appropriate database tables.

---
+++ dashboard_app/tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+Package with all tests for dashboard_app
+"""
+
+# We're gradually transitioning to the model where tests are in
+# separate modules. For the time being we're just importing everything
+# from the legacy tests module.
+
+from dashboard_app.tests.tests import *
