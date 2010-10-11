6ced00dbe36c4b80829c4ffa45ef155cdc5ca484
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2010-10-11 11:45:20 +0200

    Don't call old tests legacy, don't use needless underscores

--- dashboard_app/tests/__init__.py
+++ dashboard_app/tests/__init__.py
@@ -6,14 +6,14 @@ import unittest
 
 from testscenarios.scenarios import generate_scenarios
 
-__TESTS__ = [
-    'other.legacy_tests',
+TEST_MODULES = [
+    'other.tests',
 ]
 
 def suite():
     loader = unittest.TestLoader()
     test_suite = unittest.TestSuite()
-    for name in __TESTS__:
+    for name in TEST_MODULES:
         tests = loader.loadTestsFromName('dashboard_app.tests.' + name)
         test_suite.addTests(generate_scenarios(tests))
     return test_suite
