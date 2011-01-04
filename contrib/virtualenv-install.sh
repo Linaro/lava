a78a37424bca83dcbf8c3f88ed00779efb23975f
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-01-04 14:07:36 +0100

    Install additional dependencies

--- contrib/virtualenv-install.sh
+++ contrib/virtualenv-install.sh
@@ -2,6 +2,8 @@
 
 virtualenv --no-site-packages --clear /tmp/foo
 . /tmp/foo/bin/activate
-pip install launch-control
-pip install launch-control-tool
-
+pip install simplejson && \
+pip install launch-control-tool && \
+pip install launch-control && \
+pip install django-testscenarios && \
+echo "All packages installed okay"
