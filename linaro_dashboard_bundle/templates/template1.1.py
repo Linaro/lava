b58d3bb3dde7b1b15d12a8fd8c42bda4bcde0312
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-01-11 17:55:55 +0100

    Explicitly version templates for particular schema versions

--- linaro_dashboard_bundle/templates/template1.1.py
+++ linaro_dashboard_bundle/templates/template1.1.py
@@ -1,20 +1,24 @@
 import datetime
 import uuid
 
-from linaro_dashboard_bundle import schemas
-from linaro_json.document import DocumentTemplate
+from linaro_dashboard_bundle.schemas import format1_1 as schema
+from linaro_json import document
 
 
-class DashboardBundle(DocumentTemplate):
+class DashboardBundle(document.Template):
 
     __schema__ = schemas.DashboardBundle
 
     @property
     def format(self):
-        return "Dashboard Bundle Format 1.0.1"
+        return "Dashboard Bundle Format 1.1"
+
+    @property
+    def test_runs(self):
+        return []
 
 
-class TestRun(DocumentTemplate):
+class TestRun(document.Template):
 
     __schema__ = schemas.TestRun
 
@@ -27,3 +31,7 @@ class TestRun(DocumentTemplate):
         return datetime.datetime.now()
 
     time_check_performed = False
+
+    @property
+    def test_results(self):
+        return []
