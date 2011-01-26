5d8ad626c0978152a46e383c78e161454b80fb04
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-01-26 23:42:07 +0100

    Display test case and test name in test case admin

--- dashboard_app/admin.py
+++ dashboard_app/admin.py
@@ -107,7 +107,8 @@ class HardwareDeviceAdmin(admin.ModelAdmin):
 
 
 class TestCaseAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('test_case_id', 'test',)
+    list_filter = ('test',)
 
 
 class TestAdmin(admin.ModelAdmin):
