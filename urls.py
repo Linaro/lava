ef9d67a8f7279db1ab293de044b10ef350c35fab
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-02-10 14:48:37 +0100

    Move parasitic_cone contents to the root directory.
    The old directory was just a "place" I was using to drive new development and
    as Paul Larson noted it's not really necessary. The code was therefore moved
    to the top-level directory.
    
    In addition due to the way django imports applications and projects I had to
    move lava to a different directory not to cause import failures (you cannot
    have a project called the same way as an existing module or package). I
    therefore moved the directory to liblava.

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^parasitic_cone/', include('parasitic_cone.foo.urls')),
+    # (r'^lava/', include('lava.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
