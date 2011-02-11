5043a6e29c2037ba710d3bcdd19c9c6a46239d8a
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-02-11 01:24:48 +0100

    Add overwatch_driver and overwatch_config to Device model.
    Both of those fields allow to implement the get_overwatch() method that gives
    programmatic control over the device.

--- overwatch/models.py
+++ overwatch/models.py
@@ -27,3 +27,33 @@ class Device(models.Model):
         null=False,
     )
 
+    overwatch_driver = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+        default="dummy"
+    )
+
+    overwatch_config = models.TextField(
+        max_length=65535,
+        null=False,
+        blank=True,
+        default=""
+    )
+
+    def get_overwatch(self):
+        """
+        Return an overwatch driver that manages this device
+        """
+        from pkg_resources import working_set
+        drivers = list(working_set.iter_entry_points("lava.overwatch.drivers", self.overwatch_driver))
+        if len(drivers) == 0:
+            raise ImproperlyConfigured(
+                "Unable to load any overwatch drivers for name %r" % (
+                    self.overwatch_driver))
+        elif len(drivers) > 1:
+            raise ImproperlyConfigured(
+                "There is more than one overwatch driver for name %r" % (
+                    self.overwatch_driver))
+        driver_cls = drivers[0].load()
+        return driver_cls(self.overwatch_config)
