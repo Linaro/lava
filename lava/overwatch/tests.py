c220d217b19494778caf2ac34882f70282c3e74a
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-02-11 12:30:44 +0100

    Add test case that checks Driver.get_overwatch() works for the dummy driver
    This test is very easy to write and does not require mocking the pkg_resources
    API. A few more tests are still needed to cover two possible failures.

--- lava/overwatch/tests.py
+++ lava/overwatch/tests.py
@@ -6,4 +6,14 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from lava.overwatch.models import Device 
 
+
+class DeviceTests(TestCase):
+
+    def test_get_overwatch_success(self):
+        device = Device.objects.create(
+            overwatch_driver='dummy')
+        from lava.overwatch.drivers.dummy import DummyDriver
+        driver = device.get_overwatch()
+        self.assertTrue(isinstance(driver, DummyDriver))
