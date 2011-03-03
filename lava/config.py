52ea9d5153ca62be370d04bc5337cf60ceb00ed5
Spring Zhang <spring.zhang@linaro.org>
2011-03-03 15:34:04 +0800

    Move all configurations to module lava.config

--- lava/config.py
+++ lava/config.py
@@ -1,5 +1,3 @@
-import json
-
 """
 This is an ugly hack, the uboot commands for a given board type and the board
 type of a test machine need to come from the device registry.  This is an
@@ -8,19 +6,6 @@ around it
 """
 
 class BOARD_CFG:
-    def load(self, cfgfile="cfg/board-cfg.json"):
-        with open(cfgfile) as fd:
-            board_cfg = json.loads(fd.read())
-
-        #load board cfg
-        #same name will overwrite the pre-defined item
-        for board_type in board_cfg['board_type']:
-            BOARD_CFG.BOARD_TYPE[board_type['name']] = board_type['type']
-        #the uboot_cmd will be unicode string
-        for uboot in board_cfg['boards_uboot']:
-            BOARD_CFG.BOARDS_UBOOT[uboot['type']] = uboot['uboot_cmd']
-        fd.close()
-
     BOARDS_UBOOT = {
         "beagle":["mmc init",
             "setenv bootcmd 'fatload mmc 0:3 0x80000000 uImage; fatload mmc " \
@@ -36,6 +21,12 @@ class BOARD_CFG:
             "root=LABEL=testrootfs rootwait ro earlyprintk fixrtc nocompcache "\
             "vram=32M omapfb.vram=0:8M mem=463M ip=none'",
             "boot"],
+        "bbg":["mmc init",
+            "setenv bootcmd 'fatload mmc 0:5 0x90800000 uImage; fatload mmc " \
+            "0:5 0x90800000 uInitrd; bootm 0x90000000 0x90800000'",
+            "setenv bootargs ' console=tty0 console=ttymxc0,115200n8 " \
+            "root=LABEL=testrootfs rootwait ro'",
+            "boot"],
     }
 
     BOARD_TYPE = {
@@ -48,15 +39,6 @@ class BOARD_CFG:
         }
 
 class LAVA_SERVER_CFG:
-    def load(self, cfgfile="cfg/lava-server.json"):
-        with open(cfgfile) as fd:
-            server_cfg = json.loads(fd.read())
-        if server_cfg['ip'] != "":
-            LAVA_SERVER_CFG.IP = server_cfg['ip']
-        if server_cfg['hostname'] != "":
-            LAVA_SERVER_CFG.HOSTNAME = server_cfg['hostname']
-        fd.close()
-
     IP = "192.168.1.10"
     HOSTNAME = ""
 
@@ -68,11 +50,9 @@ if __name__ == "__main__":
     in lp:lava root directory
     """
     #load lava server cfg
-    LAVA_SERVER_CFG().load()
     print "Board type map"
     print "%s %s" % (LAVA_SERVER_CFG.IP, LAVA_SERVER_CFG.HOSTNAME)
 
-    BOARD_CFG().load()
     print "\nUboot cmd map"
     for name, type in BOARD_CFG.BOARD_TYPE.items():
         print "%s: %s" %(name, type)
