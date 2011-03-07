3386115c72222f86269904fdf10e7f4ca8f8249a
Spring Zhang <spring.zhang@linaro.org>
2011-03-07 10:55:10 +0800

    1. Add some methods for class BOARD_CFG and LAVA_SERVER_CFG 2. Remove unused variables 3. Drop main() in module

--- lava/config.py
+++ lava/config.py
@@ -6,6 +6,39 @@ around it
 """
 
 class BOARD_CFG:
+    def __init__(self):
+        self.uboot_add("bbg", ["mmc init",
+            "setenv bootcmd 'fatload mmc 0:5 0x90800000 uImage; fatload mmc " \
+            "0:5 0x90800000 uInitrd; bootm 0x90000000 0x90800000'",
+            "setenv bootargs ' console=tty0 console=ttymxc0,115200n8 " \
+            "root=LABEL=testrootfs rootwait ro'",
+            "boot"])
+        self.board_add("bbg01", "mx51evk")
+
+    def uboot_add(self, boardtype, ubootcmd):
+        """
+        Add new board uboot cmd to BOARDS_UBOOT
+        """
+        self.BOARDS_UBOOT[boardtype] = ubootcmd
+
+    def uboot_del(self, boardtype):
+        """
+        Delete BOARDS_UBOOT entry
+        """
+        del self.BOARDS_UBOOT[boardtype]
+
+    def board_add(self, boardid, boardtype):
+        """
+        Add new board to BOARD_TYPE
+        """
+        self.BOARD_TYPE[boardid] = boardtype
+
+    def board_del(self, boardid):
+        """
+        Delete board from BOARD_TYPE
+        """
+        del self.BOARD_TYPE[boardid]
+
     BOARDS_UBOOT = {
         "beagle":["mmc init",
             "setenv bootcmd 'fatload mmc 0:3 0x80000000 uImage; fatload mmc " \
@@ -21,12 +54,6 @@ class BOARD_CFG:
             "root=LABEL=testrootfs rootwait ro earlyprintk fixrtc nocompcache "\
             "vram=32M omapfb.vram=0:8M mem=463M ip=none'",
             "boot"],
-        "bbg":["mmc init",
-            "setenv bootcmd 'fatload mmc 0:5 0x90800000 uImage; fatload mmc " \
-            "0:5 0x90800000 uInitrd; bootm 0x90000000 0x90800000'",
-            "setenv bootargs ' console=tty0 console=ttymxc0,115200n8 " \
-            "root=LABEL=testrootfs rootwait ro'",
-            "boot"],
     }
 
     BOARD_TYPE = {
@@ -35,28 +62,15 @@ class BOARD_CFG:
         "beaglexm01": "beagle",
         "vexpress01": "vexpress",
         "vexpress02": "vexpress",
-        "bbg01": "mx51evk",
         }
 
 class LAVA_SERVER_CFG:
+    def  setip(self, ip):
+        """
+        set LAVA server IP
+        """
+        self.IP = ip
+
     IP = "192.168.1.10"
-    HOSTNAME = ""
 
-
-if __name__ == "__main__":
-    """
-    unit test by executing:
-    python -m lava.config
-    in lp:lava root directory
-    """
-    #load lava server cfg
-    print "Board type map"
-    print "%s %s" % (LAVA_SERVER_CFG.IP, LAVA_SERVER_CFG.HOSTNAME)
-
-    print "\nUboot cmd map"
-    for name, type in BOARD_CFG.BOARD_TYPE.items():
-        print "%s: %s" %(name, type)
-
-    for type, cmd in BOARD_CFG.BOARDS_UBOOT.items():
-        print "%s: %s" %(type, cmd)
 
