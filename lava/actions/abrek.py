2773126936d4048999e4491f5f3d5a723019b083
Spring Zhang <spring.zhang@linaro.org>
2011-03-15 10:40:13 +0800

    Modification refer to comments 1. abrek will install in master image 2. pexpect will issue timeout exception, no need to check return value

--- lava/actions/abrek.py
+++ lava/actions/abrek.py
@@ -6,37 +6,17 @@ class cmd_test_abrek(BaseAction):
     def run(self, test_name, timeout=-1):
         print "abrek run %s" % test_name
 
-        #Make sure in test image now
+        #Make sure in test image now, abrek will install in master image
         self.in_test_shell()
 
-        self.install_test_suite(self, test_name)
-
         self.test_abrek(self, test_name, timeout)
-
-    def install_test_suite(self, test_name):
-        """
-        Install test suite if not installed
-        """
-        rc = self.client.run_shell_command(
-            'abrek list-installed | grep %s' % test_name,
-            response = test_name, timeout=10)
-        if not rc:
-            #Is the installation time enough?
-            self.client.run_shell_command(
-                'abrek install %s' % test_name,
-                response = self.tester_str, timeout=600)
-        if not rc:
-            raise OperationFailed("Install test suite")
 
     def test_abrek(self, test_name, timeout):
         """
         Invoke test suite by abrek
         """
-        rc = self.client.run_shell_command('abrek run %s' % test_name,
+        self.client.run_shell_command('abrek run %s' % test_name,
             response = self.tester_str, timeout)
-        if not rc:
-            raise OperationFailed("Abrek run error with test suite %s" 
-                    % test_name)
 
     """
     Define tester_str temply, should be a constant imported from other module
