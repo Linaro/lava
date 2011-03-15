e1f22218cbbe01840d5bc8cc3cb339e5ae718745
Paul Larson <paul.larson@canonical.com>
2011-03-15 18:04:09 -0500

    Fill in method to create an image and generate tarballs

--- lava/actions/deploy.py
+++ lava/actions/deploy.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
-from commands import getoutput
+from commands import getoutput, getstatusoutput
 from lava.actions import BaseAction
+from lava.config import LAVA_IMAGE_TMPDIR
 import os
 import re
 import shutil
+from tempfile import mkdtemp
 import urllib2
 import urlparse
 
@@ -17,6 +19,7 @@ class cmd_deploy_linaro_image(BaseAction):
 
         print "Waiting for network to come up"
         self.client.wait_network_up()
+        boot_tgz, root_tgz = self.generate_tarballs(hwpack, rootfs)
 
     def _get_partition_offset(self, image, partno):
         cmd = 'parted %s -s unit b p' % image
@@ -26,6 +29,22 @@ class cmd_deploy_linaro_image(BaseAction):
             found = re.match(pattern, line)
             if found:
                 return found.group(1)
+
+    def _extract_partition(self, image, offset, tarfile):
+        """Mount a partition and produce a tarball of it
+
+        :param image: The image to mount
+        :param offset: offset of the partition, as a string
+        :param tarfile: path and filename of the tgz to output
+        """
+        mntdir = mkdtemp()
+        cmd = "mount -o loop,offset=%s %s %s" % (offset, image, mntdir)
+        rc, output = getstatusoutput(cmd)
+        cmd = "tar -C %s -czf %s ." % (mntdir, tarfile)
+        rc, output = getstatusoutput(cmd)
+        cmd = "umount %s" % mntdir
+        rc, output = getstatusoutput(cmd)
+        os.rmdir(mntdir)
 
     def _download(self, url, path=""):
         urlpath = urlparse.urlsplit(url).path
@@ -43,15 +62,28 @@ class cmd_deploy_linaro_image(BaseAction):
             raise RuntimeError("Could not retrieve %s" % url)
         return filename
 
-
-    def generate_tarballs(self):
-        """
-        TODO: need to use linaro-media-create to generate an image and
-        extract a tarball for bootfs and rootfs, then put them in a place
-        where they can be retrived via http
-
-        For reference, see magma-chamber branch, extract-image script
-        """
+    def generate_tarballs(self, hwpack_url, rootfs_url):
+        tarball_dir = mkdtemp(dir=LAVA_IMAGE_TMPDIR)
+        hwpack_path = self._download(hwpack_url, tarball_dir)
+        rootfs_path = self._download(rootfs_url, tarball_dir)
+        image_file = os.path.join(tarball_dir, "lava.img")
+        cmd = ("linaro-media-create --hwpack-force-yes --dev %s "
+               "--image_file %s --binary %s --hwpack %s" % (
+                self.client.board.type, image_file, rootfs_path,
+                hwpack_path))
+        rc, output = getstatusoutput(cmd)
+        #mx51evk has a different partition layout
+        if self.client.board.type == "mx51evk":
+            boot_offset = self._get_partition_offset(image_file, 2)
+            root_offset = self._get_partition_offset(image_file, 3)
+        else:
+            boot_offset = self._get_partition_offset(image_file, 1)
+            root_offset = self._get_partition_offset(image_file, 2)
+        boot_tgz = os.path.join(tarball_dir, "boot.tgz")
+        root_tgz = os.path.join(tarball_dir, "root.tgz")
+        self._extract_partition(image_file, boot_offset, boot_tgz)
+        self._extract_partition(image_file, root_offset, root_tgz)
+        return boot_tgz, root_tgz
 
     def deploy_linaro_rootfs(self, rootfs):
         print "Deploying linaro image"
