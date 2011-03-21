c262316c3edd70824a357118799643141051078f
Spring Zhang <spring.zhang@linaro.org>
2011-03-22 01:10:18 +0800

    add a simple temp test

--- lava/actions/launch_control.py
+++ lava/actions/launch_control.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python
 from lava.actions import BaseAction
+from xmlrpclib import ServerProxy
+import sys
 
 class cmd_submit_results(BaseAction):
     def run(self, server, stream):
-        print "submit_results"
+        srv = ServerProxy("%s:8000/xml-rpc/" % server)
+
+if __name__ == "__main__":
+    srv = ServerProxy("http://localhost:8000/xml-rpc/")
+    print srv.version()
+    with open(sys.argv[1]) as fd:
+        jn = fd.read()
+    try:
+        srv.put(jn, "abrek.json", "tests")
+    except:
+        print "error"
+        raise
