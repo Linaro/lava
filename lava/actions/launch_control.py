6731d5245f7a112e647ef5511fc4c63356a04772
Spring Zhang <spring.zhang@linaro.org>
2011-03-28 19:02:22 +0800

    1. rename remote_pathname to pathname to align with xmlrpc api 2. add stderr for some print 3. delete default pathname "/anonymous/"

--- lava/actions/launch_control.py
+++ lava/actions/launch_control.py
@@ -6,7 +6,7 @@ import sys
 import socket
 
 class cmd_submit_results(BaseAction):
-    def run(self, server, stream, remote_pathname = "/anonymous/"):
+    def run(self, server, stream, pathname):
         dashboard_url = "%s/launch-control" % server
         xmlrpc_url = "%s/launch-control/xml-rpc/" % server
         filename = "%s/%s.bundle" % (LAVA_RESULT_DIR, stream)
@@ -18,29 +18,29 @@ class cmd_submit_results(BaseAction):
         f.close()
 
         try:
-            content_sha1 = srv.put(content, filename, remote_pathname)
+            content_sha1 = srv.put(content, filename, pathname)
             return content_sha1
         except socket.error as ex:
-            print >>sys.stderr, "Unable to connect to server at %s" % (
+            print >> sys.stderr, "Unable to connect to server at %s" % (
                     dashboard_url,)
             # It seems that some errors are reported as -errno
             # while others as +errno.
             ex.errno = abs(ex.errno)
             if ex.errno == errno.ECONNREFUSED:
-                print >>sys.stderr, "Connection was refused."
+                print >> sys.stderr, "Connection was refused."
             elif ex.errno == errno.ENOENT:
-                print >>sys.stderr, "Unable to resolve address"
+                print >> sys.stderr, "Unable to resolve address"
             else:
-                print >>sys.stderr, "Socket %d: %s" % (ex.errno, ex.strerror)
+                print >> sys.stderr, "Socket %d: %s" % (ex.errno, ex.strerror)
         except xmlrpclib.ProtocolError as ex:
-            print >>sys.stderr, "Unable to exchange XML-RPC message with dashboard server"
-            print >>sys.stderr, "HTTP error code: %d/%s" % (ex.errcode, ex.errmsg)
+            print >> sys.stderr, "Unable to exchange XML-RPC message with dashboard server"
+            print >> sys.stderr, "HTTP error code: %d/%s" % (ex.errcode, ex.errmsg)
         except xmlrpclib.Fault as ex:
             if ex.faultCode == 404:
-                print "Bundle stream %s does not exist" % (
-                        remote_pathname)
+                print >> sys.stderr, "Bundle stream %s does not exist" % (
+                        pathname)
             elif ex.faultCode == 409:
-                print "You have already uploaded this bundle to the dashboard"
+                print >> sys.stderr, "You have already uploaded this bundle to the dashboard"
             else:
-                print "Unknown error"
+                print >> sys.stderr, "Unknown error"
 
