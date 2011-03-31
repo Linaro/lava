b1394baade5f01e47fbb6a3551d4c801f6277b54
Spring Zhang <spring.zhang@linaro.org>
2011-03-31 17:43:57 +0800

    push all test result bundle to l-c server

--- lava/actions/launch_control.py
+++ lava/actions/launch_control.py
@@ -2,8 +2,8 @@
 from lava.actions import BaseAction
 from lava.config import LAVA_RESULT_DIR, MASTER_STR
 import xmlrpclib
-import sys
-import socket
+import re
+import os
 
 class cmd_submit_results(BaseAction):
     def run(self, server, stream, pathname):
@@ -18,16 +18,34 @@ class cmd_submit_results(BaseAction):
         client.run_shell_command(
             'mount /dev/disk/by-label/testrootfs /mnt/root',
             response = MASTER_STR)
-        filename = "%s/%s.bundle" % (LAVA_RESULT_DIR, stream)
+        client.run_shell_command(
+            'mkdir -p %s' % LAVA_RESULT_DIR, response = MASTER_STR)
+        client.run_shell_command(
+            'cp /mnt/root/%s/*.bundle %s' % (LAVA_RESULT_DIR, LAVA_RESULT_DIR),
+            response = MASTER_STR)
+        # fix me: upload bundle files to server
 
+        #Create l-c server connection
         dashboard_url = "%s/launch-control" % server
         xmlrpc_url = "%s/launch-control/xml-rpc/" % server
 
         srv = xmlrpclib.ServerProxy(xmlrpc_url, 
                 allow_none=True, use_datetime=True)
-        f = open("%s" % filename, "rb")
-        content = f.read()
-        f.close()
+ 
+        #fix me: get serial log
 
-        content_sha1 = srv.put(content, filename, pathname)
-        return content_sha1
+        #.bundle file pattern
+        pattern = re.compile(".*\.bundle")
+        filelist = os.listdir("%s" % LAVA_RESULT_DIR)
+        for file in filelist:
+            found = re.match(pattern, file)
+            if found:
+                filename = "%s/%s" % (LAVA_RESULT_DIR, file)
+
+                f = open(filename, "rb")
+                content = f.read()
+                f.close()
+
+                #fix me: attach serial log
+
+                srv.put(content, filename, pathname)
