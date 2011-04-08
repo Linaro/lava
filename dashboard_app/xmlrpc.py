28e22e7ce3adf9cb4f4d0557e5bcaefc157c6a93
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-04-09 01:12:28 +0200

    Implement DashboardAPI.query_data_view

--- dashboard_app/xmlrpc.py
+++ dashboard_app/xmlrpc.py
@@ -23,11 +23,12 @@ XMP-RPC API
 import logging
 import xmlrpclib
 
-from django.db import IntegrityError
+from django.db import connection, IntegrityError, DatabaseError
 
 from dashboard_app import __version__
+from dashboard_app.dataview import DataViewRepository
 from dashboard_app.dispatcher import xml_rpc_signature
-from dashboard_app.models import (Bundle, BundleStream)
+from dashboard_app.models import Bundle, BundleStream
 
 
 class errors:
@@ -445,44 +446,55 @@ class DashboardAPI(object):
         -----------------
         None
         """
-        from dashboard_app.dataview import DataViewRepository
         repo = DataViewRepository.get_instance()
         return [{
             'name': data_view.name,
-            'summary': data_view.summary or "",
-            } for data_view in repo]
+            'summary': data_view.summary,
+            "documentation": data_view.documentation,
+            "arguments": [{
+                "name": arg.name,
+                "type": arg.type,
+                "help": arg.help,
+                "default": arg.default
+            } for arg in data_view.arguments]
+        } for data_view in repo]
 
     def data_view_info(self, name):
-        from dashboard_app.dataview import DataViewRepository
         repo = DataViewRepository.get_instance()
         try:
             data_view = repo[name]
         except KeyError:
             raise xmlrpclib.Fault(errors.NOT_FOUND, "Data view not found")
         else:
-            def get_backend_name(connection):
-                backend = str(type(connection))
-                if "sqlite" in backend:
-                    return "sqlite"
-                elif "postgresql" in backend:
-                    return "postgresql"
-                else:
-                    return ""
-            from django.db import connection
-            sql_backend_name = get_backend_name(connection)
-            try:
-                sql_for_connection = data_view.sql[sql_backend_name]
-            except KeyError:
-                sql_for_connection = data_view.sql.get("", None)
+            query = data_view.get_backend_specific_query(connection)
             return {
                 "name": data_view.name,
                 "summary": data_view.summary,
                 "documentation": data_view.documentation,
-                "sql:": sql_for_connection,
+                "sql": query.sql_template if query is not None else None, 
                 "arguments": [{
                     "name": arg.name,
                     "type": arg.type,
                     "help": arg.help,
                     "default": arg.default
                 } for arg in data_view.arguments]
+            }
+
+    def query_data_view(self, name, arguments):
+        repo = DataViewRepository.get_instance()
+        try:
+            data_view = repo[name]
+        except KeyError:
+            raise xmlrpclib.Fault(errors.NOT_FOUND, "Data view not found")
+        try:
+            rows, columns= data_view(connection, **arguments)
+        except (LookupError, TypeError, ValueError, DatabaseError) as exc:
+            raise xmlrpclib.Fault(errors.INTERNAL_SERVER_ERROR, str(exc))
+        else:
+            return {
+                "rows": rows,
+                "columns": [{
+                    "name": item[0],
+                    "type": item[1]
+                } for item in columns]
             }
