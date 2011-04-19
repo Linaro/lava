2c9d97b704d08ed5f3322ac3bfc960e1f768d709
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-04-19 14:11:32 +1200

    move scheduler_app out of lava.scheduler.interface product

--- lava/scheduler/interface/settings.py
+++ lava/scheduler/interface/settings.py
@@ -85,7 +85,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'lava.scheduler.interface.scheduler_app',
+    'lava.scheduler.gscheduler_app',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
 )
