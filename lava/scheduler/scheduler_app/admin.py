3b0c444d14d12919350914a6eb53f6394da8fee4
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-04-20 14:55:33 +1200

    register new tables with the admin interface

--- lava/scheduler/scheduler_app/admin.py
+++ lava/scheduler/scheduler_app/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 
 from lava.scheduler.scheduler_app.models import (
     Device,
+    DeviceType,
+    Tag,
     TestSuite,
     TestCase,
     )
 
 admin.site.register(Device)
+admin.site.register(DeviceType)
+admin.site.register(Tag)
 admin.site.register(TestSuite)
 admin.site.register(TestCase)
