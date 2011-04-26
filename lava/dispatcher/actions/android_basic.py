a1423606fd5149f0e9cb604e55e2af319d98f90f
jeremy. <chang@linaro.org>
2011-04-27 02:23:04 +0800

    Make basic and monkey result bundle deserialize-able

--- lava/dispatcher/actions/android_basic.py
+++ lava/dispatcher/actions/android_basic.py
@@ -22,7 +22,7 @@ class cmd_test_android_monkey(BaseAndroidAction):
 
         result_pattern = '## Network stats: elapsed time=(?P<measurement>\d+)ms'
         test_case_result = {}
-        test_case_result['test_case_id'] = "monkey"
+        test_case_result['test_case_id'] = "monkey-1"
         test_case_result['units'] = "mseconds"
         cmd = 'monkey -s 1 --pct-touch 10 --pct-motion 20 --pct-nav 20 --pct-majornav 30 --pct-appswitch 20 --throttle 500 50'
         self.client.proc.sendline(cmd)
@@ -30,10 +30,9 @@ class cmd_test_android_monkey(BaseAndroidAction):
 
         if id == 0:
             match_group = self.client.proc.match.groups()
-            test_case_result['measurement'] = match_group[0]
+            test_case_result['measurement'] = int(match_group[0])
             test_case_result['result'] = "pass"
         else:
-            test_case_result['measurement'] = ""
             test_case_result['result'] = "fail"
         results['test_results'].append(test_case_result)
         savebundlefile("monkey", results, timestring)
@@ -57,15 +56,14 @@ class cmd_test_android_basic(BaseAndroidAction):
         result_pattern = "([0-1])"
         test_case_result = {}
         test_case_result['test_case_id'] = "dev.bootcomplete"
-        test_case_result['units'] = ""
         cmd = "getprop dev.bootcomplete"
         self.client.proc.sendline(cmd)
         #XXX: First time booting, it will be longer to boot
         id = self.client.proc.expect([result_pattern, pexpect.EOF], timeout = 10)
         if id == 0:
             match_group = self.client.proc.match.groups()
-            test_case_result['measurement'] = match_group[0]
-            if test_case_result['measurement'] == "1":
+            test_case_result['measurement'] = int(match_group[0])
+            if test_case_result['measurement'] == 1:
                 test_case_result['result'] = "pass"
             else:
                 test_case_result['result'] = "fail"
@@ -76,23 +74,20 @@ class cmd_test_android_basic(BaseAndroidAction):
 
         test_case_result = {}
         test_case_result['test_case_id'] = "sys.boot_completed"
-        test_case_result['units'] = ""
         cmd = "getprop sys.boot_completed"
         self.client.proc.sendline(cmd)
         try:
             id = self.client.proc.expect([result_pattern, pexpect.EOF], timeout = 5)
             if id == 0:
                 match_group = self.client.proc.match.groups()
-                test_case_result['measurement'] = match_group[0]
-                if test_case_result['measurement'] == "1":
+                test_case_result['measurement'] = int(match_group[0])
+                if test_case_result['measurement'] == 1:
                     test_case_result['result'] = "pass"
                 else:
                     test_case_result['result'] = "fail"
             else:
-                test_case_result['measurement'] = ""
                 test_case_result['result'] = "unknown"
         except:
-            test_case_result['measurement'] = "exception"
             test_case_result['result'] = "fail"
             pass
 
@@ -101,19 +96,17 @@ class cmd_test_android_basic(BaseAndroidAction):
         result_pattern = "(running)"
         test_case_result = {}
         test_case_result['test_case_id'] = "init.svc.adbd"
-        test_case_result['units'] = ""
         cmd = "getprop init.svc.adbd"
         self.client.proc.sendline(cmd)
         id = self.client.proc.expect([result_pattern, pexpect.EOF], timeout = 5)
         if id == 0:
             match_group = self.client.proc.match.groups()
-            test_case_result['measurement'] = match_group[0]
-            if test_case_result['measurement'] == "running":
+            test_case_result['message'] = match_group[0]
+            if test_case_result['message'] == "running":
                 test_case_result['result'] = "pass"
             else:
                 test_case_result['result'] = "fail"
         else:
-            test_case_result['measurement'] = ""
             test_case_result['result'] = "unknown"
         results['test_results'].append(test_case_result)
 
@@ -122,8 +115,6 @@ class cmd_test_android_basic(BaseAndroidAction):
         adb_status = self.check_adb_status()
         test_case_result = {}
         test_case_result['test_case_id'] = "adb connection status"
-        test_case_result['units'] = ""
-        test_case_result['measurement'] = adb_status
         if adb_status:
             test_case_result['result'] = "pass"
         else:
