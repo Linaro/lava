4eab6f03c5f396c3e74421e197574cf171056679
jeremy. <chang@linaro.org>
2011-04-28 03:15:54 +0800

    Remove deprecated android_abrek.py and clean up the commented code

--- lava/dispatcher/android_util.py
+++ lava/dispatcher/android_util.py
@@ -6,9 +6,7 @@ import subprocess
 from lava.dispatcher.config import LAVA_RESULT_DIR
 import time
 
-# TODO: This should be replaced by 
-# Save bundle file(.bundle) to /tmp/LAVA_RESULT_DIR on host
-#XXX: 0 means we only have one run here and it's hard coded.
+# TODO: Result saving could be replaced by linaro_dashboard_bundle probably.
 def savebundlefile(testname, results, starttime):
     """
     Save results as .bundle file under /tmp/LAVA_RESULT_DIR/
@@ -22,8 +20,6 @@ def savebundlefile(testname, results, starttime):
     test_runs[0]['time_check_performed'] = False
     test_runs[0]['analyzer_assigned_date'] = starttime 
     # TODO: hw_context sw_context for android
-    #test_runs[0]['software_context'] = {}
-    #test_runs[0]['hardware_context'] = {}
     testdata['test_runs'] = test_runs
     testdata['test_runs'][0].update(results)
     bundle = testdata
