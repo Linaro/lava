2d3d41fbf0844e4c3789f283cf3ea2ddbfc6f74c
jeremy.chang@linaro.org <>
2011-04-28 02:15:54 +0800

    Add panda support

--- lava/dispatcher/android_config.py
+++ lava/dispatcher/android_config.py
@@ -25,10 +25,24 @@ class BeagleBoard(Board):
         "boot"]
     type = "beagle"
 
+
+class PandaBoard(Board):
+    uboot_cmds = ["mmc rescan 0",
+        "setenv bootcmd 'fatload mmc 0:3 0x80200000 uImage;"
+        "fatload mmc 0:3 0x81600000 uInitrd;"
+        "bootm 0x80200000 0x81600000'",
+        "setenv bootargs 'console=tty0 console=ttyO2,115200n8 "
+        "rootwait rw earlyprintk fixrtc nocompcache vram=32M "
+        "omapfb.vram=0:8M mem=456M@0x80000000 mem=512M@0xA0000000 "
+        "omapfb.debug=y omapfb.mode=dvi:1280x720MR-16@60 "
+        "init=/init androidboot.console=ttyO2'",
+        "boot"]
+    type = "panda"
 #Here, it still needs to maintain a map from boardid to board, for there is only
 #boardid in jobfile.json
 BOARDS = {
         "beagle01": BeagleBoard,
+        "panda01": PandaBoard,
         }
 
 #Main LAVA server IP in the boards farm
