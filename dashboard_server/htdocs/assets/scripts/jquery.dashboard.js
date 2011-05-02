7913b3ecc650ed8cbc2f7cb3ee7698847a280147
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-05-02 19:33:45 +0200

    Add render_to_table to JavaScript dashboard API

--- dashboard_server/htdocs/assets/scripts/jquery.dashboard.js
+++ dashboard_server/htdocs/assets/scripts/jquery.dashboard.js
@@ -7,7 +7,7 @@
       return server;
     },
 
-    render_table: function(dataset) {
+    render_table: function(dataset, options) {
       var html = "<table class='data'>";
       html += "<tr>";
       $.each(dataset.columns, function (index, column) {
@@ -17,13 +17,46 @@
       $.each(dataset.rows, function (index, row) {
         html += "<tr>";
         $.each(row, function (index, cell) {
-          html += "<td>" + cell + "</td>";
+          var column = dataset.columns[index];
+          var cell_html = undefined;
+          var cell_link = null;
+          if (cell_html == undefined) {
+            cell_html = cell;
+          }
+          if (column.name == "UUID") {
+            cell_link = "/dashboard/test-runs/" + cell + "/";
+          }
+          html += "<td>";
+          if (cell_link) {
+            html += "<a href='" + cell_link + "'>"
+            html += cell_html;
+            html += "</a>";
+          } else {
+            html += cell_html;
+          }
+          html += "</td>";
         });
         html += "</tr>";
       });
       html += "</table>";
       this.html(html);
+    },
+
+    render_to_table: function(data_view_name, data_view_arguments, options) {
+      var outer = this;
+      outer.html("Contacting XML-RPC server...");
+      outer.dashboard("init", function (server) {
+        outer.html("Querying data view...");
+        server.query_data_view(function (response) {
+          if (response.result) {
+            outer.dashboard("render_table", response.result, options);
+          } else {
+            outer.dashboard("Error: " + response.error.faultString);
+          }
+        }, data_view_name, data_view_arguments);
+      });
     }
+
   };
 
   $.fn.dashboard = function(method) {
