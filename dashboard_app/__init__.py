645760aae3d00c250ee9ed2cf555f2b6ce8cbc19
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-05-03 22:49:27 +0200

    Bump version to 0.4c1

--- dashboard_app/__init__.py
+++ dashboard_app/__init__.py
@@ -20,7 +20,7 @@
 Dashboard Application (package)
 """
 
-__version__ = (0, 4, 0, "dev", 0)
+__version__ = (0, 4, 0, "candidate", 1)
 try:
     import versiontools
     __version__ = versiontools.Version.from_tuple(__version__)
