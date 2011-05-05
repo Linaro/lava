39ab23742a6e3c2b77ea3abab2917d97c77d551d
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-05-05 03:22:07 +0200

    Update axislabel plugin

--- dashboard_server/htdocs/js/jquery.flot.axislabels.js
+++ dashboard_server/htdocs/js/jquery.flot.axislabels.js
@@ -1,134 +1,87 @@
 /*
-Axis Labels Plugin for flot. :P
-Released under the GPLv3 license by Xuan Luo, September 2010.
+Flot plugin for labeling axis
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    (xy)axis: {
+        label: "label string",
+        labelPos: "high" or "low"
+    }
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This plugin allows you to label an axis without much fuss, by
+replacing one of the extreme ticks with the chosen label string. Set
+labelPos to "high" or "low" to replace respectively the maximum or the
+minimum value of the ticks. User set axis.tickFormatter are respected
+and multiple axes supported.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
- */
+Rui Pereira
+rui (dot) pereira (at) gmail (dot) com
+*/
 (function ($) {
-    var options = { };
 
-    function init(plot) {
-        // This is kind of a hack. There are no hooks in Flot between
-        // the creation and measuring of the ticks (setTicks, measureTickLabels
-        // in setupGrid() ) and the drawing of the ticks and plot box
-        // (insertAxisLabels in setupGrid() ).
-        //
-        // Therefore, we use a trick where we run the draw routine twice:
-        // the first time to get the tick measurements, so that we can change
-        // them, and then have it draw it again.
-        var secondPass = false;
-        plot.hooks.draw.push(function (plot, ctx) {
-            if (!secondPass) {
-                // MEASURE AND SET OPTIONS
-                $.each(plot.getAxes(), function(axisName, axis) {
-                    var opts = axis.options // Flot 0.7
-                        || plot.getOptions()[axisName]; // Flot 0.6
-                    if (!opts || !opts.axisLabel)
-                        return;
+    function labelAxis(val, axis){
+        var ticks, opts = axis.options;
 
-                    var w, h;
-                    if (opts.axisLabelUseCanvas != false)
-                        opts.axisLabelUseCanvas = true;
+        // generator
+        var tmpopts = axis.n == 1? opts: (typeof opts.alignedTo != 'undefined')? opts.alignedTo.options: null;
+        // first axis or some axis aligned wrt it
+        if (tmpopts && (tmpopts.autoscaleMargin == null ||
+                (tmpopts.labelPos == 'high' && tmpopts.max != null) ||
+                (tmpopts.labelPos == 'low' && tmpopts.min != null)))
+            // cut ticks not seen
+            ticks = $.grep(axis.tickGenerator(axis), function(v){
+                return (v > axis.min && v < axis.max);
+            });
+        // standard tick generator
+        else ticks = axis.tickGenerator(axis);
 
-                    if (opts.axisLabelUseCanvas) {
-                        // canvas text
-                        if (!opts.axisLabelFontSizePixels)
-                            opts.axisLabelFontSizePixels = 14;
-                        if (!opts.axisLabelFontFamily)
-                            opts.axisLabelFontFamily = 'sans-serif';
-                        // since we currently always display x as horiz.
-                        // and y as vertical, we only care about the height
-                        w = opts.axisLabelFontSizePixels;
-                        h = opts.axisLabelFontSizePixels;
-
-                    } else {
-                        // HTML text
-                        var elem = $('<div class="axisLabels" style="position:absolute;">' + opts.axisLabel + '</div>');
-                        plot.getPlaceholder().append(elem);
-                        w = elem.outerWidth(true);
-                        h = elem.outerHeight(true);
-                        elem.remove();
-                    }
-
-                    if (axisName.charAt(0) == 'x')
-                        axis.labelHeight += h;
-                    else
-                        axis.labelWidth += w;
-                    opts.labelHeight = axis.labelHeight;
-                    opts.labelWidth = axis.labelWidth;
-                });
-                // re-draw with new label widths and heights
-                secondPass = true;
-                plot.setupGrid();
-                plot.draw();
-
-
+        // formatter
+        if ((opts.labelPos == 'high' && val == ticks[ticks.length-1]) ||
+                (opts.labelPos == 'low' && val == ticks[0]))
+            return opts.label;
+        else {
+            // user set tickFormatter
+            if ($.isFunction(opts.userFormatter)){
+                var tmp = opts.userFormatter;
+                // avoid infinite loops
+                opts.userFormatter = null;
+                return tmp(val, axis);
             } else {
-                // DRAW
-                $.each(plot.getAxes(), function(axisName, axis) {
-                    var opts = axis.options // Flot 0.7
-                        || plot.getOptions()[axisName]; // Flot 0.6
-                    if (!opts || !opts.axisLabel)
-                        return;
-
-                    if (opts.axisLabelUseCanvas) {
-                        // canvas text
-                        var ctx = plot.getCanvas().getContext('2d');
-                        ctx.save();
-                        ctx.font = opts.axisLabelFontSizePixels + 'px ' +
-                                opts.axisLabelFontFamily;
-                        var width = ctx.measureText(opts.axisLabel).width;
-                        var height = opts.axisLabelFontSizePixels;
-                        var x, y;
-                        if (axisName.charAt(0) == 'x') {
-                            x = plot.getPlotOffset().left + plot.width()/2 - width/2;
-                            y = plot.getCanvas().height;
-                        } else {
-                            x = height * 0.72;
-                            y = plot.getPlotOffset().top + plot.height()/2 - width/2;
-                        }
-                        ctx.translate(x, y);
-                        ctx.rotate((axisName.charAt(0) == 'x') ? 0 : -Math.PI/2);
-                        ctx.fillText(opts.axisLabel, 0, 0);
-                        ctx.restore();
-
-                    } else {
-                        // HTML text
-                        plot.getPlaceholder().find('#' + axisName + 'Label').remove();
-                        var elem = $('<div id="' + axisName + 'Label" " class="axisLabels" style="position:absolute;">' + opts.axisLabel + '</div>');
-                        if (axisName.charAt(0) == 'x') {
-                            elem.css('left', plot.getPlotOffset().left + plot.width()/2 - elem.outerWidth()/2 + 'px');
-                            elem.css('bottom', '0px');
-                        } else {
-                            elem.css('top', plot.getPlotOffset().top + plot.height()/2 - elem.outerHeight()/2 + 'px');
-                            elem.css('left', '0px');
-                        }
-                        plot.getPlaceholder().append(elem);
-                    }
-                });
-                secondPass = false;
+                // scientific notation for small values
+                if ((axis.datamax != 0 && Math.abs(axis.datamax) < 1e-5) ||
+                        (axis.datamin != 0 && Math.abs(axis.datamin) < 1e-5))
+                    return val.toPrecision(2);
+                else return val.toFixed(axis.tickDecimals);
             }
+        }
+    }
+
+    function init(plot){
+        plot.hooks.processOptions.push(function(plot, options){
+            // separate X and Y
+            $.each({x: options.xaxes, y: options.yaxes}, function(direction, axes){
+                // get only axes with labels
+                $.each($.grep(axes, function(v){
+                    return (typeof v.label != 'undefined' && v.label);
+                }), function(i, axis){
+                    if ($.isFunction(axis.tickFormatter))
+                        axis.userFormatter = axis.tickFormatter;
+                    if (typeof axis.alignTicksWithAxis != 'undefined')
+                        $.each(plot.getAxes(), function(k,v){
+                            if (v.n == axis.alignTicksWithAxis && v.direction == direction)
+                                axis.alignedTo = v;
+                        });
+                    axis.tickFormatter = labelAxis;
+                });
+            });
         });
     }
 
-
+    var options = { xaxis: {label: null, labelPos: 'high'},
+                    yaxis: {label: null, labelPos: 'high'} };
 
     $.plot.plugins.push({
-        init: init,
-        options: options,
-        name: 'axisLabels',
-        version: '1.0'
-    });
+                init: init,
+                options: options,
+                name: "axislabels",
+                version: "0.1"
+            });
 })(jQuery);
