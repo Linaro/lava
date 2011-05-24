828702a5a1741563df6079072688fca41ce7dda1
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-05-24 16:19:42 +0200

    Improve lava extension interface.
    Separate extension interface from simple default implementation. Make it
    possible to add new URLs to the server.

--- lava_server/extension.py
+++ lava_server/extension.py
@@ -17,7 +17,7 @@
 # along with LAVA Server.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, abstractproperty
 import logging
 
 
@@ -35,20 +35,51 @@ class ILavaServerExtension(object):
         settings module.
         """
 
+    @abstractmethod
+    def contribute_to_urlpatterns(self, urlpatterns):
+        """
+        Add application specific URLs to root URL patterns of lava-server
+        """
+
+
+class LavaServerExtension(ILavaServerExtension):
+    """
+    LAVA Server extension class.
+
+    Implements basic behavior for LAVA server extensions
+    """
+
     # TODO: Publish API objects for xml-rpc
     # TODO: Publish menu items
-    # TODO: Publish URLs (perhaps it's better to do that explicitly rather than
-    # asking everyone to shove it into contribute_to_settings()
+
+    @abstractproperty
+    def app_name(self):
+        """
+        Name of this extension's primary django application.
+        """
+
+    def contribute_to_settings(self, settings):
+        settings['INSTALLED_APPS'].append(self.app_name)
+        settings['PREPEND_LABEL_APPS'].append(self.app_name)
+
+    def contribute_to_urlpatterns(self, urlpatterns):
+        from django.conf.urls.defaults import url, include 
+        urlpatterns += [
+            url(r'^{app_name}/'.format(app_name=self.app_name),
+                include('{app_name}.urls'.format(app_name=self.app_name)))]
 
 
-class ExtensionImportError(Exception):
+class ExtensionLoadError(Exception):
+    """
+    Exception internally raised by extension loader
+    """
 
     def __init__(self, extension, message):
         self.extension = extension
         self.message = message
 
     def __repr__(self):
-        return "ExtensionImportError(extension={0!r}, message={1!r})".format(
+        return "ExtensionLoadError(extension={0!r}, message={1!r})".format(
             extension, message)
 
 
@@ -57,52 +88,70 @@ class ExtensionLoader(object):
     Helper to load extensions
     """
 
-    def __init__(self, settings):
-        self.settings = settings
+    def __init__(self):
+        self._extensions = []
+        for name in self._find_extensions():
+            try:
+                extension = self._load_extension(name)
+            except ExtensionLoadError as ex:
+                logging.exception("Unable to load extension %r: %s", name, ex.message)
+            else:
+                self._extensions.append(extension)
 
-    def find_extensions(self):
+    def contribute_to_settings(self, settings):
+        """
+        Contribute to lava-server settings module.
+
+        The settings argument is a magic dictionary returned by locals()
+        """
+        for extension in self._extensions:
+            extension.contribute_to_settings(settings)
+
+    def contribute_to_urlpatterns(self, urlpatterns):
+        """
+        Contribute to lava-server URL patterns
+        """
+        for extension in self._extensions:
+            extension.contribute_to_urlpatterns(urlpatterns)
+
+    def _find_extensions(self):
         # TODO: Implement for real
         yield "demo_app.extension:DemoExtension"
 
-    def load_extensions(self):
-        for name in self.find_extensions():
-            self.install_extension(name)
-
-    def install_extension(self, name):
-        try:
-            extension_cls = self.import_extension(name)
-            extension = extension_cls()
-            extension.contribute_to_settings(self.settings)
-        except ExtensionImportError as ex:
-            logging.exception("Unable to import extension %r: %s", name, ex.message)
-        except Exception:
-            logging.exception("Unable to install extension %r", name)
-
-    def import_extension(self, name):
+    def _load_extension(self, name):
         """
-        Import extension specified by the given name.
+        Load extension specified by the given name.
         Name must be a string like "module:class". Module may be a
         package with dotted syntax to address specific module.
 
-        @return Imported extension class implementing ILavaServerExtension
-        @raises ExtensionImportError
+        @return Imported extension instance, subclass of ILavaServerExtension
+        @raises ExtensionLoadError
         """
         try:
             module_or_package_name, class_name = name.split(":", 1)
         except ValueError:
-            raise ExtensionImportError(
+            raise ExtensionLoadError(
                 name, "Unable to split extension into module and class")
         try:
             module = __import__(module_or_package_name, fromlist=[''])
         except ImportError as ex:
-            raise ExtensionImportError(
+            raise ExtensionLoadError(
                 name, "Unable to import required modules")
         try:
             extension_cls = getattr(module, class_name)
         except AttributeError:
-            raise ExtensionImportError(
+            raise ExtensionLoadError(
                 name, "Unable to access class component")
         if not issubclass(extension_cls, ILavaServerExtension):
-            raise ExtensionImportError(
+            raise ExtensionLoadError(
                 name, "Class does not implement ILavaServerExtension interface")
-        return extension_cls
+        try:
+            extension = extension_cls()
+        except:
+            raise ExtensionLoadError(
+                name, "Unable to instantiate class")
+        return extension
+
+
+# Global loader instance
+loader = ExtensionLoader()
