9d3c81bf893c32a8da3f9008969c7ea5ed58de0f
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-05-24 18:18:15 +0200

    Add copyright headers, expose main view and version

--- demo_app/extension.py
+++ demo_app/extension.py
@@ -1,4 +1,25 @@
+# Copyright (C) 2010, 2011 Linaro Limited
+#
+# Author: Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
+#
+# This file is part of LAVA Server.
+#
+# LAVA Server is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License version 3
+# as published by the Free Software Foundation
+#
+# LAVA Server is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with LAVA Server.  If not, see <http://www.gnu.org/licenses/>.
+
+import versiontools
 from lava_server.extension import LavaServerExtension
+
+import demo_app
 
 
 class DemoExtension(LavaServerExtension):
@@ -12,5 +33,19 @@ class DemoExtension(LavaServerExtension):
         return "demo_app"
 
     @property
+    def name(self):
+        return "Demo"
+
+    @property
+    def main_view_name(self):
+        return "demo_app.views.hello"
+
+    @property
     def description(self):
         return "Demo extension for LAVA server"
+
+    @property
+    def version(self):
+        return versiontools.format_version(demo_app.__version__)
+
+
