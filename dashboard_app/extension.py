5eddf30d75c2706436014af3f0e1d4bb1c5d8156
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-05-25 13:03:50 +0200

    Add extension point for lava-server

---
+++ dashboard_app/extension.py
@@ -0,0 +1,37 @@
+from lava_server.extension import LavaServerExtension
+
+
+class DashboardExtension(LavaServerExtension):
+
+    @property
+    def app_name(self):
+        return "dashboard_app"
+
+    @property
+    def name(self):
+        return "Dashboard"
+
+    @property
+    def main_view_name(self):
+        return "dashboard_app.views.bundle_stream_list"
+
+    @property
+    def description(self):
+        return "Validation Dashboard"
+
+    @property
+    def version(self):
+        import versiontools
+        import dashboard_app 
+        return versiontools.format_version(dashboard_app.__version__)
+
+    def contribute_to_settings(self, settings):
+        super(DashboardExtension, self).contribute_to_settings(settings)
+        settings['INSTALLED_APPS'].extend([
+            "pagination",
+            "south",
+        ])
+        settings['RESTRUCTUREDTEXT_FILTER_SETTINGS'] = {
+            "initial_header_level": 4
+        }
+        # TODO: Add dataview database support
