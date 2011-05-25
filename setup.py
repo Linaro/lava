11c30bfa18270bdced8c3861e6fec874e52db550
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-05-25 13:04:28 +0200

    Update setup.py (still in progress)

--- setup.py
+++ setup.py
@@ -25,19 +25,22 @@ import versiontools
 
 
 setup(
-    name = 'launch-control',
+    name = 'lava-dashboard',
     version = versiontools.format_version(dashboard_app.__version__),
     author = "Zygmunt Krynicki",
     author_email = "zygmunt.krynicki@linaro.org",
     packages = find_packages(),
     license = "AGPL",
-    description = "Dashboard for linaro LAVA stack",
+    description = "Validation Dashboard for LAVA Server",
     long_description = """
-    Launch control is a collection of tools for distribution wide QA
-    management. It is implemented for the Linaro organization.
+    Validation Dashboard is a repository for test results.
     """,
-    url='https://launchpad.net/launch-control',
+    url='https://launchpad.net/lava-dashboard',
     #test_suite='launch_control.tests.test_suite',
+    entry_points = """
+        [lava_server.extensions]
+        dashboard=dashboard_app.extension:DashboardExtension
+        """,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -49,17 +52,15 @@ setup(
     ],
     install_requires = [
         'Django >= 1.2',
-        'django-openid-auth >= 0.2',
         'django-pagination >= 1.0.7.1',
         'django-restricted-resource >= 0.2.5',
-        "django-staticfiles >= 0.3.4",
         'docutils >= 0.6',
+        'lava-server',
         'linaro-dashboard-bundle >= 1.4',
         'linaro-json >= 2.0',
-        'python-openid >= 2.2.4', # this should be a part of django-openid-auth deps
-        'versiontools >= 1.1',
-        'south >= 0.7',
         'pygments >= 1.2',
+        'south >= 0.7',
+        'versiontools >= 1.1',
     ],
     setup_requires = [
         'versiontools >= 1.1',
@@ -69,5 +70,5 @@ setup(
     ],
     zip_safe=False,
     include_package_data=True
-),
+)
 
