16171225b07e9e2c10f0146264a860ed8a905a20
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-05-25 15:09:08 +0200

    Better exception logging when settings cannot be imported

--- lava_server/manage.py
+++ lava_server/manage.py
@@ -33,10 +33,9 @@ from django.core.management import execute_manager
 try:
     import lava_server.settings.development as settings
 except ImportError as ex:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.stderr.write("Exception details: %r\n" % ex)
-    sys.exit(1)
+    import logging
+    logging.exception("Unable to import application settings")
+    raise SystemExit(ex)
 
 
 if __name__ == "__main__":
