9fe6370fe5f4d7d7e79a71b9ae53937c7d136728
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-05-30 19:44:36 +0200

    Disable admin docs
    This is a workaround for django bug (https://code.djangoproject.com/ticket/6681)

--- lava_server/settings/common.py
+++ lava_server/settings/common.py
@@ -91,7 +91,8 @@ INSTALLED_APPS = [
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
+    # 'django.contrib.admindocs',
+    # Admin docs disabled due to: https://code.djangoproject.com/ticket/6681
 ]
 
 TEMPLATE_CONTEXT_PROCESSORS = [
