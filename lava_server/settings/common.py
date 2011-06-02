3b39c0a48286548f513220145893d2fa681ea6b5
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-06-02 17:02:02 +1200

    undo overly keen fill-paragraph

--- lava_server/settings/common.py
+++ lava_server/settings/common.py
@@ -91,8 +91,8 @@ INSTALLED_APPS = [
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs', Admin docs disabled due to:
-    # https://code.djangoproject.com/ticket/6681
+    # 'django.contrib.admindocs',
+    # Admin docs disabled due to: https://code.djangoproject.com/ticket/6681
     'linaro_django_xmlrpc',
 ]
 
