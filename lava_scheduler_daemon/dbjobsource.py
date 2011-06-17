4f6bd1cc2cd4a717a98d0e457f4e0ee68997a705
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-06-17 16:03:25 +1200

    (maybe) implement getJobForBoard

--- lava_scheduler_daemon/dbjobsource.py
+++ lava_scheduler_daemon/dbjobsource.py
@@ -4,7 +4,7 @@ from twisted.internet.threads import deferToThread
 
 from zope.interface import implements
 
-from lava_scheduler_app.models import Device
+from lava_scheduler_app.models import Device, TestJob
 from lava_scheduler_daemon.jobsource import IJobSource
 
 
@@ -27,10 +27,16 @@ class DatabaseJobSource(object):
     def getBoardList(self):
         return [d.hostname for d in Device.objects.all()]
 
-
     @defer_to_thread
     def getJobForBoard(self, board_name):
-        pass
+        device = Device.objects.get(hostname=board_name)
+        jobs_for_device = TestJob.objects(target=device)
+        jobs_for_device.order_by('submit_time')
+        jobs = jobs_for_device[:1]
+        if jobs:
+            return jobs[0]
+        else:
+            return None
 
     @defer_to_thread
     def jobCompleted(self, board_name, log_stream):
