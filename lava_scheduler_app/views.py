0f22d573eb3a4166fed736c4bb913fe2a8b688d6
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-06-22 15:10:33 +1200

    naive ui for devices

--- lava_scheduler_app/views.py
+++ lava_scheduler_app/views.py
@@ -1,6 +1,13 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+from lava_scheduler_app.models import Device
+
 def index(request):
-    return render_to_response("lava_scheduler_app/index.html", {},
-                              RequestContext(request))
+    print Device.objects.all()
+    return render_to_response(
+        "lava_scheduler_app/index.html",
+        {
+            'devices': Device.objects.all(),
+        },
+        RequestContext(request))
