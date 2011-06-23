f720a9606dfa00f0eec7b1a6493ecb4f9ac73377
Paul Larson <paul.larson@canonical.com>
2011-06-23 14:38:42 -0500

    Add headers

--- lava/dispatcher/actions/lava-test.py
+++ lava/dispatcher/actions/lava-test.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python
+
+# Copyright (C) 2011 Linaro Limited
+#
+# Author: Paul Larson <paul.larson@linaro.org>
+#
+# This file is part of LAVA Dispatcher.
+#
+# LAVA Dispatcher is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# LAVA Dispatcher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along
+# with this program; if not, see <http://www.gnu.org/licenses>.
+
 from lava.dispatcher.actions import BaseAction
 from lava.dispatcher.client import OperationFailed
 from lava.dispatcher.config import LAVA_RESULT_DIR, MASTER_STR, TESTER_STR
