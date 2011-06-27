43e30a03bd52c70e0bdf593a351d255f08a75257
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-06-27 12:02:29 +1200

    remove print

--- lava_scheduler_app/views.py
+++ lava_scheduler_app/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render_to_response
 from lava_scheduler_app.models import Device, TestJob
 
 def index(request):
-    print Device.objects.all()
     return render_to_response(
         "lava_scheduler_app/index.html",
         {
