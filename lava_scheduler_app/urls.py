82486df053b4e8dac712cd891906e91e68b3f92f
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-07-25 15:09:15 +1200

    checkpoint

--- lava_scheduler_app/urls.py
+++ lava_scheduler_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns(
     url(r'^$', 'index'),
     url(r'^alljobs$', 'alljobs'),
     url(r'^job/(?P<pk>[0-9]+)$', 'job'),
+    url(r'^job/(?P<pk>[0-9]+)/output$', 'job_output'),
     )
--- lava_scheduler_app/views.py
+++ lava_scheduler_app/views.py
@@ -1,5 +1,7 @@
+import json
 import os
 
+from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
@@ -28,14 +30,26 @@ def alljobs(request):
 def job(request, pk):
     job = TestJob.objects.get(pk=pk)
     log_file_path = '/tmp/lava-logs/job-%s.log' % job.id
-    if os.path.exists(log_file_path):
-        log_file = open(log_file_path, 'rb')
-    else:
-        log_file = None
+    log_file_present = os.path.exists(log_file_path)
     return render_to_response(
         "lava_scheduler_app/job.html",
         {
-            'log_file': log_file,
+            'log_file_present': log_file_present,
             'job': TestJob.objects.get(pk=pk),
         },
         RequestContext(request))
+
+def job_output(request, pk):
+    start = int(request.GET.get('start', 0))
+    job = TestJob.objects.get(pk=pk)
+    log_file_path = '/tmp/lava-logs/job-%s.log' % job.id
+    log_file = open(log_file_path, 'rb')
+    log_file.seek(start)
+    content = log_file.read()
+    # unicode issues...
+    data = {
+        'is_finished': job.status != TestJob.RUNNING,
+        'size': log_file.tell(),
+        'content': content,
+        }
+    return HttpResponse(json.dumps(data))
