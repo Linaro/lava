609c3607167dd5a0c2abdf0e0e82779867effefa
Michael-Doyle Hudson <michael.hudson@linaro.org>
2011-07-25 13:29:34 +1200

    slightly crummy viewing of log file

--- lava_scheduler_app/views.py
+++ lava_scheduler_app/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
@@ -24,9 +26,16 @@ def alljobs(request):
 
 
 def job(request, pk):
+    job = TestJob.objects.get(pk=pk)
+    log_file_path = '/tmp/lava-logs/job-%s.log' % job.id
+    if os.path.exists(log_file_path):
+        log_file = open(log_file_path, 'rb')
+    else:
+        log_file = None
     return render_to_response(
         "lava_scheduler_app/job.html",
         {
+            'log_file': log_file,
             'job': TestJob.objects.get(pk=pk),
         },
         RequestContext(request))
