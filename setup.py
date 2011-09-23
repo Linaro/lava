87dd7787714883efc1890ace70deea43c5046664
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-09-23 10:25:04 +0200

    Enable lava-markitup application.
    Note: This changes dependencies of lava-server!

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         'python-openid >= 2.2.4',  # this should be a part of django-openid-auth deps
         'south >= 0.7.3',
         'versiontools >= 1.3.1',
+        'markdown >= 2.0.3',
     ],
     setup_requires=[
         'versiontools >= 1.3.1',
