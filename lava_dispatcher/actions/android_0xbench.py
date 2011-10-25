3016f6a1d1c6f87bac2b234fef84a67046f23d5a
Spring Zhang <spring.zhang@linaro.org>
2011-10-25 16:32:36 +0800

    add boot_linaro_image action if not in test image

--- lava_dispatcher/actions/android_0xbench.py
+++ lava_dispatcher/actions/android_0xbench.py
@@ -26,28 +26,32 @@ import logging
 
 class cmd_test_android_0xbench(BaseAction):
     def run(self):
+        client = self.client
         #Make sure in test image now
-        self.client.in_test_shell()
+        try:
+            client.in_test_shell()
+        except:
+            client.boot_linaro_image()
         time.sleep(30)
-        if not self.client.check_sys_bootup():
+        if not client.check_sys_bootup():
             # TODO: Fetch the logcat message as attached
             logging.warning("0xbench Test: sys bootup fail, aborted")
             return
 
-        self.client.android_logcat_clear()
+        client.android_logcat_clear()
 
         package_name = 'org.zeroxlab.benchmark'
         class_name = 'org.zeroxlab.benchmark.Benchmark'
         cmd = 'am start -n %s/%s --ez math true --ez 2d true --ez 3d true \
             --ez vm true --ez autorun true' % (package_name, class_name)
-        self.client.run_shell_command(cmd)
+        client.run_shell_command(cmd)
 
         # Do the logcat and monitor the log to know 0xbench done the test
         pattern = "Displayed org.zeroxlab.benchmark/.Report"
         try:
-            self.client.android_logcat_monitor(pattern, timeout = 1200)
+            client.android_logcat_monitor(pattern, timeout=1200)
         except pexpect.TIMEOUT:
             logging.warning("0xbench Test: TIMEOUT Fail")
             raise
         finally:
-            self.client.android_logcat_stop()
+            client.android_logcat_stop()
