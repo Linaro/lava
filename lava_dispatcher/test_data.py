c44241f9507390d05ed29cee50e86f381d5b3ce8
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-11-11 15:47:22 +0100

    Split __init__.py into {context,job,test_data}.py

---
+++ lava_dispatcher/test_data.py
@@ -0,0 +1,73 @@
+# Copyright (C) 2011 Linaro Limited
+#
+# Author: Paul Larson <paul.larson@linaro.org>
+#
+# This file is part of LAVA Dispatcher.
+#
+# LAVA Dispatcher is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# LAVA Dispatcher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along
+# with this program; if not, see <http://www.gnu.org/licenses>.
+
+from datetime import datetime
+from uuid import uuid1
+import base64
+
+class LavaTestData(object):
+    def __init__(self, test_id='lava'):
+        self.job_status = 'pass'
+        self.metadata = {}
+        self._test_run = { 'test_results':[], 'attachments':[] }
+        self._test_run['test_id'] = test_id
+        self._assign_date()
+        self._assign_uuid()
+
+    def _assign_date(self):
+        TIMEFORMAT = '%Y-%m-%dT%H:%M:%SZ'
+        self._test_run['time_check_performed'] = False
+        self._test_run['analyzer_assigned_date'] = datetime.strftime(
+            datetime.now(), TIMEFORMAT)
+
+    def _assign_uuid(self):
+        self._test_run['analyzer_assigned_uuid'] = str(uuid1())
+
+    def add_result(self, test_case_id, result, message=""):
+        result_data = {
+            'test_case_id': test_case_id,
+            'result': result,
+            'message': message
+            }
+        self._test_run['test_results'].append(result_data)
+
+    def add_attachment(self, attachment):
+        self._test_run['attachments'].append(attachment)
+
+    def add_metadata(self, metadata):
+        self.metadata.update(metadata)
+
+    def get_metadata(self):
+        return self.metadata
+
+    def get_test_run(self):
+        self.add_result('job_complete', self.job_status)
+        return self._test_run
+
+    def add_seriallog(self, serial_log):
+        """
+        Add serial log to the "attachments" field, it aligns bundle 1.2 format
+        """
+        serial_log_base64 = base64.b64encode(serial_log)
+        attachment = {
+                "pathname": "serial.log",
+                "mime_type": "text/plain",
+                "content": serial_log_base64 }
+        self.add_attachment(attachment)
