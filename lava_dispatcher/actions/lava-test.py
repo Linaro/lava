2a73ec0f87e41a5f1b6548229b056fb23f047f04
Michael Hudson-Doyle <michael.hudson@linaro.org>
2011-11-18 11:42:36 +1300

    introduce "partition_session" idea, use it in lava-test install actions

--- lava_dispatcher/actions/lava-test.py
+++ lava_dispatcher/actions/lava-test.py
@@ -28,54 +28,24 @@ from lava_dispatcher.actions import BaseAction
 from lava_dispatcher.client import OperationFailed, CriticalError
 from lava_dispatcher.config import get_config
 
-def _setup_testrootfs(client):
-    #Make sure in master image
-    #, or exception can be caught and do boot_master_image()
-    try:
-        client.in_master_shell()
-    except:
-        logging.exception("in_master_shell failed")
-        client.boot_master_image()
 
-    client.run_cmd_master('mkdir -p /mnt/root')
-    client.run_cmd_master('mount /dev/disk/by-label/testrootfs /mnt/root')
-    client.run_cmd_master(
-        'cp -f /mnt/root/etc/resolv.conf /mnt/root/etc/resolv.conf.bak')
-    client.run_cmd_master('cp -L /etc/resolv.conf /mnt/root/etc')
-    #eliminate warning: Can not write log, openpty() failed
-    #                   (/dev/pts not mounted?), does not work
-    client.run_cmd_master('mount --rbind /dev /mnt/root/dev')
-
-
-def _teardown_testrootfs(client):
-    client.run_cmd_master(
-        'cp -f /mnt/root/etc/resolv.conf.bak /mnt/root/etc/resolv.conf')
-    cmd = ('cat /proc/mounts | awk \'{print $2}\' | grep "^/mnt/root/dev"'
-        '| sort -r | xargs umount')
-    client.run_cmd_master(cmd)
-    client.run_cmd_master('umount /mnt/root')
-
-
-def _install_lava_test(client):
+def _install_lava_test(client, session):
     #install bazaar in tester image
-    client.run_cmd_master(
-        'chroot /mnt/root apt-get update')
+    session.run('apt-get update')
     #Install necessary packages for build lava-test
-    cmd = ('chroot /mnt/root apt-get -y install bzr usbutils python-apt '
-        'python-setuptools python-simplejson lsb-release')
-    client.run_cmd_master(cmd, timeout=2400)
-    client.run_cmd_master("chroot /mnt/root apt-get -y install python-pip")
+    cmd = ('apt-get -y install '
+           'bzr usbutils python-apt python-setuptools python-simplejson lsb-release')
+    session.run(cmd, timeout=2400)
+    session.run("apt-get -y install python-pip")
 
     dispatcher_config = get_config("lava-dispatcher")
     lava_test_url = dispatcher_config.get("LAVA_TEST_URL")
     logging.debug("Installing %s with pip" % lava_test_url)
-    client.run_cmd_master('chroot /mnt/root pip install -e ' + lava_test_url)
+    session.run('pip install -e ' + lava_test_url)
 
     #Test if lava-test installed
     try:
-        client.run_shell_command(
-            'chroot /mnt/root lava-test help',
-            response="list-test", timeout=60)
+        client.run('lava-test help', response="list-test", timeout=60)
     except:
         tb = traceback.format_exc()
         client.sio.write(tb)
@@ -126,42 +96,36 @@ class cmd_lava_test_install(BaseAction):
     """
     def run(self, tests, install_python = None, register = None, timeout=2400):
         logging.info("Executing lava_test_install (%s) command" % ",".join(tests))
-        client = self.client
 
-        _setup_testrootfs(client)
-        _install_lava_test(client)
+        with self.client.partition_session('testrootfs') as session:
 
-        if install_python:
-            for module in install_python:
-                client.run_cmd_master("chroot /mnt/root pip install -e " + module)
+            _install_lava_test(self.client, session)
 
-        if register:
-            for test_def_url in register:
-                client.run_cmd_master('chroot /mnt/root lava-test register-test  ' + test_def_url)
+            if install_python:
+                for module in install_python:
+                    session.run("pip install -e " + module)
 
-        for test in tests:
-            client.run_cmd_master(
-                'chroot /mnt/root lava-test install %s' % test)
+            if register:
+                for test_def_url in register:
+                    session.run('lava-test register-test  ' + test_def_url)
 
-        client.run_cmd_master('rm -rf /mnt/root/lava-test')
+            for test in tests:
+                session.run('lava-test install %s' % test)
 
-        _teardown_testrootfs(client)
+            session.run('rm -rf lava-test')
 
 
 class cmd_add_apt_repository(BaseAction):
     """
     add apt repository to test image rootfs by chroot
-    arg could be 'deb uri distribution [component1] [component2][...]' or ppm:<ppa_name>
+    arg could be 'deb uri distribution [component1] [component2][...]' or ppa:<ppa_name>
     """
     def run(self, arg):
-        client = self.client
-        _setup_testrootfs(client)
+        with self.client.partition_session('testrootfs') as session:
 
-        #install add-apt-repository
-        client.run_cmd_master('chroot /mnt/root apt-get -y install python-software-properties')
+            #install add-apt-repository
+            session.run('apt-get -y install python-software-properties')
 
-        #add ppa
-        client.run_cmd_master('chroot /mnt/root add-apt-repository %s < /dev/null' % arg[0])
-        client.run_cmd_master('chroot /mnt/root apt-get update')
-
-        _teardown_testrootfs(client)
+            #add ppa
+            session.run('add-apt-repository %s < /dev/null' % arg[0])
+            session.run('apt-get update')
