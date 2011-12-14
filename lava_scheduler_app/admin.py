4d875714267b4e579495344f7840636dee1218cc
Michael Hudson-Doyle <michael.hudson@linaro.org>
2011-12-14 14:07:33 +1300

    enable tags in the admin interface

--- lava_scheduler_app/admin.py
+++ lava_scheduler_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from lava_scheduler_app.models import Device, DeviceType, TestJob
+from lava_scheduler_app.models import Device, DeviceType, TestJob, Tag
 
 admin.site.register(Device)
 admin.site.register(DeviceType)
 admin.site.register(TestJob)
+admin.site.register(Tag)
