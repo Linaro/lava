9b254e49a1f20a49313fa3f51830b48b3710807e
Michael Hudson-Doyle <michael.hudson@linaro.org>
2011-12-15 15:59:42 +1300

    oops, that sense inversion from my ancient version was not wanted

--- lava_scheduler_daemon/dbjobsource.py
+++ lava_scheduler_daemon/dbjobsource.py
@@ -97,11 +97,11 @@ class DatabaseJobSource(object):
                 select={
                     'is_targeted': 'requested_device_id is not NULL',
                     'missing_tags': '''
-                        select count(*) from lava_scheduler_app_device_tags
-                                       where device_id = '%s'
+                        select count(*) from lava_scheduler_app_testjob_tags
+                                       where testjob_id = lava_scheduler_app_testjob.id
                                        and tag_id not in (select tag_id
-                                                            from lava_scheduler_app_testjob_tags
-                                                           where testjob_id = lava_scheduler_app_testjob.id)
+                                                            from lava_scheduler_app_device_tags
+                                                           where device_id = '%s')
                         ''' % device.hostname,
                     },
                 where=['missing_tags = 0'],
