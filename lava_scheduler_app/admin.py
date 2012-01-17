ab5e19f68e2ac85b3e371376ce3d5a3520cff7ba
Spring Zhang <spring.zhang@linaro.org>
2012-01-17 16:06:13 +0800

    Mofidy DH Admin page

--- lava_scheduler_app/admin.py
+++ lava_scheduler_app/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from lava_scheduler_app.models import Device, DeviceType, TestJob, Tag, DeviceHealth
+from lava_scheduler_app.models import (
+        Device, DeviceType,
+        TestJob, Tag, DeviceHealth,
+        )
+
+class DeviceHealthAdmin(admin.ModelAdmin):
+    list_display = ('device', 'health')
 
 admin.site.register(Device)
 admin.site.register(DeviceType)
 admin.site.register(TestJob)
 admin.site.register(Tag)
-admin.site.register(DeviceHealth)
+admin.site.register(DeviceHealth, DeviceHealthAdmin)
