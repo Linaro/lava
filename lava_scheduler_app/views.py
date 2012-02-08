3d679b3b672c52a267fdd250c36aedb6765e18d2
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-02-07 16:32:55 -0800

    show when the transition occurred and how long the device was in a given state

--- lava_scheduler_app/views.py
+++ lava_scheduler_app/views.py
@@ -236,6 +236,33 @@ def job_json(request, pk):
     return HttpResponse(json_text, content_type=content_type)
 
 
+def format_duration(duration):
+    """Format timedeltas as "v days, w hours, x minutes, y.z seconds"."""
+    parts = []
+    minutes, seconds = divmod(duration.seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    seconds = seconds + (float(duration.microseconds) / 10 ** 6)
+    if duration.days > 0:
+        if duration.days == 1:
+            parts.append('%d day' % duration.days)
+        else:
+            parts.append('%d days' % duration.days)
+    if parts or hours > 0:
+        if hours == 1:
+            parts.append('%d hour' % hours)
+        else:
+            parts.append('%d hours' % hours)
+    if parts or minutes > 0:
+        if minutes == 1:
+            parts.append('%d minute' % minutes)
+        else:
+            parts.append('%d minutes' % minutes)
+    if parts or seconds > 0:
+        parts.append('%0.1f seconds' % seconds)
+
+    return ', '.join(parts)
+    
+
 @BreadCrumb("Device {pk}", parent=index, needs=['pk'])
 def device_detail(request, pk):
     device = get_object_or_404(Device, pk=pk)
@@ -247,8 +274,22 @@ def device_detail(request, pk):
             transition = None
     else:
         transition = None
-    transitions = DeviceStateTransition.objects.filter(
-                device=device).order_by('-created_on')
+    transition_models = DeviceStateTransition.objects.filter(
+                device=device).order_by('created_on')
+    transitions = []
+    if transition_models:
+        t = transition_models[0]
+        transitions.append(
+            (t.created_on, None, t.get_old_state_display(),
+             t.get_new_state_display(), t.created_by, t.message))
+        for i in range(1, len(transition_models)):
+            t = transition_models[i]
+            duration = format_duration(t.created_on - transition_models[i-1].created_on)
+            transitions.append(
+                (t.created_on, duration,
+                 t.get_old_state_display(), t.get_new_state_display(),
+                 t.created_by, t.message))
+        transitions.reverse()
     return render_to_response(
         "lava_scheduler_app/device.html",
         {
