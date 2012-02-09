04d962488377a77f7f454685943ae55ca369efe9
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2012-02-10 00:40:47 +0100

    Snapshot of queryset backend (work in progress for mwhudson)

--- lava/utils/data_tables/backends.py
+++ lava/utils/data_tables/backends.py
@@ -70,3 +70,80 @@ class ArrayBackend(_BackendBase):
         # Remember the subset of the displayed data
         response['aaData'] = data
         return response
+
+
+def _identity(obj):
+    return obj
+
+
+class Column(object):
+    """
+    Column definition for the QuerySetBackend
+    """
+
+    def __init__(self, name, filter_expr, callback=None):
+        self.name = name
+        self.filter_expr = filter_expr
+        self.callback = callback or _identity
+
+
+class QuerySetBackend(_BackendBase):
+    """
+    Database backend for data tables.
+
+    Stores and processes/computes the data in the database. Needs a queryset
+    object and a mapping between colums and query values.
+    """
+
+    def __init__(self, queryset=None, queryset_cb=None, columns=None):
+        self.queryset = queryset
+        self.queryset_cb = queryset_cb
+        self.columns = columns
+        if not queryset and not queryset_cb:
+            raise ImproperlyConfigured(
+                "QuerySetBackend requires either queryset or queryset_cb")
+        if not columns:
+            raise ImproperlyConfigured(
+                "QuerySetBackend requires columns")
+
+    def process(self, query):
+        # Get the basic response structure
+        response = super(QuerySetBackend, self).process(query)
+        # Get a queryset object
+        if self.queryset:
+            queryset = self.queryset
+        else:
+            queryset = self.queryset_cb(query.request)
+        # 1) Apply search/filtering
+        if query.sSearch:
+            if query.bRegex:
+                raise NotImplementedError("Searching with regular expresions is not implemented")
+            else:
+                raise NotImplementedError("Searching is not implemented")
+                #for column in 
+                #kwargz = {searchableColumn+"__icontains" : customSearch}
+                #outputQ = outputQ | Q(**kwargz) if outputQ else Q(**kwargz)             
+                #queryset = queryset.filter(
+                #data = [row for row in data if any((query.sSearch in unicode(cell) for cell in row))]
+        # TODO: Support per-column search
+        # Remember how many records matched filtering
+        response['iTotalDisplayRecords'] = queryset.count()
+        # 2) Apply sorting
+        order_by = [
+            "{asc_desc}{column}".format(
+                asc_desc="-" if order == 'desc' else '',
+                column=self.columns[column_index].filter_expr)
+            for column_index, order in query.sorting_columns]
+        queryset = queryset.order_by(*order_by)
+        # 3) Apply offset/limit
+        queryset = queryset[query.iDisplayStart:query.iDisplayStart + query.iDisplayLength]
+        #  Compute the response
+        # Note, despite the 'aaData'' identifier we're
+        # returing aoData-typed result (array of objects)
+        # print queryset.values(*[column.filter_expr for column in self.columns])
+        print queryset.values(*[column.filter_expr for column in self.columns])
+        response['aaData'] = [
+            dict([(column.name, unicode(column.callback(object)))
+                  for column in self.columns])
+            for object in queryset]
+        return response
