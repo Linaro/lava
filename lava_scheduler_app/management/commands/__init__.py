59beee9452c270125c954b2a7fedaef113bcfdf0
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-03-29 17:51:20 +1300

    just replace the logging config when starting up the scheduler or monitor

--- lava_scheduler_app/management/commands/__init__.py
+++ lava_scheduler_app/management/commands/__init__.py
@@ -1,6 +1,5 @@
-import logging
+import logging.config
 from optparse import make_option
-import sys
 
 from django.core.management.base import BaseCommand
 
@@ -20,23 +19,71 @@ class SchedulerCommand(BaseCommand):
 
     log_prefix = ''
 
+
+    _DEFAULT_LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'root': {
+            'level': None,
+            'handlers': ['default'],
+        },
+        'formatters': {
+            'default': {
+                'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+            },
+        },
+        'handlers': {
+            'default': {
+                'level': 'DEBUG',
+                'class': 'logging.FileHandler',
+                'formatter': 'verbose'
+            }
+        },
+    }
+
+
     def _configure(self, options):
         from django.conf import settings
+
         daemon_options = settings.SCHEDULER_DAEMON_OPTIONS.copy()
         if options['logfile'] is not None:
             daemon_options['LOG_FILE_PATH'] = options['logfile']
         if options['loglevel'] is not None:
             daemon_options['LOG_LEVEL'] = options['loglevel']
-        logger = logging.getLogger('')
+
         if daemon_options['LOG_FILE_PATH'] in [None, '-']:
-            handler = logging.StreamHandler(sys.stderr)
+            handler = {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'default',
+                }
         else:
-            handler = logging.FileHandler(daemon_options['LOG_FILE_PATH'])
+            handler = {
+                'level': 'DEBUG',
+                'class': 'logging.FileHandler',
+                'filename': daemon_options['LOG_FILE_PATH'],
+                'formatter': 'default'
+                }
+
         fmt = "%(asctime)s [%(levelname)s] [%(name)s] %(message)s"
         if self.log_prefix:
             fmt = self.log_prefix + ' ' + fmt
-        handler.setFormatter(logging.Formatter(fmt))
-        logger.addHandler(handler)
-        logger.setLevel(getattr(logging, daemon_options['LOG_LEVEL'].upper()))
+
+
+        LOGGING = {
+            'version': 1,
+            'disable_existing_loggers': True,
+            'root': {
+                'level': daemon_options['LOG_LEVEL'].upper(),
+                'handlers': ['default'],
+            },
+            'formatters': {'default': {'format': fmt}},
+            'handlers': {'default': handler}
+            }
+
+        import pprint; pprint.pprint(LOGGING)
+
+        logging.config.dictConfig(LOGGING)
+
         return daemon_options
 
