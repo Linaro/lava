017559e85893a8547b7cd94ac09fe0ec4307c818
Rafael G. Martins <rafael.martins@collabora.com>
2012-05-08 19:06:32 -0300

    Updated json-schema-validator dependency. We needs version 2.3.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
             ],
         },
     install_requires=[
-        "json-schema-validator",
+        "json-schema-validator >= 2.3",
         "lava-tool >= 0.4",
         "lava-utils-interface",
         "pexpect >= 2.3",
