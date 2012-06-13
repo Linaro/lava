e2aae21fa1d9261ad89c7a1ab65fa43f641a47bd
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-06-14 11:36:59 +1200

    small fixes

--- recipes/server.py
+++ recipes/server.py
@@ -8,14 +8,20 @@ class ServerSymlink(object):
         self.egg = zc.recipe.egg.Egg(buildout, options['recipe'], options)
         self.options = options
 
-
-    def install(self):
+    def make_link(self):
         reqs, ws = self.egg.working_set()
         lava_server_code = os.path.join(ws.require('lava-server')[0].location, 'lava_server')
         assert os.path.exists(lava_server_code)
         symlink_location = os.path.join(self.buildout['buildout']['directory'], 'server_code')
+        if os.path.exists(symlink_location):
+            os.unlink(symlink_location)
         os.symlink(lava_server_code, symlink_location)
+        return symlink_location
+
+    def install(self):
+        symlink_location = self.make_link()
         return [symlink_location]
 
     def update(self):
-        pass
+        symlink_location = self.make_link()
+        return [symlink_location]
