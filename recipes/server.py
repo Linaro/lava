20e00be917561f75e1cfaab8faec2289840c254a
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-06-14 15:18:22 +1200

    work around a ridiculous buildout bug that means it cannot remove a symlink automatically (see https://bugs.launchpad.net/zc.buildout/+bug/144228)

--- recipes/server.py
+++ recipes/server.py
@@ -2,17 +2,23 @@ import os
 
 import zc.recipe.egg
 
+
+def get_symlink_location(options):
+    return os.path.join(options['directory'], 'server_code')
+
+
 class ServerSymlink(object):
     def __init__(self, buildout, name, options):
         self.buildout = buildout
         self.egg = zc.recipe.egg.Egg(buildout, options['recipe'], options)
         self.options = options
+        options['directory'] = self.buildout['buildout']['directory']
 
     def make_link(self):
         reqs, ws = self.egg.working_set()
         lava_server_code = os.path.join(ws.require('lava-server')[0].location, 'lava_server')
         assert os.path.exists(lava_server_code)
-        symlink_location = os.path.join(self.buildout['buildout']['directory'], 'server_code')
+        symlink_location = get_symlink_location(self.options)
         if os.path.exists(symlink_location):
             os.unlink(symlink_location)
         os.symlink(lava_server_code, symlink_location)
@@ -25,3 +31,7 @@ class ServerSymlink(object):
     def update(self):
         symlink_location = self.make_link()
         return [symlink_location]
+
+def uninstall_server_symlink(name, options):
+    symlink_location = get_symlink_location(options)
+    os.unlink(symlink_location)
