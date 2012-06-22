631956e6a5fa78faeac34209fbaa86723d6afb93
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-06-22 12:02:46 +1200

    move filling out of old results bundles to a management command

---
+++ lava_scheduler_app/management/commands/testjobmigrate.py
@@ -0,0 +1,32 @@
+import sys
+
+from django.core.management.base import BaseCommand
+
+from dashboard_app.models import Bundle
+from lava_scheduler_app.models import TestJob
+
+
+class Command(BaseCommand):
+
+    help = "Fill out results_bundle on old testjobs."
+
+    option_list = BaseCommand.option_list
+
+    def handle(self, *args, **options):
+        count = 0
+        query = TestJob.objects.filter(
+            results_link__isnull=False, results_bundle__isnull=True)
+        print query.count()
+        for job in query.all():
+            count += 1
+            if count % 100 == 0:
+                sys.stdout.write('.')
+                sys.stdout.flush()
+            sha1 = job.results_link.strip('/').split('/')[-1]
+            try:
+                bundle = Bundle.objects.get(content_sha1=sha1)
+            except Bundle.DoesNotExist:
+                continue
+            job._results_bundle = bundle
+            job.save()
+
