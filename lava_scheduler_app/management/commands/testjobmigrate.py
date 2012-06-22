10e4c9dc8660c65078d9aea5befc2dc654bf3d25
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-06-22 13:52:17 +1200

    make testjobmigrate a touch more robust

--- lava_scheduler_app/management/commands/testjobmigrate.py
+++ lava_scheduler_app/management/commands/testjobmigrate.py
@@ -1,6 +1,7 @@
 import sys
 
 from django.core.management.base import BaseCommand
+from django.db.models import Q
 
 from dashboard_app.models import Bundle
 from lava_scheduler_app.models import TestJob
@@ -15,7 +16,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         count = 0
         query = TestJob.objects.filter(
-            _results_link__isnull=False, _results_bundle__isnull=True)
+            ~Q(_results_link=u''), _results_link__isnull=False,
+            _results_bundle__isnull=True)
         print query.count()
         for job in query.all():
             count += 1
