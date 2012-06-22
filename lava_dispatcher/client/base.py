e4a85afd674a5312551f94c29be5e2254b698367
Andy Doan <andy.doan@linaro.org>
2012-06-22 11:18:50 -0500

    update fastmodel client for proper android support
    base.py:
     * don't assume the default network interface is used for ADB. In
       the case of fast model's it has to be a bridged "lo" device.
    
     * don't attempt to get ip in get_default_nic_ip. we already have
       code to get the IP address in the _enable_network function
    
     * the sleep between check_device_state needed to be a bit bigger
    
     * the wait for home screen had to be increased
    
     * option support for enable_network_after_boot_android was always
      returning true based on how it was written
    
    fastmodel.py
     * fastmodels must use loopback for adb which requires setting
       the userNetPorts when launching the process
    
     * ping doesn't in user mode network, so we fake this out

--- lava_dispatcher/client/base.py
+++ lava_dispatcher/client/base.py
@@ -182,18 +182,13 @@ class AndroidTesterCommandRunner(NetworkCommandRunner):
         pexpect.run(cmd, timeout=300, logfile=sys.stdout)
 
     def get_default_nic_ip(self):
-        # XXX: IP could be assigned in other way in the validation farm
-        network_interface = self._client.default_network_interface
-        ip = None
+        network_interface = self._client.get_android_adb_interface()
         try:
             ip = self._get_default_nic_ip_by_ifconfig(network_interface)
         except:
             logging.exception("_get_default_nic_ip_by_ifconfig failed")
-            pass
+            return None
 
-        if ip is None:
-            self.get_ip_via_dhcp(network_interface)
-            ip = self._get_default_nic_ip_by_ifconfig(network_interface)
         return ip
 
     def _get_default_nic_ip_by_ifconfig(self, nic_name):
@@ -216,18 +211,11 @@ class AndroidTesterCommandRunner(NetworkCommandRunner):
                 return match_group[0]
         return None
 
-    def get_ip_via_dhcp(self, nic):
-        try:
-            self.run('netcfg %s dhcp' % nic, timeout=60)
-        except:
-            logging.exception("netcfg %s dhcp failed" % nic)
-            raise NetworkError("netcfg %s dhcp exception" % nic)
-
     def wait_until_attached(self):
         for count in range(3):
             if self.check_device_state():
                 return
-            time.sleep(1)
+            time.sleep(3)
 
         raise NetworkError(
             "The android device(%s) isn't attached" % self._client.hostname)
@@ -427,6 +415,9 @@ class LavaClient(object):
 
     # Android stuff
 
+    def get_android_adb_interface(self):
+        return self.default_network_interface
+
     def boot_linaro_android_image(self):
         """Reboot the system to the test android image."""
         self._boot_linaro_android_image()
@@ -436,7 +427,7 @@ class LavaClient(object):
         #TODO: set up proxy
 
         self._disable_suspend()
-        if self.config.get("enable_network_after_boot_android"):
+        if self.device_option_int("enable_network_after_boot_android"):
             time.sleep(1)
             self._enable_network()
 
@@ -461,7 +452,8 @@ class LavaClient(object):
         session.run('sqlite3 /data/data/com.android.providers.settings/databases/settings.db "%s"' % (stay_awake)) ## set stay awake
         session.run('sqlite3 /data/data/com.android.providers.settings/databases/settings.db "%s"' % (screen_sleep)) ## set sleep to none
         session.run('sqlite3 /data/data/com.android.providers.settings/databases/settings.db "%s"' % (lockscreen)) ##set lock screen to none
-        session.run('input keyevent 82')  ##unlock the home screen
+        #unlock the home screen 240: for fastmodels, 120 failed, 180 failed some
+        session.run('input keyevent 82', timeout=240)
         session.run('service call power 1 i32 26') ##acquireWakeLock FULL_WAKE_LOCK
 
     def _enable_network(self):
