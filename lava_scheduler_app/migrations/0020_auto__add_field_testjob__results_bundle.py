29c606469f0cad781c331cdc717c03c254422789
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-06-29 12:12:08 +1200

    make 0020_auto__add_field_testjob__results_bundle depend on the dashboard_app bundle that creates the bundle table

--- lava_scheduler_app/migrations/0020_auto__add_field_testjob__results_bundle.py
+++ lava_scheduler_app/migrations/0020_auto__add_field_testjob__results_bundle.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = [
+        ('dashboard_app', '0001_initial'),
+        ]
+
     def forwards(self, orm):
         # Adding field 'TestJob._results_bundle'
         db.add_column('lava_scheduler_app_testjob', '_results_bundle',
