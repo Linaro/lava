53025be4bb99d68dcd7293bf670e4a386acd4186
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-07-13 13:02:33 +1200

    after failing to do this with CSS: make the table expand to fit its container, but not too much

--- dashboard_app/static/js/image-report.js
+++ dashboard_app/static/js/image-report.js
@@ -1,5 +1,25 @@
-// OK, so this file is hilariously trivial for now.  But we'll add a
-// lot more when we do bug linking.
-$(function () {
-      $("#scroller").scrollLeft(10000);
-  });
+function _resize() {
+    // I couldn't figure out how to do this in CSS: resize the table
+    // so that it takes as much space as it can without expanding the
+    // page horizontally.
+    var space = parseInt($("#lava-breadcrumbs").outerWidth() - $("#outer-table").outerWidth());
+    console.log("in", parseInt($("#outer-table").outerWidth()));
+    space -= $("#lava-content").outerWidth() - $("#lava-content").width();
+    var table = $("#results-table"), scroller=$("#scroller");
+    var atRight = scroller.width() + scroller.scrollLeft() >= table.attr('scrollWidth');
+    scroller.width(scroller.width() + space);
+    console.log("out", parseInt($("#outer-table").outerWidth()));
+    if (atRight) scroller.scrollLeft(table.attr('scrollWidth'));
+}
+// Hook up the event and run resize ASAP (looks jumpy in FF if you
+// don't run it here).
+$(window).ready(
+    function () {
+      $(window).resize(_resize);
+      $("#scroller").scrollLeft(100000);
+      _resize();
+    });
+// Because what resize does depends on the final sizes of elements,
+// run it again after everything is loaded (things end up wrong in
+// chromium if you don't do this).
+$(window).load(_resize);
