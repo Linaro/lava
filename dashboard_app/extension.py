1f88e227d2dfa29242207878da779b3b5cfdbe91
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-08-16 11:42:14 +1200

    move filters to dashboard menu

--- dashboard_app/extension.py
+++ dashboard_app/extension.py
@@ -46,6 +46,7 @@ class DashboardExtension(LavaServerExtension):
             Menu("Data Views", reverse("dashboard_app.views.data_view_list")),
             Menu("Reports", reverse("dashboard_app.views.report_list")),
             Menu("Image Reports", reverse("dashboard_app.views.image_report_list")),
+            Menu("Filters", reverse("dashboard_app.views.filters_list")),
             ]
         return menu
 
@@ -86,13 +87,6 @@ class DashboardExtension(LavaServerExtension):
         settings_module['DATAREPORT_DIRS'] = [
             os.path.join(root_dir, 'examples', 'reports'),
             os.path.join(root_dir, 'production', 'reports')]
-        action = (
-            'dashboard_app.views.filters_list',
-            "Filters")
-        # contribute_to_settings can end up being called twice, for bad
-        # reasons.
-        if action not in settings_module['ME_PAGE_ACTIONS']:
-            settings_module['ME_PAGE_ACTIONS'].append(action)
 
     def contribute_to_settings_ex(self, settings_module, settings_object):
         settings_module['DATAVIEW_DIRS'] = settings_object._settings.get(
