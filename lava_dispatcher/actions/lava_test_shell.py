53419147e34a7a199e594e435a4f72dc58120bad
Andy Doan <andy.doan@linaro.org>
2012-10-01 21:57:13 -0500

    add notion of testdef to lava_test_shell action
    This adds a testdef_url and timeout to the parameters this action
    accepts. The testdef_url should point to a file that supports the
    lp:lava-test's "declarative test" model. The timeout value is how
    long in seconds the test will be given to run.
    
    This series just gets the testdef in place. The next few patches
    actually do something with this.
    
    An example declarative test would be:
    {
        "format": "Lava-Test Test Definition 1.0",
        "test_id": "passfail",
        "install": {
            "bzr-repos": ["lp:lava-test"],
            "git-repos": ["git://git.linaro.org/ubuntu/daily-prebuilt-images.git"],
            "steps": ["echo installing!"]
        },
        "run": {
            "steps": ["/bin/echo mem-stress-a7 : PASS"]
         },
         "parse": {
            "pattern": "(?P<test_case_id>.*-*)\\s+:\\s+(?P<result>(PASS|FAIL))",
            "fixupdict": {
                "PASS": "pass",
                "FAIL": "fail"
            }
        }
    }

--- lava_dispatcher/actions/lava_test_shell.py
+++ lava_dispatcher/actions/lava_test_shell.py
@@ -20,6 +20,7 @@
 # along
 # with this program; if not, see <http://www.gnu.org/licenses>.
 
+import json
 import logging
 import os
 import pexpect
@@ -29,6 +30,7 @@ import lava_dispatcher.utils as utils
 
 from lava_dispatcher.actions import BaseAction
 from lava_dispatcher.device.target import Target
+from lava_dispatcher.downloader import download_image
 
 LAVA_TEST_DIR = '%s/../../lava_test_shell' % os.path.dirname(__file__)
 LAVA_TEST_ANDROID = '%s/lava-test-runner-android' % LAVA_TEST_DIR
@@ -60,14 +62,20 @@ Target.android_deployment_data['lava_test_configure_startup'] = \
 class cmd_lava_test_shell(BaseAction):
 
     parameters_schema = {
-        'type': ['object', 'null'],
+        'type': 'object',
+        'properties': {
+            'testdef_url': {'type': 'string'},
+            'timeout': {'type': 'integer', 'optional': True},
+            },
         'additionalProperties': False,
         }
 
-    def run(self):
+    def run(self, testdef_url, timeout=-1):
         target = self.client.target_device
         self._assert_target(target)
-        self._configure_target(target)
+
+        testdef = self._get_test_definition(testdef_url, target.scratch_dir)
+        self._configure_target(target, testdef)
 
         with target.runner() as runner:
             patterns = [
@@ -75,7 +83,7 @@ class cmd_lava_test_shell(BaseAction):
                 pexpect.EOF,
                 pexpect.TIMEOUT,
                 ]
-            idx = runner._connection.expect(patterns, timeout=-1)
+            idx = runner._connection.expect(patterns, timeout=timeout)
             if idx == 0:
                 logging.info('lava_test_shell seems to have completed')
             elif idx == 1:
@@ -83,19 +91,33 @@ class cmd_lava_test_shell(BaseAction):
             elif idx == 2:
                 logging.warn('lava_test_shell has timed out')
 
+    def _get_test_definition(self, testdef_url, tmpdir):
+        testdef_file = download_image(testdef_url, self.context, tmpdir)
+        with open(testdef_file, 'r') as f:
+            logging.info('loading test definition')
+            return json.load(f)
+
     def _copy_runner(self, mntdir, target):
         runner = target.deployment_data['lava_test_runner']
         shell = target.deployment_data['lava_test_shell']
         shutil.copy(runner, '%s/bin/lava-test-runner' % mntdir)
         shutil.copy(shell, '%s/bin/lava-test-shell'% mntdir)
 
+    def _copy_test(self, mntdir, testdef):
+        tdir = '%s/tests/%s' % (mntdir, testdef['test_id'])
+        utils.ensure_directory(tdir)
+        with open('%s/testdef.json' % tdir, 'w') as f:
+            f.write(json.dumps(testdef))
+
     def _mk_runner_dirs(self, mntdir):
         utils.ensure_directory('%s/bin' % mntdir)
+        utils.ensure_directory('%s/tests' % mntdir)
 
-    def _configure_target(self, target):
+    def _configure_target(self, target, testdef):
         with target.file_system(target.config.root_part, 'lava') as d:
             self._mk_runner_dirs(d)
             self._copy_runner(d, target)
+            self._copy_test(d, testdef)
 
         with target.file_system(target.config.root_part, 'etc') as d:
             target.deployment_data['lava_test_configure_startup'](d)
