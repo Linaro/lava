210b4e3d5fa25f41b8b9ae85bb1b40cef0c579a1
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-10-12 14:40:24 +1300

    trivialities

--- lava_dispatcher/client/targetdevice.py
+++ lava_dispatcher/client/targetdevice.py
@@ -24,7 +24,6 @@ import shutil
 import time
 
 from lava_dispatcher.client.base import (
-    CommandRunner,
     CriticalError,
     LavaClient,
     )
@@ -37,10 +36,10 @@ from lava_dispatcher.utils import (
 
 
 class TargetBasedClient(LavaClient):
-    '''This is a wrapper around the lava_dispatcher.device.target class that
+    """This is a wrapper around the lava_dispatcher.device.target class that
     provides the additional functionality that's needed by lava-dispatcher
     actions that depend on a LavaClient
-    '''
+    """
 
     def __init__(self, context, config):
         super(TargetBasedClient, self).__init__(context, config)
@@ -68,7 +67,7 @@ class TargetBasedClient(LavaClient):
         self.proc = self.target_device.power_on()
 
     def _boot_linaro_android_image(self):
-        ''' booting android or ubuntu style images don't differ much'''
+        """Booting android or ubuntu style images don't differ much"""
 
         logging.info('ensuring ADB port is ready')
         while logging_system("sh -c 'netstat -an | grep 5555.*TIME_WAIT'") == 0:
