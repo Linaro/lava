b1681eda7435f26100129d5f4a106c98ba0c5690
Antonio Terceiro <antonio.terceiro@linaro.org>
2012-10-20 16:45:13 -0300

    create more realistic target instances for testing

--- lava_dispatcher/tests/test_device_version.py
+++ lava_dispatcher/tests/test_device_version.py
@@ -19,16 +19,26 @@
 
 from unittest import TestCase
 import re
-from lava_dispatcher.tests.helper import LavaDispatcherTestCase, create_device_config
+from lava_dispatcher.tests.helper import LavaDispatcherTestCase, create_device_config, create_config, __tmp_config_dir
 
 from lava_dispatcher.device.target import Target
 from lava_dispatcher.device.qemu import QEMUTarget
 from lava_dispatcher.device.fastmodel import FastModelTarget
+from lava_dispatcher.context import LavaContext
+from lava_dispatcher.config import get_config
 
-def _create_fastmodel():
+def _create_fastmodel_target():
     config = create_device_config('fastmodel01', { 'device_type': 'fastmodel', 'simulator_binary': '/path/to/fastmodel', 'license_server': 'foo.local' })
     target = FastModelTarget(None, config)
     return target
+
+def _create_qemu_target():
+    create_config('lava-dispatcher.conf', {'default_qemu_binary': 'qemu-system-arm'})
+    device_config = create_device_config('qemu01', { 'device_type': 'qemu' })
+    dispatcher_config = get_config(__tmp_config_dir)
+
+    context = LavaContext('qemu01', dispatcher_config, None, None)
+    return QEMUTarget(context, device_config)
 
 class TestDeviceVersion(LavaDispatcherTestCase):
 
@@ -37,7 +47,7 @@ class TestDeviceVersion(LavaDispatcherTestCase):
         assert(type(target.get_device_version()) is str)
 
     def test_qemu(self):
-        target = QEMUTarget(None, None)
+        target = _create_qemu_target()
         device_version = target.get_device_version()
         assert(re.search('^[0-9.]+', device_version))
 
@@ -48,12 +58,12 @@ class TestDeviceVersion(LavaDispatcherTestCase):
             "All Rights Reserved.",
             "Top component name: RTSM_VE_Cortex_A15x1_A7x1"
             ])
-        target = _create_fastmodel()
+        target = _create_fastmodel_target()
         version = target._parse_fastmodel_version(banner)
         self.assertEqual('7.1.36', version)
 
     def test_fastmodel_wrong_format(self):
-        client = _create_fastmodel()
+        client = _create_fastmodel_target()
         version = client._parse_fastmodel_version('random string')
         self.assertEqual('unknown', version)
 
