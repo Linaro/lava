d084a047d47057742f4b006398506d3eb5ae2280
Yongqin Liu <yongqin.liu@linaro.org>
2012-10-22 15:43:28 +0800

    add android_install_cts_medias action to avoid copying media files via adb

---
+++ lava_dispatcher/actions/android_install_cts_medias.py
@@ -0,0 +1,51 @@
+# Copyright (C) 2012 Linaro Limited
+#
+# Author: Paul Larson <paul.larson@linaro.org>
+#
+# This file is part of LAVA Dispatcher.
+#
+# LAVA Dispatcher is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# LAVA Dispatcher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses>.
+
+import logging
+from lava_dispatcher.actions import BaseAction
+
+
+class cmd_android_install_cts_medias(BaseAction):
+
+    parameters_schema = {
+        'type': 'object',
+        'properties': {
+            'media_url': {'type': 'string'},
+            'timeout': {'type': 'integer', 'optional': True},
+            },
+        'additionalProperties': False,
+        }
+
+    def run(self, media_url, timeout=2400):
+        if not media_url:
+            logging.error("The url for the cts media files is not specified")
+            return
+
+        with self.client.target_device._as_master() as session:
+            session.run('mkdir -p /mnt/lava/sdcard')
+            session.run(
+                'mount /dev/disk/by-label/sdcard /mnt/lava/sdcard')
+            self.client.target_device.target_extract(
+                session, driver_tarball, '/mnt/lava/sdcard', timeout=600)
+            session.run('mkdir -p /mnt/lava/sdcard/test')
+            session.run(
+                'mv /mnt/lava/sdcard/bbb_full /mnt/lava/sdcard/test/bbb_full')
+            session.run(
+                'mv /mnt/lava/sdcard/bbb_short /mnt/lava/sdcard/test/bbb_short')
+            session.run('umount /mnt/lava/sdcard')
