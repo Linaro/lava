d6bc26fcd7b42fbf3857bae01faa6da6dc01b248
Antonio Terceiro <antonio.terceiro@linaro.org>
2012-12-12 16:13:49 -0300

    sanitize terminfo hack

--- lava_dispatcher/device/nexus.py
+++ lava_dispatcher/device/nexus.py
@@ -18,6 +18,9 @@
 # along
 # with this program; if not, see <http://www.gnu.org/licenses>.
 
+# TODO document all the crazy stuff needed by LAVA to deal with the Nexus,
+# constraints, hacks, etc
+
 import subprocess
 import pexpect
 from time import sleep
@@ -66,7 +69,21 @@ class NexusTarget(Target):
         proc.sendline("") # required to put the adb shell in a reasonable state
         proc.sendline("export PS1='%s'" % self.deployment_data['TESTER_PS1'])
         self._runner = self._get_runner(proc)
+
+        self._system_etc_cleanup()
+
         return proc
+
+    def _system_etc_cleanup(self):
+        """ This makes it a lot faster to pull/push from/to /system/etc. There
+        is A LOT of useless files under /system/etc/terminfo/.
+        """
+        with self.make_filesystem_readwrite('/system'):
+            runner = self._runner
+            runner.run('mv /system/etc/terminfo/v/vt100 /system/etc')
+            runner.run('rm -r /system/etc/terminfo/*')
+            runner.run('mkdir /system/etc/terminfo/v')
+            runner.run('mv /system/etc/vt100 /system/etc/terminfo/v/')
 
     def reboot(self):
         # tell android to reboot. A failure probably means that the device is not
@@ -79,22 +96,20 @@ class NexusTarget(Target):
     # TODO implement file_system
     @contextlib.contextmanager
     def file_system(self, partition, directory):
+        # FIXME turn the device on if it's not on already
+
         mount_point = self.get_partition_mount_point(partition)
 
-        self.maybe_remount(mount_point, 'rw')
+        with self.make_filesystem_readwrite(mount_point):
+            host_dir = '%s/mnt/%s' % (self.scratch_dir, directory)
+            target_dir = '%s/%s' % (mount_point, directory)
 
-        host_dir = '%s/mnt/%s' % (self.scratch_dir, directory)
-        target_dir = '%s/%s' % (mount_point, directory)
+            subprocess.check_call(['mkdir', '-p', host_dir])
+            self.adb('pull %s %s' % (target_dir, host_dir), ignore_failure = True)
 
-        subprocess.check_call(['mkdir', '-p', host_dir])
-        self.adb('pull %s %s' % (target_dir, host_dir), ignore_failure = True)
+            yield host_dir
 
-        yield host_dir
-
-        self.adb('push %s %s' % (host_dir, target_dir))
-
-        self.maybe_remount(mount_point, 'ro')
-
+            self.adb('push %s %s' % (host_dir, target_dir))
 
     def get_partition_mount_point(self, partition):
         lookup = {
@@ -103,25 +118,12 @@ class NexusTarget(Target):
         }
         return lookup[partition]
 
-    def maybe_remount(self, mount_point, option):
+    def make_filesystem_readwrite(self, mount_point):
         if mount_point  == '/system':
-            runner = self._runner
-
-            logging.debug("HACK to speed up pull/push from /system/etc")
-
-            # FIXME (?)
-            if option == 'rw':
-                runner.run('mv /system/etc/terminfo /system/etc_terminfo')
-                runner.run('mv /system/etc/security /system/etc_security')
-                runner.run('mv /system/etc/permissions /system/etc_permissions')
-
-            runner.run("mount -o remount,%s %s" % (option, mount_point))
-
-            # FIXME (?)
-            if option == 'ro':
-                runner.run('mv /system/etc_terminfo /system/etc/terminfo')
-                runner.run('mv /system/etc_security /system/etc/security')
-                runner.run('mv /system/etc_permissions /system/etc/permissions')
+            self._runner.run("mount -o remount,rw %s" % mount_point)
+        yield
+        if mount_point  == '/system':
+            self._runner.run("mount -o remount,ro %s" % mount_point)
 
     # TODO implement extract_tarball
 
