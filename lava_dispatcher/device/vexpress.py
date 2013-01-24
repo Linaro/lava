ffdb76de3f688c2a3ea968bf67678357a65e0991
Antonio Terceiro <antonio.terceiro@linaro.org>
2013-01-24 16:30:57 -0300

    Skeleton of the vexpress device type

---
+++ lava_dispatcher/device/vexpress.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2013 Linaro Limited
+#
+# Author: Antonio Terceiro <antonio.terceiro@linaro.org>
+#
+# This file is part of LAVA Dispatcher.
+#
+# LAVA Dispatcher is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# LAVA Dispatcher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along
+# with this program; if not, see <http://www.gnu.org/licenses>.
+
+from lava_dispatcher.device.target import Target
+
+class VexpressTarget(Target):
+  pass
+
+target_class = VexpressTarget
