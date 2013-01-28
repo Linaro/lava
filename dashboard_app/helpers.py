36b011df5059240b0bca99e0d1937f1ba11d53b9
Senthil Kumaran <senthil.kumaran@linaro.org>
2013-01-28 14:13:17 +0530

    Name the new testdef data as metadata instead of attributes.

--- dashboard_app/helpers.py
+++ dashboard_app/helpers.py
@@ -732,27 +732,27 @@ class BundleFormatImporter_1_5(BundleFormatImporter_1_4):
                 "attachment-{0}.txt".format(s_attachment.pk),
                 ContentFile(content))
 
-    def _import_testdef(self, c_test_id, c_test_attributes):
+    def _import_testdef(self, c_test_id, c_testdef_metadata):
         """
         Import dashboard_app.models.TestDefinition into the database
-        based on a client-side description of a TestRun attributes.
+        based on a client-side description of a TestRun metadata.
         """
         from dashboard_app.models import TestDefinition
 
-        if c_test_attributes.get('repo_rev'):
-            version_info = c_test_attributes["repo_rev"]
+        if c_testdef_metadata.get('repo_rev'):a
+            version_info = c_testdef_metadata["repo_rev"]
         else:
-            version_info = c_test_attributes["version"]
+            version_info = c_testdef_metadata["version"]
         s_testdef, testdef_created = TestDefinition.objects.get_or_create(
             testdef_name = c_test_id,
             version = version_info,
-            description = c_test_attributes["description"],
-            testdef_format = c_test_attributes["format"],
-            testdef_location = c_test_attributes["location"],
-            url = c_test_attributes["url"],
-            testdef_environment = c_test_attributes["environment"],
-            target_os = c_test_attributes["os"],
-            target_dev_types = c_test_attributes["devices"],
+            description = c_testdef_metadata["description"],
+            testdef_format = c_testdef_metadata["format"],
+            testdef_location = c_testdef_metadata["location"],
+            url = c_testdef_metadata["url"],
+            testdef_environment = c_testdef_metadata["environment"],
+            target_os = c_testdef_metadata["os"],
+            target_dev_types = c_testdef_metadata["devices"],
             ) # required by schema
         if testdef_created:
             s_testdef.save()
@@ -766,8 +766,9 @@ class BundleFormatImporter_1_5(BundleFormatImporter_1_4):
                     relative_index=index, test_run=s_test_run)
                 self._import_test_result_attachments(c_test_result, s_test_result)
 
-        if c_test_run.get("attributes"):
-            self._import_testdef(c_test_run["test_id"], c_test_run["attributes"])
+        if c_test_run.get("testdef_metadata"):
+            self._import_testdef(c_test_run["test_id"],
+                                 c_test_run["testdef_metadata"])
 
 
 class BundleDeserializer(object):
