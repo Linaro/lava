dabec09549b646f6b216266d97fa48b44442cc65
Antonio Terceiro <antonio.terceiro@linaro.org>
2013-02-07 17:31:49 -0300

    install UEFI from Android image

--- lava_dispatcher/device/vexpress.py
+++ lava_dispatcher/device/vexpress.py
@@ -22,8 +22,14 @@ import pexpect
 
 from lava_dispatcher.device.master import MasterImageTarget
 from lava_dispatcher.errors import CriticalError
+from lava_dispatcher.utils import logging_system
+import os
 
 class VexpressTarget(MasterImageTarget):
+
+    ##################################################################
+    # methods inherited from MasterImageTarget and overriden here
+    ##################################################################
 
     def _soft_reboot(self):
         """
@@ -41,21 +47,42 @@ class VexpressTarget(MasterImageTarget):
         self._hard_reboot()
 
     def _enter_bootloader(self):
-        self._mcc_setup()
+        self._mcc_setup(self._install_test_uefi)
         super(VexpressTarget, self)._enter_bootloader()
 
     def _wait_for_master_boot(self):
-        self._mcc_setup()
+        self._mcc_setup(self._restore_uefi_backup)
         super(VexpressTarget, self)._wait_for_master_boot()
 
-    def _mcc_setup(self):
+    def _deploy_android_tarballs(self, master, boot, system, data):
+        super(VexpressTarget, self)._deploy_android_tarballs(master, boot,
+                                                             system, data)
+        tmpdir = self.scratch_dir
+        uefi_on_image = 'uefi_v2p-ca15-tc2.bin' # FIXME read dev config
+        logging_system('tar xaf %s -C %s %s' % (boot, tmpdir, uefi_on_image))
+
+        uefi_on_image = os.path.join(tmpdir, uefi_on_image)
+        test_uefi = os.path.join(tmpdir, 'uefi.bin')
+        logging_system('mv %s %s' % (uefi_on_image, test_uefi))
+
+        self.test_uefi = test_uefi
+
+    def _deploy_tarballs(self, boot_tgz, root_tgz):
+        super(VexpressTarget, self)._deploy_tarballs(boot_tgz, root_tgz)
+        # TODO implement for non-Android as well
+
+    ##################################################################
+    # implementation-specific methods
+    ##################################################################
+
+    def _mcc_setup(self, command):
         self._enter_mcc()
-        self._install_uefi_image()
+        self._prepare_uefi(command)
         self._leave_mcc()
 
     def _enter_mcc(self):
         match_id = self.proc.expect([
-            'Press Enter to stop auto boot...',
+            'Press Enter to stop auto boot...', # FIXME read dev config
             pexpect.EOF, pexpect.TIMEOUT])
         if match_id != 0:
             msg = 'Unable to intercept MCC boot prompt'
@@ -64,8 +91,39 @@ class VexpressTarget(MasterImageTarget):
         self.proc.sendline("")
         self.proc.expect(['Cmd>'])
 
-    def _install_uefi_image(self):
-        pass
+    def _prepare_uefi(self, command):
+        self.proc.sendline("USB_ON")
+        self.proc.expect(['Cmd>'])
+
+        usb_device = '/dev/disk/by-label/VEMSD' # FIXME read dev config
+        mount_point = '/mnt/vexpress' # FIXME mount inside scratch_dir
+        logging_system('mount %s %s' % (usb_device, mount_point))
+
+        command(mount_point)
+
+        logging_system('umount %s' % mount_point)
+
+    def _restore_uefi_backup(self, mount_point):
+        uefi_path = 'SOFTWARE/TC2/uefi.bin' # FIXME read dev config
+        uefi = os.path.join(mount_point, uefi_path)
+        uefi_backup_path = 'SOFTWARE/TC2/backup-uefi.bin' # FIXME read dev config
+        uefi_backup = os.path.join(mount_point, uefi_backup_path))
+
+        if os.path.exists(uefi_backup):
+            # restore the uefi backup
+            logging_system('cp %s %s' % (uefi_backup, uefi))
+        else:
+            # no existing backup yet means that this is the first time ever;
+            # the uefi in there is the good one, and we backup it up.
+            logging_system('cp %s %s' % (uefi, uefi_backup))
+
+    def _install_test_uefi(self, mount_point):
+        # FIXME duplication with the above method
+        uefi_path = 'SOFTWARE/TC2/uefi.bin' # FIXME read dev config
+        uefi = os.path.join(mount_point, uefi_path)
+        # FIXME what if self.test_uefi is not set, or points to an unexisting
+        # file?
+        logging_system('cp %s %s' % (selt.test_uefi, uefi))
 
     def _leave_mcc(self):
         self.proc.sendline("reboot")
