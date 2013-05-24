1a19675a200e889d948cfe8a4806b58607b77f5d
Neil Williams <codehelp@debian.org>
2013-05-24 15:39:49 +0100

    Add initial Debian packaging

---
+++ debian/lava-dispatcher.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lava-dispatcher initscript
+# sourced by /etc/init.d/lava-dispatcher
+# installed at /etc/default/lava-dispatcher by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
