b0592657b07c155e3696f9ef928ef50dc991e4b6
Neil Williams <codehelp@debian.org>
2013-05-24 11:46:33 +0100

    setup the tarball creation support.

--- setup.py
+++ setup.py
@@ -2,17 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='lava-server',
-    version="0.0",
-    author="Michael Hudson-Doyle",
-    author_email="michael.hudson@linaro.org",
+    version="0.0.0",
     packages=find_packages(),
     license="AGPL",
     description="LAVA Server",
+    author='Linaro Validation Team',
+    author_email='linaro-dev@lists.linaro.org',
     install_requires=[
+        "json-schema-validator >= 2.3",
+        "linaro-dashboard-bundle",
+        'versiontools >= 1.8',
     ],
     setup_requires=[
+        'versiontools >= 1.8',
     ],
-    tests_require=[
+    scripts = [
     ],
     zip_safe=False,
     include_package_data=True)
