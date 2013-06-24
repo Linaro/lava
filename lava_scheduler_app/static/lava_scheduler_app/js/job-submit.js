35ceea5f31a48519ece83ac86e05200fe7586425
Stevan Radakovic <stevan.radakovic@linaro.org>
2013-06-24 18:59:04 +0200

    Increase textarea height.

--- lava_scheduler_app/static/lava_scheduler_app/js/job-submit.js
+++ lava_scheduler_app/static/lava_scheduler_app/js/job-submit.js
@@ -1,8 +1,4 @@
 $(window).ready(
     function () {
         $("#json-input").linedtextarea();
-
-        // Need to change this because our text area field is resizable.
-        $("#lava-footer").css("position", "relative");
-        $("#lava-footer-sanitizer").css("padding-bottom", "0px");
     });
