a9ba28e9fdcf773b829524debc3a6d8ef048b586
Stevan Radakovic <stevan.radakovic@linaro.org>
2013-06-24 18:54:45 +0200

    Fix footer problems.

--- lava_scheduler_app/static/lava_scheduler_app/js/job-submit.js
+++ lava_scheduler_app/static/lava_scheduler_app/js/job-submit.js
@@ -1,4 +1,8 @@
 $(window).ready(
     function () {
         $("#json-input").linedtextarea();
+
+        // Need to change this because our text area field is resizable.
+        $("#lava-footer").css("position", "relative");
+        $("#lava-footer-sanitazer").css("padding-bottom", "0px");
     });
