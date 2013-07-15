dc3f8452769a9b37e93eb7b5a85c58312c0f72cb
Neil Williams <codehelp@debian.org>
2013-07-15 09:33:03 +0100

    whitespace PEP8 change.

--- lava_dispatcher/signals/__init__.py
+++ lava_dispatcher/signals/__init__.py
@@ -192,7 +192,7 @@ class SignalDirector(object):
             logging.error("No connection available for on_SYNC")
             return
         logging.debug("Handling signal <LAVA_SYNC %s>" % message_id)
-        msg={"request": "lava_sync", "messageID": message_id, "message": None}
+        msg = {"request": "lava_sync", "messageID": message_id, "message": None}
         reply = self.context.transport(json.dumps(msg))
         logging.debug("Node transport replied with %s" % reply)
         ret = self.connection.sendline("<LAVA_SYNC_COMPLETE> %s" % json.dumps(reply))
@@ -203,7 +203,7 @@ class SignalDirector(object):
             logging.error("No connection available for on_WAIT")
             return
         logging.debug("Handling signal <LAVA_WAIT %s>" % message_id)
-        msg={"request": "lava_wait", "messageID": message_id, "message": None}
+        msg = {"request": "lava_wait", "messageID": message_id, "message": None}
         reply = self.context.transport(json.dumps(msg))
         logging.debug("Node transport replied with %s" % reply)
         message_str = ""
@@ -216,7 +216,7 @@ class SignalDirector(object):
             logging.error("No connection available for on_WAIT_ALL")
             return
         logging.debug("Handling signal <LAVA_WAIT_ALL %s>" % message_id)
-        msg={"request": "lava_wait_all", "messageID": message_id, "role": role}
+        msg = {"request": "lava_wait_all", "messageID": message_id, "role": role}
         reply = self.context.transport(json.dumps(msg))
         logging.debug("Node transport replied with %s" % reply)
         message_str = ""
@@ -238,4 +238,3 @@ class SignalDirector(object):
                 except:
                     logging.exception(
                         "postprocessing test run with uuid %s failed", uuid)
-
