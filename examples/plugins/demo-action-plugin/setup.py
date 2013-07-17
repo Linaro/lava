701939d7fb17d77ee27c616b3fe65dae9152ab1d
Neil Williams <neil.williams@linaro.org>
2013-07-17 10:16:24 +0100

    Add PEP8 fixes for doc/ and setup.py

--- examples/plugins/demo-action-plugin/setup.py
+++ examples/plugins/demo-action-plugin/setup.py
@@ -2,18 +2,18 @@
 
 from setuptools import setup
 
-setup (
+setup(
     name='demo-action-plugin',
     version='0.0.1',
     author='Paul Larson',
     author_email='paul.larson@linaro.org',
     url='',
     description='LAVA Dispatcher plugin test',
-    packages = ['demo_action_plugin'],
+    packages=['demo_action_plugin'],
     entry_points="""
     [lava_dispatcher.actions]
     foo = demo_action_plugin.foo:cmd_foo
     """,
     zip_safe=False,
     include_package_data=True
-    )
+)
