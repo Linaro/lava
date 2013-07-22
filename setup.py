df7f732c21d3ca4a44b70be892013a8ec78b6fd3
Neil Williams <codehelp@debian.org>
2013-07-22 16:14:20 +0100

    First standalone script.

---
+++ setup.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2013 Linaro Limited
+#
+# Author: Neil Williams <neil.williams@linaro.org>
+#
+# This file is part of LAVA Group.
+#
+# LAVA Group is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# LAVA Group is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along
+# with this program; if not, see <http://www.gnu.org/licenses>.
+
+from setuptools import setup
+
+setup(
+    name='lava-group',
+    version=":versiontools:lava_group:",
+    author="Neil Williams",
+    author_email="neil.williams@linaro.org",
+    license="GPL2+",
+    description="LAVA GroupDispatcher daemon",
+    install_requires=[
+        "daemon",
+    ],
+    setup_requires=[
+        "versiontools >= 1.8",
+    ],
+    scripts=[
+        'lava_group/group'
+    ],
+    zip_safe=False,
+    include_package_data=True)
