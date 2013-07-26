d4d08b13d1334b0d7b77511b00716a18d879719f
Neil Williams <codehelp@debian.org>
2013-07-26 16:42:43 +0100

    bump version for major change in socket comms behaviour

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 setup(
     name='lava-coordinator',
-    version="0.0.3",
+    version="0.1.0",
     author="Neil Williams",
     author_email="neil.williams@linaro.org",
     license="GPL2+",
