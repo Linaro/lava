c7f88eb651a659210096f637d4f1b91972ea761d
Neil Williams <codehelp@debian.org>
2013-07-30 12:32:34 +0100

    update for new package.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 setup(
     name='lava-coordinator',
-    version="0.1.0",
+    version="0.1.1",
     author="Neil Williams",
     author_email="neil.williams@linaro.org",
     license="GPL2+",
