132bfb2af105e6976401964322180894a9d69893
Stevan Radakovic <stevan.radakovic@linaro.org>
2013-08-06 16:25:08 +0200

    Add missing admin.py file.

---
+++ google_analytics/admin.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.contrib import admin
+
+from google_analytics.models import Analytic
+
+class AnalyticAdmin(admin.ModelAdmin):
+    list_display = ('site', 'analytics_code',)
+
+if getattr(settings, 'GOOGLE_ANALYTICS_MODEL', False):
+    admin.site.register(Analytic, AnalyticAdmin)
+
