9c78c665c4365e37636680cce0959f997ecd841b
Neil Williams <codehelp@debian.org>
2013-08-08 11:35:52 +0100

    bump for completion of test suite

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lava-coordinator',
-    version="0.1.3",
+    version="0.1.4",
     author="Neil Williams",
     author_email="neil.williams@linaro.org",
     license="GPL2+",
