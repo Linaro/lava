a3b178a532c7309a45e4c836621cf999e113d052
Tyler Baker <tyler.baker@linaro.org>
2013-08-08 17:56:22 -0700

    Fix for boot_options

--- lava_dispatcher/device/master.py
+++ lava_dispatcher/device/master.py
@@ -503,6 +503,7 @@ class MasterImageTarget(Target):
                 logging.info('Overriding boot_cmds from boot_options')
                 boot_cmds_override = True
                 boot_cmds = options['boot_cmds'].value
+                boot_cmds = self.config.cp.get('__main__', boot_cmds)
 
         # Check if we have already got some values from image's boot file.
         if self.deployment_data.get('boot_cmds_dynamic') \
@@ -511,6 +512,7 @@ class MasterImageTarget(Target):
             boot_cmds = self.deployment_data['boot_cmds_dynamic']
         elif not boot_cmds_override:            
             logging.info('Loading boot_cmds from device configuration')
+            boot_cmds = self.config.cp.get('__main__', boot_cmds)
             boot_cmds = string_to_list(boot_cmds.encode('ascii'))
 
         logging.info('boot_cmds: %s', boot_cmds)
