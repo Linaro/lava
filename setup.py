210e4b7cf6912a3a81600532247c6059171836ba
Neil Williams <codehelp@debian.org>
2013-08-15 11:19:58 +0100

    bump version
    Change-Id: Ibe9f5e6df59d15faae8e30328c3c1c7fa50cb824

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lava-coordinator',
-    version="0.1.4",
+    version="0.1.5",
     author="Neil Williams",
     author_email="neil.williams@linaro.org",
     license="GPL2+",
