4e24f142bbce90bb73166b4c775d31ab8d32c325
Tyler Baker <tyler.baker@linaro.org>
2013-08-22 10:24:28 -0700

    Implement deploy_linaro_kernel in qemu class

--- lava_dispatcher/device/qemu.py
+++ lava_dispatcher/device/qemu.py
@@ -38,22 +38,52 @@ from lava_dispatcher.utils import (
     extract_targz,
     finalize_process,
 )
+from lava_dispatcher.errors import (
+    CriticalError
+)
 
 
 class QEMUTarget(Target):
 
     def __init__(self, context, config):
         super(QEMUTarget, self).__init__(context, config)
+        self._qemu_options = None
         self._sd_image = None
+
+    def deploy_linaro_kernel(self, kernel, ramdisk, dtb, rootfs, bootloader,
+                             firmware, rootfstype, bootloadertype):
+        if kernel is not None:
+            kernel = download_image(kernel, self.context)
+            self._qemu_options = " -machine accel=kvm:tcg"
+            self._qemu_options += " -kernel %s" % kernel
+            if ramdisk is not None:
+                ramdisk = download_image(ramdisk, self.context)
+                self._qemu_options += " -initrd %s" % ramdisk
+            if dtb is not None:
+                dtb = download_image(dtb, self.context)
+                self._qemu_options += " -dtb %s" % ramdisk
+            if rootfs is not None:
+                rootfs = download_image(rootfs, self.context)
+                self._qemu_options += "-hda %s" % rootfs
+            if firmware is not None:
+                firmware = download_image(firmware, self.context)
+                self._qemu_options += "-bios %s" % firmware
+            self._qemu_options += self.config.kvm_networking_options
+        else:
+            raise CriticalError("No kernel images to boot")
 
     def deploy_linaro(self, hwpack=None, rootfs=None, bootloader='u_boot'):
         odir = self.scratch_dir
         self._sd_image = generate_image(self, hwpack, rootfs, odir, bootloader)
         self._customize_linux(self._sd_image)
+        self._qemu_options = self.config.qemu_options.format(
+            DISK_IMAGE=self._sd_image)
 
     def deploy_linaro_prebuilt(self, image):
         self._sd_image = download_image(image, self.context)
         self._customize_linux(self._sd_image)
+        self._qemu_options = self.config.qemu_options.format(
+            DISK_IMAGE=self._sd_image)
 
     @contextlib.contextmanager
     def file_system(self, partition, directory):
@@ -70,8 +100,6 @@ class QEMUTarget(Target):
             extract_targz(tb, '%s/%s' % (mntdir, directory))
 
     def power_on(self):
-        qemu_options = self.config.qemu_options.format(
-            DISK_IMAGE=self._sd_image)
         qemu_cmd = '%s %s' % (self.config.qemu_binary, qemu_options)
         logging.info('launching qemu with command %r' % qemu_cmd)
         proc = self.context.spawn(qemu_cmd, timeout=1200)
