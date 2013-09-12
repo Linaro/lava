421dc857fcb84aeb701e54adb1440e6139136dab
Stevan Radakovic <stevan.radakovic@linaro.org>
2013-09-12 14:54:24 +0200

    Remove debug logs.

--- dashboard_app/views/image_reports/views.py
+++ dashboard_app/views/image_reports/views.py
@@ -256,19 +256,11 @@ def image_chart_filter_form(request, bread_crumb_trail, chart_instance=None,
                 tests = form.cleaned_data['image_chart_tests']
                 aliases = request.POST.getlist('aliases')
 
-                for test in tests:
-                    print test.id
-                print "-----------------"
-
                 for index, test in enumerate(tests):
                     chart_test = ImageChartTest()
                     chart_test.image_chart_filter = chart_filter
                     chart_test.test = test
                     chart_test.name = aliases[index]
-                    print index
-                    print test.id
-                    print test.test_id
-                    print aliases[index]
                     chart_test.save()
 
                 return HttpResponseRedirect(
