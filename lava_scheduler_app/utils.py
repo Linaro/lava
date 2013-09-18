a38e6cfa0e6256f4a9e379c63c062aac8fe7f60a
Neil Williams <codehelp@debian.org>
2013-09-18 15:56:47 +0100

    Apply change by Fu: Fix the bug of LMP init value in Multi-Node 2 - merge/186348
    Change-Id: I690a3d62d773525bb5d220122e03e7c8fab3c59e

--- lava_scheduler_app/utils.py
+++ lava_scheduler_app/utils.py
@@ -107,7 +107,8 @@ def split_multi_job(json_jobdata, target_group):
             node_json[role][c]["group_size"] = group_count
             node_json[role][c]["target_group"] = target_group
             node_json[role][c]["actions"] = node_actions[role]
-            node_json[role][c]["lmp_module"] = node_lmp[role]
+            if "lmp_module" in json_jobdata:
+                node_json[role][c]["lmp_module"] = node_lmp[role]
 
             node_json[role][c]["role"] = role
             # multinode node stage 2
