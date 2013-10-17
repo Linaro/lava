1f9fbf440cb1c822dfa65333d25676d0ca83c555
Neil Williams <codehelp@debian.org>
2013-10-17 09:33:25 +0100

    Update MANIFEST and setup.py for LMP
    Part of LAVA-759, updating the packaging of lava-dispatcher.
    Includes adding a Makefile in doc until LAVA-774 is fixed.
    
    Change-Id: Id2e520a613d86c3ab53e706940338a2d3f4059b6
    Reviewed-on: https://staging.review.linaro.org/324
    Reviewed-by: Antonio Terceiro <antonio.terceiro@linaro.org>

--- setup.py
+++ setup.py
@@ -31,17 +31,37 @@ setup(
             'default-config/lava-dispatcher/device-defaults.conf',
             'default-config/lava-dispatcher/device-types/*.conf',
             'default-config/lava-dispatcher/devices/*.conf',
+            'device/sdmux.sh',
         ],
     },
     data_files=[
         ('lava_test_shell', [
+            'lava_test_shell/lava-installed-packages',
+            'lava_test_shell/lava-os-build',
             'lava_test_shell/lava-test-case',
             'lava_test_shell/lava-test-case-attach',
             'lava_test_shell/lava-test-run-attach',
             'lava_test_shell/lava-test-runner-android',
             'lava_test_shell/lava-test-runner-ubuntu',
             'lava_test_shell/lava-test-shell',
-            ])
+            'lava_test_shell/README']),
+        ('lava_test_shell/multi_node', [
+            'lava_test_shell/multi_node/lava-group',
+            'lava_test_shell/multi_node/lava-multi-node.lib',
+            'lava_test_shell/multi_node/lava-role',
+            'lava_test_shell/multi_node/lava-self',
+            'lava_test_shell/multi_node/lava-send',
+            'lava_test_shell/multi_node/lava-sync',
+            'lava_test_shell/multi_node/lava-wait',
+            'lava_test_shell/multi_node/lava-wait-all']),
+        ('lava_test_shell/lmp', [
+            'lava_test_shell/lmp/lava-lmp-audio-jack',
+            'lava_test_shell/lmp/lava-lmp-eth',
+            'lava_test_shell/lmp/lava-lmp-hdmi',
+            'lava_test_shell/lmp/lava-lmp.lib',
+            'lava_test_shell/lmp/lava-lmp-lsgpio',
+            'lava_test_shell/lmp/lava-lmp-sata',
+            'lava_test_shell/lmp/lava-lmp-usb'])
     ],
     install_requires=[
         'json-schema-validator >= 2.3',
