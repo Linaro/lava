Linaro/lava

commit 332f4bf1c4c9c297453766fcb3db585b17853649

Add command 'lava_command_run'
- Move _read_content from lava_dispatcher/lava_test_shell.py to
  lava_dispatcher/utils.py and rename it to read_content for public uses
- Make temporary dir under scratch_dir for every lava_command action, so
  the output log will not be overwrited

Change-Id: I145233eced45b67b06e23c984e17b6e18622c262

lava_dispatcher/lava_test_shell.py
@@ -35,7 +35,7 @@ import os
 import re
 
 from lava_dispatcher.test_data import create_attachment
-
+from lava_dispatcher.utils import read_content
 
 def _get_cpus(cpuinfo):
     devices = []
@@ -125,13 +125,13 @@ def _attachments_from_dir(from_dir):
     for filename, filepath in _directory_names_and_paths(from_dir, ignore_missing=True):
         if filename.endswith('.mimetype'):
             continue
-        mime_type = _read_content(filepath + '.mimetype', ignore_missing=True).strip()
+        mime_type = read_content(filepath + '.mimetype', ignore_missing=True).strip()
         if not mime_type:
             mime_type = mimetypes.guess_type(filepath)[0]
             if mime_type is None:
                 mime_type = 'application/octet-stream'
         attachments.append(
-            create_attachment(filename, _read_content(filepath), mime_type))
+            create_attachment(filename, read_content(filepath), mime_type))
     return attachments
 
 
@@ -139,7 +139,7 @@ def _attributes_from_dir(from_dir):
     attributes = {}
     for filename, filepath in _directory_names_and_paths(from_dir, ignore_missing=True):
         if os.path.isfile(filepath):
-            attributes[filename] = _read_content(filepath)
+            attributes[filename] = read_content(filepath)
     return attributes
 
 
@@ -177,7 +177,7 @@ def _result_from_dir(res_dir, test_case_id=None):
     for fname in 'result', 'measurement', 'units', 'message', 'timestamp', 'duration':
         fpath = os.path.join(res_dir, fname)
         if os.path.isfile(fpath):
-            result[fname] = _read_content(fpath).strip()
+            result[fname] = read_content(fpath).strip()
 
     if 'measurement' in result:
         try:
@@ -280,7 +280,7 @@ def _get_test_results(test_run_dir, testdef, stdout):
 def _get_run_attachments(test_run_dir, testdef, stdout):
     attachments = [create_attachment('stdout.log', stdout),
                    create_attachment('testdef.yaml', testdef)]
-    return_code = _read_content(os.path.join(test_run_dir, 'return_code'), ignore_missing=True)
+    return_code = read_content(os.path.join(test_run_dir, 'return_code'), ignore_missing=True)
     if return_code:
         attachments.append(create_attachment('return_code', return_code))
 
@@ -302,7 +302,7 @@ def _get_run_testdef_metadata(test_run_dir):
         'environment': None
     }
 
-    metadata = _read_content(os.path.join(test_run_dir, 'testdef_metadata'))
+    metadata = read_content(os.path.join(test_run_dir, 'testdef_metadata'))
     if metadata is not '':
         testdef_metadata = yaml.safe_load(metadata)
 
@@ -312,9 +312,9 @@ def _get_run_testdef_metadata(test_run_dir):
 def _get_test_run(test_run_dir, hwcontext, build, pkginfo, testdefs_by_uuid):
     now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    testdef = _read_content(os.path.join(test_run_dir, 'testdef.yaml'))
-    stdout = _read_content(os.path.join(test_run_dir, 'stdout.log'))
-    uuid = _read_content(os.path.join(test_run_dir, 'analyzer_assigned_uuid'))
+    testdef = read_content(os.path.join(test_run_dir, 'testdef.yaml'))
+    stdout = read_content(os.path.join(test_run_dir, 'stdout.log'))
+    uuid = read_content(os.path.join(test_run_dir, 'analyzer_assigned_uuid'))
     attachments = _get_run_attachments(test_run_dir, testdef, stdout)
     attributes = _attributes_from_dir(os.path.join(test_run_dir, 'attributes'))
 
@@ -340,14 +340,6 @@ def _get_test_run(test_run_dir, hwcontext, build, pkginfo, testdefs_by_uuid):
         'testdef_metadata': _get_run_testdef_metadata(test_run_dir)
     }
 
-
-def _read_content(filepath, ignore_missing=False):
-    if not os.path.exists(filepath) and ignore_missing:
-        return ''
-    with open(filepath, 'r') as f:
-        return f.read()
-
-
 def _directory_names_and_paths(dirpath, ignore_missing=False):
     if not os.path.exists(dirpath) and ignore_missing:
         return []
@@ -361,12 +353,12 @@ def get_bundle(results_dir, testdefs_by_uuid):
     the LAVA dashboard
     """
     testruns = []
-    cpuinfo = _read_content(os.path.join(results_dir, 'hwcontext/cpuinfo.txt'), ignore_missing=True)
-    meminfo = _read_content(os.path.join(results_dir, 'hwcontext/meminfo.txt'), ignore_missing=True)
+    cpuinfo = read_content(os.path.join(results_dir, 'hwcontext/cpuinfo.txt'), ignore_missing=True)
+    meminfo = read_content(os.path.join(results_dir, 'hwcontext/meminfo.txt'), ignore_missing=True)
     hwctx = _get_hw_context(cpuinfo, meminfo)
 
-    build = _read_content(os.path.join(results_dir, 'swcontext/build.txt'), ignore_missing=True)
-    pkginfo = _read_content(os.path.join(results_dir, 'swcontext/pkgs.txt'), ignore_missing=True)
+    build = read_content(os.path.join(results_dir, 'swcontext/build.txt'))
+    pkginfo = read_content(os.path.join(results_dir, 'swcontext/pkgs.txt'), ignore_missing=True)
 
     for test_run_name, test_run_path in _directory_names_and_paths(results_dir):
         if test_run_name in ('hwcontext', 'swcontext'):

lava_dispatcher/actions/lava_command.py
@@ -1,0 +1,139 @@
+# Copyright (C) 2013 Linaro Limited
+#
+# Author: Tyler Baker <tyler.baker@linaro.org>
+#         Arthur She <arthur.she@linaro.org>
+#
+# This file is part of LAVA Dispatcher.
+#
+# LAVA Dispatcher is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# LAVA Dispatcher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses>.
+
+import logging
+import tempfile
+import re
+import os
+
+from uuid import uuid4
+from lava_dispatcher.actions import BaseAction
+from lava_dispatcher.errors import OperationFailed
+from linaro_dashboard_bundle.io import DocumentIO
+from lava_dispatcher.test_data import create_attachment
+from lava_dispatcher.utils import read_content
+from datetime import datetime
+
+class cmd_lava_command_run(BaseAction):
+
+    parameters_schema = {
+        'type': 'object',
+        'properties': {
+            'commands': {'type': 'array', 'items': {'type': 'string'},
+                    'optional': False},
+            'parser': {'type': 'string', 'optional': True},
+            'fixupdict': {'type': 'array', 'items': {'type': 'string'},
+                     'optional': True},
+            'timeout': {'type': 'integer', 'optional': True},
+            },
+        'additionalProperties': False,
+        }
+    _logfile = ""
+    _parser = None
+    _fixupdict = {}
+
+    def run(self, commands, parser=None, fixupdict=None, timeout=-1):
+        target = self.client.target_device
+        log_dir = tempfile.mkdtemp(dir=target.scratch_dir)
+        self._logfile = os.path.join(log_dir, 'stdout.log')
+        if parser is not None:
+            self._parser = parser
+        if fixupdict is not None:
+            self._fixupdict = fixupdict
+        logging.info("lava_command logfile: %s" % self._logfile)
+        with self.client.tester_session() as session:
+            for command in commands:
+                logging.info("Executing lava_command_run: %s" % command)
+                try:
+                    rc = session.run(command, timeout=timeout, log_in_host=self._logfile)
+                except:
+                    logging.exception("session.run failed")
+                    self.client.proc.sendcontrol('c')
+                    logging.exception("Killed the command")
+                if rc is None:
+                    raise OperationFailed("Getting return value failed")
+                else:
+                    logging.info("Command \"%s\" return Code: %d", command, rc)
+
+        self._write_results_bundle()
+
+    def _read_fixupdict(self):
+        fdict = {}
+        if self._fixupdict is not None:
+            for e in self._fixupdict:
+                k, v = e.split(':')
+                fdict.update({k.strip():v.strip()})
+
+        return fdict
+
+    def _get_test_results(self):
+        fixupdict = {}
+        results_from_log_file = []
+        defpat = "(?P<test_case_id>.*-*)\\s+:\\s+(?P<result>(PASS|pass|FAIL|fail|SKIP|skip|UNKNOWN|unknown))"
+        if self._parser is not None:
+            pattern = re.compile(self._parser)
+            fixupdict = self._read_fixupdict()
+        else:
+            pattern = re.compile(defpat)
+            logging.warning("""Using a default pattern to parse the test result. This may lead to empty test result in certain cases.""")
+
+        logfile = read_content(self._logfile)
+        for lineno, line in enumerate(logfile.split('\n'), 1):
+            match = pattern.match(line.strip())
+            if match:
+                res = match.groupdict()
+                if 'result' in res:
+                   if res['result'] in fixupdict:
+                        res['result'] = fixupdict[res['result']]
+                   if res['result'] not in ('pass', 'fail', 'skip', 'unknown'):
+                        logging.error('bad test result line: %s' % line.strip())
+                        continue
+                res['log_lineno'] = lineno
+                res['log_filename'] = os.path.basename(self._logfile)
+
+                results_from_log_file.append(res)
+
+        return results_from_log_file
+
+    def _get_test_runs(self):
+        now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+        attachment = [create_attachment(os.path.basename(self._logfile), read_content(self._logfile))]
+        results = self._get_test_results()
+        return {
+            'test_id': 'lava-command',
+            'analyzer_assigned_date': now,
+            'analyzer_assigned_uuid': str(uuid4()),
+            'time_check_performed': False,
+            'test_results': results,
+            'attachments': attachment
+        }
+
+    def _get_bundle(self):
+        return {
+            'test_runs': [self._get_test_runs()],
+            'format': 'Dashboard Bundle Format 1.6'
+        }
+
+    def _write_results_bundle(self):
+        rdir = self.context.host_result_dir
+        (fd, name) = tempfile.mkstemp(
+            prefix='lava-command', suffix='.bundle', dir=rdir)
+        with os.fdopen(fd, 'w') as f:
+            DocumentIO.dump(f, self._get_bundle())

lava_dispatcher/client/base.py
@@ -89,7 +89,7 @@ class CommandRunner(object):
         wait_for_prompt(self._connection, self._prompt_str, timeout)
 
     def run(self, cmd, response=None, timeout=-1,
-            failok=False, wait_prompt=True):
+            failok=False, wait_prompt=True, log_in_host=None):
         """Run `cmd` and wait for a shell response.
 
         :param cmd: The command to execute.
@@ -99,10 +99,14 @@ class CommandRunner(object):
             shell prompt, defaulting to forever.
         :param failok: The command can fail or not, if it is set False and
             command fail, an OperationFail exception will raise
+        :param log_in_host: If set, the input and output of the command will be
+            logged in it
         :return: The exit value of the command, if wait_for_rc not explicitly
             set to False during construction.
         """
         self._connection.empty_buffer()
+        if log_in_host is not None:
+            self._connection.logfile = open(log_in_host, "a")
         self._connection.sendline(cmd)
         start = time.time()
         if response is not None:

lava_dispatcher/signals/shellhooks.py
@@ -5,7 +5,7 @@ import subprocess
 import os
 import tempfile
 
-from lava_dispatcher.lava_test_shell import _read_content
+from lava_dispatcher.utils import read_content
 from lava_dispatcher.signals import SignalHandler
 from lava_dispatcher.test_data import create_attachment
 from lava_dispatcher.utils import mkdtemp
@@ -77,7 +77,7 @@ class ShellHooks(SignalHandler):
             path = case_data.get(key)
             if path is None:
                 continue
-            content = _read_content(path, ignore_missing=True)
+            content = read_content(path, ignore_missing=True)
             if content:
                 test_result['attachments'].append(
-                    create_attachment(key + '.txt', _read_content(path)))
+                    create_attachment(key + '.txt', read_content(path)))

lava_dispatcher/utils.py
@@ -288,3 +288,10 @@ def finalize_process(proc):
         logging.debug("Finalizing child proccess with PID %d" % proc.pid)
         proc.kill(9)
         proc.close()
+
+def read_content(filepath, ignore_missing=False):
+    if not os.path.exists(filepath) and ignore_missing:
+        return ''
+    with open(filepath, 'r') as f:
+        return f.read()
+

commit 1d29721405aa2dfbb505ba65d9edae054f283ff5

lava_test_shell: accept non-existing build.txt
This fixes a regression introduced by
332f4bf1c4c9c297453766fcb3db585b17853649

Change-Id: Id8e5915cd0b2b293913fae945c4625a33dce55d4
Reviewed-on: http://staging.review.linaro.org/539
Reviewed-by: lava-bot <lava-bot@linaro.org>
Reviewed-by: Tyler Baker <tyler.baker@linaro.org>

lava_dispatcher/lava_test_shell.py
@@ -389,7 +389,7 @@ def get_bundle(results_dir, testdef_objs, err_log):
     meminfo = read_content(os.path.join(results_dir, 'hwcontext/meminfo.txt'), ignore_missing=True)
     hwctx = _get_hw_context(cpuinfo, meminfo)
 
-    build = read_content(os.path.join(results_dir, 'swcontext/build.txt'))
+    build = read_content(os.path.join(results_dir, 'swcontext/build.txt'), ignore_missing=True)
     pkginfo = read_content(os.path.join(results_dir, 'swcontext/pkgs.txt'), ignore_missing=True)
 
     for test_run_name, test_run_path in _directory_names_and_paths(results_dir):
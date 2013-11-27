1d29721405aa2dfbb505ba65d9edae054f283ff5
Antonio Terceiro <antonio.terceiro@linaro.org>
2013-11-27 13:39:13 -0300

    lava_test_shell: accept non-existing build.txt
    This fixes a regression introduced by
    332f4bf1c4c9c297453766fcb3db585b17853649
    
    Change-Id: Id8e5915cd0b2b293913fae945c4625a33dce55d4
    Reviewed-on: http://staging.review.linaro.org/539
    Reviewed-by: lava-bot <lava-bot@linaro.org>
    Reviewed-by: Tyler Baker <tyler.baker@linaro.org>

--- lava_dispatcher/lava_test_shell.py
+++ lava_dispatcher/lava_test_shell.py
@@ -389,7 +389,7 @@ def get_bundle(results_dir, testdef_objs, err_log):
     meminfo = read_content(os.path.join(results_dir, 'hwcontext/meminfo.txt'), ignore_missing=True)
     hwctx = _get_hw_context(cpuinfo, meminfo)
 
-    build = read_content(os.path.join(results_dir, 'swcontext/build.txt'))
+    build = read_content(os.path.join(results_dir, 'swcontext/build.txt'), ignore_missing=True)
     pkginfo = read_content(os.path.join(results_dir, 'swcontext/pkgs.txt'), ignore_missing=True)
 
     for test_run_name, test_run_path in _directory_names_and_paths(results_dir):
