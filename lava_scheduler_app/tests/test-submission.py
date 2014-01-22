0f6d63e0c98c6e9906cf3b3663c4ec3fdf033f02
Neil Williams <neil.williams@linaro.org>
2014-01-22 13:00:10 +0000

    Fix the lava_scheduler_app unit tests
    Start a new file to keep tests discrete and
    to allow for more tests.
    
    Drop any tests using the removed getJobForBoard
    support.
    
    Change-Id: I44209e276ee9b50180227a2549136de61bbce3f4

--- lava_scheduler_app/tests/test-submission.py
+++ lava_scheduler_app/tests/test-submission.py
@@ -21,7 +21,7 @@ from lava_scheduler_app.models import (
     Tag,
     TestJob)
 from lava_scheduler_daemon.dbjobsource import DatabaseJobSource
-
+import simplejson
 
 logger = logging.getLogger()
 logger.level = logging.INFO  # change to DEBUG to see *all* output
@@ -30,10 +30,11 @@ logger.addHandler(stream_handler)
 
 
 # Based on http://www.technobabble.dk/2008/apr/02/xml-rpc-dispatching-through-django-test-client/
-class TestTransport(xmlrpclib.Transport):
+class TestTransport(xmlrpclib.Transport, object):
     """Handles connections to XML-RPC server through Django test client."""
 
     def __init__(self, user=None, password=None):
+        super(TestTransport, self).__init__()
         self.client = Client()
         if user:
             success = self.client.login(username=user, password=password)
@@ -111,7 +112,7 @@ class ModelFactory(object):
         return data
 
     def make_job_json(self, **kw):
-        return json.dumps(self.make_job_data(**kw))
+        return simplejson.dumps(self.make_job_data(**kw), sort_keys=True, indent=4 * ' ')
 
     def make_testjob(self, definition=None, submitter=None, **kwargs):
         if definition is None:
@@ -200,7 +201,7 @@ class TestTestJob(TestCaseWithFactory):
             self.factory.make_job_json(device_tags=['tag']),
             self.factory.make_user())
         self.assertEqual(
-            set(tag.name for tag in job.tags.all()), set(['tag']))
+            set(tag.name for tag in job.tags.all()), {'tag'})
 
     def test_from_json_and_user_sets_multiple_tag_from_device_tags(self):
         self.factory.ensure_device_type(name='panda')
@@ -210,7 +211,7 @@ class TestTestJob(TestCaseWithFactory):
             self.factory.make_job_json(device_tags=['tag1', 'tag2']),
             self.factory.make_user())
         self.assertEqual(
-            set(tag.name for tag in job.tags.all()), set(['tag1', 'tag2']))
+            set(tag.name for tag in job.tags.all()), {'tag1', 'tag2'})
 
     def test_from_json_and_user_reuses_tag_objects(self):
         self.factory.ensure_device_type(name='panda')
@@ -387,20 +388,6 @@ class TestDBJobSource(TransactionTestCaseWithFactory):
         User.objects.create_user(
             username='lava-health', email='lava@lava.invalid')
 
-    def test_getBoardList(self):
-        self.factory.make_device(hostname='panda01')
-        self.assertEqual(
-            [{'use_celery': False, 'hostname': 'panda01'}],
-            self.source.getBoardList())
-
-    def test_getJobForBoard_returns_json(self):
-        device = self.factory.make_device(hostname='panda01')
-        definition = self.factory.make_job_data(target='panda01')
-        self.factory.make_testjob(
-            requested_device=device, definition=json.dumps(definition))
-        self.assertEqual(
-            definition, self.source.getJobForBoard('panda01'))
-
     @property
     def health_job(self):
         return self.factory.make_job_json(health_check=True)
@@ -410,125 +397,10 @@ class TestDBJobSource(TransactionTestCaseWithFactory):
         return self.factory.make_job_json(health_check=False)
 
     def assertHealthJobAssigned(self, device):
-        job_data = self.source.getJobForBoard(device.hostname)
-        if job_data is None:
-            self.fail("no job assigned")
-        self.assertTrue(
-            job_data['health_check'],
-            'getJobForBoard did not return health check job')
+        pass
 
     def assertHealthJobNotAssigned(self, device):
-        job_data = self.source.getJobForBoard(device.hostname)
-        if job_data is None:
-            self.fail("no job assigned")
-        self.assertFalse(
-            job_data['health_check'],
-            'getJobForBoard returned health check job')
-
-    def test_getJobForBoard_returns_health_check_if_no_last_health_job(self):
-        device_type = self.factory.make_device_type(
-            health_check_job=self.health_job)
-        device = self.factory.make_device(
-            device_type=device_type, health_status=Device.HEALTH_PASS)
-        self.factory.make_testjob(
-            requested_device=device, definition=self.ordinary_job)
-        self.assertHealthJobAssigned(device)
-
-    def test_getJobForBoard_returns_health_check_if_old_last_health_job(self):
-        device_type = self.factory.make_device_type(
-            health_check_job=self.health_job)
-        device = self.factory.make_device(
-            device_type=device_type, health_status=Device.HEALTH_PASS,
-            last_health_report_job=self.factory.make_testjob(
-                end_time=datetime.datetime.now() - datetime.timedelta(weeks=1)))
-        self.factory.make_testjob(
-            requested_device=device, definition=self.ordinary_job)
-        self.assertHealthJobAssigned(device)
-
-    def test_getJobForBoard_returns_job_if_healthy_and_last_health_job_recent(self):
-        device_type = self.factory.make_device_type(
-            health_check_job=self.health_job)
-        device = self.factory.make_device(
-            device_type=device_type, health_status=Device.HEALTH_PASS,
-            last_health_report_job=self.factory.make_testjob(
-                end_time=datetime.datetime.now() - datetime.timedelta(hours=1)))
-        self.factory.make_testjob(
-            requested_device=device, definition=self.ordinary_job)
-        self.assertHealthJobNotAssigned(device)
-
-    def test_getJobForBoard_returns_health_check_if_health_unknown(self):
-        device_type = self.factory.make_device_type(
-            health_check_job=self.health_job)
-        device = self.factory.make_device(
-            device_type=device_type, health_status=Device.HEALTH_UNKNOWN,
-            last_health_report_job=self.factory.make_testjob(
-                end_time=datetime.datetime.now() - datetime.timedelta(hours=1)))
-        self.factory.make_testjob(
-            requested_device=device, definition=self.ordinary_job)
-        self.assertHealthJobAssigned(device)
-
-    def test_getJobForBoard_returns_None_if_no_job(self):
-        self.factory.make_device(hostname='panda01')
-        self.assertEqual(
-            None, self.source.getJobForBoard('panda01'))
-
-    def test_getJobForBoard_considers_device_type(self):
-        panda_type = self.factory.ensure_device_type(name='panda')
-        self.factory.make_device(hostname='panda01', device_type=panda_type)
-        definition = self.factory.make_job_data()
-        self.factory.make_testjob(
-            requested_device_type=panda_type,
-            definition=json.dumps(definition))
-        definition['target'] = 'panda01'
-        self.assertEqual(
-            definition, self.source.getJobForBoard('panda01'))
-
-    def test_getJobForBoard_prefers_older(self):
-        panda_type = self.factory.ensure_device_type(name='panda')
-        panda01 = self.factory.make_device(
-            hostname='panda01', device_type=panda_type)
-        first_definition = self.factory.make_job_data(foo='bar', target='panda01')
-        second_definition = self.factory.make_job_data(foo='baz', target='panda01')
-        self.factory.make_testjob(
-            requested_device=panda01, definition=json.dumps(first_definition),
-            submit_time=datetime.datetime.now() - datetime.timedelta(days=1))
-        self.factory.make_testjob(
-            requested_device=panda01, definition=json.dumps(second_definition),
-            submit_time=datetime.datetime.now())
-        self.assertEqual(
-            first_definition,
-            self.source.getJobForBoard('panda01'))
-
-    def test_getJobForBoard_prefers_directly_targeted(self):
-        panda_type = self.factory.ensure_device_type(name='panda')
-        panda01 = self.factory.make_device(
-            hostname='panda01', device_type=panda_type)
-        type_definition = self.factory.make_job_data()
-        self.factory.make_testjob(
-            requested_device_type=panda_type,
-            definition=json.dumps(type_definition),
-            submit_time=datetime.datetime.now() - datetime.timedelta(days=1))
-        device_definition = self.factory.make_job_data(
-            foo='baz', target='panda01')
-        self.factory.make_testjob(
-            requested_device=panda01,
-            definition=json.dumps(device_definition))
-        self.assertEqual(
-            device_definition,
-            self.source.getJobForBoard('panda01'))
-
-    def test_getJobForBoard_avoids_targeted_to_other_board_of_same_type(self):
-        panda_type = self.factory.ensure_device_type(name='panda')
-        panda01 = self.factory.make_device(
-            hostname='panda01', device_type=panda_type)
-        self.factory.make_device(hostname='panda02', device_type=panda_type)
-        definition = self.factory.make_job_data(foo='bar', target='panda01')
-        self.factory.make_testjob(
-            requested_device=panda01,
-            definition=json.dumps(definition))
-        self.assertEqual(
-            None,
-            self.source.getJobForBoard('panda02'))
+        pass
 
     def _makeBoardWithTags(self, tags):
         board = self.factory.make_device()
@@ -543,203 +415,7 @@ class TestDBJobSource(TransactionTestCaseWithFactory):
         return job
 
     def assertBoardWithTagsGetsJobWithTags(self, board_tags, job_tags):
-        board = self._makeBoardWithTags(board_tags)
-        self._makeJobWithTagsForBoard(job_tags, board)
-        self.assertEqual(
-            board.hostname,
-            self.source.getJobForBoard(board.hostname)['target'])
+        pass
 
     def assertBoardWithTagsDoesNotGetJobWithTags(self, board_tags, job_tags):
-        board = self._makeBoardWithTags(board_tags)
-        self._makeJobWithTagsForBoard(job_tags, board)
-        self.assertEqual(
-            None,
-            self.source.getJobForBoard(board.hostname))
-
-    def test_getJobForBoard_does_not_return_job_if_board_lacks_tag(self):
-        self.assertBoardWithTagsDoesNotGetJobWithTags([], ['tag'])
-
-    def test_getJobForBoard_returns_job_if_board_has_tag(self):
-        self.assertBoardWithTagsGetsJobWithTags(['tag'], ['tag'])
-
-    def test_getJobForBoard_returns_job_if_board_has_both_tags(self):
-        self.assertBoardWithTagsGetsJobWithTags(['tag1', 'tag2'], ['tag1', 'tag2'])
-
-    def test_getJobForBoard_returns_job_if_board_has_extra_tags(self):
-        self.assertBoardWithTagsGetsJobWithTags(['tag1', 'tag2'], ['tag1'])
-
-    def test_getJobForBoard_does_not_return_job_if_board_has_only_one_tag(self):
-        self.assertBoardWithTagsDoesNotGetJobWithTags(['tag1'], ['tag1', 'tag2'])
-
-    def test_getJobForBoard_does_not_return_job_if_board_has_unrelated_tag(self):
-        self.assertBoardWithTagsDoesNotGetJobWithTags(['tag1'], ['tag2'])
-
-    def test_getJobForBoard_does_not_return_job_if_only_one_tag_matches(self):
-        self.assertBoardWithTagsDoesNotGetJobWithTags(['tag1', 'tag2'], ['tag1', 'tag3'])
-
-    def test_getJobForBoard_sets_start_time(self):
-        device = self.factory.make_device(hostname='panda01')
-        job = self.factory.make_testjob(requested_device=device)
-        before = datetime.datetime.now()
-        self.source.getJobForBoard('panda01')
-        after = datetime.datetime.now()
-        # reload from the database
-        job = TestJob.objects.get(pk=job.pk)
-        self.assertTrue(before < job.start_time < after)
-
-    def test_getJobForBoard_set_statuses(self):
-        device = self.factory.make_device(hostname='panda01')
-        job = self.factory.make_testjob(requested_device=device)
-        self.source.getJobForBoard('panda01')
-        # reload from the database
-        job = TestJob.objects.get(pk=job.pk)
-        device = Device.objects.get(pk=device.pk)
-        self.assertEqual(
-            (Device.RUNNING, TestJob.RUNNING),
-            (device.status, job.status))
-
-    def test_getJobForBoard_sets_running_job(self):
-        device = self.factory.make_device(hostname='panda01')
-        job = self.factory.make_testjob(requested_device=device)
-        self.source.getJobForBoard('panda01')
-        # reload from the database
-        job = TestJob.objects.get(pk=job.pk)
-        device = Device.objects.get(pk=device.pk)
-        self.assertEqual(job, device.current_job)
-
-    def test_getJobForBoard_creates_token(self):
-        device = self.factory.make_device(hostname='panda01')
-        job = self.factory.make_testjob(requested_device=device)
-        self.source.getJobForBoard('panda01')
-        # reload from the database
-        job = TestJob.objects.get(pk=job.pk)
-        device = Device.objects.get(pk=device.pk)
-        self.assertIsNotNone(job.submit_token)
-        self.assertEqual(job.submitter, job.submit_token.user)
-
-    def test_getJobForBoard_inserts_target_into_json(self):
-        panda_type = self.factory.ensure_device_type(name='panda')
-        self.factory.make_device(hostname='panda01', device_type=panda_type)
-        definition = self.factory.make_job_data(device_type='panda')
-        self.factory.make_testjob(
-            requested_device_type=panda_type,
-            definition=json.dumps(definition))
-        json_data = self.source.getJobForBoard('panda01')
-        self.assertIn('target', json_data)
-        self.assertEqual('panda01', json_data['target'])
-
-    def test_getJobForBoard_inserts_submit_token_into_json(self):
-        panda_type = self.factory.ensure_device_type(name='panda')
-        self.factory.make_device(hostname='panda01', device_type=panda_type)
-        definition = {
-            'actions': [
-                {
-                    "command": "submit_results",
-                    "parameters":
-                    {
-                        "server": "http://test-server/RPC2/",
-                        "stream": "/private/personal/test/test/",
-                    }
-                }
-            ]
-        }
-        job = self.factory.make_testjob(
-            requested_device_type=panda_type,
-            definition=json.dumps(definition))
-        json_data = self.source.getJobForBoard('panda01')
-        job = TestJob.objects.get(pk=job.pk)
-        submit_job_params = json_data['actions'][0]['parameters']
-        self.assertIn('token', submit_job_params)
-        self.assertEqual(job.submit_token.secret, submit_job_params['token'])
-
-    def test_getJobForBoard_adds_user_to_url(self):
-        panda_type = self.factory.ensure_device_type(name='panda')
-        self.factory.make_device(hostname='panda01', device_type=panda_type)
-        user = User.objects.create_user('test', 'e@mail.invalid', 'test')
-        user.save()
-        definition = {
-            'actions': [
-                {
-                    "command": "submit_results",
-                    "parameters":
-                    {
-                        "server": "http://test-server/RPC2/",
-                        "stream": "/private/personal/test/test/",
-                    }
-                }
-            ]
-        }
-        job = self.factory.make_testjob(
-            requested_device_type=panda_type, submitter=user,
-            definition=json.dumps(definition))
-        json_data = self.source.getJobForBoard('panda01')
-        job = TestJob.objects.get(pk=job.pk)
-        submit_job_params = json_data['actions'][0]['parameters']
-        self.assertEqual("http://test@test-server/RPC2/", submit_job_params['server'])
-
-    def get_device_and_running_job(self):
-        device = self.factory.make_device(hostname='panda01')
-        job = self.factory.make_testjob(requested_device=device)
-        self.source.getJobForBoard('panda01')
-        return device, TestJob.objects.get(pk=job.pk)
-
-    def test_jobCompleted_set_statuses_success(self):
-        device, job = self.get_device_and_running_job()
-        self.source.jobCompleted('panda01', 0)
-        job = TestJob.objects.get(pk=job.pk)
-        device = Device.objects.get(pk=device.pk)
-        self.assertEqual(
-            (Device.IDLE, TestJob.COMPLETE),
-            (device.status, job.status))
-
-    def test_jobCompleted_set_statuses_failure(self):
-        device, job = self.get_device_and_running_job()
-        self.source.jobCompleted('panda01', 1)
-        job = TestJob.objects.get(pk=job.pk)
-        device = Device.objects.get(pk=device.pk)
-        self.assertEqual(
-            (Device.IDLE, TestJob.INCOMPLETE),
-            (device.status, job.status))
-
-    def test_jobCompleted_works_on_device_type_targeted(self):
-        device = self.factory.make_device(hostname='panda01')
-        job = self.factory.make_testjob(
-            requested_device_type=device.device_type)
-        self.source.getJobForBoard('panda01')
-        self.source.jobCompleted('panda01', 0)
-        job = TestJob.objects.get(pk=job.pk)
-        device = Device.objects.get(pk=device.pk)
-        self.assertEqual(
-            (Device.IDLE, TestJob.COMPLETE),
-            (device.status, job.status))
-
-    def test_jobCompleted_sets_end_time(self):
-        device, job = self.get_device_and_running_job()
-        before = datetime.datetime.now()
-        self.source.jobCompleted('panda01', 0)
-        after = datetime.datetime.now()
-        job = TestJob.objects.get(pk=job.pk)
-        self.assertTrue(before < job.end_time < after)
-
-    def test_jobCompleted_clears_current_job(self):
-        device, job = self.get_device_and_running_job()
-        self.source.jobCompleted('panda01', 0)
-        device = Device.objects.get(pk=device.pk)
-        self.assertEquals(None, device.current_job)
-
-    def test_jobCompleted_deletes_token(self):
-        device, job = self.get_device_and_running_job()
-        token = job.submit_token
-        self.source.jobCompleted('panda01', 0)
-        self.assertRaises(
-            AuthToken.DoesNotExist,
-            AuthToken.objects.get, pk=token.pk)
-
-    def test_getLogFileForJobOnBoard_returns_writable_file(self):
-        device, job = self.get_device_and_running_job()
-        definition = {'foo': 'bar'}
-        self.factory.make_testjob(
-            requested_device=device, definition=json.dumps(definition))
-        log_file = self.source.getLogFileForJobOnBoard('panda01')
-        log_file.write('a')
-        log_file.close()
+        pass
