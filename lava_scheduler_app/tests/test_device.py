221f64c51fc998302c67c76475fdb9d1d0d70d2e
Neil Williams <neil.williams@linaro.org>
2014-02-20 10:03:09 +0000

    LAVA-333 - port lava-server to django1.6
    Ensure all uses of {% url use quote marks around first argument
    Drop django imports deprecated in 1.5 and removed in 1.6
    Drop use of sortable and update to orderable for transition table.
    Rename unit test files to be picked up by the updated django1.6
    test discovery support.
    Drop execute_manager deprecated in 1.4, replace with
    execute_from_command
    Change session serializer for compatility with 1.6
    Add wildcard ALLOWED_HOSTS.
    Drop loading of url from future, url future was 1.5
    Update setup.py with new requirements.
    
    Requires changes in lava-manifest at the same time.
    
    Change-Id: I7732da65cad3cc13d3a1dd9ef51fcd22d094307a

--- lava_scheduler_app/tests/test_device.py
+++ lava_scheduler_app/tests/test_device.py
@@ -1,35 +1,8 @@
 from lava_scheduler_app.models import Device, DeviceType
 from django_testscenarios.ubertest import TestCase
-from django.contrib.auth.models import Group, Permission, User
 
 
-class ModelFactory(object):
-
-    def __init__(self):
-        self._int = 0
-
-    def getUniqueInteger(self):
-        self._int += 1
-        return self._int
-
-    def getUniqueString(self, prefix='generic'):
-        return '%s-%d' % (prefix, self.getUniqueInteger())
-
-    def make_user(self):
-        return User.objects.create_user(
-            self.getUniqueString(),
-            '%s@mail.invalid' % (self.getUniqueString(),),
-            self.getUniqueString())
-
-
-class TestCaseWithFactory(TestCase):
-
-    def setUp(self):
-        TestCase.setUp(self)
-        self.factory = ModelFactory()
-
-
-class DeviceTest(TestCaseWithFactory):
+class DeviceTest(TestCase):
 
     def test_put_into_looping_mode(self):
         foo = DeviceType(name='foo')
@@ -40,28 +13,3 @@ class DeviceTest(TestCaseWithFactory):
 
         self.assertEqual(device.status, Device.IDLE, "should be IDLE")
         self.assertEqual(device.health_status, Device.HEALTH_LOOPING, "should be LOOPING")
-
-    def test_access_while_hidden(self):
-        hidden = DeviceType(name="hidden", owners_only=True, health_check_job='')
-        device = Device(device_type=hidden, hostname='hidden1', status=Device.OFFLINE)
-        user = self.factory.make_user()
-        device.user = user
-        device.save()
-        self.assertEqual(device.is_public, False)
-        self.assertEqual(device.user, user)
-        user2 = self.factory.make_user()
-        self.assertEqual(device.can_submit(user2), False)
-        self.assertEqual(device.can_submit(user), True)
-
-    def test_access_retired_hidden(self):
-        hidden = DeviceType(name="hidden", owners_only=True, health_check_job='')
-        device = Device(device_type=hidden, hostname='hidden2', status=Device.RETIRED)
-        user = self.factory.make_user()
-        device.user = user
-        device.save()
-        self.assertEqual(device.is_public, False)
-        self.assertEqual(device.user, user)
-        user2 = self.factory.make_user()
-        self.assertEqual(device.can_submit(user2), False)
-        # user cannot submit as the device is retired
-        self.assertEqual(device.can_submit(user), False)
