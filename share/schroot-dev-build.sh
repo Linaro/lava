040096d1a6be73a2b27c13bf2e48c758060e2acd
Neil Williams <neil.williams@linaro.org>
2014-04-10 18:35:48 +0100

    Add an sbuild helper for chroot builds.
    Change-Id: I507cfd0c7e0a35ef998548192a11625f6dc0256f

---
+++ share/schroot-dev-build.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$3" ]; then
+	echo "Usage: <package> <chroot> <suite>"
+	echo "A suitable schroot must already have been prepared"
+	echo "and the Debian unstable LAVA dependencies made available"
+	echo "inside that chroot."
+	exit 1
+fi
+
+chroot=$2
+suite=$3
+PWD=`pwd`
+NAME=${1}
+VERSION=`python ./version.py`
+if [ -d './dist/' ]; then
+	rm -f ./dist/*
+fi
+python setup.py sdist
+DIR=`mktemp -d`
+mv -v ./dist/${NAME}-${VERSION}.tar.gz ${DIR}/${NAME}_${VERSION}.orig.tar.gz
+cd ${DIR}
+git clone https://github.com/Linaro/pkg-${NAME}.git
+tar -xzf ${NAME}_${VERSION}.orig.tar.gz
+cd ${DIR}/pkg-${NAME}/
+dpkg-checkbuilddeps
+git archive master debian | tar -x -C ../${NAME}-${VERSION}
+cd ${DIR}/${NAME}-${VERSION}
+dch -v ${VERSION}-1ubuntu1 -D ${suite} "Local developer build"
+sbuild -A -c ${chroot} -d ${suite}
+cd ${DIR}
+rm -rf ${DIR}/pkg-${NAME}
+rm -rf ${DIR}/${NAME}-${VERSION}
+if [ -x /usr/bin/dcmd ]; then
+	dcmd ls ${DIR}/${NAME}_${VERSION}*.changes
+else
+	echo ${DIR}
+	ls ${DIR}
+fi
