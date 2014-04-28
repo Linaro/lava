aaaa3df862183b60682f0a8853ee3fb4988282fa
Neil Williams <neil.williams@linaro.org>
2014-04-28 13:56:04 +0100

    Match up licence versions
    GPL-2 is incompatible with APL-3, so make it clear
    that all files which were GPL-2 or later are being
    distributed as GPL-3 or later to match the rest of
    the source and make a distributable release.
    
    Change-Id: I98b1f3e0c259da4442b814416f95db9077101421

--- share/add_device.py
+++ share/add_device.py
@@ -5,9 +5,9 @@
 #
 #  Copyright 2014 Neil Williams <codehelp@debian.org>
 #
-#  This program is free software; you can redistribute it and/or modify
+#  This rogram is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
+#  the Free Software Foundation; either version 3 of the License, or
 #  (at your option) any later version.
 #
 #  This program is distributed in the hope that it will be useful,
@@ -16,11 +16,7 @@
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
-#
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # The aim of this script is to be a stand-alone helper to add new
 # devices to a LAVA instance - wrapping local calls to lava-server manage
