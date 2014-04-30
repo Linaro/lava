a8007dfd6e0aa51878c93a513143777ead037dc9
Neil Williams <neil.williams@linaro.org>
2014-04-30 13:13:22 +0100

    Add initial unit tests for LavaTestJob
    Change-Id: I02138145455df18483a4af71cbe859e0cccd4928
    Signed-off-by: Antonio Terceiro <antonio.terceiro@linaro.org>
    Signed-off-by: Neil Williams <neil.williams@linaro.org>

--- lava_dispatcher/tests/__init__.py
+++ lava_dispatcher/tests/__init__.py
@@ -5,6 +5,7 @@ def test_suite():
     module_names = [
         'lava_dispatcher.tests.test_config',
         'lava_dispatcher.tests.test_device_version',
+        'lava_dispatcher.tests.test_job'
     ]
     loader = unittest.TestLoader()
     return loader.loadTestsFromNames(module_names)
