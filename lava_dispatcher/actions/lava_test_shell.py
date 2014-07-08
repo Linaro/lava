799ab6e9c7c233cee14693b88fd051df7cfc53b4
Rémi Duraffort <remi.duraffort@linaro.org>
2014-07-08 09:35:01 +0200

    Factorize http_proxy settings
    Change-Id: Ie44f13eb092f91f4d51fcbbffded591722ec0a09

--- lava_dispatcher/actions/lava_test_shell.py
+++ lava_dispatcher/actions/lava_test_shell.py
@@ -164,6 +164,11 @@ XMOD = stat.S_IRWXU | stat.S_IXGRP | stat.S_IRGRP | stat.S_IXOTH | stat.S_IROTH
 INVALID_CHARS = " $&()\"'<>/\\|;`"
 
 
+def _get_lava_proxy(context):
+    return {'http_proxy': context.config.lava_proxy,
+            'https_proxy': context.config.lava_proxy}
+
+
 def _get_testdef_git_repo(testdef_repo, tmpdir, revision, proxy_env):
     cwd = os.getcwd()
     gitdir = os.path.join(tmpdir, 'gittestrepo')
@@ -333,16 +338,14 @@ class TestDefinitionLoader(object):
         if 'git-repo' in testdef_repo:
             repo = _get_testdef_git_repo(testdef_repo['git-repo'], tmpdir,
                                          testdef_repo.get('revision'),
-                                         {'http_proxy': self.context.config.lava_proxy,
-                                          'https_proxy': self.context.config.lava_proxy})
+                                         _get_lava_proxy(self.context))
             name = os.path.splitext(os.path.basename(testdef_repo['git-repo']))[0]
             info = _git_info(testdef_repo['git-repo'], repo, name)
 
         if 'bzr-repo' in testdef_repo:
             repo = _get_testdef_bzr_repo(testdef_repo['bzr-repo'], tmpdir,
                                          testdef_repo.get('revision'),
-                                         {'http_proxy': self.context.config.lava_proxy,
-                                          'https_proxy': self.context.config.lava_proxy})
+                                         _get_lava_proxy(self.context))
             name = testdef_repo['bzr-repo'].replace('lp:', '').split('/')[-1]
             info = _bzr_info(testdef_repo['bzr-repo'], repo, name)
 
@@ -460,17 +463,6 @@ class URLTestDefinition(object):
         self.__fixupdict__ = None
         self.skip_install = None
 
-    def lava_proxy_dict(self, env_dict=None):
-        full_dict = {}
-        if env_dict is not None:
-            full_dict = env_dict
-
-        if self.context.config.lava_proxy:
-            full_dict['http_proxy'] = self.context.config.lava_proxy
-            full_dict['https_proxy'] = self.context.config.lava_proxy
-
-        return full_dict
-
     def load_signal_handler(self):
         hook_data = self.testdef.get('handler')
         if not hook_data:
@@ -504,13 +496,15 @@ class URLTestDefinition(object):
 
             for repo in self.testdef['install'].get('bzr-repos', []):
                 logging.info("bzr branch %s" % repo)
+
                 # Pass non-existent BZR_HOME value, or otherwise bzr may
                 # have non-reproducible behavior because it may rely on
                 # bzr whoami value, presence of ssh keys, etc.
+                env_dict = _get_lava_proxy(self.context)
+                env_dict.update({'BZR_HOME': '/dev/null', 'BZR_LOG': '/dev/null'})
+
                 subprocess.check_output(['bzr', 'branch', repo],
-                                        env=self.lava_proxy_dict(
-                                            {'BZR_HOME': '/dev/null',
-                                             'BZR_LOG': '/dev/null'}),
+                                        env=env_dict,
                                         stderr=subprocess.STDOUT)
                 name = repo.replace('lp:', '').split('/')[-1]
                 self._sw_sources.append(_bzr_info(repo, name, name))
@@ -518,7 +512,7 @@ class URLTestDefinition(object):
             for repo in self.testdef['install'].get('git-repos', []):
                 logging.info("git clone %s" % repo)
                 subprocess.check_output(['git', 'clone', repo],
-                                        env=self.lava_proxy_dict(),
+                                        env=_get_lava_proxy(self.context),
                                         stderr=subprocess.STDOUT)
                 name = os.path.splitext(os.path.basename(repo))[0]
                 self._sw_sources.append(_git_info(repo, name, name))
