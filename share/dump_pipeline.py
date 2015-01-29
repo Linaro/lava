933032b556ca22faa294a8438145ed74770f9ceb
Rémi Duraffort <remi.duraffort@linaro.org>
2015-01-29 11:01:03 +0100

    Add a small helper to dump a Pipeline summary
    This helper can be used to create reference descriptions to be used in the test
    definitions.
    
    Change-Id: I2a2d0428c30d194b7036a1fd734137129521a324

---
+++ share/dump_pipeline.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 Linaro Limited
+#
+# Author: Remi Duraffort <remi.duraffort@linaro.org>
+#
+# This file is part of LAVA Dispatcher.
+#
+# LAVA Dispatcher is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# LAVA Dispatcher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along
+# with this program; if not, see <http://www.gnu.org/licenses>.
+
+from lava_dispatcher.pipeline.device import NewDevice
+from lava_dispatcher.pipeline.parser import JobParser
+
+import optparse
+import yaml
+
+
+if __name__ == '__main__':
+    usage = "Usage: %prog device_name job.yaml"
+    description = "LAVA dispatcher pipeline helper. Dump the pipeline that " \
+                  "was built for the given job on the given device"
+
+    parser = optparse.OptionParser(usage=usage, description=description)
+    (options, args) = parser.parse_args()
+    if len(args) != 2:
+        print "Missing job option, try -h for help"
+        exit(1)
+
+    # Create the device
+    try:
+        device = NewDevice(args[0])
+    except RuntimeError:
+        print "No device configuration found for %s" % args[0]
+        exit(1)
+
+    # Load the job definition
+    with open(args[1], 'r') as job_data:
+        parser = JobParser()
+        job = parser.parse(job_data, device)
+
+    print yaml.dump(job.pipeline.describe(False))
