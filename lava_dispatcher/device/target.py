1017065df47f033119a0bfb8a74934c43322b272
Tyler Baker <tyler.baker@linaro.org>
2015-03-05 13:41:39 -0800

    android: fix superuser issue with prompt
    There is a problem with using 'su' on Android deployments.
    We set the expected prompt, issue 'su' which may or may not
    change the environment, including PS1. So lets invoke 'su' first,
    then set the prompt we expect.
    
    Change-Id: I295741f9cb195bcc8294e884e673b3557788eb83

--- lava_dispatcher/device/target.py
+++ lava_dispatcher/device/target.py
@@ -606,6 +606,10 @@ class Target(object):
                 pattern = self.config.test_image_prompts
 
             self._wait_for_prompt(connection, pattern, self.config.boot_linaro_timeout)
+            if self.target_distro == 'android':
+                # Gain root access 	609
+                connection.sendline('su')
+                self._wait_for_prompt(connection, pattern, timeout=10)
             connection.sendline('export PS1="%s"' % ps1,
                                 send_char=self.config.send_char)
             self._wait_for_prompt(connection, ps1_pattern, timeout=10)
