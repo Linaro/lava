9310d64a68a6e7fc2af76050a1cb24a03b1fbcb1
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2015-03-07 15:55:41 +0100

    dashboard_app: Cope with chart filters without a filter
    I didn't look into the details of why, but our image charts view
    exploded while trying to determine chartfilters without a filter where
    accessible. This is a quick fix for that issue.
    
    Change-Id: I29c1a8567a1d42c840f3f27a5d942061f57ab6b6

--- dashboard_app/models.py
+++ dashboard_app/models.py
@@ -1979,6 +1979,8 @@ class ImageReport(models.Model):
         # access to this image report as well.
         for chart in self.imagereportchart_set.all():
             for chart_filter in chart.imagechartfilter_set.all():
+                if not chart_filter.filter:
+                    continue
                 for bundle_stream in chart_filter.filter.bundle_streams.all():
                     if not bundle_stream.is_accessible_by(user):
                         return False
