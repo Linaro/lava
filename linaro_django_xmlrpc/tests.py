47c6d0af054817b0b5832fc47e8dd40f2c90ae7e
Senthil Kumaran S <senthil.kumaran@linaro.org>
2015-05-06 22:27:59 +0530

    Remove references to 'Launch Control' and add current component name.
    Change-Id: Ia3c2161bab816aadabd291e284f6b90b74979545

--- linaro_django_xmlrpc/tests.py
+++ linaro_django_xmlrpc/tests.py
@@ -2,19 +2,19 @@
 #
 # Author: Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
 #
-# This file is part of Launch Control.
+# This file is part of Linaro Django XMLRPC.
 #
-# Launch Control is free software: you can redistribute it and/or modify
+# Linaro Django XMLRPC is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License version 3
 # as published by the Free Software Foundation
 #
-# Launch Control is distributed in the hope that it will be useful,
+# Linaro Django XMLRPC is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with Launch Control.  If not, see <http://www.gnu.org/licenses/>.
+# along with Linaro Django XMLRPC.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 Unit tests for Linaro Django XML-RPC Application
