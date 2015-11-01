43f4e8cfd06bbb98b69ea4c73956301a4ad4fd8b
Senthil Kumaran S <senthil.kumaran@linaro.org>
2015-11-02 00:25:18 +0530

    LAVA-359 - Unit test for fastboot actions
    Change-Id: I6e6da53cb4fd2c08187e49b5cf5396fea3574e3a

---
+++ lava_dispatcher/pipeline/test/test_fastboot.py
@@ -0,0 +1,107 @@
+# Copyright (C) 2015 Linaro Limited
+#
+# Author: Senthil Kumaran S <senthil.kumaran@linaro.org>
+#
+# This file is part of LAVA Dispatcher.
+#
+# LAVA Dispatcher is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# LAVA Dispatcher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along
+# with this program; if not, see <http://www.gnu.org/licenses>.
+
+import os
+import glob
+import unittest
+
+from lava_dispatcher.pipeline.device import NewDevice
+from lava_dispatcher.pipeline.parser import JobParser
+from lava_dispatcher.pipeline.utils.filesystem import mkdtemp
+from lava_dispatcher.pipeline.action import JobError
+from lava_dispatcher.pipeline.test.test_basic import pipeline_reference
+from lava_dispatcher.pipeline.actions.deploy import DeployAction
+from lava_dispatcher.pipeline.actions.boot.fastboot import BootAction
+
+
+class Factory(object):  # pylint: disable=too-few-public-methods
+    """
+    Not Model based, this is not a Django factory.
+    Factory objects are dispatcher based classes, independent
+    of any database objects.
+    """
+    def create_fastboot_job(self, filename, output_dir='/tmp/'):  # pylint: disable=no-self-use
+        device = NewDevice(os.path.join(os.path.dirname(__file__), '../devices/nexus4-01.yaml'))
+        fastboot_yaml = os.path.join(os.path.dirname(__file__), filename)
+        with open(fastboot_yaml) as sample_job_data:
+            parser = JobParser()
+            job = parser.parse(sample_job_data, device, 4212, None, output_dir=output_dir)
+        return job
+
+
+class TestFastbootDeploy(unittest.TestCase):  # pylint: disable=too-many-public-methods
+
+    def setUp(self):
+        super(TestFastbootDeploy, self).setUp()
+        factory = Factory()
+        self.job = factory.create_fastboot_job('sample_jobs/fastboot.yaml',
+                                               mkdtemp())
+
+    def test_deploy_job(self):
+        self.assertEqual(self.job.pipeline.job, self.job)
+        for action in self.job.pipeline.actions:
+            if isinstance(action, DeployAction):
+                self.assertEqual(action.job, self.job)
+
+    def test_pipeline(self):
+        description_ref = pipeline_reference('fastboot.yaml')
+        self.assertEqual(description_ref, self.job.pipeline.describe(False))
+
+    def test_validate(self):
+        try:
+            self.job.pipeline.validate_actions()
+        except JobError as exc:
+            self.fail(exc)
+        for action in self.job.pipeline.actions:
+            self.assertEqual([], action.errors)
+
+    def test_overlay(self):
+        overlay = None
+        for action in self.job.pipeline.actions:
+            self.assertIsNotNone(action.name)
+            if isinstance(action, DeployAction):
+                overlay = action.pipeline.children[action.pipeline][0]
+        self.assertIsNotNone(overlay)
+        # these tests require that lava-dispatcher itself is installed, not just running tests from a git clone
+        self.assertTrue(os.path.exists(overlay.lava_test_dir))
+        self.assertIsNot(overlay.lava_test_dir, '/')
+        self.assertNotIn('lava_multi_node_test_dir', dir(overlay))
+        self.assertNotIn('lava_multi_node_cache_file', dir(overlay))
+        self.assertNotIn('lava_lmp_test_dir', dir(overlay))
+        self.assertNotIn('lava_lmp_cache_file', dir(overlay))
+        self.assertIsNotNone(overlay.parameters['deployment_data']['lava_test_results_dir'])
+        self.assertIsNotNone(overlay.parameters['deployment_data']['lava_test_sh_cmd'])
+        self.assertEqual(overlay.parameters['deployment_data']['distro'], 'android')
+        self.assertIsNotNone(overlay.parameters['deployment_data']['lava_test_results_part_attr'])
+        self.assertIsNotNone(glob.glob(os.path.join(overlay.lava_test_dir, 'lava-*')))
+
+    def test_boot(self):
+        for action in self.job.pipeline.actions:
+            if isinstance(action, BootAction):
+                # get the action & populate it
+                self.assertEqual(action.parameters['method'], 'fastboot')
+                self.assertEqual(action.parameters['prompts'],
+                                 ['shell@mako:/ $', 'shell@mako'])
+
+    def test_testdefinitions(self):
+        for action in self.job.pipeline.actions:
+            if action.name == 'test':
+                # get the action & populate it
+                self.assertEqual(len(action.parameters['definitions']), 2)
