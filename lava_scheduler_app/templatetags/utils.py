bfec3a560a0bd6bbfc95b4d77bf1755b9e771bfc
Rémi Duraffort <remi.duraffort@linaro.org>
2016-04-21 14:31:59 +0200

    Remove unused custom template tags (since 8bc5262)
    Change-Id: I98306e54ae119ddc26ed7d50a379bae015291d05

--- lava_scheduler_app/templatetags/utils.py
+++ lava_scheduler_app/templatetags/utils.py
@@ -34,66 +34,8 @@ def get_priority_select(current):
 
 
 @register.filter
-def get_type(value):
-    """
-    Detects iterable types from not iterable types
-    enough for the templates to work out if it is a value or a key.
-    """
-    if type(value) == str:
-        return 'str'
-    if type(value) == unicode:
-        return 'str'
-    if type(value) == bool:
-        return 'str'
-    if type(value) == int:
-        return 'str'
-    if type(value) == dict:
-        return 'dict'
-    if type(value) == list:
-        return 'list'
-    return type(value)
-
-
-@register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
-
-
-@register.filter
-def get_device_dictionary(data):
-    key = os.path.basename(os.path.dirname(data))
-    device_dict_obj = DeviceDictionaryTable.objects.get(id=key)
-    device_dict = device_dict_obj.lookup_device_dictionary()
-    return device_dict.to_dict()
-
-
-@register.filter
-def get_pipeline_store(data):
-    key = os.path.basename(os.path.dirname(data))
-    device_dict_obj = PipelineStore.objects.get(id=key)
-    device_dict = device_dict_obj.lookup_job_pipeline()
-    return device_dict.to_dict()
-
-
-@register.filter
-def get_device_parameters(data, key):
-    if type(data) == str:
-        return data
-    if type(data) == dict:
-        if type(key) == str and key in data:
-            return data.get(key)
-        return key.keys()
-    return (type(data), type(key), data)
-
-
-@register.filter
-def get_yaml_parameters(parameters):
-    # FIXME: it should be possible to dump this dict as YAML.
-    try:
-        ret = yaml.safe_dump(parameters, default_flow_style=False, canonical=False, default_style=None)
-    except:
-        return parameters
-    return ret
 
 
 @register.filter
