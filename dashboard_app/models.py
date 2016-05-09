044e7aeb71a1fa66e84d5092ef1da2b9eec56ce0
Rémi Duraffort <remi.duraffort@linaro.org>
2016-05-09 14:49:28 +0200

    Dashboard IR2: reduce the number of SQL queries
    On my setup, the number of queries are:
    * before: 1748 (in 1300ms)
    * after : 727  (in 730ms)
    
    Change-Id: I35355c9d7eaacbaa82f99df70e88b010807be651

--- dashboard_app/models.py
+++ dashboard_app/models.py
@@ -2005,7 +2005,9 @@ class ImageReport(models.Model):
         # If any of bundle streams is not accessible by this user, restrict
         # access to this image report as well.
         for chart in self.imagereportchart_set.all():
-            for chart_filter in chart.imagechartfilter_set.all():
+            chart_filters = chart.imagechartfilter_set.all().select_related("filter")\
+                                 .prefetch_related("filter__bundle_streams")
+            for chart_filter in chart_filters:
                 if not chart_filter.filter:
                     continue
                 for bundle_stream in chart_filter.filter.bundle_streams.all():
