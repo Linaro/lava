6ab1ed16e7a3494cf53f1111b6d0100d438836c8
Rémi Duraffort <remi.duraffort@linaro.org>
2016-05-11 10:46:34 +0200

    Remove old settings
    Change-Id: Id6b437492f39cd1d6661ab559f345553e4d25913

--- dashboard_app/extension.py
+++ dashboard_app/extension.py
@@ -35,33 +35,3 @@ class DashboardExtension(LavaServerExtension):
     def api_class(self):
         from dashboard_app.xmlrpc import DashboardAPI
         return DashboardAPI
-
-    def contribute_to_settings(self, settings_module):
-        super(DashboardExtension, self).contribute_to_settings(settings_module)
-        root_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), ".."))
-        settings_module['DATAVIEW_DIRS'] = [
-            os.path.join(root_dir, 'examples', 'views'),
-            os.path.join(root_dir, 'production', 'views')]
-        settings_module['DATAREPORT_DIRS'] = [
-            os.path.join(root_dir, 'examples', 'reports'),
-            os.path.join(root_dir, 'production', 'reports')]
-
-    def contribute_to_settings_ex(self, settings_module, settings_object):
-        settings_module['DATAVIEW_DIRS'] = settings_object._settings.get(
-            "DATAVIEW_DIRS", [])
-        settings_module['DATAVIEW_HIDE'] = settings_object._settings.get(
-            "DATAVIEW_HIDE", False)
-        settings_module['DATAREPORT_DIRS'] = settings_object._settings.get(
-            "DATAREPORT_DIRS", [])
-        settings_module['DATAREPORTS_HIDE'] = settings_object._settings.get(
-            "DATAREPORTS_HIDE", False)
-        settings_module['PM_QA_HIDE'] = settings_object._settings.get(
-            "PM_QA_HIDE", True)
-
-        # Enable constrained dataview database if requested
-        if settings_object._settings.get("use_dataview_database"):
-            # Copy everything from the default database and append _dataview to user
-            # name. The rest is out of scope (making sure it's actually setup
-            # properly, having permissions to login, permissions to view proper data)
-            settings_module['DATABASES']['dataview'] = dict(settings_module['DATABASES']['default'])
-            settings_module['DATABASES']['dataview']['USER'] += "_dataview"
