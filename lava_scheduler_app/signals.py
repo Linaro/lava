d6dd825e91c3d089ccb0a701d7f454505b0bbfaf
Rémi Duraffort <remi.duraffort@linaro.org>
2016-05-26 14:45:16 +0200

    Events: send events also for TestJob
    Change-Id: Iacecb77a84aa18af224386e358fa96d908758089

--- lava_scheduler_app/signals.py
+++ lava_scheduler_app/signals.py
@@ -9,11 +9,36 @@ import uuid
 import zmq
 from zmq.utils.strtypes import b
 
-from lava_scheduler_app.models import Device
+from lava_scheduler_app.models import Device, TestJob
 
 
 # Thread local storage for zmq socket and context
 thread_local = threading.local()
+
+
+def send_event(topic, data):
+    # Get back the thread local storage
+    try:
+        context = thread_local.context
+        socket = thread_local.socket
+        user = thread_local.user
+    except AttributeError:
+        # Create the context and socket
+        thread_local.context = context = zmq.Context.instance()
+        thread_local.socket = socket = context.socket(zmq.PUSH)
+        thread_local.user = user = pwd.getpwuid(os.geteuid()).pw_name
+        socket.connect(settings.INTERNAL_EVENT_SOCKET)
+    # The format is [topic, uuid, datetime, username, data as json]
+    msg = [
+        b(settings.EVENT_TOPIC + topic),
+        str(uuid.uuid1()),
+        datetime.datetime.utcnow().isoformat(),
+        user,
+        json.dumps(data)
+    ]
+    # Send the message in the non-blockng mode.
+    # If the consumer (lava-publisher) is not active, the message will be lost.
+    socket.send_multipart(msg, zmq.DONTWAIT)
 
 
 def device_init_handler(sender, **kwargs):
@@ -33,17 +58,6 @@ def device_post_handler(sender, **kwargs):
         # Even if an object is saved many time, we will send messages only when
         # the status change.
         instance._old_status = instance.status
-        # Get back the thread local storage
-        try:
-            context = thread_local.context
-            socket = thread_local.socket
-            user = thread_local.user
-        except AttributeError:
-            # Create the context and socket
-            thread_local.context = context = zmq.Context.instance()
-            thread_local.socket = socket = context.socket(zmq.PUSH)
-            thread_local.user = user = pwd.getpwuid(os.geteuid()).pw_name
-            socket.connect(settings.INTERNAL_EVENT_SOCKET)
 
         # Create the message
         data = {
@@ -54,18 +68,48 @@ def device_post_handler(sender, **kwargs):
         if instance.current_job:
             data["job"] = instance.current_job.display_id
 
-        # The format is [topic, uuid, datetime, username, data as json]
-        msg = [
-            b(settings.EVENT_TOPIC + '.device'),
-            str(uuid.uuid1()),
-            datetime.datetime.utcnow().isoformat(),
-            user,
-            json.dumps(data)
-        ]
-        # Send the message in the non-blockng mode.
-        # If the consumer (lava-publisher) is not active, the message will be lost.
-        socket.send_multipart(msg, zmq.DONTWAIT)
+        # Send the event
+        send_event(".device", data)
+
+
+def testjob_init_handler(sender, **kwargs):
+    # This function is called for every testJob object created
+    # Save the old status
+    instance = kwargs["instance"]
+    instance._old_status = instance.status
+
+
+def testjob_post_handler(sender, **kwargs):
+    # Called only when a Device is saved into the database
+    instance = kwargs["instance"]
+
+    # Send a signal if the status changed or this is a new TestJob
+    if instance.status != instance._old_status or kwargs["created"]:
+        # Update the status as some objects are save many times.
+        # Even if an object is saved many time, we will send messages only when
+        # the status change.
+        instance._old_status = instance.status
+
+        # Create the message
+        data = {
+            "status": instance.STATUS_CHOICES[instance.status][1],
+            "job": instance.display_id,
+            "device_type": instance.requested_device_type.name,
+            "priority": instance.priority,
+            "submit_time": instance.submit_time.isoformat(),
+        }
+        if instance.start_time:
+            data["start_time"] = instance.start_time.isoformat()
+        if instance.end_time:
+            data["end_time"] = instance.start_time.isoformat()
+        if instance.actual_device:
+            data["device"] = instance.actual_device.hostname
+
+        # Send the event
+        send_event(".testjob", data)
 
 
 post_init.connect(device_init_handler, sender=Device, weak=False, dispatch_uid="device_init_handler")
 post_save.connect(device_post_handler, sender=Device, weak=False, dispatch_uid="device_post_handler")
+post_init.connect(testjob_init_handler, sender=TestJob, weak=False, dispatch_uid="testjob_init_handler")
+post_save.connect(testjob_post_handler, sender=TestJob, weak=False, dispatch_uid="testjob_post_handler")
