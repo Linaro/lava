e360a07ab80a06a7386cf758f701aee09538b419
Rémi Duraffort <remi.duraffort@linaro.org>
2016-06-06 15:26:54 +0200

    Events: add a monitoring thread
    When running lava-publisher in DEBUG, the monitoring thread is printing all
    events send on the socket.
    
    Change-Id: Id83385a9fe824c549fd08b8263683787ec3c416a

--- lava_scheduler_app/management/commands/lava-publisher.py
+++ lava_scheduler_app/management/commands/lava-publisher.py
@@ -19,11 +19,31 @@
 # with this program; if not, see <http://www.gnu.org/licenses>.
 
 import logging
+import threading
 import zmq
 
 from django.conf import settings
 
 from lava_server.utils import OptArgBaseCommand as BaseCommand
+
+
+class Monitor(threading.Thread):
+    def __init__(self, stop):
+        super(Monitor, self).__init__()
+        self.stop = stop
+
+    def run(self):
+        logger = logging.getLogger('publisher')
+
+        context = zmq.Context.instance()
+        socket = context.socket(zmq.PULL)
+        socket.connect('inproc:///monitor')
+
+        logger.debug("Waiting for messages on the proxy")
+        while not self.stop.is_set():
+            # TODO: add a timeout
+            data = socket.recv_multipart()
+            logger.debug(data)
 
 
 class Command(BaseCommand):
@@ -58,8 +78,26 @@ class Command(BaseCommand):
         pub = context.socket(zmq.PUB)
         pub.bind(settings.EVENT_SOCKET)
 
+        # Create the monitoring thread only in DEBUG
+        monitor_in = None
+        monitor = None
+        if options['level'] == 'DEBUG':
+            monitor_in = context.socket(zmq.PUSH)
+            monitor_in.bind('inproc:///monitor')
+
+            self.logger.debug("Starting the monitor")
+            stop_monitor = threading.Event()
+            monitor = Monitor(stop_monitor)
+            monitor.start()
+
         self.logger.info("Starting the Proxy")
         try:
-            zmq.proxy(pull, pub)
+            zmq.proxy(pull, pub, monitor_in)
         except KeyboardInterrupt:
             self.logger.info("Received Ctrl+C, leaving")
+            if monitor_in is not None:
+                # Stop the monitor thread
+                stop_monitor.set()
+                # and send a message to unlock the socket
+                monitor_in.send('Finishing the monitor')
+                monitor.join()
