d31a8462c2cd41ef7b1a5a33fa3f8eed9c6dc9c3
Rémi Duraffort <remi.duraffort@linaro.org>
2016-10-12 17:25:42 +0200

    master: set the failure_comment
    When the slave returns an error, the master fills the failure comment with the
    backtrace. This is better than nothing.
    
    Change-Id: I31697d6e7414d296ba4a9caedd89231a2f5ebe5e

--- lava_scheduler_app/management/commands/dispatcher-master.py
+++ lava_scheduler_app/management/commands/dispatcher-master.py
@@ -310,13 +310,14 @@ class Command(BaseCommand):
                 status = TestJob.INCOMPLETE
                 self.logger.info("[%d] %s => END with error %d", job_id, hostname, job_status)
                 self.logger.error("[%d] Error: %s", job_id, error_msg)
+                # Set the failure comment
+                TestJob.objects.filter(id=job_id).update(failure_comment=error_msg)
             else:
                 status = TestJob.COMPLETE
                 self.logger.info("[%d] %s => END", job_id, hostname)
             try:
                 with transaction.atomic():
-                    job = TestJob.objects.select_for_update() \
-                                         .get(id=job_id)
+                    job = TestJob.objects.select_for_update().get(id=job_id)
                     if job.status == TestJob.CANCELING:
                         cancel_job(job)
                     else:
