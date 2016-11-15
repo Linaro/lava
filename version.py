01a482f61f6cdfedc5396f6e9264f9ea42ce67cf
Senthil Kumaran S <senthil.kumaran@linaro.org>
2016-11-15 22:45:06 +0530

    Fix for Debian reproducible builds.
    See https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=844102
    
    Patch by: Chris Lamb <lamby@debian.org>
    
    Change-Id: I073caf0e94dd4fad45f20fa1542114be48f63769

--- version.py
+++ version.py
@@ -39,6 +39,46 @@ def version_tag():
     from that.
     :return: a version string based on the tag and short hash
     """
+    tag_name = None
+    if os.path.exists("./.git/"):
+        tag_list = [
+            'git', 'for-each-ref', '--sort=taggerdate', '--format',
+            "'%(refname)'", 'refs/tags',
+        ]
+        hash_list = ['git', 'log', '-n', '1']
+        tag_hash_list = ['git', 'rev-list']
+        clone_data = subprocess.check_output(hash_list).strip().decode('utf-8')
+        commits = clone_data.split('\n')
+        clone_hash = commits[0].replace('commit ', '')[:8]
+        tag_data = subprocess.check_output(tag_list).strip().decode('utf-8')
+        tags = tag_data.split('\n')
+        if len(tags) < 2:
+            return clone_hash
+        tag_line = str(tags[len(tags) - 1]).replace('\'', '').strip()
+        tag_name = tag_line.split("/")[2]
+        tag_hash_list.append(tag_name)
+        tag_hash = subprocess.check_output(tag_hash_list).strip().decode('utf-8')
+        tags = tag_hash.split('\n')
+        tag_hash = tags[0][:8]
+        if tag_hash == clone_hash:
+            return tag_name
+        else:
+            # tag, month end and release are now out of sync.
+            # use the rev-list count to always ensure that we are building
+            # a newer version to cope with date changes at month end.
+            # use short git hash for reference.
+            dev_stamp = ['git', 'rev-list', '--count', 'HEAD']
+            dev_count = subprocess.check_output(dev_stamp).strip().decode('utf-8')
+            dev_short = ['git', 'rev-parse', '--short', 'HEAD']
+            dev_hash = subprocess.check_output(dev_short).strip().decode('utf-8')
+            return "%s+%s.%s" % (tag_name, dev_count, dev_hash)
+    if not tag_name and os.path.exists('debian/changelog'):
+        deb_version = subprocess.check_output(('dpkg-parsechangelog',
+                                               '--show-field',
+                                               'Version')).strip().decode(
+                                                   'utf-8')
+        # example version returned would be '2016.11'
+        return deb_version.split('-')[0]
     if not os.path.exists("./.git/"):
         base = os.path.basename(os.getcwd())
         name_list = ['grep', 'name=', 'setup.py']
@@ -46,37 +86,6 @@ def version_tag():
         name_data = name_data.replace("name=\'", '')
         name_data = name_data.replace("\',", '')
         return base.replace("%s-" % name_data, '')
-    tag_list = [
-        'git', 'for-each-ref', '--sort=taggerdate', '--format',
-        "'%(refname)'", 'refs/tags',
-    ]
-    hash_list = ['git', 'log', '-n', '1']
-    tag_hash_list = ['git', 'rev-list']
-    clone_data = subprocess.check_output(hash_list).strip().decode('utf-8')
-    commits = clone_data.split('\n')
-    clone_hash = commits[0].replace('commit ', '')[:8]
-    tag_data = subprocess.check_output(tag_list).strip().decode('utf-8')
-    tags = tag_data.split('\n')
-    if len(tags) < 2:
-        return clone_hash
-    tag_line = str(tags[len(tags) - 1]).replace('\'', '').strip()
-    tag_name = tag_line.split("/")[2]
-    tag_hash_list.append(tag_name)
-    tag_hash = subprocess.check_output(tag_hash_list).strip().decode('utf-8')
-    tags = tag_hash.split('\n')
-    tag_hash = tags[0][:8]
-    if tag_hash == clone_hash:
-        return tag_name
-    else:
-        # tag, month end and release are now out of sync.
-        # use the rev-list count to always ensure that we are building
-        # a newer version to cope with date changes at month end.
-        # use short git hash for reference.
-        dev_stamp = ['git', 'rev-list', '--count', 'HEAD']
-        dev_count = subprocess.check_output(dev_stamp).strip().decode('utf-8')
-        dev_short = ['git', 'rev-parse', '--short', 'HEAD']
-        dev_hash = subprocess.check_output(dev_short).strip().decode('utf-8')
-        return "%s+%s.%s" % (tag_name, dev_count, dev_hash)
 
 
 def main():
