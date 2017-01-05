2cc64b70ab9c2e26728c0892a169922441a399b5
Neil Williams <neil.williams@linaro.org>
2017-01-05 16:04:24 +0000

    Reinstate git version_tag in version.py
    version.py cannot call into any of the lava_dispatcher module code
    without having all of the imports which would mean adding a lot
    more packages to the build dependencies for no benefit.
    Remove version_tag from filesystem utils and drop the redundant
    unit test
    
    Change-Id: I34edf089cd64b665f53c8dbdbf399a49e9529403

--- version.py
+++ version.py
@@ -25,9 +25,53 @@
 
 import os
 import subprocess
-from lava_dispatcher.pipeline.utils.filesystem import version_tag
 
 # pylint: disable=superfluous-parens,too-many-locals
+
+
+def version_tag():
+    """
+    Parses the git status to determine if this is a git tag
+    or a developer commit and builds a version string combining
+    the two. If there is no git directory, relies on this being
+    a directory created from the tarball created by setup.py when
+    it uses this script and retrieves the original version string
+    from that.
+    :return: a version string based on the tag and short hash
+    """
+    tag_name = None
+    if os.path.exists("./.git/"):
+        tag_list = [
+            'git', 'for-each-ref', '--sort=taggerdate', '--format',
+            "'%(refname)'", 'refs/tags',
+        ]
+        hash_list = ['git', 'log', '-n', '1']
+        tag_hash_list = ['git', 'rev-list']
+        clone_data = subprocess.check_output(hash_list).strip().decode('utf-8')
+        commits = clone_data.split('\n')
+        clone_hash = commits[0].replace('commit ', '')[:8]
+        tag_data = subprocess.check_output(tag_list).strip().decode('utf-8')
+        tags = tag_data.split('\n')
+        if len(tags) < 2:
+            return clone_hash
+        tag_line = str(tags[len(tags) - 1]).replace('\'', '').strip()
+        tag_name = tag_line.split("/")[2]
+        tag_hash_list.append(tag_name)
+        tag_hash = subprocess.check_output(tag_hash_list).strip().decode('utf-8')
+        tags = tag_hash.split('\n')
+        tag_hash = tags[0][:8]
+        if tag_hash == clone_hash:
+            return tag_name
+        else:
+            # tag, month end and release are now out of sync.
+            # use the rev-list count to always ensure that we are building
+            # a newer version to cope with date changes at month end.
+            # use short git hash for reference.
+            dev_stamp = ['git', 'rev-list', '--count', 'HEAD']
+            dev_count = subprocess.check_output(dev_stamp).strip().decode('utf-8')
+            dev_short = ['git', 'rev-parse', '--short', 'HEAD']
+            dev_hash = subprocess.check_output(dev_short).strip().decode('utf-8')
+            return "%s+%s.%s" % (tag_name, dev_count, dev_hash)
 
 
 def local_debian_package_version():
