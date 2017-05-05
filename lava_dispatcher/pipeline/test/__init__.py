477a36b7bf3737864be4ae0ebca8b6d0c647bd52
Rémi Duraffort <remi.duraffort@linaro.org>
2017-05-05 10:13:01 +0200

    tests: make ShellCommand.logger a DummyLogger
    Fix crashes when some ressources are not available during tests.
    
    Change-Id: I00fbcf9fd777bbdb32b7073b0d4a3f973c05634b

--- lava_dispatcher/pipeline/test/__init__.py
+++ lava_dispatcher/pipeline/test/__init__.py
@@ -1,2 +1,14 @@
 import logging
+from lava_dispatcher.pipeline.test.utils import DummyLogger
+from lava_dispatcher.pipeline.shell import ShellCommand
+
+
 logging.getLogger("requests").setLevel(logging.WARNING)
+
+
+def shellcommand_dummy_logger_init(self, command, lava_timeout, logger=None, cwd=None):
+    self.__old_init__(command, lava_timeout, DummyLogger(), cwd)
+
+
+ShellCommand.__old_init__ = ShellCommand.__init__
+ShellCommand.__init__ = shellcommand_dummy_logger_init
