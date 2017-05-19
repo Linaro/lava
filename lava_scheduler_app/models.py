9253497ee797bc07d20a2d10143c1ba7a635f6da
Rémi Duraffort <remi.duraffort@linaro.org>
2017-05-19 16:40:09 +0200

    Only the master should create log directories
    Change-Id: I59ac1ad22ebcb6a2bf6b798c099b4f91d91b1d6e

--- lava_scheduler_app/models.py
+++ lava_scheduler_app/models.py
@@ -1591,17 +1591,16 @@ class TestJob(RestrictedResource):
 
     @property
     def output_dir(self):
-        old_path = os.path.join(settings.MEDIA_ROOT, 'job-output', 'job-%s' % self.id)
-        date_path = os.path.join(settings.MEDIA_ROOT, 'job-output',
-                                 "%02d" % self.submit_time.year,
-                                 "%02d" % self.submit_time.month,
-                                 "%02d" % self.submit_time.day,
-                                 str(self.id))
-        if not os.path.exists(old_path) and not os.path.exists(date_path):
-            utils.mkdir(date_path)
-        if os.path.exists(date_path):
-            return date_path
-        return old_path
+        # Fallback to the old path if it does exist
+        old_path = os.path.join(settings.MEDIA_ROOT, 'job-output',
+                                'job-%s' % self.id)
+        if os.path.exists(old_path):
+            return old_path
+        return os.path.join(settings.MEDIA_ROOT, 'job-output',
+                            "%02d" % self.submit_time.year,
+                            "%02d" % self.submit_time.month,
+                            "%02d" % self.submit_time.day,
+                            str(self.id))
 
     def output_file(self):
         filename = 'output.yaml' if self.is_pipeline else 'output.txt'
