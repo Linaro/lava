f78c58811a2a3455d926a8beb143b31f90b45b31
Luca Di Stefano <luca.distefano@linaro.org>
2017-06-14 17:57:02 +0100

    Added device type for Cavium ThunderX
    Add a unit test to ensure that both VLANd support and the
    boot character delay settings are maintained.
    
    Change-Id: Ia44f67df25e2ff47eaf2deca0982a1503d747863

--- lava_scheduler_app/tests/test_templates.py
+++ lava_scheduler_app/tests/test_templates.py
@@ -255,6 +255,34 @@ class TestTemplates(unittest.TestCase):
                 self.assertIn('dhcp net1', value['commands'])
                 self.assertNotIn('dhcp net0', value['commands'])
 
+    def test_thunderx_template(self):
+        data = """{% extends 'thunderx.jinja2' %}
+{% set map = {'iface0': {'lngswitch03': 13}, 'iface1': {'lngswitch03': 1}, 'iface2': {'lngswitch02': 9}, 'iface3': {'lngswitch02': 10}} %}
+{% set tags = {'iface0': [], 'iface1': ['RJ45', '1G', '10G'], 'iface2': ['SFP+', '1G', '10G'], 'iface3': ['SFP+', '1G', '10G']} %}
+{% set mac_addr = {'iface0': '00:00:1a:1b:8b:f6', 'iface1': '00:00:1a:1b:8b:f7', 'iface2': '00:11:0a:68:94:30', 'iface3': '00:11:0a:68:94:31'} %}
+{% set interfaces = ['iface0', 'iface1', 'iface2', 'iface3'] %}
+{% set sysfs = {'iface0': '/sys/devices/platform/AMDI8001:00/net/',
+'iface1': '/sys/devices/platform/AMDI8001:01/net/',
+'iface2': '/sys/devices/pci0000:00/0000:00:02.1/0000:01:00.0/net/',
+'iface3': '/sys/devices/pci0000:00/0000:00:02.1/0000:01:00.1/net/'} %}
+{% set hard_reset_command = '/usr/bin/pduclient --daemon localhost --hostname lngpdu01 --command reboot --port 19' %}
+{% set power_off_command = '/usr/bin/pduclient --daemon localhost --hostname lngpdu01 --command off --port 19' %}
+{% set power_on_command = '/usr/bin/pduclient --daemon localhost --hostname lngpdu01 --command on --port 19' %}
+{% set connection_command = 'telnet localhost 7333' %}
+{% set exclusive = 'True' %}"""
+        self.assertTrue(self.validate_data('staging-thunderx-01', data))
+        test_template = prepare_jinja_template('staging-thunderx-01', data)
+        rendered = test_template.render()
+        template_dict = yaml.load(rendered)
+        self.assertIn('character_delays', template_dict)
+        self.assertIn('boot', template_dict['character_delays'])
+        self.assertEqual(150, template_dict['character_delays']['boot'])
+        self.assertIn('interfaces', template_dict['parameters'])
+        self.assertIn('iface2', template_dict['parameters']['interfaces'])
+        self.assertIn('iface1', template_dict['parameters']['interfaces'])
+        self.assertIn('iface0', template_dict['parameters']['interfaces'])
+        self.assertIn('sysfs', template_dict['parameters']['interfaces']['iface2'])
+
     def test_beaglebone_black_template(self):
         data = """{% extends 'beaglebone-black.jinja2' %}
 {% set map = {'eth0': {'lngswitch03': 19}, 'eth1': {'lngswitch03': 8}} %}
