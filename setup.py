5eea8b612fdfde8fa700f625a581634831744883
Neil Williams <neil.williams@linaro.org>
2017-09-28 15:46:00 +0100

    Add missing requirement for magic binding
    Allows packaging to determine dependency on python-magic
    and python3-magic.
    
    Change-Id: Idd679f133c64fca927be02c7fe18c664a99585bc

--- setup.py
+++ setup.py
@@ -105,6 +105,7 @@ setup(
         'configobj',
         'pyudev >= 0.21',
         'pytz',
+        'file-magic',
         'setproctitle >= 1.1.8'
     ],
     test_suite='lava_dispatcher.tests.test_suite',
