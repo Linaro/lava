f7a1c97673a4b5ec2c70ced6b47a5c5dafd32ea2
Rémi Duraffort <remi.duraffort@linaro.org>
2017-10-20 11:30:45 +0200

    docker: allow to pass extra options
    Change-Id: I4a54ea5a289e57b724a62b3222be0ffc80b2f4d9

--- lava_scheduler_app/tests/test_templates.py
+++ lava_scheduler_app/tests/test_templates.py
@@ -1182,4 +1182,19 @@ class TestTemplates(unittest.TestCase):
         template_dict = yaml.load(rendered)
         self.assertEqual('docker', template_dict['device_type'])
         self.assertEqual({'docker': None}, template_dict['actions']['deploy']['methods'])
-        self.assertEqual({'docker': None}, template_dict['actions']['boot']['methods'])
+        self.assertEqual({'docker': {'options': {'cpus': 0.0, 'memory': 0, 'volumes': []}}},
+                         template_dict['actions']['boot']['methods'])
+
+        data = """{% extends 'docker.jinja2' %}
+{% set docker_cpus=2.1 %}
+{% set docker_memory="120M" %}
+{% set docker_volumes=["/home", "/tmp"] %}"""
+        self.assertTrue(self.validate_data('docker-01', data))
+        test_template = prepare_jinja_template('docker-01', data)
+        rendered = test_template.render()
+        template_dict = yaml.load(rendered)
+        self.assertEqual('docker', template_dict['device_type'])
+        self.assertEqual({'docker': None}, template_dict['actions']['deploy']['methods'])
+        self.assertEqual({'docker': {'options': {'cpus': 2.1, 'memory': "120M",
+                                                 'volumes': ["/home", "/tmp"]}}},
+                         template_dict['actions']['boot']['methods'])
