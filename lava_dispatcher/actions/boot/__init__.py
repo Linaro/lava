b5ebe130e51e505d1eb24558cc82e47979a24cfd
Stevan Radakovic <stevan.radakovic@linaro.org>
2017-11-22 17:47:53 +0100

    LAVA-1127 - Extend bootloader reset messages.
    Add one alternative bootloader reset message for U-boot:
    "Must RESET board to recover".
    
    Change-Id: I79d754a4a813063e3ee7f242361d6cbac0951fbb

--- lava_dispatcher/actions/boot/__init__.py
+++ lava_dispatcher/actions/boot/__init__.py
@@ -601,13 +601,14 @@ class BootloaderCommandsAction(Action):
         if self.parameters.get('prompts', None):
             connection.prompt_str = [
                 self.params.get('boot_message',
-                                self.job.device.get_constant('boot-message')),
-                self.job.device.get_constant('cpu-reset-message')
-            ]
+                                self.job.device.get_constant('boot-message'))]
+            connection.prompt_str.extend(
+                self.job.device.get_constant('cpu-reset-messages'))
+
             self.logger.debug("Changing prompt to boot_message %s",
                               connection.prompt_str)
             index = self.wait(connection)
-            if connection.prompt_str[index] == self.job.device.get_constant('cpu-reset-message'):
+            if connection.prompt_str[index] in self.job.device.get_constant('cpu-reset-messages'):
                 self.logger.error("Bootloader reset detected: Bootloader "
                                   "failed to load the required file into "
                                   "memory correctly so the bootloader reset "
