c351c661eb24f366b4de599da982f8dd8edd4494
Senthil Kumaran S <senthil.kumaran@linaro.org>
2018-04-16 15:44:15 +0530

    Remove unused constant OVERRIDE_CLAMP_DURATION
    Change-Id: Id141e97a67df2975ea12f90ff30a0069ed60fb7f

--- lava_dispatcher/utils/constants.py
+++ lava_dispatcher/utils/constants.py
@@ -41,9 +41,6 @@ HTTP_DOWNLOAD_CHUNK_SIZE = 32768
 # Size of the chunks when downloading over scp
 SCP_DOWNLOAD_CHUNK_SIZE = 32768
 
-# Clamp on the maximum timeout allowed for overrides
-OVERRIDE_CLAMP_DURATION = 300
-
 # dispatcher temporary directory
 # This is distinct from the TFTP daemon directory
 # Files here are for download using the Apache /tmp alias.
