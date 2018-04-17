3f7e95f8130ac7be358a0580ca8145c8aba63e3c
Senthil Kumaran S <senthil.kumaran@linaro.org>
2018-04-17 09:39:47 +0530

    Add necessary imports.
    The race condition in _save of FileSystemStorage was fixed via
    https://code.djangoproject.com/ticket/21602 hence it is redundant to
    re-implement it here.
    
    Change-Id: I7cdae9314ce019e18cfaaef16e53b5fef0770587

--- dashboard_app/models.py
+++ dashboard_app/models.py
@@ -24,8 +24,8 @@ from __future__ import unicode_literals
 
 import gzip
 
+from django.core.files import File
 from django.core.files.storage import FileSystemStorage
-from django.db import models
 
 
 class GzipFileSystemStorage(FileSystemStorage):
@@ -35,50 +35,3 @@ class GzipFileSystemStorage(FileSystemStorage):
         gzip_file = gzip.GzipFile(full_path, mode)
         gzip_file.name = full_path
         return File(gzip_file)
-
-    # This is a copy-paste-hack of FileSystemStorage._save
-    def _save(self, name, content):
-        full_path = self.path(name)
-
-        directory = os.path.dirname(full_path)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        elif not os.path.isdir(directory):
-            raise IOError("%s exists and is not a directory." % directory)
-
-        # There's a potential race condition between get_available_name and
-        # saving the file; it's possible that two threads might return the
-        # same name, at which point all sorts of fun happens. So we need to
-        # try to create the file, but if it already exists we have to go back
-        # to get_available_name() and try again.
-
-        while True:
-            try:
-                # This fun binary flag incantation makes os.open throw an
-                # OSError if the file already exists before we open it.
-                fd = os.open(full_path, os.O_WRONLY | os.O_CREAT | os.O_EXCL | getattr(os, 'O_BINARY', 0))
-                # This line, and the use of gz_file.write below, are the
-                # changes from the original version of this.
-                gz_file = gzip.GzipFile(fileobj=os.fdopen(fd, 'wb'))
-                try:
-                    locks.lock(fd, locks.LOCK_EX)
-                    for chunk in content.chunks():
-                        gz_file.write(chunk)
-                finally:
-                    locks.unlock(fd)
-                    gz_file.close()
-            except OSError as e:
-                if e.errno == errno.EEXIST:
-                    # Ooops, the file exists. We need a new file name.
-                    name = self.get_available_name(name)
-                    full_path = self.path(name)
-                else:
-                    raise
-            else:
-                # OK, the file save worked. Break out of the loop.
-                break
-
-        if settings.FILE_UPLOAD_PERMISSIONS is not None:
-            os.chmod(full_path, settings.FILE_UPLOAD_PERMISSIONS)
-
-        return name
