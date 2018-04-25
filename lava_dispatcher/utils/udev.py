534f1a9b83a459cb12cadf370fef289889aa5a4a
Neil Williams <neil.williams@linaro.org>
2018-04-25 11:19:26 +0100

    LAVA-1300 - fail if static devices cannot be found
    If a static device is defined but udev is unable to find it when
    attempting to add it to the LXC, fail the test job as an
    infrastructure error. A static device is required to exist for
    udev once defined for a device.
    
    Change-Id: Idc40a122201c47d08a70a8a306f64a5954b6b8ab

--- lava_dispatcher/utils/udev.py
+++ lava_dispatcher/utils/udev.py
@@ -20,8 +20,11 @@
 
 import pyudev
 import time
-
-from lava_dispatcher.action import Action, LAVABug
+from lava_dispatcher.action import (
+    Action,
+    LAVABug,
+    InfrastructureError,
+)
 
 
 class WaitUSBSerialDeviceAction(Action):
@@ -227,6 +230,7 @@ def get_udev_devices(job=None, logger=None, device_info=None):
         devices = device_info
     if not devices:
         return []
+    added = set()
     for usb_device in devices:
         board_id = str(usb_device.get('board_id', ''))
         usb_vendor_id = str(usb_device.get('usb_vendor_id', ''))
@@ -244,8 +248,10 @@ def get_udev_devices(job=None, logger=None, device_info=None):
                     for child in device.children:
                         if child.device_node:
                             device_paths.add(child.device_node)
+                            added.add(board_id)
                     for link in device.device_links:
                         device_paths.add(link)
+                        added.add(board_id)
             elif board_id and usb_vendor_id and not usb_product_id:
                 # try with parameters such as board id, usb_vendor_id
                 if (device.get('ID_SERIAL_SHORT') == board_id) \
@@ -254,8 +260,10 @@ def get_udev_devices(job=None, logger=None, device_info=None):
                     for child in device.children:
                         if child.device_node:
                             device_paths.add(child.device_node)
+                            added.add(board_id)
                     for link in device.device_links:
                         device_paths.add(link)
+                        added.add(board_id)
             elif board_id and not usb_vendor_id and not usb_product_id:
                 # try with board id alone
                 if device.get('ID_SERIAL_SHORT') == board_id:
@@ -263,8 +271,10 @@ def get_udev_devices(job=None, logger=None, device_info=None):
                     for child in device.children:
                         if child.device_node:
                             device_paths.add(child.device_node)
+                            added.add(board_id)
                     for link in device.device_links:
                         device_paths.add(link)
+                        added.add(board_id)
             elif usb_fs_label:
                 # Just restrict by filesystem label.
                 if device.get('ID_FS_LABEL') == usb_fs_label:
@@ -272,8 +282,15 @@ def get_udev_devices(job=None, logger=None, device_info=None):
                     for child in device.children:
                         if child.device_node:
                             device_paths.add(child.device_node)
+                            added.add(usb_fs_label)
                     for link in device.device_links:
                         device_paths.add(link)
+                        added.add(usb_fs_label)
+    if device_info:
+        for static_device in device_info:
+            for _, value in static_device.items():
+                if value not in added:
+                    raise InfrastructureError("Unable to add all static devices: board_id '%s' was not found" % value)
     if logger and device_paths:
         logger.debug("Adding %s", ', '.join(device_paths))
     return list(device_paths)
