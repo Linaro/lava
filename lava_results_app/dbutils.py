Linaro/lava

commit 0d1e0a0705e03916694aab9e516e36649215be53

Fix test for extra results metadata in callback data
The callback results data includes some metadata which in turn
includes some arbitrary and optional extra data parsed from a YAML
file generated by the test job.  The string with the path to this
optional YAML can be either a `str' or a `unicode' object, and due to
other changes was a `str' in 2017.12 but is now a `unicode' in 2018.2.
So the extra data is not loaded from the YAML file any more; instead
the path to the file is left in the callback data.

Fix this by testing if the extra data path is a `basestring' instance
which is a parent type for both `str' and `unicode'.  Also remove the
redundant check if extra_data is empty since None is not a basestring,
and a file path with an empty string does not exist.

Change-Id: Ia06929aa214584f515d11870fa391cf4db8e770f
Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

lava_results_app/dbutils.py
@@ -431,7 +431,7 @@ def export_testcase(testcase, with_buglinks=False):
     metadata = dict(testcase.action_metadata) if testcase.action_metadata else {}
     extra_source = []
     extra_data = metadata.get('extra', None)
-    if extra_data and isinstance(extra_data, str) and os.path.exists(extra_data):
+    if isinstance(extra_data, basestring) and os.path.exists(extra_data):
         with open(metadata['extra'], 'r') as extra_file:
             items = yaml.load(extra_file, Loader=yaml.CLoader)
         # hide the !!python OrderedDict prefix from the output.

commit 1cf21c2ed6b8aec24b328492ee14d8a6ef1db0be

Remove unnecessary Python 2 imports
In Python 3, all the strings are of the `str' type.  Python 2.x used
to have `basestring' but as this code base now only supports Python 3,
there is no need to be compatible with Python 2.x.  Remove the
`basestring' reference to `str' to reflect this.

Also remove the Python 2.x specific import of `quote' for similar
reasons.

Change-Id: I8b6f329e40bcd57ed82360caed8a25a2af190909
Fixes: 0d1e0a0705e0 ("Fix test for extra results metadata in callback data")
Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

lava_results_app/dbutils.py
@@ -25,6 +25,7 @@ import yaml
 import sys
 import logging
 import decimal
+from urllib.parse import quote
 
 from collections import OrderedDict  # pylint: disable=unused-import
 from lava_results_app.models import (
@@ -38,14 +39,6 @@ from lava_results_app.models import (
 from lava_results_app.utils import debian_package_version
 from django.core.exceptions import MultipleObjectsReturned
 from lava_common.timeout import Timeout
-
-if sys.version_info[0] == 2:
-    # Python 2.x
-    from urllib import quote
-elif sys.version_info[0] == 3:
-    # For Python 3.0 and later
-    from urllib.parse import quote
-    basestring = str
 
 
 def _check_for_testset(result_dict, suite):
@@ -446,7 +439,7 @@ def export_testcase(testcase, with_buglinks=False):
     metadata = dict(testcase.action_metadata) if testcase.action_metadata else {}
     extra_source = []
     extra_data = metadata.get('extra', None)
-    if isinstance(extra_data, basestring) and os.path.exists(extra_data):
+    if isinstance(extra_data, str) and os.path.exists(extra_data):
         with open(metadata['extra'], 'r') as extra_file:
             items = yaml.load(extra_file, Loader=yaml.CLoader)
         # hide the !!python OrderedDict prefix from the output.