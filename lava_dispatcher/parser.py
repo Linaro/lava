e11c8c9f9f220e6d64b6499b9ba0474956fbce94
Rémi Duraffort <remi.duraffort@linaro.org>
2018-06-20 15:32:40 +0200

    Fix job timeout name
    Set the name of the timeout to "job" to be consistent with the action timeouts.
    
    Change-Id: I1dab18db0208ecf4157522bcdc065f502e619f6a

--- lava_dispatcher/parser.py
+++ lava_dispatcher/parser.py
@@ -110,10 +110,9 @@ class JobParser(object):
         return mapping
 
     def _timeouts(self, data, job):
-        if data.get('timeouts', None) is not None:
-            if 'job' in data['timeouts']:
-                duration = Timeout.parse(data['timeouts']['job'])
-                job.timeout = Timeout(data['job_name'], duration)
+        if 'job' in data.get('timeouts', {}):
+            duration = Timeout.parse(data['timeouts']['job'])
+            job.timeout = Timeout('job', duration)
 
     # pylint: disable=too-many-locals,too-many-statements
     def parse(self, content, device, job_id, logger, dispatcher_config,
