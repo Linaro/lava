087d8ad739af92d945f5a9d395032536d7c1838b
Rémi Duraffort <remi.duraffort@linaro.org>
2018-06-21 13:27:11 +0200

    LAVA-1359 Fix lava-publisher shutdown
    When receiving a signal, unsubscribe from the event socket, forward all
    remaining messages and then close the sockets.
    
    Change-Id: I7a099dcdc03d989bb9680278b00d8b7b2723a2ab

--- lava_server/management/commands/lava-publisher.py
+++ lava_server/management/commands/lava-publisher.py
@@ -18,13 +18,16 @@
 # along
 # with this program; if not, see <http://www.gnu.org/licenses>.
 
+import os
 import zmq
+from zmq.utils.strtypes import u
 
 from django.conf import settings
 
 from lava_server.cmdutils import LAVADaemonCommand
 
 
+TIMEOUT = 5
 FORMAT = "%(asctime)-15s %(levelname)7s %(message)s"
 
 
@@ -32,68 +35,96 @@ class Command(LAVADaemonCommand):
     help = "LAVA event publisher"
     default_logfile = "/var/log/lava-server/lava-publisher.log"
 
+    def forward_event(self, leaving):
+        try:
+            sockets = dict(self.poller.poll(TIMEOUT * 1000 if leaving else None))
+        except zmq.error.ZMQError as exc:
+            self.logger.error("[POLL] Received a ZMQ error: %s", exc)
+            return True
+
+        if sockets.get(self.pipe_r) == zmq.POLLIN:
+            # remove the message from the queue
+            os.read(self.pipe_r, 1)
+            if leaving:
+                self.logger.warning("[POLL] signal already handled, please wait for the process to exit")
+                return True
+            else:
+                self.logger.info("[POLL] received a signal, leaving")
+                return False
+
+        if sockets.get(self.pull) == zmq.POLLIN:
+            msg = self.pull.recv_multipart()
+            self.logger.debug("[POLL] Forwarding: %s", msg)
+            self.pub.send_multipart(msg)
+            for (i, sock) in enumerate(self.additional_sockets):
+                # Send in non blocking mode and print an error message if
+                # the HWM is reached (because the receiver does not grab
+                # the messages fast enough).
+                try:
+                    sock.send_multipart(msg, flags=zmq.DONTWAIT)
+                except zmq.error.Again:
+                    self.logger.warning("[POLL] Fail to forward to socket %d", i)
+        return not leaving
+
     def handle(self, *args, **options):
         self.setup_logging("lava-publisher", options["level"],
                            options["log_file"], FORMAT)
 
-        self.logger.info("Dropping privileges")
+        self.logger.info("[INIT] Dropping privileges")
         if not self.drop_privileges(options['user'], options['group']):
-            self.logger.error("Unable to drop privileges")
+            self.logger.error("[INIT] Unable to drop privileges")
             return
 
         if not settings.EVENT_NOTIFICATION:
-            self.logger.error("'EVENT_NOTIFICATION' is set to False, "
+            self.logger.error("[INIT] 'EVENT_NOTIFICATION' is set to False, "
                               "LAVA won't generated any events")
 
-        self.logger.info("Creating the input socket at %s",
+        self.logger.info("[INIT] Creating the input socket at %s",
                          settings.INTERNAL_EVENT_SOCKET)
         context = zmq.Context.instance()
-        pull = context.socket(zmq.PULL)
-        pull.bind(settings.INTERNAL_EVENT_SOCKET)
-        poller = zmq.Poller()
-        poller.register(pull, zmq.POLLIN)
+        self.pull = context.socket(zmq.PULL)
+        self.pull.bind(settings.INTERNAL_EVENT_SOCKET)
+        self.poller = zmq.Poller()
+        self.poller.register(self.pull, zmq.POLLIN)
 
         # Translate signals into zmq messages
-        (pipe_r, _) = self.setup_zmq_signal_handler()
-        poller.register(pipe_r, zmq.POLLIN)
+        (self.pipe_r, _) = self.setup_zmq_signal_handler()
+        self.poller.register(self.pipe_r, zmq.POLLIN)
 
         # Create the default publishing socket
-        self.logger.info("Creating the publication socket at %s",
+        self.logger.info("[INIT] Creating the publication socket at %s",
                          settings.EVENT_SOCKET)
-        pub = context.socket(zmq.PUB)
-        pub.bind(settings.EVENT_SOCKET)
+        self.pub = context.socket(zmq.PUB)
+        self.pub.bind(settings.EVENT_SOCKET)
         # Create the additional PUSH sockets
-        self.logger.info("Creating the additional sockets:")
-        additional_sockets = []
+        if settings.EVENT_ADDITIONAL_SOCKETS:
+            self.logger.info("[INIT] Creating the additional sockets:")
+        self.additional_sockets = []
         for url in settings.EVENT_ADDITIONAL_SOCKETS:
-            self.logger.info(" * %s", url)
+            self.logger.info("[INIT]  * %s", url)
             sock = context.socket(zmq.PUSH)
             # Allow zmq to keep 10000 pending messages in each queue
             sock.setsockopt(zmq.SNDHWM, 10000)
             sock.connect(url)
-            additional_sockets.append(sock)
+            self.additional_sockets.append(sock)
 
-        self.logger.info("Starting the proxy")
-        while True:
-            try:
-                sockets = dict(poller.poll(None))
-            except zmq.error.ZMQError as exc:
-                self.logger.error("Received a ZMQ error: %s", exc)
-                continue
+        self.logger.info("[INIT] Starting the proxy")
+        while self.forward_event(False):
+            pass
 
-            if sockets.get(pipe_r) == zmq.POLLIN:
-                self.logger.info("Received a signal, leaving")
-                break
+        # Carefully close the logging socket as we don't want to lose messages
+        self.logger.info("[EXIT] Disconnect pull socket and process messages")
+        endpoint = u(self.pull.getsockopt(zmq.LAST_ENDPOINT))
+        self.logger.debug("[EXIT] unbinding from '%s'", endpoint)
+        self.pull.unbind(endpoint)
 
-            if sockets.get(pull) == zmq.POLLIN:
-                msg = pull.recv_multipart()
-                self.logger.debug("Forwarding: %s", msg)
-                pub.send_multipart(msg)
-                for (i, sock) in enumerate(additional_sockets):
-                    # Send in non blocking mode and print an error message if
-                    # the HWM is reached (because the receiver does not grab
-                    # the messages fast enough).
-                    try:
-                        sock.send_multipart(msg, flags=zmq.DONTWAIT)
-                    except zmq.error.Again:
-                        self.logger.warning("Fail to forward to socket %d", i)
+        while self.forward_event(True):
+            pass
+
+        # Close the sockets allowing 1s each to leave
+        self.logger.info("[EXIT] Closing the sockets: the queue is empty")
+        self.pull.close(linger=1)
+        self.pub.close(linger=1)
+        for socket in self.additional_sockets:
+            socket.close(linger=1)
+        context.term()
