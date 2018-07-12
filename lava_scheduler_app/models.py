Linaro/lava

commit 792b63ad3cd538d8afcfb266fa771cba22c0d36e

LAVA-1368 provide access to the callback data
Change-Id: Ib42499f38f1bb86a84844df0afe21d74eb731004

lava_scheduler_app/models.py
@@ -7,6 +7,7 @@ import logging
 import os
 import re
 import uuid
+import gzip
 import simplejson
 import smtplib
 import socket
@@ -45,6 +46,7 @@ from lava_scheduler_app.schema import (
     validate_submission,
     SubmissionException
 )
+
 from lava_common.exceptions import ConfigurationError
 from lava_scheduler_app import utils
 from linaro_django_xmlrpc.models import AuthToken
@@ -1844,6 +1846,56 @@ class TestJob(RestrictedResource):
         return (user.is_superuser or
                 user.has_perm('lava_scheduler_app.cancel_resubmit_testjob'))
 
+    def create_job_data(self, token=None, output=False, results=False):
+        """
+        Populates a dictionary used by the NotificationCallback
+        and by the REST API, containing data about the test job.
+        If output is True, the entire test job log file is included.
+        If results is True, an export of all test cases is included.
+        """
+        # Python3 circular dependency issues need this here.
+        from lava_results_app.dbutils import export_testcase
+
+        data = {
+            "id": self.pk,
+            "status": self.get_legacy_status(),
+            "status_string": self.get_legacy_status_display().lower(),
+            "state": self.state,
+            "state_string": self.get_state_display(),
+            "health": self.health,
+            "health_string": self.get_health_display(),
+            "submit_time": str(self.submit_time),
+            "start_time": str(self.start_time),
+            "end_time": str(self.end_time),
+            "submitter_username": self.submitter.username,
+            "failure_comment": self.failure_comment,
+            "priority": self.priority,
+            "description": self.description,
+            "actual_device_id": self.actual_device_id,
+            "definition": self.definition,
+            "metadata": self.get_metadata_dict(),
+        }
+
+        # Only add the token if it's not empty
+        if token is not None:
+            data["token"] = token
+
+        # Logs.
+        output_file = self.output_file()
+        if output_file and output:
+            data["log"] = self.output_file().read()
+
+        # Results.
+        if results:
+            data["results"] = {}
+            for test_suite in self.testsuite_set.all():
+                yaml_list = []
+                for test_case in test_suite.testcase_set.all():
+                    yaml_list.append(export_testcase(test_case))
+                data["results"][test_suite.name] = yaml.dump(yaml_list)
+
+        return data
+
     def set_failure_comment(self, message):
         if not self.failure_comment:
             self.failure_comment = message
@@ -2704,7 +2756,22 @@ class NotificationCallback(models.Model):
 
     def invoke_callback(self):
         logger = logging.getLogger('lava_scheduler_app')
-        callback_data = self.get_callback_data()
+        callback_data = None
+
+        if self.method != NotificationCallback.GET:
+            output = self.dataset in [
+                NotificationCallback.LOGS,
+                NotificationCallback.ALL]
+            results = self.dataset in [
+                NotificationCallback.RESULTS,
+                NotificationCallback.ALL]
+            callback_data = self.notification.test_job.create_job_data(
+                token=self.token, output=output, results=results)
+            # store callback_data for later retrieval & triage
+            job_data_file = os.path.join(job.output_dir, 'job_data.gz')
+            if callback_data and not os.path.exists(job_data_file):
+                with gzip.open(job_data_file, 'wb') as output:
+                    output.write(simplejson.dumps(callback_data).encode('utf-8'))
         headers = {}
 
         if callback_data:
@@ -2726,56 +2793,6 @@ class NotificationCallback(models.Model):
             except Exception as ex:
                 logger.warning("Problem sending request to %s: %s" % (
                     self.url, ex))
-
-    def get_callback_data(self):
-
-        from lava_results_app.dbutils import export_testcase
-
-        if self.method == NotificationCallback.GET:
-            return None
-        else:
-
-            data = {
-                "id": self.notification.test_job.pk,
-                "status": self.notification.test_job.get_legacy_status(),
-                "status_string": self.notification.test_job.get_legacy_status_display().lower(),
-                "state": self.notification.test_job.state,
-                "state_string": self.notification.test_job.get_state_display(),
-                "health": self.notification.test_job.health,
-                "health_string": self.notification.test_job.get_health_display(),
-                "submit_time": str(self.notification.test_job.submit_time),
-                "start_time": str(self.notification.test_job.start_time),
-                "end_time": str(self.notification.test_job.end_time),
-                "submitter_username": self.notification.test_job.submitter.username,
-                "failure_comment": self.notification.test_job.failure_comment,
-                "priority": self.notification.test_job.priority,
-                "description": self.notification.test_job.description,
-                "actual_device_id": self.notification.test_job.actual_device_id,
-                "definition": self.notification.test_job.definition,
-                "metadata": self.notification.test_job.get_metadata_dict(),
-            }
-            # Only add the token if it's not empty
-            if self.token is not None:
-                data["token"] = self.token
-
-            # Logs.
-            output_file = self.notification.test_job.output_file()
-            if output_file and self.dataset in [
-                    NotificationCallback.LOGS,
-                    NotificationCallback.ALL]:
-                data["log"] = self.notification.test_job.output_file().read()
-
-            # Results.
-            if self.dataset in [NotificationCallback.RESULTS,
-                                NotificationCallback.ALL]:
-                data["results"] = {}
-                for test_suite in self.notification.test_job.testsuite_set.all():
-                    yaml_list = []
-                    for test_case in test_suite.testcase_set.all():
-                        yaml_list.append(export_testcase(test_case))
-                    data["results"][test_suite.name] = yaml.dump(yaml_list)
-
-            return data
 
 
 @receiver(pre_save, sender=TestJob, dispatch_uid="process_notifications")

lava_scheduler_app/tests/test_submission.py
@@ -260,6 +260,24 @@ class TestTestJob(TestCaseWithFactory):  # pylint: disable=too-many-ancestors,to
         yaml.safe_load(job.definition)
         self.assertIsInstance(job.definition, str)
 
+    def test_job_data(self):
+        self.factory.cleanup()
+        user = self.factory.make_user()
+        user.user_permissions.add(
+            Permission.objects.get(codename='add_testjob'))
+        user.save()
+        dt = self.factory.make_device_type(name='qemu')
+        device = self.factory.make_device(device_type=dt, hostname='qemu-1')
+        device.save()
+        definition = self.factory.make_job_data_from_file('qemu-pipeline-first-job.yaml')
+        job = testjob_submission(definition, user, None)
+        data = job.create_job_data()
+        self.assertIsNotNone(data)
+        self.assertIn('start_time', data)
+        # job has not started but job_data explicitly turns start_time into str()
+        self.assertEqual(data['start_time'], "None")
+        self.assertEqual(data['state_string'], TestJob.STATE_CHOICES[TestJob.STATE_SUBMITTED][1])
+
 
 class TestHiddenTestJob(TestCaseWithFactory):  # pylint: disable=too-many-ancestors
 

lava_scheduler_app/urls.py
@@ -7,7 +7,7 @@ from lava_scheduler_app.views import (
     device_restrict_device, all_device_types, device_type_detail, device_dictionary,
     device_dictionary_plain, maintenance_devices,
     device_type_reports, edit_worker_desc,
-    failure_report, favorite_jobs,
+    failure_report, favorite_jobs, job_fetch_data,
     health_job_list, healthcheck, index,
     job_annotate_failure, job_cancel, job_fail, job_change_priority,
     job_definition, job_definition_plain, job_description_yaml, job_detail,
@@ -23,7 +23,7 @@ from lava_scheduler_app.views import (
     download_device_type_template, similar_jobs,)
 
 
-urlpatterns = [
+urlpatterns = [  # pylint: disable=invalid-name
     url(r'^$', index, name='lava.scheduler'),
     url(r'^reports$', reports, name='lava.scheduler.reports'),
     url(r'^reports/failures$', failure_report, name='lava.scheduler.failure_report'),
@@ -91,6 +91,9 @@ urlpatterns = [
     url(r'^job/(?P<pk>[0-9]+|[0-9]+\.[0-9]+)/log_pipeline_incremental$',
         job_log_incremental,
         name='lava.scheduler.job.log_incremental'),
+    url(r'^job/(?P<pk>[0-9]+|[0-9]+\.[0-9]+)/job_data$',
+        job_fetch_data,
+        name='lava.scheduler.job.fetch_data'),
     url(r'^myjobs$', myjobs, name='lava.scheduler.myjobs'),
     url(r'^favorite-jobs$', favorite_jobs, name='lava.scheduler.favorite_jobs'),
     url(r'^job/(?P<pk>[0-9]+|[0-9]+\.[0-9]+)/priority$', job_change_priority,

lava_scheduler_app/views.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=too-many-lines,invalid-namlog_e
+# pylint: disable=too-many-lines,invalid-name
 
-from collections import OrderedDict
 import contextlib
 import datetime
 import io
-import jinja2
 import logging
 import os
 import simplejson
-import sys
 import tarfile
 import re
 import yaml
@@ -109,7 +106,7 @@ from lava_scheduler_app.tables import (
     RunningTable,
 )
 
-# pylint: disable=too-many-attributes,too-many-ancestors,too-many-arguments,too-many-locals
+# pylint: disable=too-many-ancestors,too-many-arguments,too-many-locals
 # pylint: disable=too-many-statements,too-many-branches,too-many-return-statements
 # pylint: disable=no-self-use,too-many-nested-blocks,too-few-public-methods
 
@@ -1272,6 +1269,22 @@ def multinode_job_definition_plain(request, pk):
     filename = "job_%d.yaml" % job.id
     response['Content-Disposition'] = \
         "attachment; filename=multinode_%s" % filename
+    return response
+
+
+def job_fetch_data(request, pk):
+    job = get_restricted_job(request.user, pk, request=request)
+    if job.state != TestJob.STATE_FINISHED:
+        raise Http404()
+    path = os.path.join(job.output_dir, 'job_data.gz')
+    if not os.path.exists(path):
+        raise Http404()
+    # FileResponse could be used here with Django2.0 but
+    # currently it doesn't set a useful extension.
+    with open(path, 'rb') as data:
+        response = HttpResponse(data, content_type='application/gzip')
+    response['Content-Disposition'] = 'attachment; filename="%s"' % os.path.basename(path)
+    # response['Content-Encoding'] = 'gzip'
     return response
 
 

commit 9bb6ee3ce52593d3490acacd5e37b6f53cac97d0

Fix crash introduced by 792b63ad
Change-Id: I998e35611479ae7c78e269c0c4aaee76dba619eb

lava_scheduler_app/models.py
@@ -2768,7 +2768,7 @@ class NotificationCallback(models.Model):
             callback_data = self.notification.test_job.create_job_data(
                 token=self.token, output=output, results=results)
             # store callback_data for later retrieval & triage
-            job_data_file = os.path.join(job.output_dir, 'job_data.gz')
+            job_data_file = os.path.join(self.notification.test_job.output_dir, 'job_data.gz')
             if callback_data and not os.path.exists(job_data_file):
                 with gzip.open(job_data_file, 'wb') as output:
                     output.write(simplejson.dumps(callback_data).encode('utf-8'))