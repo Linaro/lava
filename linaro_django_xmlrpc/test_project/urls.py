60c137be0ee2eb9cfc81148171ee520ab288219c
Rémi Duraffort <remi.duraffort@linaro.org>
2018-07-12 16:44:55 +0200

    Remove unused sources
    Change-Id: I6154819210bff5d816f6c385a19257a4604da76e

--- linaro_django_xmlrpc/test_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url, include
-
-urlpatterns = patterns(
-    '',
-    url(r'', include('linaro_django_xmlrpc.urls')),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-)
