55c52bb6d17bd779b15be5f87fa8d230249d8a52
Rémi Duraffort <remi.duraffort@linaro.org>
2018-07-27 09:55:18 +0200

    LAVA-1156 When running under pytest, fake requests
    The lava_dispatcher test suite is now faster and resilient to network errors.
    Some tests (listed in conftest.py) that do require network access can still use
    the network.
    
    Change-Id: I4df941df68df3ac3dd2a8baf617e2dd68cc2ed80

---
+++ lava_dispatcher/test/conftest.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 Linaro Limited
+#
+# Author: Remi Duraffort <remi.duraffort@linaro.org>
+#
+# This file is part of LAVA.
+#
+# LAVA is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# LAVA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along
+# with this program; if not, see <http://www.gnu.org/licenses>.
+
+import pytest
+import requests
+
+
+@pytest.fixture(autouse=True)
+def no_requests(monkeypatch, request):
+    def get(url, allow_redirects, stream):
+        assert allow_redirects is True
+        assert stream is True
+        res = requests.Response()
+        res.status_code = requests.codes.OK
+        res.close = lambda: None
+        return res
+
+    # List of tests that should have access to the network
+    # When pytest is mandatory, we can use pytest marks
+    # See https://stackoverflow.com/a/38763328
+    skip_tests = ["test_download_decompression", "TestChecksum", "test_xz_nfs"]
+    if set(skip_tests) & set(request.keywords.keys()):
+        return
+    monkeypatch.setattr(requests, "get", get)
