e2bddc70df933100895ebb2cec5af5c190f96419
Rémi Duraffort <remi.duraffort@linaro.org>
2018-09-20 23:54:18 +0200

    Add tests to base lava images

---
+++ lava-dispatcher-base/test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+echo "Clone lava.git"
+cd /root/
+git clone https://git.lavasoftware.org/lava/lava.git
+cd lava/
+echo "done"
+echo
+
+echo "Check stretch dependencies"
+PACKAGES=$(./share/requires.py -p lava-dispatcher -d debian -s stretch -n)
+for pkg in $PACKAGES
+do
+  echo "* $pkg"
+  dpkg -l "$pkg" > /dev/null
+done
+echo "Check stretch-backports dependencies"
+PACKAGES=$(./share/requires.py -p lava-dispatcher -d debian -s stretch-backports -n)
+for pkg in $PACKAGES
+do
+  echo "* $pkg"
+  [ -n $(dpkg-query --show --showformat '${Version}\n' "$pkg" | grep bpo) ]
+done
