4ef4a7322f35374c2b1e68cf29b4de578f0dbf83
Rémi Duraffort <remi.duraffort@linaro.org>
2018-10-02 16:51:20 +0200

    CI: fix typo in setup
    This typo prevents scripts from installing the required dependencies.
    Fix #107

--- .gitlab-ci/test/server-debian-10.sh
+++ .gitlab-ci/test/server-debian-10.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$1" = "setup" ]
 then
   set -x
-  apt-get install --no-install-recommends --yes $(./share/requires.py -p lava-dispatcher -d debian -s buster -n)
+  apt-get install --no-install-recommends --yes $(./share/requires.py -p lava-server -d debian -s buster -n)
 else
   set -x
   PYTHONPATH=. py.test-3 --cache-clear -v --junitxml=common.xml lava_common/test
