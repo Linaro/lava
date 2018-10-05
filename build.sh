5bea98cc6337a20e2cf6c345aa116e900f4f5e8e
Rémi Duraffort <remi.duraffort@linaro.org>
2018-10-05 17:34:53 +0200

    Add a script to build and test base images

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+IMAGES=$(find . -maxdepth 1 -name "*base" -type d \( ! -path './.*' \) \( ! -path . \) | sort | sed "s#^./##")
+IMAGES=${1:-$IMAGES}
+
+for image in $IMAGES
+do
+  echo "$image:"
+  echo "* building"
+  hash=$(docker build --force-rm -q "$image")
+  echo "=> $hash"
+  echo "* testing"
+  rm -f "$image.log"
+  docker run --rm --volume "$PWD/$image/test.sh:/root/test.sh" "$hash" /root/test.sh > "$image.log" 2>&1
+  echo "=> done"
+  echo
+done
