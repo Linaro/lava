66244c7c84281ccaca4e35e11f28d9d0afd75130
Rémi Duraffort <remi.duraffort@linaro.org>
2018-10-15 16:56:47 +0200

    lava-server improve entrypoint
    When starting lava-master, lava-logs and lava-server-gunicorn, we are now
    taking into account configuration files (like /etc/lava-server/lava-master).
    
    This replicating what systemd does on standard systems.

--- lava-server/entrypoint.sh
+++ lava-server/entrypoint.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
+
 set -e
 
-# Keep track of the PIDs
+#############
+# Variables #
+#############
 GUNICORN_PID=0
 LAVA_LOGS_PID=0
 LAVA_MASTER_PID=0
 LAVA_PUBLISHER_PID=0
 
-# Signal handler
+
+##################
+# Signal handler #
+##################
 handler() {
     tail_pid="${!}"
     echo "Killing:"
@@ -41,6 +47,43 @@ handler() {
     exit 0
 }
 
+
+#################
+# Start helpers #
+#################
+start_lava_logs() {
+    LOGLEVEL=DEBUG
+    [ -e /etc/default/lava-logs ] && . /etc/default/lava-logs
+    [ -e /etc/lava-server/lava-logs ] && . /etc/lava-server/lava-logs
+    /usr/bin/lava-server manage lava-logs --level $LOGLEVEL $SOCKET $MASTER_SOCKET $IPV6 $ENCRYPT $MASTER_CERT $SLAVES_CERTS &
+    LAVA_LOGS_PID=$!
+}
+
+
+start_lava_master() {
+    LOGLEVEL=DEBUG
+    [ -e /etc/default/lava-master ] && . /etc/default/lava-master
+    [ -e /etc/lava-server/lava-master ] && . /etc/lava-server/lava-master
+    /usr/bin/lava-server manage lava-master --level $LOGLEVEL $MASTER_SOCKET $IPV6 $ENCRYPT $MASTER_CERT $SLAVES_CERTS &
+    LAVA_MASTER_PID=$!
+}
+
+
+start_lava_server_gunicorn() {
+    LOGLEVEL="DEBUG"
+    WORKERS="4"
+    LOGFILE="/var/log/lava-server/gunicorn.log"
+    [ -e /etc/default/lava-server-gunicorn ] && . /etc/default/lava-server-gunicorn
+    [ -e /etc/lava-server/lava-server-gunicorn ] && . /etc/lava-server/lava-server-gunicorn
+    /usr/bin/gunicorn3 lava_server.wsgi --log-level $LOGLEVEL --log-file $LOGFILE -u lavaserver -g lavaserver --workers $WORKERS $RELOAD &
+    GUNICORN_PID=$!
+}
+
+
+########
+# Main #
+########
+
 # setup handlers
 trap 'handler' SIGTERM
 
@@ -56,8 +99,7 @@ echo "done"
 echo
 
 echo "Starting gunicorn3"
-gunicorn3 lava_server.wsgi&
-GUNICORN_PID=$!
+start_lava_server_gunicorn
 echo "done"
 echo
 
@@ -67,24 +109,24 @@ echo "done"
 echo
 
 echo "Starting lava-logs"
-lava-server manage lava-logs&
-LAVA_LOGS_PID=$!
+start_lava_logs
 echo "done"
 echo
 
 echo "Starting lava-publisher"
-lava-server manage lava-publisher&
+lava-server manage lava-publisher &
 LAVA_PUBLISHER_PID=$!
 echo "done"
 echo
 
 echo "Starting lava-master"
-lava-server manage lava-master&
-LAVA_MASTER_PID=$!
+start_lava_master
 echo "done"
 echo
 
-# Wait for signals
+################
+# Wait forever #
+################
 cd /var/log/lava-server
 while true
 do
