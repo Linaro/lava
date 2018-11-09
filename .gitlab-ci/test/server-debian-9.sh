f40f0f2c5fa4c6b6d20daa69b38ea87c26abee96
Neil Williams <neil.williams@linaro.org>
2018-11-09 13:18:08 +0000

    Ensure apt is updated before trying to install
    This caused a failure in !181
    
    Signed-off-by: Neil Williams <neil.williams@linaro.org>

--- .gitlab-ci/test/server-debian-9.sh
+++ .gitlab-ci/test/server-debian-9.sh
@@ -5,6 +5,7 @@ set -e
 if [ "$1" = "setup" ]
 then
   set -x
+  apt-get -q update
   DEPS=$(./share/requires.py -p lava-server -d debian -s stretch -n)
   apt-get install --no-install-recommends --yes $DEPS
   DEPS=$(./share/requires.py -p lava-server -d debian -s stretch-backports -n)
