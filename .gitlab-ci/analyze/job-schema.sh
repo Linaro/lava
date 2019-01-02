5a92736ada17a5a6d276f63da2496373b3bbcad9
Rémi Duraffort <remi.duraffort@linaro.org>
2019-01-02 16:20:31 +0100

    CI: test templates against the schema
    We are only testing lava_results_app templates for the moment.
    
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

---
+++ .gitlab-ci/analyze/job-schema.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+if [ "$1" = "setup" ]
+then
+  apt-get -q update
+  apt-get install --no-install-recommends --yes python3 python3-nose python3-voluptuous python3-yaml
+else
+  # documentation
+  EXCLUDE="artifact-conversion-download.yaml bbb-2serial.yaml bbb-lxc-ssh-guest.yaml first-multinode-job.yaml hi6220-hikey-bl.yaml hikey-new-connection.yaml multiple-serial-ports-lxc.yaml mustang-ssh-guest.yaml namespace-connections-example1.yaml qemu-debian-installer.yaml second-multinode-job.yaml x15-recovery.yaml"
+  CMD="--exclude $(echo ${EXCLUDE} | sed "s# # --exclude #g#")"
+  PYTHONPATH=. ./share/lava-schema.py job ${CMD} doc/v2/examples/test-jobs/*.yaml
+
+  # lava_dispatcher
+  EXCLUDE="basics.yaml bbb-group-vland-alpha.yaml bbb-group-vland-beta.yaml bbb-ssh-guest.yaml cubietruck-removable-with-writer.yaml cubietruck-removable.yaml download.yaml hi6220-hikey.yaml hi6220-recovery.yaml hikey-console.yaml hikey-grub-lxc.yaml kvm-multinode-client.yaml kvm-multinode-server.yaml kvm-repeat.yaml mustang-secondary-media.yaml qemu-debian-installer.yaml ssh-boot.yaml ssh-deploy.yaml test_action-1.yaml test_action-2.yaml x15-recovery.yaml"
+  CMD="--exclude $(echo ${EXCLUDE} | sed "s# # --exclude #g#")"
+  PYTHONPATH=. ./share/lava-schema.py job ${CMD} lava_dispatcher/tests/sample_jobs/*.yaml
+
+  # lava_results_app
+  PYTHONPATH=. ./share/lava-schema.py job lava_results_app/tests/*.yaml
+
+  # lava_scheduler_app
+  EXCLUDE="bbb-bbb-vland-group.yaml bbb-cubie-vlan-group.yaml bbb-qemu-multinode.yaml hikey_multinode.yaml kvm-multinode.yaml kvm-multinode-client.yaml kvm-multinode-server.yaml lxc-multinode.yaml mustang-ssh-multinode.yaml nexus4_multinode.yaml panda-lxc-aep.yaml qemu-ssh-guest-1.yaml qemu-ssh-guest-2.yaml qemu-ssh-guest.yaml qemu-ssh-parent.yaml x86-vlan.yaml"
+  CMD="--exclude $(echo ${EXCLUDE} | sed "s# # --exclude #g#")"
+  PYTHONPATH=. ./share/lava-schema.py job ${CMD} lava_scheduler_app/tests/sample_jobs/*.yaml
+fi
---
+++ share/lava-schema.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 Linaro Limited
+#
+# Author: Remi Duraffort <remi.duraffort@linaro.org>
+#
+# This file is part of LAVA Dispatcher.
+#
+# LAVA Dispatcher is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# LAVA Dispatcher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses>.
+
+import argparse
+import pathlib
+import sys
+import voluptuous as v
+import yaml
+
+from lava_common.exceptions import JobError, LAVABug
+from lava_common.schemas import job, validate
+
+
+def check_job(data, options, prefix=""):
+    data = yaml.safe_load(data)
+    schema = v.Schema(job.schema(), extra=not options.strict)
+    try:
+        schema(data)
+        for action in data["actions"]:
+            # The job schema does already check the we have only one key
+            action_type = next(iter(action.keys()))
+            data = action[action_type]
+            cls = None
+            if action_type == "boot":
+                cls = "boot." + data.get("method", "")
+            elif action_type == "command":
+                cls = "command"
+            elif action_type == "deploy":
+                cls = "deploy." + data.get("to", "")
+            elif action_type == "test":
+                if "definitions" in data:
+                    cls = "test.definition"
+                elif "interactive" in data:
+                    cls = "test.interactive"
+                elif "monitors" in data:
+                    cls = "test.monitor"
+            if cls is None:
+                raise v.Invalid("invalid action", path=["actions", action_type])
+            cls = cls.replace("-", "_")
+            validate(cls, data, strict=options.strict)
+    except v.Invalid as exc:
+        print("%sInvalid job definition:" % prefix)
+        print("%skey: %s" % (prefix, exc.path))
+        print("%smgs: %s" % (prefix, exc.msg))
+        return 1
+    except LAVABug as exc:
+        print("%sInvalid job definition:" % prefix)
+        print("%sUnknown action type '%s'" % (prefix, cls))
+        return 2
+    except JobError as exc:
+        print("%sInvalid job definition:" % prefix)
+        print("%sInvalid action '%s': %s" % (prefix, cls, exc))
+        return 3
+    return 0
+
+
+def handle_job(options):
+    failed = 0
+    for jobfile in options.jobs:
+        if jobfile.is_dir() and options.recursive:
+            job_iter = jobfile.rglob("*.yaml")
+        else:
+            job_iter = [jobfile]
+        for job in job_iter:
+            if not job.is_file():
+                continue
+            if job.name in options.exclude:
+                print("* %s [skip]" % str(job))
+                continue
+            print("* %s" % str(job))
+            if check_job(job.read_text(encoding="utf-8"), options, prefix="  -> "):
+                failed += 1
+
+    return failed
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    sub = parser.add_subparsers(dest="sub_command")
+    sub.required = True
+    job_parser = sub.add_parser("job", help="check job schema")
+
+    job_parser.add_argument("jobs", type=pathlib.Path, nargs="+", help="job definition")
+    job_parser.add_argument(
+        "--exclude", type=str, default=[], action="append", help="exclude some jobs"
+    )
+    job_parser.add_argument(
+        "--recursive",
+        "-r",
+        action="store_true",
+        default=False,
+        help="recurse on directories",
+    )
+    job_parser.add_argument(
+        "--strict", action="store_true", default=False, help="make the validator strict"
+    )
+
+    options = parser.parse_args()
+
+    if options.sub_command == "job":
+        return handle_job(options)
+    raise NotImplementedError("Unsupported command")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
