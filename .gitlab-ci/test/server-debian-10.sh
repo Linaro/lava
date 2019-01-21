69053fb825b9804473eafc3c2c5649e62cdcb397
Neil Williams <neil.williams@linaro.org>
2019-01-21 15:36:58 +0000

    Ignore DeprecatedWarnings on buster in CI
    These will need to be addressed in future milestones but do not
    need to clog up the job output at this time.
    
    Signed-off-by: Neil Williams <neil.williams@linaro.org>

--- .gitlab-ci/test/server-debian-10.sh
+++ .gitlab-ci/test/server-debian-10.sh
@@ -12,6 +12,8 @@ then
   apt-get install --no-install-recommends --yes $DEPS
 else
   set -x
-  PYTHONPATH=. pytest-3 --cache-clear -v --junitxml=common.xml lava_common/tests
-  PYTHONPATH=. pytest-3 --cache-clear --ds lava_server.settings.development -v --junitxml=server.xml lava_scheduler_app/tests lava_results_app/tests linaro_django_xmlrpc/tests.py lava_rest_app/tests.py
+  PYTHONPATH=. pytest-3 --cache-clear -W ignore::DeprecationWarning -v --junitxml=common.xml lava_common/tests
+  PYTHONPATH=. pytest-3 --cache-clear -W ignore::DeprecationWarning \
+  --ds lava_server.settings.development -v --junitxml=server.xml \
+  lava_scheduler_app/tests lava_results_app/tests linaro_django_xmlrpc/tests.py lava_rest_app/tests.py
 fi
