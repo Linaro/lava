02097dd41e979a3e7c6ee09e20093be33db601eb
Neil Williams <neil.williams@linaro.org>
2019-02-04 16:07:23 +0000

    Fixup 4ebbc234 - put new test in correct location
    Signed-off-by: Neil Williams <neil.williams@linaro.org>
    Reviewed-by: Rémi Duraffort <remi.duraffort@linaro.org>
    Tested-by: _Marge_Bot <https://git.lavasoftware.org/lava/lava/merge_requests/367>

--- lava_dispatcher/tests/test_flasher.py
+++ lava_dispatcher/tests/test_flasher.py
@@ -18,13 +18,22 @@
 # along
 # with this program; if not, see <http://www.gnu.org/licenses>.
 
-import unittest
 import os
+import pytest  # pylint: disable=unused-import
+import select
+import subprocess  # nosec - unit tests
 
-from lava_dispatcher.device import NewDevice
+from lava_dispatcher.device import NewDevice, PipelineDevice
 from lava_dispatcher.parser import JobParser
 from lava_dispatcher.tests.test_basic import Factory, StdoutTestCase
 from lava_dispatcher.tests.utils import DummyLogger
+from lava_dispatcher.action import Pipeline
+from lava_dispatcher.actions.deploy.flasher import Flasher, FlasherAction
+from lava_dispatcher.job import Job
+
+# This will be monkey patched
+import lava_dispatcher.utils.shell  # pylint: disable=unused-import
+import lava_dispatcher.actions.deploy.docker  # pylint: disable=unused-import
 
 
 class FlasherFactory(Factory):
@@ -47,3 +56,95 @@ class TestFlasher(StdoutTestCase):
         job.validate()
         description_ref = self.pipeline_reference("b2260-flasher.yaml", job=job)
         self.assertEqual(description_ref, job.pipeline.describe(False))
+
+
+def test_run(monkeypatch):
+    class FD:
+        def readlines(self):
+            return []
+
+    class Proc:
+        def __init__(self):
+            self.stderr = FD()
+            self.stdout = FD()
+
+        def poll(self):
+            return 0
+
+        def wait(self):
+            return 0
+
+    class Poller:
+        def register(self, fd, flag):
+            pass
+
+    commands = [
+        ["nice", "/home/lava/bin/PiCtrl.py", "PowerPlug", "0", "off"],
+        ["nice", "touch"],
+    ]
+
+    def Popen(
+        cmd, cwd, stdout, stderr, bufsize, universal_newlines
+    ):  # nosec - unit test
+        assert cmd == commands.pop(0)  # nosec - unit test
+        assert stdout == subprocess.PIPE  # nosec - unit test
+        assert stderr == subprocess.PIPE  # nosec - unit test
+        assert bufsize == 1  # nosec - unit test
+        assert universal_newlines  # nosec - unit test
+        return Proc()
+
+    monkeypatch.setattr(subprocess, "Popen", Popen)
+    monkeypatch.setattr(select, "epoll", lambda: Poller())
+
+    action = FlasherAction()
+    device = PipelineDevice(
+        {
+            "actions": {
+                "deploy": {
+                    "methods": {
+                        "flasher": {"commands": ["{HARD_RESET_COMMAND}", "touch"]}
+                    }
+                }
+            },
+            "commands": {"hard_reset": "/home/lava/bin/PiCtrl.py PowerPlug 0 off"},
+        }
+    )
+    action.job = Job(1234, {}, None)
+    action.job.device = device
+    action.parameters = {"namespace": "common", "images": {}}
+    action.section = Flasher.action_type
+
+    # self.commands is populated by validate
+    action.validate()
+    assert action.errors == []  # nosec - unit test
+
+    # Run the action
+    action.run(None, 10)
+    assert commands == []  # nosec - unit test
+
+
+def test_accepts():
+    pipe = Pipeline(job=Job(1234, {}, None))
+    pipe.add_action = lambda a, b: None
+    flasher = Flasher(pipe, {})
+
+    # Normal case
+    device = {"actions": {"deploy": {"methods": "flasher"}}}
+    params = {"to": "flasher"}
+    assert flasher.accepts(device, params) == (True, "accepted")  # nosec - unit test
+
+    # Flasher is not defined
+    device = {"actions": {"deploy": {"methods": "tftp"}}}
+    params = {"to": "flasher"}
+    assert flasher.accepts(device, params) == (  # nosec - unit test
+        False,
+        "'flasher' not in the device configuration deploy methods",
+    )
+
+    # Flasher is not requested
+    device = {"actions": {"deploy": {"methods": "flasher"}}}
+    params = {"to": "tftp"}
+    assert flasher.accepts(device, params) == (  # nosec - unit test
+        False,
+        '"to" parameter is not "flasher"',
+    )
