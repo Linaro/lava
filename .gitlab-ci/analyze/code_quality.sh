5662f734a77bd9a8cb416e69e1a8a96b602be584
Rémi Duraffort <remi.duraffort@linaro.org>
2019-02-06 17:44:28 +0100

    code_quality: install radeo in the setup step
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

--- .gitlab-ci/analyze/code_quality.sh
+++ .gitlab-ci/analyze/code_quality.sh
@@ -4,11 +4,10 @@ set -e
 
 if [ "$1" = "setup" ]
 then
-  echo "nothing to do"
+  apt -y -q install python3 radon
 else
   set -x
   export LC_ALL=C.UTF-8
   export LANG=C.UTF-8
-  apt -y -q install python3 radon
   python3 .gitlab-ci/analyze/parse.py
 fi
