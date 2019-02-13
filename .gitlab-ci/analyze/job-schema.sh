7b9119bd70b1d550beff226a503d9d8c8e18e377
Rémi Duraffort <remi.duraffort@linaro.org>
2019-02-13 17:05:12 +0100

    CI check more sample jobs
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

--- .gitlab-ci/analyze/job-schema.sh
+++ .gitlab-ci/analyze/job-schema.sh
@@ -8,7 +8,7 @@ then
   apt-get install --no-install-recommends --yes python3 python3-nose python3-voluptuous python3-yaml
 else
   # documentation
-  EXCLUDE="artifact-conversion-download.yaml bbb-2serial.yaml bbb-lxc-ssh-guest.yaml first-multinode-job.yaml hi6220-hikey-bl.yaml hikey-new-connection.yaml multiple-serial-ports-lxc.yaml mustang-ssh-guest.yaml namespace-connections-example1.yaml qemu-debian-installer.yaml second-multinode-job.yaml x15-recovery.yaml"
+  EXCLUDE="artifact-conversion-download.yaml bbb-2serial.yaml bbb-lxc-ssh-guest.yaml hi6220-hikey-bl.yaml hikey-new-connection.yaml multiple-serial-ports-lxc.yaml mustang-ssh-guest.yaml x15-recovery.yaml"
   CMD="--exclude $(echo ${EXCLUDE} | sed "s# # --exclude #g#")"
   PYTHONPATH=. ./share/lava-schema.py job ${CMD} doc/v2/examples/test-jobs/*.yaml
 
@@ -21,7 +21,7 @@ else
   PYTHONPATH=. ./share/lava-schema.py job lava_results_app/tests/*.yaml
 
   # lava_scheduler_app
-  EXCLUDE="bbb-bbb-vland-group.yaml bbb-cubie-vlan-group.yaml bbb-qemu-multinode.yaml hikey_multinode.yaml kvm-multinode.yaml kvm-multinode-client.yaml kvm-multinode-server.yaml lxc-multinode.yaml mustang-ssh-multinode.yaml nexus4_multinode.yaml panda-lxc-aep.yaml qemu-ssh-guest-1.yaml qemu-ssh-guest-2.yaml qemu-ssh-guest.yaml qemu-ssh-parent.yaml x86-vlan.yaml"
+  EXCLUDE="kvm-multinode-client.yaml kvm-multinode-server.yaml mustang-ssh-multinode.yaml qemu-ssh-guest-1.yaml qemu-ssh-guest-2.yaml qemu-ssh-parent.yaml"
   CMD="--exclude $(echo ${EXCLUDE} | sed "s# # --exclude #g#")"
   PYTHONPATH=. ./share/lava-schema.py job ${CMD} lava_scheduler_app/tests/sample_jobs/*.yaml
 fi
