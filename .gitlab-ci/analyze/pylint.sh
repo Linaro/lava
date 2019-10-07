46d9c59402dab93288e5620bfbfaf0ef478fc167
Rémi Duraffort <remi.duraffort@linaro.org>
2019-10-07 15:54:02 +0200

    CI: use more pylint features
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

--- .gitlab-ci/analyze/pylint.sh
+++ .gitlab-ci/analyze/pylint.sh
@@ -19,8 +19,12 @@ else
   #  R0201: Method could be a function
   #  R0401: Cyclic import (%s -> %s)
   # Enabled:
+  #  W0235: Useless super delegation in method %r
   #  W0404: Reimport %r (imported line %s) Used when a module is reimported multiple times.
-  #  W1505: Using deprecated method warn()
   #  W0611: Unused %s Used when an imported module or variable is not used.
-  pylint3 --disable=all --enable=elif,exceptions,stdlib,imports,variables,string,string_constant,logging,newstyle,classes --disable=C0411,C0412,E0401,E0611,R0201,R0401,W --enable=W0404,W1505,W0611 --ignore=lava/coordinator lava lava_common lava_dispatcher lava_rest_app lava_results_app lava_scheduler_app lava_server linaro_django_xmlrpc share
+  #  W1401: Anomalous backslash in string: '%s'.
+  #  W1402: Anomalous Unicode escape in byte string: '%s'.
+  #  W1403: Implicit string concatenation found in %s
+  #  W1505: Using deprecated method warn()
+  pylint3 --disable=all --enable=elif,exceptions,stdlib,imports,variables,string,string_constant,logging,newstyle,classes --disable=C0411,C0412,E0401,E0611,R0201,R0401,W --enable=W0235,W0404,W0611,W1401,W1402,W1403,W1505 --ignore=lava/coordinator lava lava_common lava_dispatcher lava_rest_app lava_results_app lava_scheduler_app lava_server linaro_django_xmlrpc share
 fi
