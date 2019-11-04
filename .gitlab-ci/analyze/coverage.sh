1adbc0f6371fb4c93f4cfe218080cdba65f7e77a
Stevan Radakovic <stevan.radakovic@linaro.org>
2019-11-04 10:28:27 +0100

    Use --cov-fail-under instead of arbitrary code for test coverage failures.

--- .gitlab-ci/analyze/coverage.sh
+++ .gitlab-ci/analyze/coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-COVERAGE_MIN_PERCENTAGE=51.00
+COVERAGE_MIN_PERCENTAGE=51
 
 set -e
 
@@ -20,17 +20,5 @@ else
   set -x
   PYTHONPATH=. pytest-3 --cache-clear -v --cov --cov-report= lava_dispatcher/tests
   PYTHONPATH=. pytest-3 --cache-clear -v --ds lava_server.settings.development --cov --cov-append --cov-report= lava_scheduler_app/tests lava_results_app/tests linaro_django_xmlrpc/tests.py lava_rest_app/tests.py
-  PYTHONPATH=. pytest-3 --cache-clear -v --cov --cov-append --cov-report=term --cov-report=html lava_common/tests
-
-  set +x
-  total=`PYTHONPATH=. pytest-3 --cache-clear -v --cov --cov-append --cov-report=term lava_common/tests | grep TOTAL | awk '{print $4}' | tr -d "%"`
-  result=`echo "$total $COVERAGE_MIN_PERCENTAGE" | awk '{if ($1 < $2) print 1; else print 0}'`
-
-  if [ $result = 1 ]
-  then
-    echo "Coverage needs to be greater then $COVERAGE_MIN_PERCENTAGE"
-    return 1
-  else
-    return 0
-  fi
+  PYTHONPATH=. pytest-3 --cache-clear -v --cov --cov-append --cov-report=term --cov-report=html --cov-fail-under=$COVERAGE_MIN_PERCENTAGE lava_common/tests
 fi
