0ca5963c2635dfd1ba653ba21c6408e2fb914f8e
Rémi Duraffort <remi.duraffort@linaro.org>
2019-11-14 08:02:54 +0100

    coordinator: fix pylint warnings
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

--- .gitlab-ci/analyze/pylint.sh
+++ .gitlab-ci/analyze/pylint.sh
@@ -27,5 +27,5 @@ else
   #  W1402: Anomalous Unicode escape in byte string: '%s'.
   #  W1403: Implicit string concatenation found in %s
   #  W1505: Using deprecated method warn()
-  pylint3 --disable=all --enable=elif,exceptions,stdlib,imports,variables,string,string_constant,logging,newstyle,classes --disable=C0411,C0412,E0401,E0611,R0201,R0401,W --enable=W0235,W0404,W0611,W1401,W1402,W1403,W1505 --ignore=lava/coordinator $dirs
+  pylint3 --disable=all --enable=elif,exceptions,stdlib,imports,variables,string,string_constant,logging,newstyle,classes --disable=C0411,C0412,E0401,E0611,R0201,R0401,W --enable=W0235,W0404,W0611,W1401,W1402,W1403,W1505 $dirs
 fi
