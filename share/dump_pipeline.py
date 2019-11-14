1630776d49bca947522ee867ad4c83a95b87390d
Rémi Duraffort <remi.duraffort@linaro.org>
2019-11-14 09:24:58 +0100

    dump_pipeline: improve this dev helper
    Fi the command line interface and allow to give device templates.
    
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

--- share/dump_pipeline.py
+++ share/dump_pipeline.py
@@ -5,14 +5,14 @@
 #
 # Author: Remi Duraffort <remi.duraffort@linaro.org>
 #
-# This file is part of LAVA Dispatcher.
+# This file is part of LAVA.
 #
-# LAVA Dispatcher is free software; you can redistribute it and/or modify
+# LAVA is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# LAVA Dispatcher is distributed in the hope that it will be useful,
+# LAVA is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
@@ -21,37 +21,52 @@
 # along
 # with this program; if not, see <http://www.gnu.org/licenses>.
 
+import argparse
+import jinja2
+import os
+import sys
+import yaml
+
 from lava_dispatcher.device import NewDevice
 from lava_dispatcher.parser import JobParser
 
-import optparse
-import yaml
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("device", type=argparse.FileType(), help="Device template")
+    parser.add_argument(
+        "--path", type=str, action="append", help="Templates lookup path"
+    )
+    parser.add_argument("job", type=argparse.FileType(), help="Job definition")
+
+    options = parser.parse_args()
+
+    # Set the default path
+    if options.path is None:
+        options.path = [
+            os.path.join(
+                os.path.dirname(__file__),
+                "..",
+                "etc",
+                "dispatcher-config",
+                "device-types",
+            )
+        ]
+
+    # Rendre the device template
+    env = jinja2.Environment(  # nosec - used to render yaml
+        autoescape=False, loader=jinja2.FileSystemLoader(options.path)
+    )
+    # Load the device configuration
+    data = env.from_string(options.device.read()).render()
+    device = NewDevice(yaml.safe_load(data))
+
+    # Load the job definition
+    parser = JobParser()
+    job = parser.parse(options.job.read(), device, 0, None, None, None)
+
+    print(yaml.dump(job.pipeline.describe(False)))
 
 
 if __name__ == "__main__":
-    usage = "Usage: %prog device_name job.yaml"
-    description = (
-        "LAVA dispatcher pipeline helper. Dump the pipeline that "
-        "was built for the given job on the given device"
-    )
-
-    parser = optparse.OptionParser(usage=usage, description=description)
-    (options, args) = parser.parse_args()
-    if len(args) != 2:
-        print("Missing job option, try -h for help")
-        exit(1)
-
-    # Create the device
-    device = None
-    try:
-        device = NewDevice(args[0])
-    except RuntimeError:
-        print("No device configuration found for %s" % args[0])
-        exit(1)
-
-    # Load the job definition
-    with open(args[1], "r") as job_data:
-        parser = JobParser()
-        job = parser.parse(job_data, device, 0, None, None, None)
-
-    print(yaml.dump(job.pipeline.describe(False)))
+    sys.exit(main())
