a00aca1b7e1052101c3cd2b708f21362b9b0cc04
Rémi Duraffort <remi.duraffort@linaro.org>
2019-12-03 13:39:11 +0100

    Test lavfa_dispatcher.utils.strings
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

---
+++ lava_dispatcher/tests/test_utils_strings.py
@@ -0,0 +1,39 @@
+from lava_dispatcher.utils.strings import (
+    indices,
+    map_kernel_uboot,
+    seconds_to_str,
+    substitute,
+)
+
+
+def test_indices():
+    assert indices("abcdeabcdea", "a") == [0, 5, 10]
+    assert indices("abcdeabcdea", "q") == []
+
+
+def test_substitute():
+    assert substitute(["hello", "world"], {}) == ["hello", "world"]
+    assert substitute(["hello", "world"], {"hello": "strange"}) == ["strange", "world"]
+    assert substitute(["hello", "world"], {"nice": "strange"}) == ["hello", "world"]
+    assert substitute(["hello", "world"], {"nice": "strange"}, drop=True) == [
+        "hello",
+        "world",
+    ]
+    assert substitute(["hello", "world"], {"hello": None}) == ["hello", "world"]
+    assert substitute(["hello", "world"], {"hello": None}, drop=True) == ["world"]
+
+
+def test_seconds_to_str():
+    assert seconds_to_str(0) == "00:00:00"
+    assert seconds_to_str(1) == "00:00:01"
+    assert seconds_to_str(62) == "00:01:02"
+    assert seconds_to_str(147) == "00:02:27"
+    assert seconds_to_str(3641) == "01:00:41"
+
+
+def test_map_kernel_uboot():
+    assert map_kernel_uboot("uimage", {}) == "bootm"
+    assert map_kernel_uboot("zimage", {}) == "bootm"
+    assert map_kernel_uboot("zimage", {"bootz": None}) == "bootz"
+    assert map_kernel_uboot("image", {}) == "bootm"
+    assert map_kernel_uboot("image", {"booti": None}) == "booti"
