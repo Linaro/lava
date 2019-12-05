59ba084affcab80211f39f0b544bfce6b0db6ba9
Rémi Duraffort <remi.duraffort@linaro.org>
2019-12-05 10:05:09 +0100

    CI: simplify debian build scripts
    Also remove unsed shell variables.
    
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

--- .gitlab-ci/build/amd64/pkg-debian-9.sh
+++ .gitlab-ci/build/amd64/pkg-debian-9.sh
@@ -7,25 +7,12 @@ then
   true
 else
   set -x
-  # when changes are applied, git is left in detached state.
-  # to change back from the scratch branch and clean up,
-  # the dev script needs to have a base branch.
-  git status
-  git branch -D cibase || true
-  git branch -D lavadevscratch || true
-  git checkout -b cibase
-  git status
-  export GIT_COMMITTER_NAME="lava-dev debian build script"
-  export GIT_COMMITTER_EMAIL="lava-dev@lavasoftware.org"
-  export GIT_AUTHOR_NAME="lava-dev debian build script"
-  export GIT_AUTHOR_EMAIL="lava-dev@lavasoftware.org"
   # build the full package, including original source, for stretch.
   ./share/debian-dev-build.sh -o _build -s stretch
   debc $(find _build -name 'lava_*_amd64.changes' 2>/dev/null|head -n1)
-  git branch -D cibase || true
 
   # Check dependencies
-  .gitlab-ci/build/check-debian-deps.py --suite stretch-backports --package lava-common _build/lava-common_*
-  .gitlab-ci/build/check-debian-deps.py --suite stretch-backports --package lava-dispatcher _build/lava-dispatcher_*
-  .gitlab-ci/build/check-debian-deps.py --suite stretch-backports --package lava-server _build/lava-server_*
+  .gitlab-ci/build/check-debian-deps.py --suite stretch-backports --package lava-common _build/lava-common_*stretch*
+  .gitlab-ci/build/check-debian-deps.py --suite stretch-backports --package lava-dispatcher _build/lava-dispatcher_*stretch*
+  .gitlab-ci/build/check-debian-deps.py --suite stretch-backports --package lava-server _build/lava-server_*stretch*
 fi
