97e9e764399e8a47c6eb0fb6f53c0691cc5cef81
Rémi Duraffort <remi.duraffort@linaro.org>
2020-01-09 12:30:15 +0100

    Rewrite setup.py from scratch
    This version is now able to instal each package or all of them.
    The configuration file and the empty directory are created whenever needed.
    
    Use setup.cfg for the common configuration.
    
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 # Copyright (C) 2010-2019 Linaro Limited
 #
 # Author: Neil Williams <neil.williams@linaro.org>
+#         Remi Duraffort <remi.duraffort@linaro.org>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -18,134 +19,189 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 import glob
-import fnmatch
+import shutil
+import sys
+
+import distutils.command.install_scripts
 from setuptools import setup, find_packages
 
-from lava_common.version import __version__
+
+class rename_scripts(distutils.command.install_scripts.install_scripts):
+    """
+    Custom install script to rename 'manage.py' to 'lava-server'
+    """
+
+    SCRIPTS = {"manage.py": "lava-server", "lava-coordinator.py": "lava-coordinator"}
+
+    def run(self):
+        super().run()
+        for script in self.get_outputs():
+            for key in self.SCRIPTS:
+                if script.endswith(key):
+                    new_name = script.replace(key, self.SCRIPTS[key])
+                    print("Rename %r to %r" % (script, new_name))
+                    shutil.move(script, new_name)
+                    continue
 
 
-# based on https://wiki.python.org/moin/Distutils/Tutorial
-def find_data_files(srcdir, *wildcards):
-    badnames = [".pyc", "~"]
-
-    def walk_helper(arg, dirname, files):
-        names = []
-        lst, wilds = arg
-        for wcard in wilds:
-            wc_name = os.path.normpath(os.path.join(dirname, wcard))
-            for listed in files:
-                filename = os.path.normpath(os.path.join(dirname, listed))
-                if not any(bad in filename for bad in badnames):
-                    if fnmatch.fnmatch(filename, wc_name) and not os.path.isdir(
-                        filename
-                    ):
-                        names.append(filename)
-        if names:
-            lst.append(("/etc/lava-server/dispatcher-config/device-types", names))
-
-    file_list = []
-    walk_helper(
-        (file_list, wildcards),
-        srcdir,
-        [
-            os.path.basename(f)
-            for f in glob.glob(os.path.normpath(os.path.join(srcdir, "*")))
-        ],
-    )
-    return file_list
+def modules(name):
+    pkgs = [name + "." + p for p in find_packages(name)]
+    return [name] + pkgs
 
 
-setup(
-    name="lava",
-    version=__version__,
-    author="Neil Williams",
-    author_email="lava-team@linaro.org",
-    namespace_packages=["lava"],
-    packages=find_packages(),
-    test_suite="lava_server.tests.run_tests",
-    license="AGPL",
-    description="LAVA",
-    long_description="""
-     LAVA is a continuous integration system for deploying operating
-     systems onto physical and virtual hardware for running tests.
-     Tests can be simple boot testing, bootloader testing and system
-     level testing. Extra hardware may be required for some
-     system tests. Results are tracked over time and data can be
-    exported for further analysis.
-    """,
-    url="https://www.lavasoftware.org/",
-    package_data={
-        "lava_dispatcher": [
-            "dynamic_vm_keys/lava*",
-            "devices/*.yaml",
-            "lava_test_shell/lava-add-keys",
-            "lava_test_shell/lava-add-sources",
-            "lava_test_shell/lava-background-process-start",
-            "lava_test_shell/lava-background-process-stop",
-            "lava_test_shell/lava-echo-ipv4",
-            "lava_test_shell/lava-installed-packages",
-            "lava_test_shell/lava-install-packages",
-            "lava_test_shell/lava-lxc-device-add",
-            "lava_test_shell/lava-lxc-device-wait-add",
-            "lava_test_shell/lava-os-build",
-            "lava_test_shell/lava-probe-channel",
-            "lava_test_shell/lava-probe-ip",
-            "lava_test_shell/lava-target-ip",
-            "lava_test_shell/lava-target-mac",
-            "lava_test_shell/lava-target-storage",
-            "lava_test_shell/lava-test-case",
-            "lava_test_shell/lava-test-feedback",
-            "lava_test_shell/lava-test-raise",
-            "lava_test_shell/lava-test-reference",
-            "lava_test_shell/lava-test-runner",
-            "lava_test_shell/lava-test-set",
-            "lava_test_shell/lava-test-shell",
-            "lava_test_shell/multi_node/*",
-            "lava_test_shell/vland/*",
-            "lava_test_shell/lmp/*",
-            "lava_test_shell/distro/fedora/*",
-            "lava_test_shell/distro/android/*",
-            "lava_test_shell/distro/ubuntu/*",
-            "lava_test_shell/distro/debian/*",
-            "lava_test_shell/distro/oe/*",
-        ]
-    },
-    scripts=[
-        "lava/dispatcher/lava-run",
-        "lava/dispatcher/lava-slave",
-        "lava_dispatcher_host/lava-dispatcher-host",
-    ],
-    data_files=[
-        ("/usr/share/lava-dispatcher/", ["etc/tftpd-hpa", "etc/dispatcher.yaml"]),
-        ("/etc/exports.d", ["etc/lava-dispatcher-nfs.exports"]),
-        ("/etc/modprobe.d/", ["etc/lava-modules.conf"]),
-        ("/etc/logrotate.d/", ["etc/logrotate.d/lava-slave-log"]),
-        ("/usr/share/lava-dispatcher/", ["etc/lava-slave.service"]),
-        ("/etc/lava-server", ["etc/settings.conf", "etc/env.yaml"]),
-        ("/etc/apache2/sites-available", ["etc/lava-server.conf"]),
+COMMON = {
+    "name": "lava-common",
+    "description": "LAVA common",
+    "packages": modules("lava_common"),
+    "scripts": [],
+    "data_files": [
         (
-            "/etc/logrotate.d",
+            "/usr/share/lava-common/",
+            ["share/create_certificate.py", "share/lava-schema.py"],
+        )
+    ],
+}
+
+COORDINATOR = {
+    "name": "lava-coordinator",
+    "description": "LAVA coordinator",
+    "packages": ["lava.coordinator"],
+    "scripts": ["lava/coordinator/lava-coordinator.py"],
+    "data_files": [
+        ("/etc/lava-coordinator/", ["etc/lava-coordinator.conf"]),
+        ("/etc/logrotate.d/", ["etc/logrotate.d/lava-coordinator-log"]),
+    ],
+    "cmdclass": {"install_scripts": rename_scripts},
+}
+
+DISPATCHER = {
+    "name": "lava-dispatcher",
+    "description": "LAVA dispatcher",
+    "packages": modules("lava_dispatcher"),
+    "package_data": {
+        "lava_dispatcher": ["dynamic_vm_keys/lava*", "lava_test_shell/**"]
+    },
+    "scripts": ["lava/dispatcher/lava-run", "lava/dispatcher/lava-slave"],
+    "data_files": [
+        ("/etc/exports.d/", ["etc/lava-dispatcher-nfs.exports"]),
+        ("/etc/lava-dispatcher/", ["etc/lava-slave"]),
+        ("/etc/lava-dispatcher/certificates.d/", []),
+        ("/etc/logrotate.d/", ["etc/logrotate.d/lava-slave-log"]),
+        ("/etc/modprobe.d/", ["etc/lava-modules.conf"]),
+        ("/lib/systemd/system/", ["etc/lava-slave.service"]),
+        ("/etc/systemd/system/systemd-udevd.service.d/", ["etc/udev/override.conf"]),
+        (
+            "/usr/share/lava-dispatcher/",
+            ["etc/tftpd-hpa", "share/lava_lxc_device_add.py"],
+        ),
+        ("/usr/share/lava-dispatcher/apache2/", ["share/apache2/lava-dispatcher.conf"]),
+        ("/var/lib/lava/dispatcher/tmp/", []),
+        ("/var/log/lava-dispatcher/", []),
+    ],
+}
+
+DISPATCHER_HOST = {
+    "name": "lava-dispatcher-host",
+    "description": "LAVA dispatcher host",
+    "packages": modules("lava_dispatcher_host"),
+    "scripts": ["lava_dispatcher_host/lava-dispatcher-host"],
+}
+
+SERVER = {
+    "name": "lava-server",
+    "description": "LAVA server",
+    "packages": (
+        modules("lava_rest_app")
+        + modules("lava_results_app")
+        + modules("lava_scheduler_app")
+        + modules("lava_server")
+        + modules("linaro_django_xmlrpc")
+    ),
+    "scripts": ["manage.py"],
+    "data_files": [
+        ("/etc/apache2/sites-available/", ["etc/lava-server.conf"]),
+        ("/etc/lava-dispatcher/certificates.d/", []),
+        (
+            "/etc/lava-server/",
+            [
+                "etc/env.yaml",
+                "etc/lava-logs",
+                "etc/lava-master",
+                "etc/lava-server-gunicorn",
+                "etc/settings.conf",
+            ],
+        ),
+        ("/etc/lava-server/dispatcher-config/devices/", []),
+        (
+            "/etc/lava-server/dispatcher-config/device-types/",
+            glob.glob("etc/dispatcher-config/device-types/*.jinja2"),
+        ),
+        ("/etc/lava-server/dispatcher-config/health-checks/", []),
+        (
+            "/etc/logrotate.d/",
             [
                 "etc/logrotate.d/django-log",
+                "etc/logrotate.d/lava-logs-log",
                 "etc/logrotate.d/lava-master-log",
                 "etc/logrotate.d/lava-publisher-log",
                 "etc/logrotate.d/lava-server-gunicorn-log",
             ],
         ),
+        ("/etc/lava-server/dispatcher.d/", []),
         (
-            "/usr/share/lava-server",
+            "/lib/systemd/system/",
             [
+                "etc/lava-logs.service",
                 "etc/lava-master.service",
                 "etc/lava-publisher.service",
-                "etc/lava-logs.service",
-                "etc/instance.conf.template",
-                "share/render-template.py",
+                "etc/lava-server-gunicorn.service",
             ],
         ),
+        (
+            "/usr/share/lava-server/",
+            ["etc/dispatcher.yaml", "etc/instance.conf.template", "share/postinst.py"],
+        ),
+        ("/var/lib/lava-server/default/media/job-output/", []),
+        ("/var/log/lava-server/", []),
     ],
-    tests_require=["django-testscenarios >= 0.9.1"],
-    zip_safe=False,
-    include_package_data=True,
-)
+    "cmdclass": {"install_scripts": rename_scripts},
+}
+
+PKGS = {
+    "lava-common": COMMON,
+    "lava-coordinator": COORDINATOR,
+    "lava-dispatcher": DISPATCHER,
+    "lava-dispatcher-host": DISPATCHER_HOST,
+    "lava-server": SERVER,
+}
+
+
+def merge_data_files(srcs):
+    data_files = [{i[0]: i[1] for i in src} for src in srcs if src]
+    ret = {}
+    for data in data_files:
+        for (k, v) in data.items():
+            if k in ret:
+                ret[k].extend(v)
+            else:
+                ret[k] = v
+    return sorted((k, sorted(set(v))) for (k, v) in ret.items())
+
+
+if sys.argv[-1].startswith("lava-"):
+    # For lava-server
+    # TODO: check file ownership
+    pkg = sys.argv.pop()
+    setup(**PKGS[pkg])
+else:
+    LAVA = {
+        "name": "lava",
+        "description": "LAVA",
+        "packages": sorted(set([n for p in PKGS for n in PKGS[p]["packages"]])),
+        "package_data": PKGS["lava-dispatcher"]["package_data"],
+        "scripts": sorted([n for p in PKGS for n in PKGS[p].get("scripts", [])]),
+        "data_files": merge_data_files([PKGS[p].get("data_files", []) for p in PKGS]),
+        "cmdclass": PKGS["lava-server"]["cmdclass"],
+    }
+    setup(**LAVA)
