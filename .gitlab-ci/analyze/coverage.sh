786f48c5a079e7b544445c5bbbe6019974b0af53
Rémi Duraffort <remi.duraffort@linaro.org>
2020-01-29 14:28:29 +0100

    CI: coverage is already running under Buster
    Fix the setup script.
    
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

--- .gitlab-ci/analyze/coverage.sh
+++ .gitlab-ci/analyze/coverage.sh
@@ -8,13 +8,9 @@ if [ "$1" = "setup" ]
 then
   set -x
   apt-get -q update
-  DEPS=$(./share/requires.py -p lava-dispatcher -d debian -s stretch -n)
+  DEPS=$(./share/requires.py -p lava-dispatcher -d debian -s buster -n)
   apt-get install --no-install-recommends --yes $DEPS
-  DEPS=$(./share/requires.py -p lava-dispatcher -d debian -s stretch-backports -n)
-  apt-get install --no-install-recommends --yes $DEPS
-  DEPS=$(./share/requires.py -p lava-server -d debian -s stretch -n)
-  apt-get install --no-install-recommends --yes $DEPS
-  DEPS=$(./share/requires.py -p lava-server -d debian -s stretch-backports -n)
+  DEPS=$(./share/requires.py -p lava-server -d debian -s buster -n)
   apt-get install --no-install-recommends --yes $DEPS
 else
   set -x
