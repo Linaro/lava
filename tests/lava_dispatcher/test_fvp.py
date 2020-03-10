41395535bf463272cdc3dbf6c72825bdbba312fb
Rémi Duraffort <remi.duraffort@linaro.org>
2020-03-10 17:35:35 +0100

    CI: fix error found by pylint
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

--- tests/lava_dispatcher/test_fvp.py
+++ tests/lava_dispatcher/test_fvp.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along
 # with this program; if not, see <http://www.gnu.org/licenses>.
-from lava_common.exceptions import JobError
 from lava_dispatcher.action import Action
 from tests.lava_dispatcher.test_basic import StdoutTestCase, Factory
 from lava_dispatcher.actions.deploy import docker
