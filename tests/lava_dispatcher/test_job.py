Linaro/lava

commit 05af31736ad5476bd1c437479b3c0ef7a6307190

dispatcher: ensure that Job.tmp_dir is never None

lava_dispatcher/job.py
@@ -80,8 +80,6 @@ class Job:
         self.compatibility = 2
         # Was the job cleaned
         self.cleaned = False
-        # Root directory for the job tempfiles
-        self.tmp_dir = None
         # override in use
         self.base_overrides = {}
         self.started = False
@@ -114,6 +112,11 @@ class Job:
             "pipeline": self.pipeline.describe(),
         }
 
+    @property
+    def tmp_dir(self):
+        prefix = self.parameters.get("dispatcher", {}).get("prefix", "")
+        return os.path.join(DISPATCHER_DOWNLOAD_DIR, "%s%s" % (prefix, self.job_id))
+
     def mkdtemp(self, action_name, override=None):
         """
         Create a tmp directory in DISPATCHER_DOWNLOAD_DIR/{job_id}/ because
@@ -121,12 +124,8 @@ class Job:
         easier.
         """
         if override is None:
-            if self.tmp_dir is None:
-                create_base_dir = True
-                base_dir = DISPATCHER_DOWNLOAD_DIR
-            else:
-                create_base_dir = False
-                base_dir = self.tmp_dir
+            create_base_dir = True
+            base_dir = self.tmp_dir
         else:
             if override in self.base_overrides:
                 create_base_dir = False
@@ -137,13 +136,9 @@ class Job:
 
         if create_base_dir:
             # Try to create the directory.
-            prefix = self.parameters["dispatcher"].get("prefix", "")
-            base_dir = os.path.join(base_dir, "%s%s" % (prefix, self.job_id))
             os.makedirs(base_dir, mode=0o755, exist_ok=True)
             # Save the path for the next calls (only if that's not an override)
-            if override is None:
-                self.tmp_dir = base_dir
-            else:
+            if override:
                 self.base_overrides[override] = base_dir
 
         # Create the sub-directory

commit 589ba328a3d452b16f87e5a5ddb5b82f1e06c201

dispatcher: fix Job.mkdtemp with overriden base dir
This fixes a regression introduced in 05af31736ad5476bd1c437479b3c0ef7a6307190

lava_dispatcher/job.py
@@ -114,8 +114,11 @@ class Job:
 
     @property
     def tmp_dir(self):
+        return self.get_basedir(DISPATCHER_DOWNLOAD_DIR)
+
+    def get_basedir(self, path):
         prefix = self.parameters.get("dispatcher", {}).get("prefix", "")
-        return os.path.join(DISPATCHER_DOWNLOAD_DIR, "%s%s" % (prefix, self.job_id))
+        return os.path.join(path, "%s%s" % (prefix, self.job_id))
 
     def mkdtemp(self, action_name, override=None):
         """
@@ -132,7 +135,7 @@ class Job:
                 base_dir = self.base_overrides[override]
             else:
                 create_base_dir = True
-                base_dir = override
+                base_dir = self.get_basedir(override)
 
         if create_base_dir:
             # Try to create the directory.

tests/lava_dispatcher/test_job.py
@@ -1,0 +1,44 @@
+from pathlib import Path
+import pytest
+from lava_dispatcher.job import Job
+
+
+@pytest.fixture
+def job():
+    return Job(42, {}, None)
+
+
+def test_tmp_dir(job):
+    assert job.tmp_dir is not None
+    tmp_dir = Path(job.tmp_dir)
+    assert not tmp_dir.exists()
+    assert tmp_dir.name == "42"
+
+
+def test_tmp_dir_with_prefix(job):
+    job.parameters["dispatcher"] = {"prefix": "FOOBAR-"}
+    tmp_dir = Path(job.tmp_dir)
+    assert tmp_dir.name == "FOOBAR-42"
+
+
+def test_mkdtemp(job):
+    d = job.mkdtemp("my-action")
+    assert Path(d).exists()
+    assert "my-action" in d
+
+
+def test_mkdtemp_with_prefix(job):
+    job.parameters["dispatcher"] = {"prefix": "FOOBAR-"}
+    d = Path(job.mkdtemp("my-action"))
+    assert d.parent.name == "FOOBAR-42"
+
+
+def test_mktemp_with_override(job, tmpdir):
+    override = tmpdir / "override"
+    first = Path(job.mkdtemp("my-action", override=override))
+    second = Path(job.mkdtemp("my-assert", override=override))
+    assert first.exists()
+    assert second.exists()
+    assert first != second
+    assert first.parent == second.parent
+    assert first.parent.name == str(job.job_id)