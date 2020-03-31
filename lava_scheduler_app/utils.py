Linaro/lava

commit 578b99831db12b0a617b515a22dd8409f065673b

Move encryption settings to django settings instead of the cmd args.
We need to move encryption file location settings to django
settings so that the encryption keys can be downloaded/uploaded
via the API.

lava_scheduler_app/utils.py
@@ -22,6 +22,7 @@ import copy
 import errno
 import os
 import subprocess  # nosec verified
+import warnings
 
 from django.conf import settings
 
@@ -319,3 +320,22 @@ def get_ldap_user_properties(ldap_user):
                     return user_properties
             except ldap.NO_SUCH_OBJECT:
                 raise
+
+
+def get_encryption_settings(self, options):
+    # Get encryption settings from command line arguments. If not present,
+    # fall back to django settings.
+    encryption_settings = {}
+    encryption_settings["encrypt"] = False
+    if options["encrypt"]:
+        warnings.warn(
+            "Using encryption options in command line is deprecated. Switch to django settings module for encryption configuration.",
+            DeprecationWarning,
+        )
+        encryption_settings["encrypt"] = True
+        encryption_settings["master_cert"] = options["master_cert"]
+        encryption_settings["slaves_certs"] = options["slaves_certs"]
+    elif settings.ENCRYPT:
+        encryption_settings["encrypt"] = True
+        encryption_settings["master_cert"] = settings.MASTER_CERT
+        encryption_settings["slaves_certs"] = settings.SLAVES_CERTS

lava_server/management/commands/lava-logs.py
@@ -37,7 +37,7 @@ from lava_results_app.models import TestCase
 from lava_server.cmdutils import LAVADaemonCommand, watch_directory
 from lava_scheduler_app.models import TestJob
 from lava_scheduler_app.signals import send_event
-from lava_scheduler_app.utils import mkdir
+from lava_scheduler_app.utils import mkdir, get_encryption_settings
 from lava_scheduler_app.logutils import line_count, write_logs
 from lava_results_app.dbutils import map_scanned_results, create_metadata_store
 
@@ -158,21 +158,26 @@ class Command(LAVADaemonCommand):
             self.log_socket.setsockopt(zmq.IPV6, 1)
             self.controler.setsockopt(zmq.IPV6, 1)
 
-        if options["encrypt"]:
+        encryption_settings = get_encryption_settings(options)
+        if encryption_settings["encrypt"]:
             self.logger.info("[INIT] Starting encryption")
             try:
                 self.auth = ThreadAuthenticator(context)
                 self.auth.start()
                 self.logger.debug(
-                    "[INIT] Opening master certificate: %s", options["master_cert"]
+                    "[INIT] Opening master certificate: %s",
+                    encryption_settings["master_cert"],
                 )
                 master_public, master_secret = zmq.auth.load_certificate(
-                    options["master_cert"]
+                    encryption_settings["master_cert"]
                 )
                 self.logger.debug(
-                    "[INIT] Using slaves certificates from: %s", options["slaves_certs"]
+                    "[INIT] Using slaves certificates from: %s",
+                    encryption_settings["slaves_certs"],
                 )
-                self.auth.configure_curve(domain="*", location=options["slaves_certs"])
+                self.auth.configure_curve(
+                    domain="*", location=encryption_settings["slaves_certs"]
+                )
             except OSError as err:
                 self.logger.error("[INIT] %s", err)
                 self.auth.stop()
@@ -184,9 +189,9 @@ class Command(LAVADaemonCommand):
             self.controler.curve_secretkey = master_secret
             self.controler.curve_serverkey = master_public
 
-        self.logger.debug("[INIT] Watching %s", options["slaves_certs"])
-        self.cert_dir_path = options["slaves_certs"]
-        self.inotify_fd = watch_directory(options["slaves_certs"])
+        self.logger.debug("[INIT] Watching %s", encryption_settings["slaves_certs"])
+        self.cert_dir_path = encryption_settings["slaves_certs"]
+        self.inotify_fd = watch_directory(encryption_settings["slaves_certs"])
         if self.inotify_fd is None:
             self.logger.error("[INIT] Unable to start inotify")
 
@@ -240,7 +245,7 @@ class Command(LAVADaemonCommand):
             self.flush_test_cases()
             self.logger.info("[EXIT] Closing the logging socket: the queue is empty")
             self.log_socket.close()
-            if options["encrypt"]:
+            if encryption_settings["encrypt"]:
                 self.auth.stop()
             context.term()
 

lava_server/management/commands/lava-master.py
@@ -41,7 +41,7 @@ from lava_results_app.models import TestCase, TestSuite
 from lava_scheduler_app.dbutils import parse_job_description
 from lava_scheduler_app.models import TestJob, Worker
 from lava_scheduler_app.scheduler import schedule
-from lava_scheduler_app.utils import mkdir
+from lava_scheduler_app.utils import mkdir, get_encryption_settings
 from lava_server.cmdutils import LAVADaemonCommand, watch_directory
 
 
@@ -652,21 +652,26 @@ class Command(LAVADaemonCommand):
             self.controler.setsockopt(zmq.IPV6, 1)
             self.event_socket.setsockopt(zmq.IPV6, 1)
 
-        if options["encrypt"]:
+        encryption_settings = get_encryption_settings(options)
+        if encryption_settings["encrypt"]:
             self.logger.info("[INIT] Starting encryption")
             try:
                 self.auth = ThreadAuthenticator(context)
                 self.auth.start()
                 self.logger.debug(
-                    "[INIT] Opening master certificate: %s", options["master_cert"]
+                    "[INIT] Opening master certificate: %s",
+                    encryption_settings["master_cert"],
                 )
                 master_public, master_secret = zmq.auth.load_certificate(
-                    options["master_cert"]
+                    encryption_settings["master_cert"]
                 )
                 self.logger.debug(
-                    "[INIT] Using slaves certificates from: %s", options["slaves_certs"]
+                    "[INIT] Using slaves certificates from: %s",
+                    encryption_settings["slaves_certs"],
                 )
-                self.auth.configure_curve(domain="*", location=options["slaves_certs"])
+                self.auth.configure_curve(
+                    domain="*", location=encryption_settings["slaves_certs"]
+                )
             except OSError as err:
                 self.logger.error(err)
                 self.auth.stop()
@@ -675,8 +680,8 @@ class Command(LAVADaemonCommand):
             self.controler.curve_secretkey = master_secret
             self.controler.curve_server = True
 
-            self.logger.debug("[INIT] Watching %s", options["slaves_certs"])
-            self.inotify_fd = watch_directory(options["slaves_certs"])
+            self.logger.debug("[INIT] Watching %s", encryption_settings["slaves_certs"])
+            self.inotify_fd = watch_directory(encryption_settings["slaves_certs"])
             if self.inotify_fd is None:
                 self.logger.error("[INIT] Unable to start inotify")
 
@@ -717,12 +722,13 @@ class Command(LAVADaemonCommand):
             )
             self.controler.close(linger=0)
             self.event_socket.close(linger=0)
-            if options["encrypt"]:
+            if encryption_settings["encrypt"]:
                 self.auth.stop()
             context.term()
 
     def main_loop(self, options):
         last_schedule = last_dispatcher_check = time.time()
+        encryption_settings = get_encryption_settings(options)
 
         while True:
             try:
@@ -772,15 +778,15 @@ class Command(LAVADaemonCommand):
                     if self.auth is not None:
                         self.logger.info(
                             "[AUTH] Reloading certificates from %s",
-                            options["slaves_certs"],
+                            encryption_settings["slaves_certs"],
                         )
                         self.auth.configure_curve(
-                            domain="*", location=options["slaves_certs"]
+                            domain="*", location=encryption_settings["slaves_certs"]
                         )
                     else:
                         self.logger.error(
                             "[AUTH] New certificates in %s but encryption is disabled",
-                            options["slaves_certs"],
+                            encryption_settings["slaves_certs"],
                         )
 
                 # Check dispatchers status

lava_server/settings/common.py
@@ -238,3 +238,8 @@ EXTRA_CONTEXT_VARIABLES = []
 
 # Default length value for all tables
 DEFAULT_TABLE_LENGTH = 25
+
+# Encryption settings
+ENCRYPT = False
+MASTER_CERT = "/etc/lava-dispatcher/certificates.d/master.key_secret"
+SLAVES_CERTS = "/etc/lava-dispatcher/certificates.d/"

commit 67df57729552ef8dd427fda80df1914f0a375ff4

Fix crashes introduced by 578b99831d
Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

lava_scheduler_app/utils.py
@@ -322,11 +322,15 @@ def get_ldap_user_properties(ldap_user):
                 raise
 
 
-def get_encryption_settings(self, options):
+def get_encryption_settings(options):
     # Get encryption settings from command line arguments. If not present,
     # fall back to django settings.
-    encryption_settings = {}
-    encryption_settings["encrypt"] = False
+    encryption_settings = {
+        "encrypt": settings.ENCRYPT,
+        "master_cert": settings.MASTER_CERT,
+        "slaves_certs": settings.SLAVES_CERTS,
+    }
+
     if options["encrypt"]:
         warnings.warn(
             "Using encryption options in command line is deprecated. Switch to django settings module for encryption configuration.",
@@ -335,7 +339,5 @@ def get_encryption_settings(self, options):
         encryption_settings["encrypt"] = True
         encryption_settings["master_cert"] = options["master_cert"]
         encryption_settings["slaves_certs"] = options["slaves_certs"]
-    elif settings.ENCRYPT:
-        encryption_settings["encrypt"] = True
-        encryption_settings["master_cert"] = settings.MASTER_CERT
-        encryption_settings["slaves_certs"] = settings.SLAVES_CERTS
+
+    return encryption_settings