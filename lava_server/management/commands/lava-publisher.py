6d6f3c45168364072b1a42550e5371c9fccd7153
Rémi Duraffort <remi.duraffort@linaro.org>
2020-06-08 15:33:38 +0200

    lava-publisher: push events using event sockets
    lava-publisher is using aiohttp to handle web sockets. Apache2 is forwarding
    /ws/ requests to lava-publisher at port 8001.
    
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

--- lava_server/management/commands/lava-publisher.py
+++ lava_server/management/commands/lava-publisher.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2015-2018 Linaro Limited
+# Copyright (C) 2015-present Linaro Limited
 #
 # Author: Remi Duraffort <remi.duraffort@linaro.org>
 #
@@ -17,8 +17,12 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with LAVA.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
+import aiohttp
+from aiohttp import web
+import asyncio
+import weakref
 import zmq
+import zmq.asyncio
 from zmq.utils.strtypes import u
 
 from django.conf import settings
@@ -31,42 +35,120 @@ TIMEOUT = 5
 FORMAT = "%(asctime)-15s %(levelname)7s %(message)s"
 
 
+async def zmq_proxy(app):
+    logger = app["logger"]
+
+    context = zmq.asyncio.Context()
+
+    logger.info("[INIT] Create input socket at %r", settings.INTERNAL_EVENT_SOCKET)
+    pull = context.socket(zmq.PULL)
+    pull.bind(settings.INTERNAL_EVENT_SOCKET)
+
+    logger.info("[INIT] Create the pub socket at %r", settings.EVENT_SOCKET)
+    pub = context.socket(zmq.PUB)
+    pub.setsockopt(zmq.HEARTBEAT_IVL, 5000)
+    pub.setsockopt(zmq.HEARTBEAT_TIMEOUT, 15000)
+    pub.setsockopt(zmq.HEARTBEAT_TTL, 15000)
+    pub.bind(settings.EVENT_SOCKET)
+
+    if settings.EVENT_ADDITIONAL_SOCKETS:
+        logger.info("[INIT] Creating the additional sockets:")
+    additional_sockets = []
+    for url in settings.EVENT_ADDITIONAL_SOCKETS:
+        logger.info("[INIT]  * %r", url)
+        sock = context.socket(zmq.PUSH)
+        # Allow zmq to keep 10000 pending messages in each queue
+        sock.setsockopt(zmq.SNDHWM, 10000)
+        # Ask zmq to send heart beats
+        # See api.zeromq.org/4-2:zmq-setsockopt#toc17
+        sock.setsockopt(zmq.HEARTBEAT_IVL, 5000)
+        sock.setsockopt(zmq.HEARTBEAT_TIMEOUT, 15000)
+        sock.setsockopt(zmq.HEARTBEAT_TTL, 15000)
+        # connect
+        sock.connect(url)
+        additional_sockets.append(sock)
+
+    async def forward_event(msg):
+        app["logger"].debug("[PROXY] Forwarding: %s", msg)
+        data = [s.decode("utf-8") for s in msg]
+        futures = [
+            pub.send_multipart(msg),
+            *[ws.send_json(data) for ws in app["websockets"]],
+            *[s.send_multipart(msg, flags=zmq.DONTWAIT) for s in additional_sockets],
+        ]
+        await asyncio.gather(*futures)
+
+    try:
+        while True:
+            try:
+                msg = await pull.recv_multipart()
+                await forward_event(msg)
+            except zmq.error.ZMQError as exc:
+                logger.error("[PROXY] Received a ZMQ error: %s", exc)
+    except asyncio.CancelledError:
+        # Carefully close the logging socket as we don't want to lose messages
+        logger.info("[EXIT] Disconnect pull socket and process messages")
+        endpoint = u(pull.getsockopt(zmq.LAST_ENDPOINT))
+        logger.debug("[EXIT] unbinding from %r", endpoint)
+        pull.unbind(endpoint)
+
+        while True:
+            try:
+                msg = await asyncio.wait_for(pull.recv_multipart(), TIMEOUT)
+                await forward_event(msg)
+            except zmq.error.ZMQError as exc:
+                logger.error("[EXIT] Received a ZMQ error: %s", exc)
+            except asyncio.TimeoutError:
+                break
+    logger.info("[EXIT] Closing the sockets: the queue is empty")
+    pull.close(linger=1)
+    pub.close(linger=1)
+    for socket in additional_sockets:
+        socket.close(linger=1)
+    context.term()
+
+
+async def websocket_handler(request):
+    logger = request.app["logger"]
+    logger.info("[WS] connection from %r", request.remote)
+
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+    request.app["websockets"].add(ws)
+
+    try:
+        async for msg in ws:
+            if msg.type == aiohttp.WSMsgType.ERROR:
+                logger.exception(ws.exception())
+    finally:
+        request.app["websockets"].discard(ws)
+
+    logger.info("[WS] connection closed from %r", request.remote)
+    return ws
+
+
+async def on_startup(app):
+    app["zmq_proxy"] = asyncio.create_task(zmq_proxy(app))
+
+
+async def on_shutdown(app):
+    # Stop the zmq proxy
+    if app["zmq_proxy"] is not None:
+        app["zmq_proxy"].cancel()
+        await app["zmq_proxy"]
+
+    for ws in set(app["websockets"]):
+        await ws.close(code=aiohttp.WSCloseCode.GOING_AWAY, message="Server shutdown")
+
+
 class Command(LAVADaemonCommand):
     help = "LAVA event publisher"
     default_logfile = "/var/log/lava-server/lava-publisher.log"
 
-    def forward_event(self, leaving):
-        try:
-            sockets = dict(self.poller.poll(TIMEOUT * 1000 if leaving else None))
-        except zmq.error.ZMQError as exc:
-            self.logger.error("[POLL] Received a ZMQ error: %s", exc)
-            return True
-
-        if sockets.get(self.pipe_r) == zmq.POLLIN:
-            # remove the message from the queue
-            os.read(self.pipe_r, 1)
-            if leaving:
-                self.logger.warning(
-                    "[POLL] signal already handled, please wait for the process to exit"
-                )
-                return True
-            else:
-                self.logger.info("[POLL] received a signal, leaving")
-                return False
-
-        if sockets.get(self.pull) == zmq.POLLIN:
-            msg = self.pull.recv_multipart()
-            self.logger.debug("[POLL] Forwarding: %s", msg)
-            self.pub.send_multipart(msg)
-            for (i, sock) in enumerate(self.additional_sockets):
-                # Send in non blocking mode and print an error message if
-                # the HWM is reached (because the receiver does not grab
-                # the messages fast enough).
-                try:
-                    sock.send_multipart(msg, flags=zmq.DONTWAIT)
-                except zmq.error.Again:
-                    self.logger.warning("[POLL] Fail to forward to socket %d", i)
-        return not leaving
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument("--host", default="*", help="host to bind to")
+        parser.add_argument("--port", default=8001, type=int, help="port to bind to")
 
     def handle(self, *args, **options):
         self.setup_logging(
@@ -87,66 +169,23 @@ class Command(LAVADaemonCommand):
                 "LAVA won't generated any events"
             )
 
+        # Create the aiohttp application
+        app = web.Application()
+
+        # Variables
+        app["logger"] = self.logger
+        app["websockets"] = weakref.WeakSet()
+        app["zmq_proxy"] = None
+
+        # Routes
+        app.add_routes([web.get("/ws/", websocket_handler)])
+
+        # signals
+        app.on_startup.append(on_startup)
+        app.on_shutdown.append(on_shutdown)
+
+        # Run the application
         self.logger.info(
-            "[INIT] Creating the input socket at %s", settings.INTERNAL_EVENT_SOCKET
+            "[INIT] Listening on http://%s:%d", options["host"], options["port"]
         )
-        context = zmq.Context.instance()
-        self.pull = context.socket(zmq.PULL)
-        self.pull.bind(settings.INTERNAL_EVENT_SOCKET)
-        self.poller = zmq.Poller()
-        self.poller.register(self.pull, zmq.POLLIN)
-
-        # Translate signals into zmq messages
-        (self.pipe_r, _) = self.setup_zmq_signal_handler()
-        self.poller.register(self.pipe_r, zmq.POLLIN)
-
-        # Create the default publishing socket
-        self.logger.info(
-            "[INIT] Creating the publication socket at %s", settings.EVENT_SOCKET
-        )
-        self.pub = context.socket(zmq.PUB)
-        # Ask zmq to send heart beats
-        # See api.zeromq.org/4-2:zmq-setsockopt#toc17
-        self.pub.setsockopt(zmq.HEARTBEAT_IVL, 5000)
-        self.pub.setsockopt(zmq.HEARTBEAT_TIMEOUT, 15000)
-        self.pub.setsockopt(zmq.HEARTBEAT_TTL, 15000)
-        # bind
-        self.pub.bind(settings.EVENT_SOCKET)
-        # Create the additional PUSH sockets
-        if settings.EVENT_ADDITIONAL_SOCKETS:
-            self.logger.info("[INIT] Creating the additional sockets:")
-        self.additional_sockets = []
-        for url in settings.EVENT_ADDITIONAL_SOCKETS:
-            self.logger.info("[INIT]  * %s", url)
-            sock = context.socket(zmq.PUSH)
-            # Allow zmq to keep 10000 pending messages in each queue
-            sock.setsockopt(zmq.SNDHWM, 10000)
-            # Ask zmq to send heart beats
-            # See api.zeromq.org/4-2:zmq-setsockopt#toc17
-            self.pub.setsockopt(zmq.HEARTBEAT_IVL, 5000)
-            self.pub.setsockopt(zmq.HEARTBEAT_TIMEOUT, 15000)
-            self.pub.setsockopt(zmq.HEARTBEAT_TTL, 15000)
-            # connect
-            sock.connect(url)
-            self.additional_sockets.append(sock)
-
-        self.logger.info("[INIT] Starting the proxy")
-        while self.forward_event(False):
-            pass
-
-        # Carefully close the logging socket as we don't want to lose messages
-        self.logger.info("[EXIT] Disconnect pull socket and process messages")
-        endpoint = u(self.pull.getsockopt(zmq.LAST_ENDPOINT))
-        self.logger.debug("[EXIT] unbinding from '%s'", endpoint)
-        self.pull.unbind(endpoint)
-
-        while self.forward_event(True):
-            pass
-
-        # Close the sockets allowing 1s each to leave
-        self.logger.info("[EXIT] Closing the sockets: the queue is empty")
-        self.pull.close(linger=1)
-        self.pub.close(linger=1)
-        for socket in self.additional_sockets:
-            socket.close(linger=1)
-        context.term()
+        web.run_app(app, host=options["host"], port=options["port"], print=False)
