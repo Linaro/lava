f3f84d602fe0d92890809a92ee962d1c72e7c10b
Antonio Terceiro <antonio.terceiro@linaro.org>
2020-06-09 15:46:54 -0300

    lava-dispatcher-host: setup general logging with --debug-log
    This allows to catch later messages instead of just logging the command
    line arguments.

--- lava_dispatcher_host/cmdline.py
+++ lava_dispatcher_host/cmdline.py
@@ -70,6 +70,10 @@ def setup_logger(data):
 
 def finish_logger():
     if logger:
+        try:
+            logger.close
+        except AttributeError:
+            return
         logger.close(linger=LINGER)
 
 
@@ -192,7 +196,11 @@ def main(argv):
 
     if options.func:
         if options.debug_log:
-            options.debug_log.write("Called with args %r\n" % argv)
+            global logger
+            logger = logging.getLogger("dispatcher")
+            logger.addHandler(logging.StreamHandler(options.debug_log))
+            logger.setLevel(logging.DEBUG)
+            logger.debug("Called with args %r" % argv)
         options.func(options)
         if options.debug_log:
             options.debug_log.close()
