0576ce358050760de497e571b0df776746d4a34b
Rémi Duraffort <remi.duraffort@linaro.org>
2020-06-09 14:19:58 +0200

    lava_dispatcher: do not crash when finalizing a connection
    This is happening when:
    1/ the serial connection was closed by the remote host
    2/ the device is using connection_commands
    
    See https://git.lavasoftware.org/lava/lava/-/merge_requests/1117#note_16289
    
    ```
    Disconnecting ShellSession
    Disconnecting from telnet: Finalise
    Traceback (most recent call last):
      File "/usr/lib/python3/dist-packages/lava_dispatcher/power.py", line 330, in run
        connection = super().run(connection, max_end_time)
      File "/usr/lib/python3/dist-packages/lava_dispatcher/action.py", line 794, in run
        return self.pipeline.run_actions(connection, max_end_time)
      File "/usr/lib/python3/dist-packages/lava_dispatcher/action.py", line 245, in run_actions
        new_connection = action.run(connection, action_max_end_time)
      File "/usr/lib/python3/dist-packages/lava_dispatcher/power.py", line 296, in run
        feedback[1].finalise()
      File "/usr/lib/python3/dist-packages/lava_dispatcher/connection.py", line 172, in finalise
        self.disconnect(reason="Finalise")
      File "/usr/lib/python3/dist-packages/lava_dispatcher/shell.py", line 220, in disconnect
        super().disconnect(reason)
      File "/usr/lib/python3/dist-packages/lava_dispatcher/connection.py", line 147, in disconnect
        self.sendcontrol("]")
      File "/usr/lib/python3/dist-packages/lava_dispatcher/connection.py", line 129, in sendcontrol
        raise LAVABug("sendcontrol")
    ```
    
    Reported-by: Larry Shen <larry.shen@nxp.com>
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

--- lava_dispatcher/connection.py
+++ lava_dispatcher/connection.py
@@ -141,25 +141,30 @@ class Connection:
             and not set(RECOGNIZED_TAGS) & set(self.tags)
         ):
             raise LAVABug("'disconnect' not implemented")
-        try:
-            if "telnet" in self.tags:
-                logger.info("Disconnecting from telnet: %s", reason)
-                self.sendcontrol("]")
-                self.sendline("quit", disconnecting=True)
-            elif "ssh" in self.tags:
-                logger.info("Disconnecting from ssh: %s", reason)
-                self.sendline("", disconnecting=True)
-                self.sendline("~.", disconnecting=True)
-            elif self.name == "LxcSession":
-                logger.info("Disconnecting from lxc: %s", reason)
-                self.sendline("", disconnecting=True)
-                self.sendline("exit", disconnecting=True)
-            elif self.name == "QemuSession":
-                logger.info("Disconnecting from qemu: %s", reason)
-            else:
-                raise LAVABug("'disconnect' not supported for %s" % self.tags)
-        except ValueError:  # protection against file descriptor == -1
+
+        if self.connected:
+            try:
+                if "telnet" in self.tags:
+                    logger.info("Disconnecting from telnet: %s", reason)
+                    self.sendcontrol("]")
+                    self.sendline("quit", disconnecting=True)
+                elif "ssh" in self.tags:
+                    logger.info("Disconnecting from ssh: %s", reason)
+                    self.sendline("", disconnecting=True)
+                    self.sendline("~.", disconnecting=True)
+                elif self.name == "LxcSession":
+                    logger.info("Disconnecting from lxc: %s", reason)
+                    self.sendline("", disconnecting=True)
+                    self.sendline("exit", disconnecting=True)
+                elif self.name == "QemuSession":
+                    logger.info("Disconnecting from qemu: %s", reason)
+                else:
+                    raise LAVABug("'disconnect' not supported for %s" % self.tags)
+            except ValueError:  # protection against file descriptor == -1
+                logger.debug("Already disconnected")
+        else:
             logger.debug("Already disconnected")
+
         self.connected = False
         if self.raw_connection:
             self.raw_connection.close(force=True)
