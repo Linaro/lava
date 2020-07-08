31a8c8b47110b66cdd40b9ac6ae78479c395188c
Antonio Terceiro <antonio.terceiro@linaro.org>
2020-07-08 13:02:47 -0300

    docker test shell: wait for container to start
    This avoids yet another timing issue, where the USB device is already
    up, but the container didn't quiet start yet, and sharing the device
    with it fails.

--- lava_dispatcher/actions/test/docker.py
+++ lava_dispatcher/actions/test/docker.py
@@ -167,6 +167,9 @@ class DockerTestShell(TestShellAction, GetBoardId, DeviceContainerMappingMixin):
         devices = get_udev_devices(
             device_info=self.device_info, logger=self.logger, required=False
         )
+
+        docker.wait()
+
         for dev in devices:
             share_device_with_container_docker(container, dev)
 
