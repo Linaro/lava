6f14eb7763db47949387df5a18ed23e3321a5e60
stevanradakovic <stevan.radakovic@linaro.org>
2021-11-25 15:07:59 +0000

    Revert "Merge branch 'flasher-docker' into 'master'"
    This reverts merge request !1340

--- lava_dispatcher/utils/docker.py
+++ lava_dispatcher/utils/docker.py
@@ -39,7 +39,6 @@ class DockerRun:
         self.__bind_mounts__ = []
         self.__environment__ = []
         self.__interactive__ = False
-        self.__privileged__ = False
         self.__tty__ = False
         self.__docker_options__ = []
         self.__docker_run_options__ = []
@@ -54,16 +53,6 @@ class DockerRun:
         run.suffix(suffix)
         run.network(params.get("network_from", None))
         run.local(params.get("local", False))
-        if "privileged" in params and params["privileged"]:
-            run.privileged()
-        if "bind_mounts" in params:
-            for bind_m in params["bind_mounts"]:
-                if ":" in bind_m:
-                    run.bind_mount(
-                        bind_m.split(":")[0], destination=bind_m.split(":")[1]
-                    )
-                else:
-                    run.bind_mount(bind_m)
         return run
 
     def local(self, local):
@@ -104,9 +93,6 @@ class DockerRun:
     def interactive(self):
         self.__interactive__ = True
 
-    def privileged(self):
-        self.__privileged__ = True
-
     def tty(self):
         self.__tty__ = True
 
@@ -127,8 +113,6 @@ class DockerRun:
         )
         if self.__interactive__:
             cmd.append("--interactive")
-        if self.__privileged__:
-            cmd.append("--privileged")
         if self.__tty__:
             cmd.append("--tty")
         if self.__name__:
