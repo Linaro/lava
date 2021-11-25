321be7af261f18898b0addc7fd70e97dcaeba461
Paweł Wieczorek <pawiecz@collabora.com>
2021-11-25 07:26:39 +0100

    lava_db_generator: Generate dummy database
    This patch uses tests side-effect of leaving generated database for
    inspection. It can be later used for further performance tests (not
    necessarily related to internal LAVA tests).
    
    To generate dummy database set up the development environment first [1]
    and then run:
    
    $ pytest-3 --reuse-db lava_db_generator/tests.py
    
    (within repository root).
    
    Please note that generating database entries takes ~100s in a
    constrained environment (VM with 2 vCPUs, 2048GB RAM).
    
    More importantly, if the command above is executed multiple times (to
    e.g. grow the data set) the database is read first with takes ~150s per
    100k entries (so ~4 minutes on second run, ~6:20 on third, ~9 on fourth
    and so on).
    
    [1] https://lava.readthedocs.io/en/latest/developer/development/setup/#from-sources
    
    Signed-off-by: Paweł Wieczorek <pawiecz@collabora.com>

---
+++ lava_db_generator/factories.py
@@ -0,0 +1,20 @@
+import factory
+
+from django.contrib.auth.models import User
+from lava_scheduler_app.models import TestJob
+
+from lava_common.decorators import nottest
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+        django_get_or_create = ("username",)
+
+    username = factory.Faker("user_name")
+
+
+@nottest
+class TestJobFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = TestJob
