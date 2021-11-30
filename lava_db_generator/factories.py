3d11a93ae6a23b49434d628e2063d79ccc0503ec
Paweł Wieczorek <pawiecz@collabora.com>
2021-11-30 23:35:51 +0100

    lava_db_generator: Create project-based viewing group relation
    This patch introduces support for creating
    "lava_scheduler_app_testjob_viewing_groups" for two main projects at
    given ratios (to the all TestJobs and between the two projects).
    
    Example of using the management command:
    
    $ python3 manage.py generatedb \
        --auth_group 10 --auth_user 10 --testjob 100 \
        --project_group 2 --project_group_ratio 0.2 --project_ratio 2 1
    
    This way viewing groups will be set for ~20% of generated TestJobs.
    First project group will be set for ~2/3 of these TestJobs.
    
    Author realizes that TestJobFactory depends on the initial data
    population and it should be improved. Current approach is used for PoC
    purposes only (famous last words).
    
    Signed-off-by: Paweł Wieczorek <pawiecz@collabora.com>

--- lava_db_generator/factories.py
+++ lava_db_generator/factories.py
@@ -1,7 +1,9 @@
 import factory
 import factory.fuzzy
 
-from django.contrib.auth.models import User
+import random
+
+from django.contrib.auth.models import User, Group
 from lava_scheduler_app.models import TestJob
 
 from lava_common.decorators import nottest
@@ -15,9 +17,33 @@ class UserFactory(factory.django.DjangoModelFactory):
     username = factory.Faker("user_name")
 
 
+class GroupFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Group
+        django_get_or_create = ("name",)
+
+    name = factory.Faker("user_name")
+
+
+class ProjectGroupFactory(GroupFactory):
+    name = factory.Sequence(lambda n: f"project{n}")
+
+
 @nottest
 class TestJobFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = TestJob
 
     submitter = factory.fuzzy.FuzzyChoice(User.objects.all())
+
+    @factory.post_generation
+    def viewing_groups(self, create, _, **kwargs):
+        if not create:
+            return
+
+        project_ratios = kwargs["project_ratios"]
+        project_groups = Group.objects.filter(name__istartswith="project")
+        project_groups = project_groups[: len(project_ratios)]
+
+        if random.random() < kwargs["project_group_ratio"]:
+            self.viewing_groups.add(random.choices(project_groups, project_ratios)[0])
--- lava_db_generator/management/commands/generatedb.py
+++ lava_db_generator/management/commands/generatedb.py
@@ -1,15 +1,31 @@
 from django.core.management.base import BaseCommand
 
-from lava_db_generator.factories import UserFactory, TestJobFactory
+from lava_db_generator.factories import (
+    UserFactory,
+    GroupFactory,
+    ProjectGroupFactory,
+    TestJobFactory,
+)
 
 
 class Command(BaseCommand):
     help = "Generates dummy database"
 
     def add_arguments(self, parser):
-        parser.add_argument("users", type=int)
-        parser.add_argument("jobs", type=int)
+        parser.add_argument("--auth_user", metavar="SIZE", type=int)
+        parser.add_argument("--auth_group", metavar="SIZE", type=int)
+        parser.add_argument("--project_group", metavar="SIZE", type=int)
+        parser.add_argument("--project_group_ratio", metavar="SIZE", type=float)
+        parser.add_argument("--project_ratios", metavar="SIZE", type=int, nargs="*")
+        parser.add_argument("--testjob", metavar="SIZE", type=int)
 
     def handle(self, *args, **options):
-        UserFactory.create_batch(size=options["users"])
-        TestJobFactory.create_batch(size=options["jobs"])
+        UserFactory.create_batch(size=options["auth_user"])
+        GroupFactory.create_batch(size=options["auth_group"])
+        ProjectGroupFactory.create_batch(size=options["project_group"])
+
+        TestJobFactory.create_batch(
+            size=options["testjob"],
+            viewing_groups__project_group_ratio=options["project_group_ratio"],
+            viewing_groups__project_ratios=options["project_ratios"],
+        )
