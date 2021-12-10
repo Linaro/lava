c7ddd9e4c37d5033cdf1e95f2d22ebb628c5ebdf
Paweł Wieczorek <pawiecz@collabora.com>
2021-12-10 19:25:27 +0100

    lava_db_generator: Add device generation
    This patch also changes TestJob generation: only jobs scheduled to the
    device can have viewing groups assigned. This approach has been chosen
    for simplicity and will probably be changed later.
    
    Signed-off-by: Paweł Wieczorek <pawiecz@collabora.com>

--- lava_db_generator/factories.py
+++ lava_db_generator/factories.py
@@ -1,8 +1,10 @@
 import factory
 import factory.fuzzy
 
+from datetime import datetime, timedelta, timezone
+
 from django.contrib.auth.models import User, Group
-from lava_scheduler_app.models import TestJob
+from lava_scheduler_app.models import DeviceType, Device, TestJob
 
 from lava_common.decorators import nottest
 
@@ -27,20 +29,45 @@ class ProjectGroupFactory(GroupFactory):
     name = factory.Sequence(lambda n: f"project{n}")
 
 
+class DeviceTypeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = DeviceType
+        django_get_or_create = ("name",)
+
+    name = factory.Sequence(lambda n: f"devicetype{n}")
+
+
+class DeviceFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Device
+        django_get_or_create = ("hostname",)
+
+    hostname = factory.Sequence(lambda n: f"device{n}")
+    device_type = factory.fuzzy.FuzzyChoice(DeviceType.objects.all())
+
+
 @nottest
 class TestJobFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = TestJob
 
+    is_public = True
     submitter = factory.fuzzy.FuzzyChoice(User.objects.all())
 
 
 @nottest
-class TestJobWithViewingGroupFactory(TestJobFactory):
+class TestJobWithActualDevice(TestJobFactory):
+    actual_device = factory.fuzzy.FuzzyChoice(Device.objects.all())
+    end_time = factory.fuzzy.FuzzyDateTime(
+        datetime.now(tz=timezone.utc),
+        datetime.now(tz=timezone.utc) + timedelta(hours=1),
+    )
+
     @factory.post_generation
     def viewing_groups(self, create, extracted, **kwargs):
         if not create:
             return
 
         if extracted:
+            self.is_public = False
             self.viewing_groups.add(extracted)
--- lava_db_generator/management/commands/generatedb.py
+++ lava_db_generator/management/commands/generatedb.py
@@ -3,11 +3,13 @@ import random
 from django.core.management.base import BaseCommand
 
 from lava_db_generator.factories import (
+    DeviceTypeFactory,
+    DeviceFactory,
     UserFactory,
     GroupFactory,
     ProjectGroupFactory,
     TestJobFactory,
-    TestJobWithViewingGroupFactory,
+    TestJobWithActualDevice,
 )
 
 
@@ -15,14 +17,20 @@ class Command(BaseCommand):
     help = "Generates dummy database"
 
     def add_arguments(self, parser):
+        parser.add_argument("--device_type", metavar="SIZE", type=int)
+        parser.add_argument("--device", metavar="SIZE", type=int)
         parser.add_argument("--auth_user", metavar="SIZE", type=int)
         parser.add_argument("--auth_group", metavar="SIZE", type=int)
         parser.add_argument("--project_group", metavar="SIZE", type=int)
         parser.add_argument("--project_group_ratio", metavar="SIZE", type=float)
         parser.add_argument("--project_ratios", metavar="SIZE", type=int, nargs="*")
+        parser.add_argument("--scheduled_ratio", metavar="SIZE", type=float)
         parser.add_argument("--testjob", metavar="SIZE", type=int)
 
     def handle(self, *args, **options):
+        DeviceTypeFactory.create_batch(size=options["device_type"])
+        DeviceFactory.create_batch(size=options["device"])
+
         UserFactory.create_batch(size=options["auth_user"])
         GroupFactory.create_batch(size=options["auth_group"])
         pgs = ProjectGroupFactory.create_batch(size=options["project_group"])
@@ -30,8 +38,11 @@ class Command(BaseCommand):
         project_ratios = options["project_ratios"]
         projects = pgs[: len(project_ratios)]
         for _ in range(options["testjob"]):
-            if random.random() < options["project_group_ratio"]:
-                vg = random.choices(projects, project_ratios)[0]
-                TestJobWithViewingGroupFactory(viewing_groups=vg)
-            else:
+            if random.random() < options["scheduled_ratio"]:
                 TestJobFactory()
+            else:
+                if random.random() < options["project_group_ratio"]:
+                    vg = random.choices(projects, project_ratios)[0]
+                    TestJobWithActualDevice(viewing_groups=vg)
+                else:
+                    TestJobWithActualDevice()
