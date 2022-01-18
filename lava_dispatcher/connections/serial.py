Linaro/lava

commit 5674a4efabdf9c32fffdc19a5645f84810c86373

dispatcher: connection: Add shell tag to implement disconnect
Add a 'shell' tag and a recognized name to implement the
disconnect function for shell's connection.

Signed-off-by: Mylène Josserand <mylene.josserand@collabora.com>

lava_dispatcher/connections/serial.py
@@ -302,7 +302,7 @@ class DisconnectDevice(ConnectDevice):
 
         if connection:
             self.logger.debug("Stopping connection")
-            connection.disconnect()
+            connection.disconnect(reason="")
             connection.connected = False
             self.set_namespace_data(
                 action="shared",

lava_dispatcher/connection.py
@@ -27,7 +27,7 @@ from lava_common.exceptions import LAVABug, TestError
 from lava_common.timeout import Timeout
 
 
-RECOGNIZED_TAGS = ("telnet", "ssh")
+RECOGNIZED_TAGS = ("telnet", "ssh", "shell")
 
 
 class SignalMatch(InternalObject):
@@ -106,7 +106,7 @@ class Connection:
         self.connected = True
         self.check_char = "#"
         self.tags = []
-        self.recognized_names = ["LxcSession", "QemuSession"]
+        self.recognized_names = ["LxcSession", "QemuSession", "ShellSession"]
 
     def corruption_check(self):
         self.sendline(self.check_char)
@@ -159,6 +159,8 @@ class Connection:
                     self.sendline("exit", disconnecting=True)
                 elif self.name == "QemuSession":
                     logger.info("Disconnecting from qemu: %s", reason)
+                elif self.name == "ShellSession":
+                    logger.info("Disconnecting from shell: %s", reason)
                 else:
                     raise LAVABug("'disconnect' not supported for %s" % self.tags)
             except ValueError:  # protection against file descriptor == -1

lava_dispatcher/shell.py
@@ -211,16 +211,13 @@ class ShellSession(Connection):
         self.__runner__ = None
         self.timeout = shell_command.lava_timeout
         self.__logger__ = None
+        self.tags = ["shell"]
 
     @property
     def logger(self):
         if not self.__logger__:
             self.__logger__ = logging.getLogger("dispatcher")
         return self.__logger__
-
-    def disconnect(self, reason=""):
-        self.logger.debug("Disconnecting %s", self.name)
-        super().disconnect(reason)
 
     # FIXME: rename prompt_str to indicate it can be a list or str
     @property

tests/lava_dispatcher/test_connections.py
@@ -679,9 +679,9 @@ class TestDisconnect(StdoutTestCase):
         the_exception = exc.exception
         err_msg = (
             "Invalid job data: ["
-            "\"LAVA does not know how to disconnect: ensure that primary connection has one of the following tags: ('telnet', 'ssh')\", "
-            "\"LAVA does not know how to disconnect: ensure that primary connection has one of the following tags: ('telnet', 'ssh')\", "
-            "\"LAVA does not know how to disconnect: ensure that primary connection has one of the following tags: ('telnet', 'ssh')\", "
-            "\"LAVA does not know how to disconnect: ensure that primary connection has one of the following tags: ('telnet', 'ssh')\"]"
+            "\"LAVA does not know how to disconnect: ensure that primary connection has one of the following tags: ('telnet', 'ssh', 'shell')\", "
+            "\"LAVA does not know how to disconnect: ensure that primary connection has one of the following tags: ('telnet', 'ssh', 'shell')\", "
+            "\"LAVA does not know how to disconnect: ensure that primary connection has one of the following tags: ('telnet', 'ssh', 'shell')\", "
+            "\"LAVA does not know how to disconnect: ensure that primary connection has one of the following tags: ('telnet', 'ssh', 'shell')\"]"
         )
         self.assertEqual(str(the_exception).strip(), err_msg)

commit 1c1301fb128b5caffc1ccf9f4e649a79f3c24bef

Fix crash introduced by 5674a4efab
Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

lava_dispatcher/connections/serial.py
@@ -258,7 +258,7 @@ class QemuSession(ShellSession):
         self.sendline("poweroff", disconnecting=True)
         self.listen_feedback(5)
         self.connected = False
-        super().disconnect()
+        super().disconnect(reason)
 
 
 class DisconnectDevice(ConnectDevice):