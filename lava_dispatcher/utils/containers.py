Linaro/lava

commit 0374b7530ca7c114c13993f25b8a5473990ec468

DockerDriver: ensure unique container names
This avoids a race condition where two commands are run in quick
succession, and the second fails because it cannot start a container
with the same name as the one before. Containers are cleaned up right
after the command finishes, but there is a possible race condition
between the removal of the first container (which is done in the
background by the docker daemon), and starting a new one.

We mitigate this be ensuring that each command run through docker has a
unique name, by appending a UUID to it.

Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

lava_dispatcher/utils/containers.py
@@ -20,6 +20,7 @@
 
 
 import os
+import uuid
 from lava_common.exceptions import InfrastructureError
 from lava_dispatcher.action import Action, InternalObject
 from lava_dispatcher.utils.docker import DockerRun
@@ -120,10 +121,18 @@ class DockerDriver(NullDriver):
         self.docker_options = []
         self.docker_run_options = []
         self.copied_files = []
+        self.uuid = uuid.uuid4()
 
     @property
     def container_name(self):
-        return "lava-" + str(self.action.job.job_id) + "-" + self.action.level
+        return (
+            "lava-"
+            + str(self.action.job.job_id)
+            + "-"
+            + self.action.level
+            + "-"
+            + str(self.uuid)
+        )
 
     def build(self, cls):
         docker = cls.from_parameters(self.params, self.action.job)

commit a7e12b5a59ad47ca8212d6097e7735c4dfbab58a

DockerDriver: really ensure unique container names
The uuid was still the same for a given instance of DockerDriver, which
is unique for each action, so the random suffix needs to be added for
each new run() or get_output() call.

Fixes: 0374b7530ca7 ("DockerDriver: ensure unique container names")

lava_dispatcher/utils/containers.py
@@ -121,17 +121,15 @@ class DockerDriver(NullDriver):
         self.docker_options = []
         self.docker_run_options = []
         self.copied_files = []
-        self.uuid = uuid.uuid4()
 
-    @property
-    def container_name(self):
+    def get_container_name(self):
         return (
             "lava-"
             + str(self.action.job.job_id)
             + "-"
             + self.action.level
             + "-"
-            + str(self.uuid)
+            + str(uuid.uuid4())
         )
 
     def build(self, cls):
@@ -150,10 +148,11 @@ class DockerDriver(NullDriver):
 
     def run(self, cmd):
         docker = self.build(DockerContainer)
-        docker.name(self.container_name)
+        name = self.get_container_name()
+        docker.name(name)
         docker.start(self.action)
         try:
-            self.__map_devices__()
+            self.__map_devices__(name)
             docker.run(cmd, self.action)
         finally:
             docker.stop()
@@ -161,10 +160,11 @@ class DockerDriver(NullDriver):
     def get_output(self, cmd):
         # FIXME duplicates most of run()
         docker = self.build(DockerContainer)
-        docker.name(self.container_name)
+        name = self.get_container_name()
+        docker.name(name)
         docker.start(self.action)
         try:
-            self.__map_devices__()
+            self.__map_devices__(name)
             return docker.get_output(cmd, self.action)
         finally:
             docker.stop()
@@ -174,9 +174,9 @@ class DockerDriver(NullDriver):
             self.copied_files.append(src)
         return src
 
-    def __map_devices__(self):
+    def __map_devices__(self, container_name):
         action = self.action
-        action.add_device_container_mappings(self.container_name, "docker")
+        action.add_device_container_mappings(container_name, "docker")
         for dev in self.__get_device_nodes__():
             if not os.path.islink(dev):
                 action.trigger_share_device_with_container(dev)