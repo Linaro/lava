c3a7f7cb2da5bcec2899763a07311a929c838c94
Igor Ponomarev <igor.ponomarev@collabora.com>
2022-04-13 17:19:34 +0300

    lava_db_generator: Simplify generator command for now

--- lava_db_generator/management/commands/generatedb.py
+++ lava_db_generator/management/commands/generatedb.py
@@ -6,9 +6,7 @@ from lava_db_generator.factories import (
     DeviceTypeFactory,
     DeviceFactory,
     UserFactory,
-    GroupFactory,
     ProjectGroupFactory,
-    TestJobFactory,
     TestJobWithActualDevice,
 )
 
@@ -36,35 +34,10 @@ class Command(BaseCommand):
             type=int,
         )
         parser.add_argument(
-            "--number-of-generated-groups",
-            metavar="SIZE",
-            required=True,
-            type=int,
-        )
-        parser.add_argument(
             "--number-of-generated-project-groups",
             metavar="SIZE",
             required=True,
             type=int,
-        )
-        parser.add_argument(
-            "--ratio-of-jobs-beloning-to-projects",
-            metavar="RATIO",
-            required=True,
-            type=float,
-        )
-        parser.add_argument(
-            "--number-of-projects-that-can-have-jobs",
-            metavar="SIZE",
-            type=int,
-            required=True,
-            nargs="*",
-        )
-        parser.add_argument(
-            "--ratio-of-jobs-with-actual-devices",
-            metavar="SIZE",
-            required=True,
-            type=float,
         )
         parser.add_argument(
             "--number-of-test-jobs",
@@ -81,21 +54,9 @@ class Command(BaseCommand):
 
         UserFactory.create_batch(
             size=options["number_of_generated_users"])
-        GroupFactory.create_batch(
-            size=options["number_of_generated_groups"])
-        pgs = ProjectGroupFactory.create_batch(
+        projects = ProjectGroupFactory.create_batch(
             size=options["number_of_generated_project_groups"])
 
-        project_ratios = options["number_of_projects_that_can_have_jobs"]
-        projects = pgs[: len(project_ratios)]
         for _ in range(options["number_of_test_jobs"]):
-            if random.random() < options["ratio_of_jobs_with_actual_devices"]:
-                TestJobFactory()
-            else:
-                if (random.random()
-                        <
-                        options["ratio_of_jobs_beloning_to_projects"]):
-                    vg = random.choices(projects, project_ratios)[0]
-                    TestJobWithActualDevice(viewing_groups=vg)
-                else:
-                    TestJobWithActualDevice()
+            vg = random.choice(projects)
+            TestJobWithActualDevice(viewing_groups=vg)
