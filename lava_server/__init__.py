a8f816bcf9dc91805980b1ab6a82b24df3d7544e
Igor Ponomarev <igor.ponomarev@collabora.com>
2022-05-18 17:11:33 +0300

    Add helper for structlog to set it up with LOGGING config value
    `structlog` takes the exact classes or funcitons rather than
    strings. This helper method converts strings from YAML file to
    actual classes.
    
    Because imports are within the function `structlog` should
    remain optional.
    
    Example YAML:
    
    ```
    LOGGING:
      version: 1
      disable_existing_loggers: True
      filters:
        require_debug_false:
          (): django.utils.log.RequireDebugFalse
      formatters:
        json_formatter:
          (): lava_server.structlog_processor_formatter
          processor_name: JSONRenderer
          foreign_pre_chain_names:
            - structlog.stdlib.add_logger_name
            - structlog.processors.TimeStamper:
                fmt: iso
      handlers:
        console:
          class: logging.StreamHandler
          formatter: json_formatter
          level: DEBUG
      loggers:
        django:
          handlers:
          - console
          level: DEBUG
          propagate: false
        django_auth_ldap:
          handlers:
          - console
          level: INFO
          propagate: false
        lava_results_app:
          handlers:
          - console
          level: INFO
          propagate: false
        lava_scheduler_app:
          handlers:
          - console
          level: INFO
          propagate: false
    ```

--- lava_server/__init__.py
+++ lava_server/__init__.py
@@ -1,5 +1,44 @@
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
+from typing import List, Dict, Union
 
 __all__ = ("celery_app",)
+
+
+def structlog_processor_formatter(
+    processor_name: str,
+    foreign_pre_chain_names: Union[List[str], Dict[str, Dict[str, str]]],
+    *args,
+    **kwrags
+):
+    from structlog.stdlib import ProcessorFormatter
+
+    import structlog.processors as processors_module
+
+    processors_class = getattr(processors_module, processor_name)
+    processor = processors_class()
+
+    from importlib import import_module
+
+    prechains = []
+    for n in foreign_pre_chain_names:
+        if isinstance(n, str):
+            module_name, attribute_name = n.rsplit(".", 1)
+
+            chains_module = import_module(module_name)
+            prechains.append(getattr(chains_module, attribute_name))
+        elif isinstance(n, dict):
+            absolute_path = list(n)[0]
+
+            module_name, attribute_name = absolute_path.rsplit(".", 1)
+
+            chains_module = import_module(module_name)
+            chain = getattr(chains_module, attribute_name)
+            prechains.append(chain(**n[absolute_path]))
+        else:
+            raise TypeError
+
+    return ProcessorFormatter(
+        processor=processor, foreign_pre_chain=prechains, *args, **kwrags
+    )
