26a64fcff07e8908a024eaabe401e2cac41e295f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 10:47:14 +0000

    Adding tarfile member sanitization to extractall()

--- lava_dispatcher/actions/deploy/testdef.py
+++ lava_dispatcher/actions/deploy/testdef.py
@@ -426,7 +426,26 @@ class TarRepoAction(RepoAction):
                 write_tar.write(decoded_out.getvalue())
 
             with tarfile.open(temp_tar) as tar:
-                tar.extractall(path=runner_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=runner_path)
         except OSError as exc:
             raise InfrastructureError(
                 "Unable to extract the tar archive: %s" % str(exc)
--- lava_dispatcher/utils/compression.py
+++ lava_dispatcher/utils/compression.py
@@ -100,7 +100,26 @@ def create_tarfile(indir, outfile):
 def untar_file(infile, outdir):
     try:
         with tarfile.open(infile, encoding="utf-8") as tar:
-            tar.extractall(outdir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, outdir)
     except tarfile.TarError as exc:
         raise JobError("Unable to unpack %s: %s" % (infile, str(exc)))
     except OSError as exc:
