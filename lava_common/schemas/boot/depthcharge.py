7c4980c5a36e3091b140e2e0376287c550a68c79
Laura Nao <laura.nao@collabora.com>
2022-10-21 18:09:44 +0200

    schema: allow to use auto_login in depthcharge boot action
    This is already supported by Depthcharge.
    
    Signed-off-by: Laura Nao <laura.nao@collabora.com>

--- lava_common/schemas/boot/depthcharge.py
+++ lava_common/schemas/boot/depthcharge.py
@@ -30,6 +30,7 @@ def schema():
         Required("method"): Msg("depthcharge", "'method' should be 'depthcharge'"),
         Required("commands"): Any(str, [str]),
         Optional("extra_kernel_args"): str,
+        Optional("auto_login"): boot.auto_login(),
         Optional("prompts"): boot.prompts(),
         Optional("use_bootscript"): bool,
         Optional("transfer_overlay"): boot.transfer_overlay(),
