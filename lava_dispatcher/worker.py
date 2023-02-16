18aee35b61b43a1a5357859a6c8b4736593f3e3e
Igor Ponomarev <igor.ponomarev@collabora.com>
2023-02-16 20:04:07 +0600

    lava_dispatcher/worker.py: Enable postponed annotations
    Postponed annotations improve start-up speed and ensures compatibility
    with future python versions.

--- lava_dispatcher/worker.py
+++ lava_dispatcher/worker.py
@@ -19,6 +19,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses>.
+from __future__ import annotations
 
 import asyncio
 import contextlib
