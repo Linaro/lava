Linaro/lava

commit cbd57f068ccaece3631ac44020f3d8df9d6767dd

Do not run every commands under nice
Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

lava_dispatcher/action.py
@@ -642,7 +642,7 @@ class Action:
         :param: cwd - the current working directory for this command
         :return: return code of the command
         """
-        # Build the command list (adding 'nice' at the front)
+        # Build the command list
         if isinstance(command_list, str):
             command_list = shlex.split(command_list)
         elif not isinstance(command_list, list):
@@ -650,20 +650,20 @@ class Action:
         command_list = [str(s) for s in command_list]
 
         # Build the error message
-        log_error_msg = "Unable to run 'nice' '%s'" % "' '".join(command_list)
+        log_error_msg = "Unable to run '%s'" % "' '".join(command_list)
         if error_msg is None:
             error_msg = log_error_msg
 
         # Start the subprocess
-        self.logger.debug("Calling: 'nice' '%s'", "' '".join(command_list))
+        self.logger.debug("Calling: '%s'", "' '".join(command_list))
         start = time.monotonic()
 
         cmd_logger = CommandLogger(self.logger)
         ret = None
         try:
             proc = pexpect.spawn(
-                "nice",
-                command_list,
+                command_list[0],
+                command_list[1:],
                 cwd=cwd,
                 encoding="utf-8",
                 codec_errors="replace",
@@ -691,7 +691,7 @@ class Action:
 
         # Check the return value
         if ret != 0 and not allow_fail:
-            self.logger.error("Unable to run 'nice' '%s'", command_list)
+            self.logger.error("Unable to run '%s'", command_list)
             raise self.command_exception(error_msg)
         return ret
 

tests/lava_dispatcher/test_flasher.py
@@ -57,8 +57,8 @@ def test_run(monkeypatch):
             assert arg == pexpect.EOF
 
     commands = [
-        ["nice", "/home/lava/bin/PiCtrl.py", "PowerPlug", "0", "off"],
-        ["nice", "touch"],
+        ["/home/lava/bin/PiCtrl.py", "PowerPlug", "0", "off"],
+        ["touch"],
     ]
 
     def spawn(

commit c93c8907af97419f7c622d820755dfdddac7756c

Fix run_cmd issue with commands with spaces
Introduced in cbd57f068.

Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

lava_dispatcher/action.py
@@ -662,8 +662,7 @@ class Action:
         ret = None
         try:
             proc = pexpect.spawn(
-                command_list[0],
-                command_list[1:],
+                shlex.join(command_list),
                 cwd=cwd,
                 encoding="utf-8",
                 codec_errors="replace",

tests/lava_dispatcher/test_flasher.py
@@ -18,6 +18,8 @@
 # along
 # with this program; if not, see <http://www.gnu.org/licenses>.
 
+import shlex
+
 import pexpect
 
 import lava_dispatcher.actions.deploy.docker  # pylint: disable=unused-import
@@ -61,12 +63,9 @@ def test_run(monkeypatch):
         ["touch"],
     ]
 
-    def spawn(
-        cmd, args, cwd, encoding, codec_errors, logfile, timeout, searchwindowsize
-    ):
+    def spawn(cmd, cwd, encoding, codec_errors, logfile, timeout, searchwindowsize):
         command = commands.pop(0)
-        assert cmd == command[0]
-        assert args == command[1:]
+        assert cmd == shlex.join(command)
         assert encoding == "utf-8"
         assert codec_errors == "replace"
         assert searchwindowsize == 10