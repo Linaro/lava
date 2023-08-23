Linaro/lava

commit 1a7a85b05c6f0f4260e5bc32b37a957af6571956

Allow to choose the index in the HTTP_X_FORWARDED_FOR list
When using one or multiple reverse proxy, changing this index is required.
By default `HTTP_X_FORWARDED_FOR_INDEX` is set to -1 (last ip in the list)
which is valid for a system with one reverse proxy.

Closes #600

Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

lava_scheduler_app/utils.py
@@ -18,6 +18,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with LAVA.  If not, see <http://www.gnu.org/licenses/>.
 
+import contextlib
 import copy
 import errno
 import ipaddress
@@ -348,9 +349,14 @@ def get_ldap_user_properties(ldap_user):
 
 def get_user_ip(request):
     if "HTTP_X_FORWARDED_FOR" in request.META:
-        return request.META["HTTP_X_FORWARDED_FOR"].split(",")[0]
-    if "REMOTE_ADDR" in request.META:
-        return request.META["REMOTE_ADDR"]
+        ips = [
+            ip.strip(" ")
+            for ip in request.META["HTTP_X_FORWARDED_FOR"].split(",")
+            if ip
+        ]
+        with contextlib.suppress(IndexError):
+            return ips[settings.HTTP_X_FORWARDED_FOR_INDEX]
+        return None
     return None
 
 

lava_server/settings/common.py
@@ -251,6 +251,12 @@ DEFAULT_TABLE_LENGTH = 25
 # Extra context variables when validating the job definition schema
 EXTRA_CONTEXT_VARIABLES = []
 
+# Index of the user ip in HTTP_X_FORWARDED_FOR
+# In fact, this header is a list of all the reverse proxy involved.
+# By default, use the latest element in the list. If the system involve more
+# than one reverse proxy, the variable should be updated.
+HTTP_X_FORWARDED_FOR_INDEX = -1
+
 # Use default instance name
 INSTANCE_NAME = "default"
 

commit 728fe4f8da84346965b2b42e85cbb3835864bef1

get_user_ip: consider REMOTE_ADDR to get the user IP
This was removed, apparently by mistake, when the
`HTTP_X_FORWARDED_FOR_INDEX` setting was implemented. That removal left
autoregistration of the local worker in development mode broken, as
there would be no IP address to check against
`WORKER_AUTO_REGISTER_NETMASK`.

Fixes: 1a7a85b0 ("Allow to choose the index in the HTTP_X_FORWARDED_FOR list")

lava_scheduler_app/utils.py
@@ -344,6 +344,8 @@ def get_user_ip(request):
         with contextlib.suppress(IndexError):
             return ips[settings.HTTP_X_FORWARDED_FOR_INDEX]
         return None
+    if "REMOTE_ADDR" in request.META:
+        return request.META["REMOTE_ADDR"]
     return None
 
 

tests/lava_scheduler_app/test_utils.py
@@ -1,0 +1,20 @@
+from lava_scheduler_app.utils import get_user_ip
+
+
+def test_get_user_ip_forwarded_for(mocker):
+    req = mocker.MagicMock()
+    req.META = {"HTTP_X_FORWARDED_FOR": "88.77.66.55"}
+    assert get_user_ip(req) == "88.77.66.55"
+
+
+def test_get_user_ip_forwarded_for_index(mocker, settings):
+    settings.HTTP_X_FORWARDED_FOR_INDEX = 1
+    req = mocker.MagicMock()
+    req.META = {"HTTP_X_FORWARDED_FOR": "88.77.66.55,44.33.22.11"}
+    assert get_user_ip(req) == "44.33.22.11"
+
+
+def test_get_user_ip_remote_addr(mocker):
+    req = mocker.MagicMock()
+    req.META = {"REMOTE_ADDR": "127.0.0.1"}
+    assert get_user_ip(req) == "127.0.0.1"
728fe4f8da84346965b2b42e85cbb3835864bef1
Antonio Terceiro <antonio.terceiro@linaro.org>
2023-08-23 18:19:34 -0300

    get_user_ip: consider REMOTE_ADDR to get the user IP
    This was removed, apparently by mistake, when the
    `HTTP_X_FORWARDED_FOR_INDEX` setting was implemented. That removal left
    autoregistration of the local worker in development mode broken, as
    there would be no IP address to check against
    `WORKER_AUTO_REGISTER_NETMASK`.
    
    Fixes: 1a7a85b0 ("Allow to choose the index in the HTTP_X_FORWARDED_FOR list")

---
+++ tests/lava_scheduler_app/test_utils.py
@@ -0,0 +1,20 @@
+from lava_scheduler_app.utils import get_user_ip
+
+
+def test_get_user_ip_forwarded_for(mocker):
+    req = mocker.MagicMock()
+    req.META = {"HTTP_X_FORWARDED_FOR": "88.77.66.55"}
+    assert get_user_ip(req) == "88.77.66.55"
+
+
+def test_get_user_ip_forwarded_for_index(mocker, settings):
+    settings.HTTP_X_FORWARDED_FOR_INDEX = 1
+    req = mocker.MagicMock()
+    req.META = {"HTTP_X_FORWARDED_FOR": "88.77.66.55,44.33.22.11"}
+    assert get_user_ip(req) == "44.33.22.11"
+
+
+def test_get_user_ip_remote_addr(mocker):
+    req = mocker.MagicMock()
+    req.META = {"REMOTE_ADDR": "127.0.0.1"}
+    assert get_user_ip(req) == "127.0.0.1"
