bacd226575e50b69d25079876b318e9a1b4ced26
Igor Ponomarev <igor.ponomarev@collabora.com>
2023-11-29 17:47:16 +0600

    tests: Use settings module instead of fixture for django settings
    The randomized order testing revealed that the `update_settings`
    fixture was prone to race conditions and can fail tests if
    the order of tests changes.
    
    The fixture always used the same values.
    
    Instead use a `DJANGO_SETTINGS_MODULE` pytest config value to
    pass a new settings python module `tests.settings`. This new module
    will import the settings from the already used `lava_server.settings.dev`
    module and modify the values for testing environment.
    
    Only two settings actually needed to be modified: `DEVICES_PATH`
    and `HEALTH_CHECKS_PATH`. The previous fixture updated other values
    to the copied values from the `lava_server.settings.dev`.
    
    The settings module will no longer have race conditions because
    it is only loaded once at the beginning and the values will stay
    the same.

--- tests/lava_scheduler_app/conftest.py
+++ tests/lava_scheduler_app/conftest.py
@@ -1,39 +1,8 @@
-import pathlib
-
 import pytest
 
 
 @pytest.fixture(autouse=True)
-def update_settings(settings, mocker, tmp_path):
-    base = pathlib.Path(__file__).parent.parent.parent
-    settings.DEVICES_PATH = str(base / "tests" / "lava_scheduler_app" / "devices")
-    settings.DEVICE_TYPES_PATHS = [
-        str(base / "etc" / "dispatcher-config" / "device-types")
-    ]
-    settings.HEALTH_CHECKS_PATH = str(
-        base / "tests" / "lava_scheduler_app" / "health-checks"
-    )
-    mocker.patch(
-        "lava_server.files.File.KINDS",
-        {
-            "device": ([settings.DEVICES_PATH], "{name}.jinja2"),
-            "device-type": (settings.DEVICE_TYPES_PATHS, "{name}.jinja2"),
-            "dispatcher": [
-                "/etc/lava-server/dispatcher.d/{name}/dispatcher.yaml",
-                "/etc/lava-server/dispatcher.d/{name}.yaml",
-            ],
-            "env": [
-                "/etc/lava-server/dispatcher.d/{name}/env.yaml",
-                "/etc/lava-server/env.yaml",
-            ],
-            "env-dut": [
-                "/etc/lava-server/dispatcher.d/{name}/env-dut.yaml",
-                "/etc/lava-server/env-dut.yaml",
-            ],
-            "health-check": ([settings.HEALTH_CHECKS_PATH], "{name}.yaml"),
-        },
-    )
-
+def update_settings(mocker, tmp_path):
     mocker.patch(
         "lava_scheduler_app.models.TestJob.output_dir", str(tmp_path / "job-output")
     )
