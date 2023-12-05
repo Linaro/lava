e772fb157ea159c58bd697c51f62a91dc35eff2e
Igor Ponomarev <igor.ponomarev@collabora.com>
2023-12-05 18:19:09 +0600

    Add `yaml_quote` function
    It is equivalent to `shlex.quote` but for YAML. Takes any
    arbitrary Python object that can be converted to YAML such
    as dictionary, list, integer, float or string  and returns
    a valid YAML single line string. The strings would be converted
    to double quoted scalars.
    
    Will be used for template filters.
    
    This is implemented using the Dumper class of the `yaml` module
    and the StringIO class from `io`. The Dumper is configured to use
    flow style double quoted YAML objects in to a buffer. The buffer
    is parsed in to string and then a leading YAML object indicator
    `--- ` and trailing newline is cut to create a string that can be
    embedded in the templates without any newlines. The buffer is
    reused between every quote which improves the performance.
    
    When C YAML extension is available it will be used.
    
    Also add unit tests to verify behavior including quoting
    dictionaries, unprintable characters and extremely long lines.
    Test the pure Python implementation of `yaml` if C implementation
    has been tested.

--- lava_common/yaml.py
+++ lava_common/yaml.py
@@ -5,7 +5,14 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 from __future__ import annotations
 
+from contextvars import ContextVar
+from io import StringIO
+from typing import TYPE_CHECKING
+
 from yaml import dump, load
+
+if TYPE_CHECKING:
+    from typing import Union  # For Python 3.9 compatibility
 
 # Handle compatibility with system without C yaml
 try:
@@ -27,3 +34,39 @@ def yaml_safe_dump(data, *args, **kwargs):
     # sort_keys=False will break the CI because
     # some dispatcher tests check the order of dict keys
     return dump(data, *args, Dumper=SafeDumper, **kwargs)
+
+
+yaml_quote_dumper: ContextVar[tuple[StringIO, SafeDumper]] = ContextVar(
+    "yaml_quote_dumper"
+)
+
+
+def _get_largest_width_possible() -> int:
+    # A largest value that both C and Python implementation accept
+    from ctypes import c_uint
+
+    return c_uint(-1).value // 2
+
+
+def yaml_quote(obj: Union[str, int, float, dict, list]) -> str:
+    try:
+        stream, yaml_dumper = yaml_quote_dumper.get()
+    except LookupError:
+        stream = StringIO()
+        yaml_dumper = SafeDumper(
+            stream,
+            default_flow_style=True,  # Output in a single line
+            width=_get_largest_width_possible(),  # Disable automatic line breaks
+            default_style='"',  # Use "double quoted" scalar type
+            explicit_start=True,  # Always output "--- " YAML document start
+            explicit_end=False,  # Do not output "\n..." at the end
+        )
+        yaml_dumper.open()  # Start accepting any objects
+        yaml_quote_dumper.set((stream, yaml_dumper))
+    else:
+        # Clean-up existing stream
+        stream.seek(0)
+        stream.truncate(0)
+
+    yaml_dumper.represent(obj)
+    return stream.getvalue()[4:-1]  # Skip "--- " and newline
