Linaro/lava

commit 8661ae47040d45983f2fe0944ec86e9f68b743e8

Refactor `get_restricted_job`
First, it was placed in the `lava_scheduler_app.views` module
even though it was not an actual view. Move it to a `@classmethod`
of the `TestJob` model just as `get_by_job_number` is.

Second, it awkwardly imported and used a function `check_request_auth`
from the `lava_results_app.utils` which created a circular dependency
in `lava_results_app` therefore that app did not make use of
`get_restricted_job` and instead reimplemented it everywhere
it wanted to check authentication.

One potentially significant difference is that `get_restricted_job`
will no longer check for `AuthToken`. This is because both REST API
and XMLRPC have their own mechanisms to replace request's user
based on the token. (in `lava_rest_app.authentication` and
`linaro_django_xmlrpc.views` respectively)

lava_results_app/views/__init__.py
@@ -41,14 +41,12 @@ from lava_results_app.models import (
 from lava_results_app.tables import ResultsTable, SuiteTable, TestJobResultsTable
 from lava_results_app.utils import (
     StreamEcho,
-    check_request_auth,
     export_testcase,
     get_testcases_with_limit,
     testcase_export_fields,
 )
 from lava_scheduler_app.models import TestJob
 from lava_scheduler_app.tables import pklink
-from lava_scheduler_app.views import get_restricted_job
 from lava_server.bread_crumbs import BreadCrumb, BreadCrumbTrail
 from lava_server.compat import djt2_paginator_class
 from lava_server.lavatable import LavaView
@@ -149,7 +147,7 @@ def query(request):
 
 @BreadCrumb("Test job {job}", parent=index, needs=["job"])
 def testjob(request, job):
-    job = get_restricted_job(request.user, pk=job, request=request)
+    job = TestJob.get_restricted_job(job, request.user)
     data = ResultsView(request, model=TestSuite, table_class=TestJobResultsTable)
     suite_table = TestJobResultsTable(
         data.get_table_data().filter(pk=job.id), request=request
@@ -181,8 +179,7 @@ def testjob(request, job):
 
 
 def testjob_csv(request, job):
-    job = get_object_or_404(TestJob, pk=job)
-    check_request_auth(request, job)
+    job = TestJob.get_restricted_job(job, request.user)
 
     def testjob_stream(test_cases, pseudo_buffer):
         fieldnames = testcase_export_fields()
@@ -206,8 +203,7 @@ def testjob_csv(request, job):
 
 
 def testjob_yaml(request, job):
-    job = get_object_or_404(TestJob, pk=job)
-    check_request_auth(request, job)
+    job = TestJob.get_restricted_job(job, request.user)
 
     test_cases = TestCase.objects.filter(suite__job=job).select_related("suite")
 
@@ -222,8 +218,7 @@ def testjob_yaml(request, job):
 
 
 def testjob_yaml_summary(request, job):
-    job = get_object_or_404(TestJob, pk=job)
-    check_request_auth(request, job)
+    job = TestJob.get_restricted_job(job, request.user)
     suites = job.testsuite_set.all()
     response = HttpResponse(content_type="text/yaml")
     filename = "lava_%s_summary.yaml" % job.id
@@ -237,7 +232,7 @@ def testjob_yaml_summary(request, job):
 
 @BreadCrumb("Suite {pk}", parent=testjob, needs=["job", "pk"])
 def suite(request, job, pk):
-    job = get_restricted_job(request.user, pk=job, request=request)
+    job = TestJob.get_restricted_job(job, request.user)
     test_suite = get_object_or_404(TestSuite, name=pk, job=job)
     data = SuiteView(request, model=TestCase, table_class=SuiteTable)
     suite_table = SuiteTable(
@@ -264,8 +259,7 @@ def suite(request, job, pk):
 
 
 def suite_csv(request, job, pk):
-    job = get_object_or_404(TestJob, pk=job)
-    check_request_auth(request, job)
+    job = TestJob.get_restricted_job(job, request.user)
     test_suite = get_object_or_404(TestSuite, name=pk, job=job)
     querydict = request.GET
     offset = int(querydict.get("offset", default=0))
@@ -296,9 +290,8 @@ def suite_csv_stream(request, job, pk):
     https://docs.djangoproject.com/en/3.2/ref/request-response/#django.http.StreamingHttpResponse
     https://docs.djangoproject.com/en/3.2/howto/outputting-csv/
     """
-    job = get_object_or_404(TestJob, pk=job)
+    job = TestJob.get_restricted_job(job, request.user)
     test_suite = get_object_or_404(TestSuite, name=pk, job=job)
-    check_request_auth(request, job)
     querydict = request.GET
     offset = int(querydict.get("offset", default=0))
     limit = int(querydict.get("limit", default=0))
@@ -316,8 +309,7 @@ def suite_csv_stream(request, job, pk):
 
 
 def suite_yaml(request, job, pk):
-    job = get_object_or_404(TestJob, pk=job)
-    check_request_auth(request, job)
+    job = TestJob.get_restricted_job(job, request.user)
     test_suite = get_object_or_404(TestSuite, name=pk, job=job)
     querydict = request.GET
     offset = int(querydict.get("offset", default=0))
@@ -334,8 +326,7 @@ def suite_yaml(request, job, pk):
 
 
 def suite_testcase_count(request, job, pk):
-    job = get_object_or_404(TestJob, pk=job)
-    check_request_auth(request, job)
+    job = TestJob.get_restricted_job(job, request.user)
     test_suite = get_object_or_404(TestSuite, name=pk, job=job)
     test_case_count = test_suite.testcase_set.all().count()
     return HttpResponse(test_case_count, content_type="text/plain")
@@ -348,8 +339,7 @@ def metadata_export(request, job):
     CSV is not supported as the user-supplied metadata can
     include nested dicts or lists.
     """
-    job = get_object_or_404(TestJob, pk=job)
-    check_request_auth(request, job)
+    job = TestJob.get_restricted_job(job, request.user)
     # testdata from job & export
     if not hasattr(job, "testdata"):
         raise Http404("No TestData present in test job.")
@@ -366,7 +356,7 @@ def metadata_export(request, job):
 
 @BreadCrumb("TestSet {case}", parent=testjob, needs=["job", "pk", "ts", "case"])
 def testset(request, job, ts, pk, case):
-    job = get_restricted_job(request.user, pk=job, request=request)
+    job = TestJob.get_restricted_job(job, request.user)
     test_suite = get_object_or_404(TestSuite, name=pk, job=job)
     test_set = get_object_or_404(TestSet, name=ts, suite=test_suite)
     test_cases = TestCase.objects.filter(name=case, test_set=test_set)
@@ -412,9 +402,9 @@ def testcase(request, case_id, job=None, pk=None):
     if not job:
         job = case.suite.job
         # Auth check purposes only.
-        job = get_restricted_job(request.user, pk=job.id, request=request)
+        job = TestJob.get_restricted_job(job.id, request.user)
     else:
-        job = get_restricted_job(request.user, pk=job, request=request)
+        job = TestJob.get_restricted_job(job, request.user)
     if not pk:
         test_suite = case.suite
     else:
@@ -469,7 +459,8 @@ def testcase(request, case_id, job=None, pk=None):
 
 def testcase_yaml(request, pk):
     testcase = get_object_or_404(TestCase, pk=pk)
-    check_request_auth(request, testcase.suite.job)
+    # Check that user allowed to view job
+    TestJob.get_restricted_job(testcase.suite.job, request.user)
     response = HttpResponse(content_type="text/yaml")
     filename = "lava_%s.yaml" % testcase.name
     response["Content-Disposition"] = 'attachment; filename="%s"' % filename

lava_server/api/__init__.py
@@ -22,8 +22,8 @@ from lava_scheduler_app.models import (
     DeviceType,
     GroupDevicePermission,
     GroupDeviceTypePermission,
+    TestJob,
 )
-from lava_scheduler_app.views import get_restricted_job
 from linaro_django_xmlrpc.models import Mapper, SystemAPI, errors
 
 
@@ -213,11 +213,11 @@ class LavaSystemAPI(SystemAPI):
             raise xmlrpc.client.Fault(
                 errors.BAD_REQUEST, "job list argument must be a list"
             )
-        username = self._switch_user(username)
+        user = self._switch_user(username)
         retval = {}
         for job_id in job_list:
             try:
-                get_restricted_job(username, job_id)
+                TestJob.get_restricted_job(job_id, user)
             except Http404:
                 continue
             except PermissionDenied:

lava_results_app/utils.py
@@ -10,12 +10,10 @@ import logging
 import os
 
 import yaml
-from django.core.exceptions import PermissionDenied
 from django.db import DataError
 from django.utils.translation import ngettext_lazy
 
 from lava_common.yaml import yaml_safe_load
-from linaro_django_xmlrpc.models import AuthToken
 
 
 def help_max_length(max_length):
@@ -53,28 +51,6 @@ def description_data(job):
         logger.exception(exc)
     # This should be a dictionary, None is not acceptable
     return data if data else {}
-
-
-# FIXME: relocate these two functions into dbutils to avoid needing django settings here.
-# other functions in utils can be run outside django. Remove import of AuthToken.
-def anonymous_token(request, job):
-    querydict = request.GET
-    user = querydict.get("user", default=None)
-    token = querydict.get("token", default=None)
-    # safe to call with (None, None) - returns None
-    auth_user = AuthToken.get_user_for_secret(username=user, secret=token)
-    return auth_user
-
-
-def check_request_auth(request, job):
-    if not request.user.is_authenticated:
-        if not job.can_view(request.user):
-            # handle anonymous access
-            auth_user = anonymous_token(request, job)
-            if not auth_user or not job.can_view(auth_user):
-                raise PermissionDenied()
-    elif not job.can_view(request.user):
-        raise PermissionDenied()
 
 
 def get_testcases_with_limit(testsuite, limit=None, offset=None):

lava_scheduler_app/models.py
@@ -4,6 +4,7 @@
 #         Remi Duraffort <remi.duraffort@linaro.org>
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
+from __future__ import annotations
 
 import contextlib
 import datetime
@@ -28,6 +29,7 @@ from django.core.exceptions import (
 )
 from django.db import models, transaction
 from django.db.models import Q
+from django.http import Http404
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.crypto import get_random_string
@@ -2071,6 +2073,18 @@ class TestJob(models.Model):
             job = query.get(sub_id=job_id)
         else:
             job = query.get(pk=job_id)
+        return job
+
+    @classmethod
+    def get_restricted_job(cls, job_id, user, for_update: bool = False) -> TestJob:
+        try:
+            job = TestJob.get_by_job_number(job_id, for_update)
+        except TestJob.DoesNotExist as exc:
+            raise Http404(*exc.args)
+
+        if not job.can_view(user):
+            raise PermissionDenied()
+
         return job
 
     @property

lava_scheduler_app/views.py
@@ -70,11 +70,7 @@ from lava_results_app.models import (
     TestCase,
     TestData,
 )
-from lava_results_app.utils import (
-    check_request_auth,
-    description_data,
-    description_filename,
-)
+from lava_results_app.utils import description_data, description_filename
 from lava_scheduler_app.dbutils import (
     device_summary,
     device_type_summary,
@@ -673,20 +669,6 @@ def mydevices_health_history_log(request):
             ),
         },
     )
-
-
-def get_restricted_job(user, pk, request=None, for_update=False):
-    """Returns JOB which is a TestJob object after checking for USER
-    accessibility to the object via the UI login AND via the REST API.
-    """
-    try:
-        job = TestJob.get_by_job_number(pk, for_update)
-    except TestJob.DoesNotExist:
-        raise Http404("No TestJob matches the given query.")
-    # handle REST API querystring as well as UI logins
-    if request:
-        check_request_auth(request, job)
-    return job
 
 
 class ActiveDeviceView(DeviceTableView):
@@ -1702,7 +1684,7 @@ def job_submit(request):
 
 @BreadCrumb("{pk}", parent=job_list, needs=["pk"])
 def job_detail(request, pk):
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
 
     pipeline = description_data(job).get("pipeline", {})
 
@@ -1788,7 +1770,7 @@ def job_detail(request, pk):
 
 @BreadCrumb("Definition", parent=job_detail, needs=["pk"])
 def job_definition(request, pk):
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     return render(
         request,
         "lava_scheduler_app/job_definition.html",
@@ -1805,7 +1787,7 @@ def job_definition(request, pk):
 
 
 def job_description_yaml(request, pk):
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     filename = description_filename(job)
     if not filename:
         raise Http404()
@@ -1819,7 +1801,7 @@ def job_description_yaml(request, pk):
 
 
 def job_definition_plain(request, pk):
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     response = HttpResponse(job.display_definition, content_type="text/plain")
     filename = "job_%d.yaml" % job.id
     response["Content-Disposition"] = "attachment; filename=%s" % filename
@@ -1828,7 +1810,7 @@ def job_definition_plain(request, pk):
 
 @BreadCrumb("Multinode definition", parent=job_detail, needs=["pk"])
 def multinode_job_definition(request, pk):
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     return render(
         request,
         "lava_scheduler_app/multinode_job_definition.html",
@@ -1847,7 +1829,7 @@ def multinode_job_definition(request, pk):
 
 
 def multinode_job_definition_plain(request, pk):
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     response = HttpResponse(job.multinode_definition, content_type="text/plain")
     filename = "job_%d.yaml" % job.id
     response["Content-Disposition"] = "attachment; filename=multinode_%s" % filename
@@ -1855,7 +1837,7 @@ def multinode_job_definition_plain(request, pk):
 
 
 def job_fetch_data(request, pk):
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     if job.state != TestJob.STATE_FINISHED:
         raise Http404()
     path = os.path.join(job.output_dir, "job_data.gz")
@@ -1976,7 +1958,7 @@ def favorite_jobs(request):
 
 
 def job_status(request, pk):
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     response_dict = {
         "actual_device": "<i>...</i>",
         "duration": "<i>...</i>",
@@ -2023,7 +2005,7 @@ def job_status(request, pk):
 
 
 def job_timing(request, pk):
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     try:
         data = logs_instance.read(job)
         logs = yaml_safe_load(data)
@@ -2123,7 +2105,7 @@ def job_configuration(request, pk):
         with contextlib.suppress(OSError):
             tar.add(filename)
 
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     data = ""
     pwd = os.getcwd()
     try:
@@ -2147,7 +2129,7 @@ def job_configuration(request, pk):
 
 
 def job_log_file_plain(request, pk):
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     try:
         data = logs_instance.open(job)
         size = logs_instance.size(job)
@@ -2160,7 +2142,7 @@ def job_log_file_plain(request, pk):
 
 
 def job_log_incremental(request, pk):
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     # Start from this line
     try:
         first_line = int(request.GET.get("line", 0))
@@ -2205,7 +2187,7 @@ def job_log_incremental(request, pk):
 
 @transaction.atomic
 def job_cancel(request, pk):
-    job = get_restricted_job(request.user, pk, request=request, for_update=True)
+    job = TestJob.get_restricted_job(pk, request.user, for_update=True)
     try:
         job.cancel(request.user)
         return redirect(job)
@@ -2222,7 +2204,7 @@ def job_fail(request, pk):
         )
 
     with transaction.atomic():
-        job = get_restricted_job(request.user, pk, request=request, for_update=True)
+        job = TestJob.get_restricted_job(pk, request.user, for_update=True)
         if job.state != TestJob.STATE_CANCELING:
             return HttpResponseForbidden(
                 "Job should be canceled before being failed", content_type="text/plain"
@@ -2240,7 +2222,7 @@ def job_resubmit(request, pk):
         "bread_crumb_trail": BreadCrumbTrail.leading_to(job_list),
     }
 
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     if job.can_resubmit(request.user):
         response_data["is_authorized"] = True
 
@@ -2314,7 +2296,7 @@ class FailureForm(forms.ModelForm):
 
 @require_POST
 def job_change_priority(request, pk):
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     if not job.can_change_priority(request.user):
         raise PermissionDenied()
     requested_priority = request.POST["priority"]
@@ -2337,7 +2319,7 @@ def job_toggle_favorite(request, pk):
 
 
 def job_annotate_failure(request, pk):
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
     if not job.can_annotate(request.user):
         raise PermissionDenied()
 
@@ -2784,7 +2766,7 @@ def download_device_type_template(request, pk):
 @require_POST
 def similar_jobs(request, pk):
     logger = logging.getLogger("lava-scheduler")
-    job = get_restricted_job(request.user, pk, request=request)
+    job = TestJob.get_restricted_job(pk, request.user)
 
     entity = ContentType.objects.get_for_model(TestJob).model
 

tests/lava_scheduler_app/test_views.py
@@ -28,7 +28,6 @@ from lava_scheduler_app.models import (
 )
 from lava_scheduler_app.views import (
     device_report_data,
-    get_restricted_job,
     job_report_data,
     type_report_data,
 )
@@ -1247,24 +1246,19 @@ def test_running(client, setup):
     assert ret.context["running_table"].data[1].name == "qemu"  # nosec
 
 
-@pytest.fixture
-def check_request_auth_patched(monkeypatch):
-    monkeypatch.setattr(
-        "lava_scheduler_app.views.check_request_auth", lambda request, job: None
-    )
-
-
 @pytest.mark.django_db
-def test_get_restricted_job_non_existing(client, check_request_auth_patched, setup):
+def test_get_restricted_job_non_existing(client, setup):
     user = User.objects.get(username="tester")
     with pytest.raises(Http404):
-        get_restricted_job(user, -1)
+        TestJob.get_restricted_job(-1, user)
 
 
 @pytest.mark.django_db
-def test_get_restricted_job(client, check_request_auth_patched, setup):
+def test_get_restricted_job(client, setup):
     user = User.objects.get(username="tester")
-    job = get_restricted_job(user, TestJob.objects.get(description="test job 01").pk)
+    job = TestJob.get_restricted_job(
+        TestJob.objects.get(description="test job 01").pk, user
+    )
     assert job.description == "test job 01"  # nosec
 
 

commit d79ae21d55ae5f5ee01d47d1d8c2ca953187c74d

Fix crashes introduced by 8661ae4704
Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

lava_results_app/views/__init__.py
@@ -404,7 +404,7 @@ def testcase(request, case_id, job=None, pk=None):
         # Auth check purposes only.
         job = TestJob.get_restricted_job(job.id, request.user)
     else:
-        job = TestJob.get_restricted_job(job, request.user)
+        job = TestJob.get_restricted_job(job.id, request.user)
     if not pk:
         test_suite = case.suite
     else:
@@ -460,7 +460,7 @@ def testcase(request, case_id, job=None, pk=None):
 def testcase_yaml(request, pk):
     testcase = get_object_or_404(TestCase, pk=pk)
     # Check that user allowed to view job
-    TestJob.get_restricted_job(testcase.suite.job, request.user)
+    TestJob.get_restricted_job(testcase.suite.job.id, request.user)
     response = HttpResponse(content_type="text/yaml")
     filename = "lava_%s.yaml" % testcase.name
     response["Content-Disposition"] = 'attachment; filename="%s"' % filename