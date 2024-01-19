bc4f3b0a10846c7a26309d82c4c0a4495bb4f7cf
Igor Ponomarev <igor.ponomarev@collabora.com>
2024-01-19 21:12:36 +0600

    Pin REST API version v0.2 and start on REST API v0.3
    Currently unit tests use the latest REST API version. However,
    REST API version v0.3 will not copy the API of version v0.2.
    This means the version v0.2 should be used for existing unit tests.
    
    Also add a default router for REST API v0.3 to indicate that the
    work started on it.

--- lava_rest_app/urls.py
+++ lava_rest_app/urls.py
@@ -8,12 +8,14 @@ from django.conf.urls import include
 
 from lava_rest_app.v02.routers import router as router_v02
 from lava_rest_app.v02.views import LavaObtainAuthToken
+from lava_rest_app.v03.routers import router as router_v03
 from lava_server.compat import url
 
 from . import versions
 
 urlpatterns = [
     url(r"^(?P<version>(v0.2))/", include(router_v02.urls)),
+    url(r"^(?P<version>(v0.3))/", include(router_v03.urls)),
     url(
         r"^^(?P<version>(%s))/token/" % versions.urlpattern(),
         LavaObtainAuthToken.as_view(),
