6b8e3316a33fc307973181aba91ae42a5f79d1b5
Igor Ponomarev <igor.ponomarev@collabora.com>
2024-03-14 17:45:51 +0500

    tests: Speed-up lava_dispatcher tests by reusing Jinja environment
    Currently the Jinja environment is discarded after every test. This has a
    significant negative impact on performance because compiling Jinja
    templates is a very expensive.
    
    Instead use a shared Jinja environment which loads templates from
    "etc/dispatcher-config/device-types" and "tests/lava_scheduler_app/devices".
    
    The only unit tests that had to be adjusted are from `test_recovery.py`
    which for some reason copy pasted the device template rendering code.
    Instead make them use the same `create_job` function that every
    other test uses.

--- tests/lava_dispatcher/test_recovery.py
+++ tests/lava_dispatcher/test_recovery.py
@@ -5,126 +5,40 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 
-import os
 import unittest
 
-from lava_common.yaml import yaml_safe_load
-from lava_dispatcher.device import NewDevice
-from lava_dispatcher.parser import JobParser
 from lava_dispatcher.utils.udev import allow_fs_label
 from tests.lava_dispatcher.test_basic import Factory, LavaDispatcherTestCase
-from tests.utils import DummyLogger, infrastructure_error_multi_paths
-
-
-class FastBootFactory(Factory):
-    """
-    Not Model based, this is not a Django factory.
-    Factory objects are dispatcher based classes, independent
-    of any database objects.
-    """
-
-    def create_hikey_bl_device(self, hostname):
-        """
-        Create a device configuration on-the-fly from in-tree
-        device-type Jinja2 template.
-        """
-        with open(
-            os.path.join(
-                os.path.dirname(__file__),
-                "..",
-                "..",
-                "tests",
-                "lava_scheduler_app",
-                "devices",
-                "hi6220-hikey-bl-01.jinja2",
-            )
-        ) as hikey:
-            data = hikey.read()
-        test_template = self.prepare_jinja_template(hostname, data)
-        rendered = test_template.render()
-        return (rendered, data)
-
-    def create_hikey_bl_job(self, filename):
-        (data, device_dict) = self.create_hikey_bl_device("hi6220-hikey-01")
-        device = NewDevice(yaml_safe_load(data))
-        self.validate_data("hi6220-hikey-01", device_dict)
-        fastboot_yaml = os.path.join(os.path.dirname(__file__), filename)
-        with open(fastboot_yaml) as sample_job_data:
-            parser = JobParser()
-            job = parser.parse(sample_job_data, device, 4212, None, "")
-            job.logger = DummyLogger()
-        return job
-
-
-class UBootFactory(Factory):
-    """
-    Not Model based, this is not a Django factory.
-    Factory objects are dispatcher based classes, independent
-    of any database objects.
-    """
-
-    def create_x15_bl_device(self, hostname):
-        """
-        Create a device configuration on-the-fly from in-tree
-        device-type Jinja2 template.
-        """
-        with open(
-            os.path.join(
-                os.path.dirname(__file__),
-                "..",
-                "..",
-                "tests",
-                "lava_scheduler_app",
-                "devices",
-                "x15-bl-01.jinja2",
-            )
-        ) as x15:
-            data = x15.read()
-        test_template = self.prepare_jinja_template(hostname, data)
-        rendered = test_template.render()
-        return (rendered, data)
-
-    def create_x15_bl_job(self, filename):
-        (data, device_dict) = self.create_x15_bl_device("x15-bl-01")
-        device = NewDevice(yaml_safe_load(data))
-        self.validate_data("x15-bl-01", device_dict)
-        uboot_yaml = os.path.join(os.path.dirname(__file__), filename)
-        with open(uboot_yaml) as sample_job_data:
-            parser = JobParser()
-            job = parser.parse(sample_job_data, device, 4213, None, "")
-            job.logger = DummyLogger()
-        return job
+from tests.utils import infrastructure_error_multi_paths
 
 
 class TestRecoveryMode(LavaDispatcherTestCase):
-    def setUp(self):
-        super().setUp()
-        self.fastboot_factory = FastBootFactory()
-        self.uboot_factory = UBootFactory()
-        self.fastboot_job = self.fastboot_factory.create_hikey_bl_job(
-            "sample_jobs/hi6220-recovery.yaml"
+    def create_fastboot_job(self):
+        return Factory().create_job(
+            "hi6220-hikey-bl-01.jinja2", "sample_jobs/hi6220-recovery.yaml"
         )
-        self.uboot_job = self.uboot_factory.create_x15_bl_job(
-            "sample_jobs/x15-recovery.yaml"
-        )
+
+    def create_uboot_job(self):
+        return Factory().create_job("x15-bl-01.jinja2", "sample_jobs/x15-recovery.yaml")
 
     @unittest.skipIf(
         infrastructure_error_multi_paths(["lxc-info", "img2simg", "simg2img"]),
         "lxc or img2simg or simg2img not installed",
     )
     def test_structure_fastboot(self):
-        self.assertIsNotNone(self.fastboot_job)
-        self.fastboot_job.validate()
+        fastboot_job = self.create_fastboot_job()
+        self.assertIsNotNone(fastboot_job)
+        fastboot_job.validate()
 
         description_ref = self.pipeline_reference(
-            "hi6220-recovery.yaml", job=self.fastboot_job
+            "hi6220-recovery.yaml", job=fastboot_job
         )
-        self.assertEqual(description_ref, self.fastboot_job.pipeline.describe())
+        self.assertEqual(description_ref, fastboot_job.pipeline.describe())
 
-        requires_board_id = not allow_fs_label(self.fastboot_job.device)
+        requires_board_id = not allow_fs_label(fastboot_job.device)
         self.assertFalse(requires_board_id)
-        if "device_info" in self.fastboot_job.device:
-            for usb_device in self.fastboot_job.device["device_info"]:
+        if "device_info" in fastboot_job.device:
+            for usb_device in fastboot_job.device["device_info"]:
                 if (
                     usb_device.get("board_id", "") in ["", "0000000000"]
                     and requires_board_id
@@ -136,18 +50,17 @@ class TestRecoveryMode(LavaDispatcherTestCase):
         "lxc or img2simg or simg2img not installed",
     )
     def test_structure_uboot(self):
-        self.assertIsNotNone(self.uboot_job)
-        self.uboot_job.validate()
+        uboot_job = self.create_uboot_job()
+        self.assertIsNotNone(uboot_job)
+        uboot_job.validate()
 
-        description_ref = self.pipeline_reference(
-            "x15-recovery.yaml", job=self.uboot_job
-        )
-        self.assertEqual(description_ref, self.uboot_job.pipeline.describe())
+        description_ref = self.pipeline_reference("x15-recovery.yaml", job=uboot_job)
+        self.assertEqual(description_ref, uboot_job.pipeline.describe())
 
-        requires_board_id = not allow_fs_label(self.uboot_job.device)
+        requires_board_id = not allow_fs_label(uboot_job.device)
         self.assertFalse(requires_board_id)
-        if "device_info" in self.uboot_job.device:
-            for usb_device in self.uboot_job.device["device_info"]:
+        if "device_info" in uboot_job.device:
+            for usb_device in uboot_job.device["device_info"]:
                 if (
                     usb_device.get("board_id", "") in ["", "1900d00f3b1400a2"]
                     and requires_board_id
@@ -155,9 +68,10 @@ class TestRecoveryMode(LavaDispatcherTestCase):
                     self.fail("[LXC_CREATE] board_id unset")
 
     def test_fastboot_commands(self):
+        fastboot_job = self.create_fastboot_job()
         enter = [
             action
-            for action in self.fastboot_job.pipeline.actions
+            for action in fastboot_job.pipeline.actions
             if action.name == "recovery-boot"
         ][0]
         mode = [
@@ -165,19 +79,21 @@ class TestRecoveryMode(LavaDispatcherTestCase):
             for action in enter.pipeline.actions
             if action.name == "switch-recovery"
         ][0]
-        recovery = self.fastboot_job.device["actions"]["deploy"]["methods"]["recovery"]
+        recovery = fastboot_job.device["actions"]["deploy"]["methods"]["recovery"]
         self.assertIsNotNone(recovery["commands"].get(mode.mode))
         self.assertEqual(
             [
-                "/home/neil/lava-lab/shared/lab-scripts/eth008_control -a 10.15.0.171 -r 1 -s off",
-                "/home/neil/lava-lab/shared/lab-scripts/eth008_control -a 10.15.0.171 -r 2 -s on",
+                "/home/neil/lava-lab/shared/lab-scripts/eth008_control "
+                "-a 10.15.0.171 -r 1 -s off",
+                "/home/neil/lava-lab/shared/lab-scripts/eth008_control "
+                "-a 10.15.0.171 -r 2 -s on",
             ],
             recovery["commands"][mode.mode],
         )
         self.assertEqual("recovery_mode", mode.mode)
         exit_mode = [
             action
-            for action in self.fastboot_job.pipeline.actions
+            for action in fastboot_job.pipeline.actions
             if action.name == "recovery-boot"
         ][1]
         mode = [
@@ -188,17 +104,20 @@ class TestRecoveryMode(LavaDispatcherTestCase):
         self.assertIsNotNone(recovery["commands"].get(mode.mode))
         self.assertEqual(
             [
-                "/home/neil/lava-lab/shared/lab-scripts/eth008_control -a 10.15.0.171 -r 1 -s on",
-                "/home/neil/lava-lab/shared/lab-scripts/eth008_control -a 10.15.0.171 -r 2 -s off",
+                "/home/neil/lava-lab/shared/lab-scripts/eth008_control "
+                "-a 10.15.0.171 -r 1 -s on",
+                "/home/neil/lava-lab/shared/lab-scripts/eth008_control "
+                "-a 10.15.0.171 -r 2 -s off",
             ],
             recovery["commands"][mode.mode],
         )
         self.assertEqual("recovery_exit", mode.mode)
 
     def test_uboot_commands(self):
+        uboot_job = self.create_uboot_job()
         enter = [
             action
-            for action in self.uboot_job.pipeline.actions
+            for action in uboot_job.pipeline.actions
             if action.name == "recovery-boot"
         ][0]
         mode = [
@@ -206,7 +125,7 @@ class TestRecoveryMode(LavaDispatcherTestCase):
             for action in enter.pipeline.actions
             if action.name == "switch-recovery"
         ][0]
-        recovery = self.uboot_job.device["actions"]["deploy"]["methods"]["recovery"]
+        recovery = uboot_job.device["actions"]["deploy"]["methods"]["recovery"]
         self.assertIsNotNone(recovery["commands"].get(mode.mode))
         self.assertEqual(
             [
@@ -218,9 +137,11 @@ class TestRecoveryMode(LavaDispatcherTestCase):
             recovery["commands"][mode.mode],
         )
         self.assertEqual("recovery_mode", mode.mode)
+
+        fastboot_job = self.create_fastboot_job()
         exit_mode = [
             action
-            for action in self.fastboot_job.pipeline.actions
+            for action in fastboot_job.pipeline.actions
             if action.name == "recovery-boot"
         ][1]
         mode = [
