a1fbf1d65ad381da16cf17be2157c8e090b76c3e
Igor Ponomarev <igor.ponomarev@collabora.com>
2024-03-18 17:59:36 +0500

    tests: Make shell logger tests more advanced
    Test the logged inputs as well as sending multiple lines of input
    to the spawned shell.

--- tests/lava_dispatcher/test_shell_logger.py
+++ tests/lava_dispatcher/test_shell_logger.py
@@ -19,20 +19,25 @@ class TestShellLogger(TestCase):
         super().setUp()
         self.logger_mock = MagicMock()
 
-    def run_shell_command(self, shell_cmd: str, input_line: str | None = None) -> None:
-        command = ShellCommand(
+    def create_shell_command(self, shell_cmd: str) -> ShellCommand:
+        return ShellCommand(
             shell_cmd,
             Timeout("test_shell_logger", None),
             logger=self.logger_mock,
         )
-        if input_line is not None:
-            command.sendline(input_line)
-        command.expect(pexpect_eof)
-        command.flush()
 
-    def assert_logger_target_calls(self, expected_log_fragments: list[str]) -> None:
+    def assert_logger_target_calls(
+        self,
+        input_logs: list[str],
+        target_logs: list[str],
+    ) -> None:
+        input_mock = self.logger_mock.input
         target_mock = self.logger_mock.target
-        target_mock.assert_has_calls([call(x) for x in expected_log_fragments])
+        with self.subTest("Check input logs"):
+            input_mock.assert_has_calls([call(x) for x in input_mock])
+
+        with self.subTest("Check target logs"):
+            target_mock.assert_has_calls([call(x) for x in target_logs])
 
     # Because pexpect runs commands in pseudo TTY
     # it will mangle the output of commands.
@@ -40,17 +45,55 @@ class TestShellLogger(TestCase):
     # \r\n -> \r\r\n
     # ShellLogger is expected to split on both \r\n and \r\r\n
     def test_shell_output_newline(self) -> None:
-        self.run_shell_command("printf 'foo\nbar'")
-        self.assert_logger_target_calls(["foo", "bar"])
+        command = self.create_shell_command(r"printf 'foo\nbar'")
+        command.expect(pexpect_eof)
+        command.flush()
+        self.assertEqual(command.exitstatus, 0)
+        self.assert_logger_target_calls([], ["foo", "bar"])
 
     def test_shell_output_carriadge_return_newline(self) -> None:
-        self.run_shell_command("printf 'foo\r\nbar'")
-        self.assert_logger_target_calls(["foo", "bar"])
+        command = self.create_shell_command(r"printf 'foo\r\nbar'")
+        command.expect(pexpect_eof)
+        command.flush()
+        self.assertEqual(command.exitstatus, 0)
+        self.assert_logger_target_calls([], ["foo", "bar"])
 
     # Because of pseudo TTY all input will be echoed to output.
     # Also pexpect will log the input as well.
     # This means all input is double logged.
     # The pexpect input log will not mangle lines but pseudo TTY echo would.
     def test_shell_input(self) -> None:
-        self.run_shell_command("true", input_line="foo")
-        self.assert_logger_target_calls(["foo", "foo"])
+        command = self.create_shell_command(
+            "sh -c 'read ONE TWO; echo \"$ONE, $TWO!\"'"
+        )
+        command.sendline("Hello World")
+        command.expect(pexpect_eof)
+        command.flush()
+        self.assertEqual(command.exitstatus, 0)
+        self.assert_logger_target_calls(
+            ["Hello", "World"],
+            [
+                "Hello World",  # Input
+                "Hello, World!",  # Actual output
+            ],
+        )
+
+    def test_shell_input_two_inputs(self) -> None:
+        command = self.create_shell_command(
+            "sh -c 'read ONE; read TWO; echo \"$ONE, $TWO!\"'"
+        )
+        command.sendline("Hello")
+        command.sendline("World")
+        command.expect(pexpect_eof)
+        command.flush()
+        self.assertEqual(command.exitstatus, 0)
+        self.assert_logger_target_calls(
+            ["Hello", "World"],
+            [
+                "Hello",  # Input
+                "World",
+                "Hello",  # Echo
+                "World",
+                "Hello, World!",  # Actual output
+            ],
+        )
