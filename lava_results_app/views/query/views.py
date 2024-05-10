Linaro/lava

commit 9c580a4fae42f290cbdacf164625ccfdc64f19e3

Use django-tables for lava_results Query conditions
Instead of fragile javascript formatted HTML use django-tables
python package that is already used for all other tables.
This also allows for sorting by a column.

Since javascript no longer updates the table a reload is required
after adding a new condition. However, deleting a condition
already required reloading the page.

Also require POST to delete query conditions to prevent CSRF.

lava_results_app/views/query/views.py
@@ -21,6 +21,7 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect, JsonRespons
 from django.shortcuts import get_object_or_404, render
 from django.template import defaultfilters
 from django.urls import reverse
+from django.views.decorators.http import require_POST
 from django_tables2 import RequestConfig
 
 from lava_results_app.models import (
@@ -40,6 +41,7 @@ from lava_results_app.views.query.forms import QueryConditionForm, QueryForm
 from lava_results_app.views.query.tables import (
     GroupQueryTable,
     OtherQueryTable,
+    QueryConditionsTable,
     QueryTestCaseTable,
     QueryTestJobTable,
     QueryTestSuiteTable,
@@ -270,13 +272,17 @@ def query_custom(request):
 @login_required
 def query_detail(request, username, name):
     query = get_object_or_404(Query, owner__username=username, name=name)
-    query_conditions = Query.serialize_conditions(query.querycondition_set.all())
+    query_conditions_table = QueryConditionsTable(
+        query.querycondition_set.select_related("table").all(), request=request
+    )
+    config = RequestConfig(request)
+    config.configure(query_conditions_table)
     return render(
         request,
         "lava_results_app/query_detail.html",
         {
             "query": query,
-            "query_conditions": query_conditions,
+            "query_conditions_table": query_conditions_table,
             "bread_crumb_trail": BreadCrumbTrail.leading_to(
                 query_detail, username=username, name=name
             ),
@@ -510,6 +516,7 @@ def query_edit_condition(request, username, name, id):
     )
 
 
+@require_POST
 @login_required
 @ownership_required
 def query_remove_condition(request, username, name, id):

lava_results_app/static/lava_results_app/js/query.js
@@ -160,42 +160,7 @@ $(document).ready(function () {
                     $("#condition_errors").html(error_msg);
                     return false;
                 } else {
-                    condition = data[0].fields;
-
-                    condition_row_html =
-                        '<td>' + data[1].fields.model + '</td>' +
-                        '<td>' + condition.field + '</td>' +
-                        '<td>' + condition.operator + '</td>' +
-                        '<td>' + condition.value + '</td>' +
-			'<td><a class="glyphicon glyphicon-edit" ' +
-                        'aria-hidden="true" href="javascript: void(0);" ' +
-                        'onclick="open_condition_modal(\'' +
-                        query_name + '\',\'' + data[0].pk + '\',\'' +
-                        condition.table + '\',\'' + condition.field + '\',\'' +
-                        condition.operator + '\',\'' +
-			$("<textarea/>").text(condition.value).html() +
-			'\');"></a></td>' +
-                        '<td><a class="glyphicon glyphicon-remove" ' +
-                        'aria-hidden="true" href="/results/query/~' +
-                        query_user + '/' + query_name + '/' +
-                        data[0].pk + '/+remove-condition"></a></td>';
-
-                    if ($("#condition_id").val() != "") {
-                        $("#condition_row_" + $("#condition_id").val()).html(
-                            condition_row_html);
-                    } else {
-                        $("#conditions_container").find("tbody").append(
-                            '<tr id="condition_row_' + data[0].pk + '">' +
-                                condition_row_html + '</tr>'
-                        );
-                        if (is_live == "False") {
-                            // Enable 'Run query' button.
-                            $("#query_refresh").removeClass('disabled');
-                            $("#query_refresh").attr('title', '');
-                            add_refresh_click_event();
-                        }
-                    }
-                    $(dialog_selector).modal('hide');
+                    location.reload();
                 }
             },
         });

lava_results_app/templates/lava_results_app/query_detail.html
@@ -157,48 +157,7 @@
 <dl class="dl-horizontal">
 
 <div class="fields-container">
-  <table id="conditions_container" class="table table-bordered table-striped">
-    <thead>
-        <tr>
-            <th>Entity</th>
-            <th>Field</th>
-            <th>Operator</th>
-            <th>Value</th>
-            {% if is_accessible %}
-            <th>Edit</th>
-            <th>Remove</th>
-            {% endif %}
-        </tr>
-    </thead>
-    <tbody>
-    {% for condition in query.querycondition_set.all %}
-    <tr id="condition_row_{{ condition.id }}">
-      <td>
-	{{ condition.table.model }}
-      </td>
-      <td>
-	{{ condition.field }}
-      </td>
-      <td>
-	{{ condition.operator }}
-      </td>
-      <td>
-	{{ condition.value|safe }}
-      </td>
-      {% if is_accessible %}
-      <td>
-	<a class="glyphicon glyphicon-edit" aria-hidden="true" href="javascript: void(0);" onclick="open_condition_modal('{{ query.name }}','{{ condition.id }}','{{ condition.table.id }}','{{ condition.field }}','{{ condition.operator }}','{{ condition.value }}');">
-	</a>
-      </td>
-      <td>
-	<a class="glyphicon glyphicon-remove" aria-hidden="true" href="{% url 'lava.results.query_remove_condition' query.owner.username query.name condition.id %}">
-	</a>
-      </td>
-      {% endif %}
-    </tr>
-    {% endfor %}
-    </tbody>
-  </table>
+  {% render_table query_conditions_table %}
   <a onclick="open_condition_modal('{{ query.name }}');" class="btn btn-sm btn-primary {% if not is_accessible%}disabled{% endif %}" href="javascript: void(0);" >
     Add new condition
   </a>

lava_results_app/views/query/tables.py
@@ -199,3 +199,35 @@ class QueryTestSuiteTable(LavaTable):
         template_name = "lazytables.html"
         attrs = {"class": "table table-hover", "id": "query-results-table"}
         per_page_field = "length"
+
+
+class QueryConditionsTable(LavaTable):
+    table_model = tables.Column(accessor="table.model", verbose_name="Entity")
+    field = tables.Column(verbose_name="Field")
+    operator = tables.Column(verbose_name="Operator")
+    value = tables.Column(verbose_name="Value")
+    edit = tables.TemplateColumn(
+        (
+            '<a class="glyphicon glyphicon-edit" aria-hidden="true" '
+            'href="javascript: void(0);" '
+            "onclick=\"open_condition_modal('{{ query.name }}','{{ record.id }}',"
+            "'{{ record.table.id }}','{{ record.field }}',"
+            "'{{ record.operator }}','{{ record.value }}');\"></a>"
+        ),
+        orderable=False,
+    )
+    remove = tables.TemplateColumn(
+        (
+            '<a class="glyphicon glyphicon-remove" aria-hidden="true" '
+            'href="javascript:document.getElementById('
+            "'query-remove-condition-{{record.id}}').submit();\"></a>"
+            '<form hidden id="query-remove-condition-{{record.id}}"'
+            "action=\"{% url 'lava.results.query_remove_condition' "
+            'query.owner.username query.name record.id %}" '
+            'method="post">{% csrf_token %}</form>'
+        ),
+        orderable=False,
+    )
+
+    class Meta(LavaTable.Meta):
+        ...

commit 04a412121950462c95f4c31b0e351b86c0c367cf

Fix query_detail.html run query and copy buttons not working
The 9c580a4fae42f290cbdacf164625ccfdc64f19e3 accidentally removed
the `query_conditions` template variable which those buttons depend
on. Restore `query_conditions`.

Reported by @chase-qi

lava_results_app/views/query/views.py
@@ -272,6 +272,7 @@ def query_custom(request):
 @login_required
 def query_detail(request, username, name):
     query = get_object_or_404(Query, owner__username=username, name=name)
+    query_conditions = Query.serialize_conditions(query.querycondition_set.all())
     query_conditions_table = QueryConditionsTable(
         query.querycondition_set.select_related("table").all(), request=request
     )
@@ -282,6 +283,7 @@ def query_detail(request, username, name):
         "lava_results_app/query_detail.html",
         {
             "query": query,
+            "query_conditions": query_conditions,
             "query_conditions_table": query_conditions_table,
             "bread_crumb_trail": BreadCrumbTrail.leading_to(
                 query_detail, username=username, name=name