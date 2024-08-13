Linaro/lava

commit 7f02689c28e2e93bb8e205d92ebf975cd0064703

Allow to use u-boot based dfu instead of the hardware one
On some boards, the hardware dfu serial number is set to 0000000.
If the boards provides two boot media (NOR and NAND for example), we can have a
recovery u-boot on one media that will be used to flash the second media using
dfu.

Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

lava_dispatcher/actions/boot/dfu.py
@@ -19,12 +19,13 @@
 # with this program; if not, see <http://www.gnu.org/licenses>.
 
 from lava_common.exceptions import ConfigurationError, InfrastructureError
+
 from lava_dispatcher.action import Action, Pipeline
-from lava_dispatcher.logical import Boot, RetryAction
-from lava_dispatcher.actions.boot import BootAction
-from lava_dispatcher.utils.udev import WaitDFUDeviceAction
+from lava_dispatcher.actions.boot import BootAction, BootloaderInterruptAction
 from lava_dispatcher.connections.serial import ConnectDevice
+from lava_dispatcher.logical import Boot, RetryAction
 from lava_dispatcher.power import ResetDevice
+from lava_dispatcher.utils.udev import WaitDFUDeviceAction
 from lava_dispatcher.utils.shell import which
 from lava_dispatcher.utils.strings import substitute
 
@@ -73,13 +74,46 @@ class BootDFURetry(RetryAction):
     summary = "boot dfu image"
 
     def populate(self, parameters):
+        dfu = self.job.device["actions"]["boot"]["methods"]["dfu"]
+        parameters = dfu["parameters"]
+
         self.internal_pipeline = Pipeline(
             parent=self, job=self.job, parameters=parameters
         )
         self.internal_pipeline.add_action(ConnectDevice())
         self.internal_pipeline.add_action(ResetDevice())
+        if dfu.get("implementation") == "u-boot":
+            self.internal_pipeline.add_action(
+                BootloaderInterruptAction(method="u-boot")
+            )
+            self.internal_pipeline.add_action(EnterDFU())
         self.internal_pipeline.add_action(WaitDFUDeviceAction())
         self.internal_pipeline.add_action(FlashDFUAction())
+
+
+class EnterDFU(Action):
+
+    name = "enter-dfu"
+    description = "enter software dfu mode"
+    summary = "enter software dfu mode"
+
+    def validate(self):
+        super().validate()
+        parameters = self.job.device["actions"]["boot"]["methods"]["dfu"]["parameters"]
+        if "enter-commands" not in parameters:
+            self.errors = '"enter-commands" is not defined'
+        elif not isinstance(parameters["enter-commands"], list):
+            self.errors = '"enter-commands" should be a list'
+
+    def run(self, connection, max_end_time):
+        connection = super().run(connection, max_end_time)
+        parameters = self.job.device["actions"]["boot"]["methods"]["dfu"]["parameters"]
+        for (index, cmd) in enumerate(parameters["enter-commands"]):
+            connection.sendline(cmd)
+            # Do not wait for the bootloader prompt for the last command.
+            # This command does not return.
+            if index + 1 < len(parameters["enter-commands"]):
+                connection.wait()
 
 
 class FlashDFUAction(Action):
@@ -147,19 +181,38 @@ class FlashDFUAction(Action):
             action="shared", label="shared", key="connection", deepcopy=False
         )
         connection = super().run(connection, max_end_time)
+        dfu = self.job.device["actions"]["boot"]["methods"]["dfu"]
+        reset_works = dfu.get("reset_works", True)
+        implementation = dfu.get("implementation", "hardware")
+
+        # Store the previous prompt_str to restore it afterward
+        prompt_str = connection.prompt_str
+        if implementation != "hardware":
+            connection.prompt_str = self.job.device.get_constant(
+                "dfu-download", prefix=implementation
+            )
+
         for (index, dfu_command) in enumerate(self.exec_list):
-            if index + 1 == len(self.exec_list):
-                if self.job.device["actions"]["boot"]["methods"]["dfu"].get(
-                    "reset_works", True
-                ):
-                    dfu_command.extend(["--reset"])
+            # add --reset for the last command (if reset works)
+            if index + 1 == len(self.exec_list) and reset_works:
+                dfu_command.extend(["--reset"])
             dfu = " ".join(dfu_command)
             output = self.run_command(dfu.split(" "))
+            # Check the output as dfu-util can return 0 in case of errors.
             if output:
                 if "No error condition is present\nDone!\n" not in output:
                     raise InfrastructureError("command failed: %s" % dfu)
             else:
                 raise InfrastructureError("command failed: %s" % dfu)
+
+            # Wait only for non-hardware implementations
+            # In fact, the booloader will print some strings when the transfert
+            # is finished.
+            if implementation != "hardware":
+                connection.wait()
+
+        # Restore the prompts
+        connection.prompt_str = prompt_str
         self.set_namespace_data(
             action="shared", label="shared", key="connection", value=connection
         )

lava_dispatcher/tests/pipeline_refs/rzn1d-dfu.yaml
@@ -1,0 +1,82 @@
+- class: actions.deploy.image.DeployImagesAction
+  name: deployimages
+  pipeline:
+  - class: actions.deploy.download.DownloaderAction
+    name: download-retry
+    pipeline:
+    - {class: actions.deploy.download.HttpDownloadAction, name: http-download}
+  - class: actions.deploy.download.DownloaderAction
+    name: download-retry
+    pipeline:
+    - {class: actions.deploy.download.HttpDownloadAction, name: http-download}
+  - class: actions.deploy.download.DownloaderAction
+    name: download-retry
+    pipeline:
+    - {class: actions.deploy.download.HttpDownloadAction, name: http-download}
+  - class: actions.deploy.download.DownloaderAction
+    name: download-retry
+    pipeline:
+    - {class: actions.deploy.download.HttpDownloadAction, name: http-download}
+- {class: actions.commands.CommandAction, name: user-command}
+- class: actions.boot.dfu.BootDFU
+  name: boot-dfu-image
+  pipeline:
+  - class: actions.boot.dfu.BootDFURetry
+    name: boot-dfu-retry
+    pipeline:
+    - {class: connections.serial.ConnectDevice, name: connect-device}
+    - class: power.ResetDevice
+      name: reset-device
+      pipeline:
+      - {class: power.PDUReboot, name: pdu-reboot}
+    - {class: actions.boot.BootloaderInterruptAction, name: bootloader-interrupt}
+    - {class: actions.boot.dfu.EnterDFU, name: enter-dfu}
+    - {class: utils.udev.WaitDFUDeviceAction, name: wait-dfu-device}
+    - {class: actions.boot.dfu.FlashDFUAction, name: flash-dfu}
+- {class: actions.commands.CommandAction, name: user-command}
+- class: actions.deploy.overlay.OverlayAction
+  name: lava-overlay
+  pipeline:
+  - {class: actions.deploy.overlay.SshAuthorize, name: ssh-authorize}
+  - {class: actions.deploy.overlay.VlandOverlayAction, name: lava-vland-overlay}
+  - {class: actions.deploy.overlay.MultinodeOverlayAction, name: lava-multinode-overlay}
+  - class: actions.deploy.testdef.TestDefinitionAction
+    name: test-definition
+    pipeline:
+    - {class: actions.deploy.testdef.GitRepoAction, name: git-repo-action}
+    - {class: actions.deploy.testdef.TestOverlayAction, name: test-overlay}
+    - {class: actions.deploy.testdef.TestInstallAction, name: test-install-overlay}
+    - {class: actions.deploy.testdef.TestRunnerAction, name: test-runscript-overlay}
+  - {class: actions.deploy.overlay.CompressOverlay, name: compress-overlay}
+  - {class: actions.deploy.overlay.PersistentNFSOverlay, name: persistent-nfs-overlay}
+- class: actions.boot.bootloader.BootBootloaderAction
+  name: boot-bootloader
+  pipeline:
+  - {class: connections.serial.ConnectDevice, name: connect-device}
+  - {class: actions.boot.BootloaderCommandOverlay, name: bootloader-overlay}
+  - class: actions.boot.bootloader.BootBootloaderRetry
+    name: boot-bootloader-retry
+    pipeline:
+    - class: power.ResetDevice
+      name: reset-device
+      pipeline:
+      - {class: power.PDUReboot, name: pdu-reboot}
+    - {class: actions.boot.BootloaderInterruptAction, name: bootloader-interrupt}
+    - {class: actions.boot.BootloaderCommandsAction, name: bootloader-commands}
+- class: actions.boot.minimal.MinimalBoot
+  name: minimal-boot
+  pipeline:
+  - {class: connections.serial.ConnectDevice, name: connect-device}
+  - {class: actions.boot.AutoLoginAction, name: auto-login-action}
+  - {class: shell.ExpectShellSession, name: expect-shell-connection}
+  - {class: actions.boot.OverlayUnpack, name: overlay-unpack}
+  - {class: actions.boot.environment.ExportDeviceEnvironment, name: export-device-env}
+- class: actions.test.shell.TestShellRetry
+  name: lava-test-retry
+  pipeline:
+  - {class: actions.test.shell.TestShellAction, name: lava-test-shell}
+- class: power.FinalizeAction
+  name: finalize
+  pipeline:
+  - {class: power.PowerOff, name: power-off}
+  - {class: power.ReadFeedback, name: read-feedback}

lava_dispatcher/tests/sample_jobs/rzn1d-dfu.yaml
@@ -1,0 +1,92 @@
+device_type: rzn1
+job_name: flash rzn1 using u-boot dfu
+
+timeouts:
+  job:
+    minutes: 10
+  action:
+    minutes: 5
+  connection:
+    minutes: 2
+priority: medium
+visibility: public
+
+actions:
+- deploy:
+    namespace: dfu
+    to: tmpfs
+    images:
+      sf_fsbl:
+        image_arg: --alt sf_fsbl --download {sf_fsbl}
+        url: http://.../rzn1d-snarc-fsbl-secmon.img
+      sf_uboot0:
+        image_arg: --alt sf_uboot0 --download {sf_uboot0}
+        url: http://.../u-boot-lces2-ddr.itb
+      sf_uboot1:
+        image_arg: --alt sf_uboot1 --download {sf_uboot1}
+        url: http://.../u-boot-lces2-ddr.itb
+      n_kernel1:
+        image_arg: --alt n_kernel1 --download {n_kernel1}
+        url: http://.../fitImage-1.0-r0-rzn1-snarc.itb
+
+- command:
+    namespace: dfu
+    name: set_boot_to_nand
+
+- boot:
+    namespace: dfu
+    method: dfu
+    timeout:
+      minutes: 10
+
+- command:
+    namespace: test
+    name: set_boot_to_qspi
+
+- deploy:
+    namespace: test
+    to: overlay
+
+- boot:
+    namespace: test
+    connection-namespace: dfu
+    method: bootloader
+    bootloader: u-boot
+    prompts: ["=>"]
+    commands: ["run linux_bestla"]
+
+- boot:
+    namespace: test
+    timeout:
+      minutes: 5
+    method: minimal
+    reset: false
+    auto_login:
+      login_prompt: 'login:'
+      username: root
+      password_prompt: "Password:"
+      password: "P@ssword-1"
+      login_commands:
+      - P@ssword-1
+      - azertAZERT12345
+      - azertAZERT12345
+      - azertAZERT12345
+    prompts:
+    - "root@rzn1-snarc:~# "
+    - "root@rzn1-snarc:/tmp# "
+    - "Current password: "
+    - "New password: "
+    - "Retype new password: "
+    transfer_overlay:
+      download_command: unset http_proxy ; dhclient eth1 ; cd /tmp ; wget
+      unpack_command: tar -C / -xzf
+
+- test:
+    namespace: test
+    timeout:
+      minutes: 5
+    definitions:
+    - repository: http://git.linaro.org/qa/test-definitions.git
+      from: git
+      path: automated/linux/busybox/busybox.yaml
+      name: busybox

ci-run
@@ -96,7 +96,7 @@ if [ -n "${TEMPLATE}" ]
 then
     echo "Testing templates"
     echo
-    PYTHONPATH=. pytest-3 --cache-clear -v lava_scheduler_app/tests/test_base_templates.py lava_scheduler_app/tests/test_templates.py lava_scheduler_app/tests/test_barebox_templates.py lava_scheduler_app/tests/test_uboot_templates.py lava_scheduler_app/tests/test_fastboot_templates.py lava_scheduler_app/tests/test_grub_templates.py lava_scheduler_app/tests/test_qemu_templates.py
+    PYTHONPATH=. pytest-3 --cache-clear -v lava_scheduler_app/tests/test_base_templates.py lava_scheduler_app/tests/test_templates.py lava_scheduler_app/tests/test_barebox_templates.py lava_scheduler_app/tests/test_uboot_templates.py lava_dispatcher/tests/test_uboot_dfu.py lava_scheduler_app/tests/test_fastboot_templates.py lava_scheduler_app/tests/test_grub_templates.py lava_scheduler_app/tests/test_qemu_templates.py
 fi
 
 if [ -n "${COMMON}" ]

lava_dispatcher/tests/test_uboot_dfu.py
@@ -1,0 +1,41 @@
+# Copyright (C) 2019 Linaro Limited
+#
+# Author: Remi Duraffort <remi.duraffort@linaro.org>
+#
+# This file is part of LAVA Dispatcher.
+#
+# LAVA Dispatcher is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# LAVA Dispatcher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along
+# with this program; if not, see <http://www.gnu.org/licenses>.
+
+from lava_dispatcher.tests.test_basic import Factory, StdoutTestCase
+
+
+class UBootDFUFactory(Factory):
+    def create_rzn1d_job(self, filename):
+        return self.create_job("rzn1d-01.jinja2", filename)
+
+
+class TestUbootDFUAction(StdoutTestCase):
+    def setUp(self):
+        super().setUp()
+        self.factory = UBootDFUFactory()
+
+    def test_enter_dfu_action(self):
+        job = self.factory.create_rzn1d_job("sample_jobs/rzn1d-dfu.yaml")
+        self.assertIsNotNone(job)
+
+        description_ref = self.pipeline_reference("rzn1d-dfu.yaml", job=job)
+        self.assertEqual(description_ref, job.pipeline.describe(False))
+
+        self.assertIsNone(job.validate())

lava_scheduler_app/tests/device-types/arduino101.jinja2
@@ -17,6 +17,5 @@ actions:
     methods:
       dfu:
         parameters:
-          command:
-            dfu-util
+          command: dfu-util
 {% endblock body -%}

lava_scheduler_app/tests/device-types/base-uboot.jinja2
@@ -139,6 +139,7 @@ actions:
       flasher:
         commands: {{ flasher_deploy_commands }}
 {% endif %}
+      image:
       lxc:
       overlay:
       usb:
@@ -184,6 +185,13 @@ actions:
     methods:
       minimal:
       ssh:
+      dfu:
+        implementation: u-boot
+        reset_works: False
+        parameters:
+          enter-commands:
+          - dfu
+          command: dfu-util
 {% if fastboot_boot_uboot %}
       fastboot: {{ fastboot_sequence }}
 {% endif %}

lava_scheduler_app/tests/device-types/base.jinja2
@@ -102,6 +102,7 @@ constants:
 {% if uboot_extra_error_message %}
       - '{{ uboot_extra_error_message }}'
 {% endif %}
+    dfu-download: '{{ dfu_download|default("DOWNLOAD \\.\\.\\. OK\\r\\nCtrl\\+C to exit \\.\\.\\.") }}'
   grub:
     interrupt-prompt: '{{ grub_autoboot_prompt | default("for a command-line") }}'
     interrupt-character: '{{ grub_interrupt_character | default ("c") }}'

commit 1a4a91133fd7c99f24196ec6bae099321d986769

Fix BootDFURetry discarding action parameters
It overwrote them with the DFU parameters from the device definition.
This resulted in the nested Actions timeouts not being properly set.
See the `rzn1d-dfu.yaml` job from unit tests where DFU actions used
the default 300 seconds timeout even though the 10 minutes
timeout was defined in the action block.

All nested Actions acquire the DFU parameters from the device so
there is no point in trying to merge parameters.

This bug was introduced in 7f02689c28e2e93bb8e205d92ebf975cd0064703.

lava_dispatcher/actions/boot/dfu.py
@@ -44,8 +44,6 @@ class BootDFURetry(RetryAction):
 
     def populate(self, parameters):
         dfu = self.job.device["actions"]["boot"]["methods"]["dfu"]
-        parameters = dfu["parameters"]
-
         self.pipeline = Pipeline(parent=self, job=self.job, parameters=parameters)
         self.pipeline.add_action(ConnectDevice(self.job))
         self.pipeline.add_action(ResetDevice(self.job))

tests/lava_dispatcher/pipeline_refs/rzn1d-dfu.yaml
@@ -62,7 +62,7 @@
   pipeline:
   - {class: ConnectDevice, name: connect-device, level: '3.1', max_retries: 1, description: use
       the configured command to connect serial to the device, summary: run connection
-      command, timeout: 300}
+      command, timeout: 600}
   - class: ResetDevice
     name: reset-device
     level: '3.2'
@@ -78,11 +78,11 @@
     description: interrupt bootloader, summary: interrupt bootloader to get an interactive
       shell, timeout: 600}
   - {class: EnterDFU, name: enter-dfu, level: '3.4', max_retries: 1, description: enter
-      software dfu mode, summary: enter software dfu mode, timeout: 300}
+      software dfu mode, summary: enter software dfu mode, timeout: 600}
   - {class: WaitDFUDeviceAction, name: wait-dfu-device, level: '3.5', max_retries: 1,
-    description: wait for DFU device, summary: wait for DFU device, timeout: 300}
+    description: wait for DFU device, summary: wait for DFU device, timeout: 600}
   - {class: FlashDFUAction, name: flash-dfu, level: '3.6', max_retries: 1, description: use
-      dfu to flash the images, summary: use dfu to flash the images, timeout: 300}
+      dfu to flash the images, summary: use dfu to flash the images, timeout: 600}
 - {class: CommandAction, name: user-command, level: '4', max_retries: 1, description: execute
     one of the commands listed by the admin, summary: execute commands, timeout: 300}
 - class: OverlayAction