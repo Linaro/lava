Linaro/lava

commit 38053f6be81bd27f1b0c26ab8e75b9ed927c8c58

Do not rely on decompression auto naming
This is equivalent to the 44c99781a546e7c76a8097ee20b1a0965abf506e
but for decompression. While currently no compression algorithm has
issues with decompression auto naming it is better to make LAVA
have authority over the file names.

Also add type hints. The `compression` argument can be None which
should result in the file not being decompressed at all.

lava_dispatcher/utils/compression.py
@@ -11,7 +11,6 @@
 # vexpress recovery images: any compression though usually zip
 from __future__ import annotations
 
-import os
 import subprocess  # nosec - internal use.
 import tarfile
 from pathlib import Path
@@ -52,36 +51,30 @@ def compress_file(infile: str, compression: str) -> str:
 
     with open(infile, mode="rb") as in_file, open(out_file_path, mode="wb") as out_file:
         try:
-            subprocess.run(
-                args=cmd,
-                stdin=in_file,
-                stdout=out_file,
-                check=True,
-            )
+            subprocess.run(args=cmd, stdin=in_file, stdout=out_file, check=True)
             return out_file_path
         except (OSError, subprocess.CalledProcessError) as exc:
             raise InfrastructureError("unable to compress file %s: %s" % (infile, exc))
 
 
-def decompress_file(infile, compression):
+def decompress_file(infile: str, compression: str | None) -> str:
     if not compression:
         return infile
     if compression not in decompress_command_map.keys():
         raise JobError("Cannot find shell command to decompress: %s" % compression)
 
+    # Assume infile is an absolute path
+    out_file_path = infile.removesuffix(f".{compression}")
+
     # Check that the command does exists
     which(decompress_command_map[compression][0])
+    # local copy for idempotency
+    cmd = decompress_command_map[compression][:]
 
-    with chdir(os.path.dirname(infile)):
-        # local copy for idempotency
-        cmd = decompress_command_map[compression][:]
-        cmd.append(infile)
-        outfile = infile
-        if infile.endswith(compression):
-            outfile = infile[: -(len(compression) + 1)]
+    with open(infile, mode="rb") as in_file, open(out_file_path, mode="wb") as out_file:
         try:
-            subprocess.check_output(cmd)  # nosec - internal use.
-            return outfile
+            subprocess.run(args=cmd, stdin=in_file, stdout=out_file, check=True)
+            return out_file_path
         except (OSError, subprocess.CalledProcessError) as exc:
             raise InfrastructureError(
                 "unable to decompress file %s: %s" % (infile, exc)

commit 697225a4be3948bc80c5d80bb0f5474e01c10586

lava_dispatcher: fix file decompression with multiple files inside
The regression is introduced by
38053f6be81bd27f1b0c26ab8e75b9ed927c8c58. When multiple files are
compressed inside a compressed file, the commit decompresses the
multiple files into one file, which is unexpected.

Signed-off-by: Chase Qi <chase.qi@linaro.org>

lava_dispatcher/utils/compression.py
@@ -11,6 +11,7 @@
 # vexpress recovery images: any compression though usually zip
 from __future__ import annotations
 
+import os
 import subprocess  # nosec - internal use.
 import tarfile
 from pathlib import Path
@@ -30,12 +31,13 @@ compress_command_map: Mapping[str, tuple[str, ...]] = {
     "bz2": ("bzip2",),
     "zstd": ("zstd", "-T0"),
 }
-decompress_command_map: Mapping[str, tuple[str, ...]] = {
-    "xz": ("unxz",),
-    "gz": ("gunzip",),
-    "bz2": ("bunzip2",),
-    "zip": ("unzip",),
-    "zstd": ("unzstd", "-T0"),
+
+decompress_command_map: Mapping[str, list[str]] = {
+    "xz": ["unxz"],
+    "gz": ["gunzip"],
+    "bz2": ["bunzip2"],
+    "zip": ["unzip"],
+    "zstd": ["unzstd"],
 }
 
 
@@ -88,19 +90,11 @@ def decompress_file(infile: str, compression: str | None) -> str:
     which(decompress_command_map[compression][0])
     # local copy for idempotency
     cmd = decompress_command_map[compression][:]
+    cmd.append(infile)
 
     try:
-        with open(infile, mode="rb") as in_file, open(
-            out_file_path, mode="wb"
-        ) as out_file:
-            subprocess.run(
-                args=cmd,
-                stdin=in_file,
-                stdout=out_file,
-                check=True,
-                stderr=subprocess.PIPE,
-                text=True,
-            )
+        with chdir(os.path.dirname(infile)):
+            subprocess.check_output(cmd)
         return out_file_path
     except subprocess.CalledProcessError as proc_exc:
         raise JobError(

tests/lava_dispatcher/test_compression.py
@@ -181,16 +181,16 @@ class TestCompressionBinaries(TestCase):
         with TemporaryDirectory("test-decompression-failure") as tmp_dir:
             tmp_dir_path = Path(tmp_dir)
 
-            with self.subTest("decompression OSError"), self.assertRaisesRegex(
-                InfrastructureError, r"unable to decompress"
-            ):
-                decompress_file(str(tmp_dir_path / "does_not_exist.zstd"), "zstd")
-
             test_input_file = tmp_dir_path / "test.zstd"
             test_input_file.write_text("🤡🤡🤡🤡🤡🤡🤡🤡🤡🤡🤡🤡🤡🤡🤡🤡🤡")
 
             with self.assertRaisesRegex(JobError, r"unable to decompress.*exit code"):
                 decompress_file(str(test_input_file), "zstd")
+
+            with self.subTest("decompression OSError"), self.assertRaisesRegex(
+                InfrastructureError, r"unable to decompress"
+            ):
+                decompress_file("does_not_exist_dir/does_not_exist.zstd", "zstd")
 
     def test_compression_error(self) -> None:
         with TemporaryDirectory("test-compression-failure") as tmp_dir: