4c6a9edd227714915d3fd28a88100998eebc927c
Igor Ponomarev <igor.ponomarev@collabora.com>
2025-06-18 13:17:53 +0100

    Add mypy CI stage
    `mypy` is a type checker to verify that the functions and methods
    take and return the expected objects. For example, a function might
    return None but the code does not verify that case.
    
    To add type checking incrementally keep a list of files that can
    be type checked and gradually add new files as type hints are added
    to them. Use `--follow-imports=silent` to prevent mypy from printing
    errors found in the imported files.
    
    Use `--python-version 3.11` as Debian 12 ships Python 3.11.

---
+++ .gitlab-ci/analyze/mypy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+if [ "$1" = "setup" ]
+then
+  apt-get -q update
+  apt-get install --no-install-recommends --yes mypy python3-typeshed
+else
+  set -x
+  FILES="lava_dispatcher/utils/shell.py"
+  mypy --python-version 3.11 --pretty --strict --follow-imports=silent $FILES
+fi
