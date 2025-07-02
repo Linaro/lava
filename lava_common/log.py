Linaro/lava

commit 975019ec7e205c889924424d7aa4119999cef650

Pass job id to HTTPHandler
This avoids having to akwardly split and extract the job id from
the URL when the 404 HTTP code is encountered. It also makes it
more future proof if the URL format changes.

Use string as this is what lava-run uses for the job id.

lava_common/log.py
@@ -49,11 +49,13 @@ class JobOutputSender:
         url: str,
         token: str,
         max_time: int,
+        job_id: str,
     ):
         self.conn = conn
         self.url = url
         self.token = token
         self.max_time = max_time
+        self.job_id = job_id
         self.max_records = 1000
 
         self.headers = {"User-Agent": f"lava {__version__}", "LAVA-Token": token}
@@ -142,11 +144,10 @@ class JobOutputSender:
                     self.records[0:count] = []
                     self.index += count
             elif ret.status_code == 404:
-                job_id = self.url.strip("/").split("/")[-2]
                 json_data = {}
                 try:
                     json_data = ret.json()
-                    if json_data.get("error") == f"Unknown job '{job_id}'":
+                    if json_data.get("error") == f"Unknown job '{self.job_id}'":
                         self.records[:] = []
                         records_to_send.clear()
                         os.kill(os.getppid(), signal.SIGUSR1)
@@ -204,17 +205,19 @@ def run_output_sender(
     url: str,
     token: str,
     max_time: int,
+    job_id: str,
 ) -> None:
     JobOutputSender(
         conn=conn,
         url=url,
         token=token,
         max_time=max_time,
+        job_id=job_id,
     ).run()
 
 
 class HTTPHandler(logging.Handler):
-    def __init__(self, url, token, interval):
+    def __init__(self, url, token, interval, job_id):
         super().__init__()
         self.formatter = logging.Formatter("%(message)s")
         # Create the multiprocess sender
@@ -252,8 +255,8 @@ class YAMLLogger(logging.Logger):
         self.markers = {}
         self.line = 0
 
-    def addHTTPHandler(self, url, token, interval):
-        self.handler = HTTPHandler(url, token, interval)
+    def addHTTPHandler(self, url, token, interval, job_id):
+        self.handler = HTTPHandler(url, token, interval, job_id)
         self.addHandler(self.handler)
         return self.handler
 

lava/dispatcher/lava-run
@@ -130,6 +130,7 @@ def setup_logger(options):
             f"{options.url}/scheduler/internal/v1/jobs/{options.job_id}/logs/",
             options.token,
             options.job_log_interval,
+            options.job_id,
         )
     else:
         logger.addHandler(logging.StreamHandler())

tests/lava_common/test_log.py
@@ -28,7 +28,7 @@ def test_sender(mocker):
     conn.recv_bytes = mocker.MagicMock()
     conn.recv_bytes.side_effect = [f"{i:04}".encode() for i in range(0, 1001)] + [b""]
 
-    JobOutputSender(conn, "http://localhost", "my-token", 1).run()
+    JobOutputSender(conn, "http://localhost", "my-token", 1, "1234").run()
     assert len(conn.poll.mock_calls) == 2000
     assert len(conn.recv_bytes.mock_calls) == 1002
 
@@ -62,7 +62,7 @@ def test_sender_exceptions(mocker):
     conn.recv_bytes = mocker.MagicMock()
     conn.recv_bytes.side_effect = [b"hello world", b""]
 
-    JobOutputSender(conn, "http://localhost", "my-token", 1).run()
+    JobOutputSender(conn, "http://localhost", "my-token", 1, "1234").run()
     assert len(post.mock_calls) == 3
     for c in post.mock_calls:
         assert c[1] == ("http://localhost",)
@@ -94,6 +94,7 @@ def test_sender_404(mocker):
         f"http://localhost/scheduler/internal/v1/jobs/{job_id}/logs/",
         "my-token",
         1,
+        job_id,
     ).run()
 
     os_getppid.assert_called_once()
@@ -104,7 +105,7 @@ def test_http_handler(mocker):
     Process = mocker.Mock()
     mocker.patch("multiprocessing.Process", return_value=Process)
     mocker.patch("multiprocessing.Pipe", return_value=(mocker.Mock(), mocker.Mock()))
-    handler = HTTPHandler("http://localhost/", "token", 1)
+    handler = HTTPHandler("http://localhost/", "token", 1, "1234")
 
     assert len(Process.start.mock_calls) == 1
     assert len(Process.start.mock_calls) == 1
@@ -143,7 +144,7 @@ def test_yaml_logger(mocker):
 
     logger = YAMLLogger("lava")
     assert logger.handler is None
-    logger.addHTTPHandler("http://localhost/", "my-token", 1)
+    logger.addHTTPHandler("http://localhost/", "my-token", 1, "1234")
     assert isinstance(logger.handler, HTTPHandler) is True
 
     def check(logger, lvl, lvlno, msg=None, mock_calls=1):

commit 90cb2e7fcffe88c18bd92b7750f0b2caacb3d855

Fix the broken job log uploading pipe
It is a regression introduced by 975019ec7e205c889924424d7aa4119999cef650.

Now the `run_output_sender` requires the `job_id` as the 5th argument.

Signed-off-by: Chase Qi <chase.qi@linaro.org>

lava_common/log.py
@@ -227,7 +227,7 @@ class HTTPHandler(logging.Handler):
         # TODO: block more signals?
         signal.pthread_sigmask(signal.SIG_BLOCK, [signal.SIGINT])
         self.proc = multiprocessing.Process(
-            target=run_output_sender, args=(reader, url, token, interval)
+            target=run_output_sender, args=(reader, url, token, interval, job_id)
         )
         self.proc.start()
         signal.pthread_sigmask(signal.SIG_UNBLOCK, [signal.SIGINT])