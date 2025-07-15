e8e04b1945b5035378620be44b0d79a638ddcfd2
Igor Ponomarev <igor.ponomarev@collabora.com>
2025-07-15 11:53:25 +0100

    Improve Timeout class type checking
    Use `X | None` instead of `Optional[X]`. This is a new syntax
    but it won't be evaluated on Debian 11 because of
    `from __future__ import annotations`.
    
    `can_skip` method: Add a type ignore for returning Any. This can
    only be fixed once action parameters are typed.
    
    Make `action_max_end_time` parameter take a float or a None.
    Chaining timeouts is very common which means a yield type of float
    will be used to as `action_max_end_time` of another timeout.
    The root timeout of the job will be called with None. Add extra
    check to the if statement to prevent the action being called
    with the parent action but without `action_max_end_time` float
    value.

--- .gitlab-ci/analyze/mypy.sh
+++ .gitlab-ci/analyze/mypy.sh
@@ -8,6 +8,6 @@ then
   apt-get install --no-install-recommends --yes mypy python3-typeshed
 else
   set -x
-  FILES="lava_dispatcher/utils/shell.py"
+  FILES="lava_dispatcher/utils/shell.py lava_common/timeout.py"
   mypy --python-version 3.11 --pretty --strict --follow-imports=silent $FILES
 fi
