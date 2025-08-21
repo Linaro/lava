3cfb73cf7d2f5269aa8ab4dbfb522739fd0ee387
Igor Ponomarev <igor.ponomarev@collabora.com>
2025-08-21 15:57:57 +0100

    Type check lava_dispatcher/utils/messages.py
    Add extra `TypedDict` where untyped dictionaries where used.
    In the future they can be changed to `@dataclass`.
    
    Signed-off-by: Igor Ponomarev <igor.ponomarev@collabora.com>

--- .gitlab-ci/analyze/mypy.sh
+++ .gitlab-ci/analyze/mypy.sh
@@ -41,6 +41,7 @@ else
     'lava_dispatcher/utils/ext4.py'
     'lava_dispatcher/utils/filesystem.py'
     'lava_dispatcher/utils/installers.py'
+    'lava_dispatcher/utils/messages.py'
     'lava_dispatcher/utils/network.py'
     'lava_dispatcher/utils/shell.py'
     'lava_dispatcher/utils/storage.py'
