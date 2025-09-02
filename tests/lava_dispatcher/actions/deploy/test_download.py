Linaro/lava

commit 4d9f0ebdae9ca53baf6633f4a35e716183bd293c

lava_dispatcher: save "downloads://*" file to unique path by default
The same `flasher` command passes with `http(s)://` schema but may fail
with `downloads://` schema. This is because the download action for the
former schema downloads file to a unique path but uses the same
directory to download files for the latter schema.

This change unify the default behavior cross `http(s)://`, `file://` and
`downloads://` schemas and it provides the `uniquify` parameter for
overwriting.

The downloaded file `self.path` is only used internally, so this change
shouldn't introduce regressions.

Signed-off-by: Chase Qi <chase.qi@linaro.org>

lava_dispatcher/actions/deploy/download.py
@@ -93,7 +93,7 @@ class DownloaderAction(RetryAction):
             action = LxcDownloadAction(self.job, self.key, self.path, url)
         elif url.scheme == "downloads":
             action = PreDownloadedAction(
-                self.job, self.key, url, self.path, params=self.params
+                self.job, self.key, url, self.path, self.uniquify, params=self.params
             )
         else:
             raise JobError("Unsupported url protocol scheme: %s" % url.scheme)
@@ -767,11 +767,13 @@ class PreDownloadedAction(Action):
     description = "Map to the correct downloaded path"
     summary = "pre downloaded"
 
-    def __init__(self, job: Job, key, url, path, params=None):
+    def __init__(self, job: Job, key, url, path, uniquify=True, params=None):
         super().__init__(job)
         self.key = key
         self.url = url
         self.path = path
+        if uniquify:
+            self.path = os.path.join(path, key)
         self.params = params
 
     def validate(self):

tests/lava_dispatcher/actions/deploy/test_download.py
@@ -791,7 +791,7 @@ class TestDowload(LavaDispatcherTestCase):
         )
         job.validate()
 
-    def test_predownloaded(self):
+    def test_predownloaded_uniquify_false(self):
         params = {
             "to": "tmpfs",
             "rootfs": {"url": "downloads://rootfs.xz"},
@@ -800,7 +800,12 @@ class TestDowload(LavaDispatcherTestCase):
         job = self.create_simple_job()
         destdir = job.mkdtemp("some-other-action")
         action = PreDownloadedAction(
-            job, "rootfs", urlparse("downloads://rootfs.xz"), destdir, params
+            job,
+            "rootfs",
+            urlparse("downloads://rootfs.xz"),
+            destdir,
+            uniquify=False,
+            params=params,
         )
         action.parameters = params
 
@@ -818,12 +823,43 @@ class TestDowload(LavaDispatcherTestCase):
         self.assertEqual(mapped_path, (destdir + "/rootfs.xz"))
         self.assertTrue(Path(mapped_path).exists())
 
+    def test_predownloaded(self):
+        params = {
+            "to": "tmpfs",
+            "rootfs": {"url": "downloads://rootfs.xz"},
+            "namespace": "common",
+        }
+        job = self.create_simple_job()
+        destdir = job.mkdtemp("some-other-action")
+        action = PreDownloadedAction(
+            job, "rootfs", urlparse("downloads://rootfs.xz"), destdir, params=params
+        )
+        action.parameters = params
+
+        filename = Path(action.job.tmp_dir) / "downloads/common/rootfs.xz"
+        filename.parent.mkdir(parents=True)
+        filename.touch()
+
+        action.data = {}
+        action.parameters = {"namespace": "common"}
+        action.validate()
+        action.run(None, 4242)
+        mapped_path = action.get_namespace_data(
+            action="download-action", label="rootfs", key="file"
+        )
+        self.assertEqual(mapped_path, (destdir + "/rootfs/rootfs.xz"))
+        self.assertTrue(Path(mapped_path).exists())
+
     def test_predownloaded_subdirectory(self):
         params = {"to": "tmpfs", "rootfs": {"url": "downloads://subdir/rootfs.xz"}}
         job = self.create_simple_job()
         destdir = job.mkdtemp("some-other-action")
         action = PreDownloadedAction(
-            job, "rootfs", urlparse("downloads://subdir/rootfs.xz"), destdir, params
+            job,
+            "rootfs",
+            urlparse("downloads://subdir/rootfs.xz"),
+            destdir,
+            params=params,
         )
         action.parameters = params
 
@@ -838,7 +874,7 @@ class TestDowload(LavaDispatcherTestCase):
         mapped_path = action.get_namespace_data(
             action="download-action", label="rootfs", key="file"
         )
-        self.assertEqual(mapped_path, (destdir + "/subdir/rootfs.xz"))
+        self.assertEqual(mapped_path, (destdir + "/rootfs/subdir/rootfs.xz"))
         self.assertTrue(Path(mapped_path).exists())
 
     def test_predownloaded_missing_file(self):

commit 8fdb2795b627a7f02ac49a76ef7bd00c8a4a38f4

lava_dispatcher: fix u-boot via tftp with `downloads://` schema
* Pass kernel type to boot action to prepare kernel load address
* Fix the regression introduced in commit
  4d9f0ebdae9ca53baf6633f4a35e716183bd293c. Although
  tftp/nbd/iso-installer deploy methods do not allow to pass `uniquify`
  parameter for now, adding the image key to tftp loading path only when
  the parameter is true should be more robust.

Signed-off-by: Chase Qi <chase.qi@linaro.org>

lava_dispatcher/actions/deploy/download.py
@@ -152,16 +152,17 @@ class DownloadHandler(Action):
 
         self.path = path
         # Store the files in a sub-directory to keep the path unique.
-        if uniquify:
+        self.uniquify = uniquify
+        if self.uniquify:
             self.path = os.path.join(path, key)
-        self.fname = None
+        self.fname: str = ""
         self.params = params
 
     def reader(self):
         raise LAVABug("'reader' function unimplemented")
 
     def cleanup(self, connection):
-        if self.fname is not None and os.path.exists(self.fname):
+        if self.fname and os.path.exists(self.fname):
             self.logger.debug("Cleaning up downloaded image: %s", self.fname)
             try:
                 os.remove(self.fname)
@@ -177,7 +178,7 @@ class DownloadHandler(Action):
             return False
         return self.params.get("compression", False)
 
-    def _url_to_fname(self):
+    def _url_to_fname(self) -> str:
         compression = self._compression()
         filename = os.path.basename(self.url.path)
 
@@ -455,27 +456,31 @@ class DownloadHandler(Action):
             self._check_checksum("sha512", sha512.hexdigest(), sha512sum)
 
         # certain deployments need prefixes set
-        if self.parameters.get("to"):
-            if self.parameters["to"] == "tftp" or self.parameters["to"] == "nbd":
-                suffix = self.get_namespace_data(
+        # TFTP deploy base dir suffix (e.g., "job_id/tftp-deploy-uid") will be used as
+        # the TFTP file loading path prefix.
+        if self.parameters.get("to") in ["tftp", "nbd", "iso-installer"]:
+            if self.parameters["to"] in ["tftp", "nbd"]:
+                prefix: str | None = self.get_namespace_data(
                     action="tftp-deploy", label="tftp", key="suffix"
                 )
-                self.set_namespace_data(
-                    action="download-action",
-                    label="file",
-                    key=self.key,
-                    value=os.path.join(suffix, self.key, os.path.basename(self.fname)),
-                )
             elif self.parameters["to"] == "iso-installer":
-                suffix = self.get_namespace_data(
+                prefix = self.get_namespace_data(
                     action="deploy-iso-installer", label="iso", key="suffix"
                 )
+
+            if prefix:
+                _path_dirs = [prefix]
+                if self.uniquify:
+                    _path_dirs.append(self.key)
+                _path_dirs.append(os.path.basename(self.fname))
                 self.set_namespace_data(
                     action="download-action",
                     label="file",
                     key=self.key,
-                    value=os.path.join(suffix, self.key, os.path.basename(self.fname)),
+                    value=os.path.join(*_path_dirs),
                 )
+            else:
+                raise JobError(f"'{self.key}' path prefix is required but not found")
 
         # xnbd protocol needs to know the location
         nbdroot = self.get_namespace_data(
@@ -772,7 +777,8 @@ class PreDownloadedAction(Action):
         self.key = key
         self.url = url
         self.path = path
-        if uniquify:
+        self.uniquify = uniquify
+        if self.uniquify:
             self.path = os.path.join(path, key)
         self.params = params
 
@@ -790,6 +796,14 @@ class PreDownloadedAction(Action):
                 label=self.key,
                 key="image_arg",
                 value=image_arg,
+            )
+
+        if self.key == "kernel" and ("kernel" in self.parameters):
+            self.set_namespace_data(
+                action="download-action",
+                label="type",
+                key=self.key,
+                value=self.parameters[self.key].get("type"),
             )
 
     def run(self, connection, max_end_time):
@@ -830,16 +844,22 @@ class PreDownloadedAction(Action):
         self.set_namespace_data(
             action="download-action", label="file", key=self.key, value=str(dest)
         )
-        if self.parameters["to"] == "tftp" or self.parameters["to"] == "nbd":
-            suffix = self.get_namespace_data(
+        if self.parameters.get("to") in ["tftp", "nbd"]:
+            if prefix := self.get_namespace_data(
                 action="tftp-deploy", label="tftp", key="suffix"
-            )
-            self.set_namespace_data(
-                action="download-action",
-                label="file",
-                key=self.key,
-                value=os.path.join(suffix, dest.name),
-            )
+            ):
+                _path_dirs = [prefix]
+                if self.uniquify:
+                    _path_dirs.append(self.key)
+                _path_dirs.append(os.path.basename(dest.name))
+                self.set_namespace_data(
+                    action="download-action",
+                    label="file",
+                    key=self.key,
+                    value=os.path.join(*_path_dirs),
+                )
+            else:
+                raise JobError(f"'{self.key}' path prefix is required but not found")
 
         if "lava-xnbd" in self.parameters and str(self.key) == "nbdroot":
             self.parameters["lava-xnbd"]["nbdroot"] = str(dest)

lava_dispatcher/actions/deploy/prepare.py
@@ -90,7 +90,7 @@ class UBootPrepareKernelAction(Action):
         if "parameters" not in self.job.device["actions"]["deploy"]:
             return
         self.params = self.job.device["actions"]["deploy"]["parameters"]
-        self.kernel_type = self.get_namespace_data(
+        self.kernel_type: str | None = self.get_namespace_data(
             action="download-action", label="type", key="kernel"
         )
         self.bootcommand = None

tests/lava_dispatcher/actions/deploy/test_download.py
@@ -792,7 +792,7 @@ class TestDowload(LavaDispatcherTestCase):
         job.validate()
 
     def test_predownloaded_uniquify_false(self):
-        params = {
+        parameters = {
             "to": "tmpfs",
             "rootfs": {"url": "downloads://rootfs.xz"},
             "namespace": "common",
@@ -805,9 +805,9 @@ class TestDowload(LavaDispatcherTestCase):
             urlparse("downloads://rootfs.xz"),
             destdir,
             uniquify=False,
-            params=params,
+            params=parameters["rootfs"],
         )
-        action.parameters = params
+        action.parameters = parameters
 
         filename = Path(action.job.tmp_dir) / "downloads/common/rootfs.xz"
         filename.parent.mkdir(parents=True)
@@ -822,6 +822,135 @@ class TestDowload(LavaDispatcherTestCase):
         )
         self.assertEqual(mapped_path, (destdir + "/rootfs.xz"))
         self.assertTrue(Path(mapped_path).exists())
+
+    def test_predownloaded_validate_set_kernel_type(self):
+        parameters = {
+            "to": "tftp",
+            "kernel": {"url": "downloads://zimage", "type": "zimage"},
+            "namespace": "common",
+        }
+
+        job = self.create_simple_job()
+        destdir = job.mkdtemp("some-other-action")
+        action = PreDownloadedAction(
+            job,
+            "kernel",
+            urlparse("downloads://zimage"),
+            destdir,
+            uniquify=True,
+            params=parameters["kernel"],
+        )
+        action.parameters = parameters
+        action.validate()
+
+        self.assertEqual(
+            action.get_namespace_data(
+                action="download-action", label="type", key="kernel"
+            ),
+            "zimage",
+        )
+
+    def test_predownloaded_tftp_file_path(self):
+        parameters = {
+            "to": "tftp",
+            "kernel": {"url": "downloads://zimage", "type": "zimage"},
+            "namespace": "common",
+        }
+
+        job = self.create_simple_job()
+        destdir = job.mkdtemp("some-other-action")
+
+        action = PreDownloadedAction(
+            job,
+            "kernel",
+            urlparse("downloads://zimage"),
+            destdir,
+            uniquify=True,
+            params=parameters["kernel"],
+        )
+        action.parameters = parameters
+        action.data = {}
+        action.parameters = {"namespace": "common"}
+        filename = Path(action.job.tmp_dir) / "downloads/common/zimage"
+        filename.parent.mkdir(parents=True)
+        filename.touch()
+        action.set_namespace_data(
+            action="tftp-deploy", label="tftp", key="suffix", value="tftp-deploy-uid"
+        )
+
+        action.run(None, 4242)
+
+        self.assertEqual(
+            action.get_namespace_data(
+                action="download-action", label="file", key="kernel"
+            ),
+            "tftp-deploy-uid/kernel/zimage",
+        )
+
+    def test_predownloaded_tftp_file_path_uniquify_false(self):
+        parameters = {
+            "to": "tftp",
+            "kernel": {"url": "downloads://zimage", "type": "zimage"},
+            "namespace": "common",
+        }
+
+        job = self.create_simple_job()
+        destdir = job.mkdtemp("some-other-action")
+
+        action = PreDownloadedAction(
+            job,
+            "kernel",
+            urlparse("downloads://zimage"),
+            destdir,
+            uniquify=False,
+            params=parameters["kernel"],
+        )
+        action.parameters = parameters
+        action.data = {}
+        action.parameters = {"namespace": "common"}
+        filename = Path(action.job.tmp_dir) / "downloads/common/zimage"
+        filename.parent.mkdir(parents=True)
+        filename.touch()
+        action.set_namespace_data(
+            action="tftp-deploy", label="tftp", key="suffix", value="tftp-deploy-uid"
+        )
+
+        action.run(None, 4242)
+
+        self.assertEqual(
+            action.get_namespace_data(
+                action="download-action", label="file", key="kernel"
+            ),
+            "tftp-deploy-uid/zimage",
+        )
+
+    def test_predownloaded_tftp_file_path_prefix_not_found(self):
+        parameters = {
+            "to": "tftp",
+            "kernel": {"url": "downloads://zimage", "type": "zimage"},
+            "namespace": "common",
+        }
+
+        job = self.create_simple_job()
+        destdir = job.mkdtemp("some-other-action")
+
+        action = PreDownloadedAction(
+            job,
+            "kernel",
+            urlparse("downloads://zimage"),
+            destdir,
+            uniquify=True,
+            params=parameters["kernel"],
+        )
+        action.parameters = parameters
+        action.data = {}
+        action.parameters = {"namespace": "common"}
+        filename = Path(action.job.tmp_dir) / "downloads/common/zimage"
+        filename.parent.mkdir(parents=True)
+        filename.touch()
+
+        with self.assertRaises(JobError):
+            action.run(None, 4242)
 
     def test_predownloaded(self):
         params = {