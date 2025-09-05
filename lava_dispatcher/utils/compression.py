Linaro/lava

commit a60fedbef91f6265a8ba6d1e0f416f821a818cee

Avoid switching working directory when using cpio
Switching global working directory is very thread unsafe and can
cause difficult to diagnose bugs.

Instead use the `cwd=` parameter when invoking `cpio` which will
spawn the subprocess in the assigned working directory.

Avoid having to call `find` command first and collecting all of its
output. Instead spawn `find` subprocess and connect its stdout
to `cpio` stdin.

lava_dispatcher/utils/compression.py
@@ -133,38 +133,46 @@ def untar_file(infile: str, outdir: str) -> None:
 def cpio(directory: str, filename: str) -> str:
     which("cpio")
     which("find")
-    with chdir(directory):
-        try:
-            find = subprocess.check_output(
-                ["find", "."], stderr=subprocess.STDOUT
-            )  # nosec
-            return subprocess.check_output(  # nosec
-                ["cpio", "--create", "--format", "newc", "--file", filename],
-                input=find,
-                stderr=subprocess.STDOUT,
-            ).decode("utf-8", errors="replace")
-        except Exception as exc:
-            raise InfrastructureError(
-                "Unable to create cpio archive %r: %s" % (filename, exc)
+
+    try:
+        find = subprocess.Popen(
+            args=("find", "."),
+            cwd=directory,
+            stdout=subprocess.PIPE,
+        )
+        with find:
+            cpio = subprocess.run(
+                args=("cpio", "--create", "--format", "newc", "--file", filename),
+                cwd=directory,
+                check=True,
+                encoding="utf-8",
+                errors="replace",
+                stdin=find.stdout,
+                stderr=subprocess.PIPE,
             )
+        return cpio.stderr
+    except Exception as exc:
+        raise InfrastructureError(
+            f"Unable to create cpio archive {filename!r}: %s"
+        ) from exc
 
 
 def uncpio(filename: str, directory: str) -> None:
     which("cpio")
-    with chdir(directory):
-        try:
-            subprocess.check_output(  # nosec
-                [
-                    "cpio",
-                    "--extract",
-                    "--make-directories",
-                    "--unconditional",
-                    "--file",
-                    filename,
-                ],
-                stderr=subprocess.STDOUT,
-            )
-        except subprocess.SubprocessError as exc:
-            raise InfrastructureError(
-                "Unable to extract cpio archive %r: %s" % (filename, exc)
-            )
+    try:
+        subprocess.run(
+            args=(
+                "cpio",
+                "--extract",
+                "--make-directories",
+                "--unconditional",
+                "--file",
+                filename,
+            ),
+            check=True,
+            cwd=directory,
+        )
+    except subprocess.CalledProcessError as exc:
+        raise InfrastructureError(
+            f"Unable to extract cpio archive {filename!r}"
+        ) from exc

commit 21bd91abf854fcdcb15381e6b42b27d61b8bb95e

Fix regressions introduced by a60fedbef91
Found by meta-lava, see https://gitlab.com/lava/meta-lava/-/jobs/11253596619

1. Missing format argument in exception handler
2. stdout/stderr capture in subprocess.run

lava_dispatcher/utils/compression.py
@@ -156,12 +156,13 @@ def cpio(directory: str, filename: str) -> str:
                 encoding="utf-8",
                 errors="replace",
                 stdin=find.stdout,
-                stderr=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
             )
-        return cpio.stderr
+        return cpio.stdout
     except Exception as exc:
         raise InfrastructureError(
-            f"Unable to create cpio archive {filename!r}: %s"
+            f"Unable to create cpio archive {filename!r}: {exc}"
         ) from exc
 
 
@@ -179,8 +180,10 @@ def uncpio(filename: str, directory: str) -> None:
             ),
             check=True,
             cwd=directory,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
         )
     except subprocess.CalledProcessError as exc:
         raise InfrastructureError(
-            f"Unable to extract cpio archive {filename!r}"
+            f"Unable to extract cpio archive {filename!r}: {exc}"
         ) from exc