Linaro/lava

commit 1057c66a3df69d24cd2e80fb7e20f29520c42de8

Do not switch working directory when running CompressOverlay
Switching working directory is very thread unsafe and can lead to
hard to diagnose bugs. Instead use the second argument to `tar.add`
to specify the name the file will be archived under.

Use `os.path.abspath("%s/%s" % (location, lava_test_results_dir))`
to calculate the overlay working directory same as in other actions.
The lava directory name used in archive can be calculated using
`os.path.relpath(lava_path, location)`. This can handle situations
where lava directory is not a root directory.

lava_dispatcher/actions/deploy/overlay.py
@@ -18,7 +18,6 @@ from lava_dispatcher.action import Action, Pipeline
 from lava_dispatcher.actions.deploy.testdef import TestDefinitionAction
 from lava_dispatcher.protocols.multinode import MultinodeProtocol
 from lava_dispatcher.protocols.vland import VlandProtocol
-from lava_dispatcher.utils.contextmanager import chdir
 from lava_dispatcher.utils.filesystem import check_ssh_identity_file
 from lava_dispatcher.utils.network import dispatcher_ip
 
@@ -593,25 +592,21 @@ class CompressOverlay(Action):
             self.logger.error(self.errors)
             return connection
         connection = super().run(connection, max_end_time)
-        # This will split the path into two parts. Second part is only the
-        # bottom level directory. The first part is the rest.
-        # Example: /run/mount/lava/ will be split to /run/mount/ and lava/
-        # Used to create a tarball with only the bottom level dir included and
-        # not with the whole lava_test_results_dir property.
-        results_dir_list = os.path.split(os.path.normpath(lava_test_results_dir))
-        with chdir(
-            os.path.join(location, os.path.relpath(results_dir_list[0], os.sep))
-        ):
-            try:
-                with tarfile.open(output, "w:gz") as tar:
-                    tar.add("%s" % results_dir_list[1])
-                    # ssh authorization support
-                    if os.path.exists("./root/"):
-                        tar.add(".%s" % "/root/")
-            except tarfile.TarError as exc:
-                raise InfrastructureError(
-                    "Unable to create lava overlay tarball: %s" % exc
-                )
+
+        # For example, location="/foo/bar" and lava_test_results_dir="/lava-12345"
+        # The overlay working directory will be "/foo/bar/lava-12345".
+        # The tar archive will contain this directory as "lava-12345"
+        lava_path = os.path.abspath("%s/%s" % (location, lava_test_results_dir))
+        lava_dir_name = os.path.relpath(lava_path, location)
+        try:
+            with tarfile.open(output, "w:gz") as tar:
+                tar.add(lava_path, lava_dir_name)
+                # ssh authorization support
+                root_dir = os.path.join(location, "root")
+                if os.path.exists(root_dir):
+                    tar.add(root_dir, "./root")
+        except tarfile.TarError as exc:
+            raise InfrastructureError("Unable to create lava overlay tarball") from exc
 
         self.set_namespace_data(
             action=self.name, label="output", key="file", value=output

commit a34396a9d6ce2a2dca8b54ee2202f5068c6863e8

lava_dispatcher: fix CompressOverlay action for custom lava_test_results_dir
When `lava_test_results_dir` is not under root, e.g., `/opt/lava-%s`,
`os.path.basename(lava_test_results_dir)` ensures that only the
bottom-level dir is archived; while the current code archives
`opt/lava-%s` and leads to `FileNotFoundError`.

The issue is introduced by 1057c66a3df69d24cd2e80fb7e20f29520c42de8.

Signed-off-by: Chase Qi <chase.qi@linaro.org>

lava_dispatcher/actions/deploy/overlay.py
@@ -597,10 +597,9 @@ class CompressOverlay(Action):
         # The overlay working directory will be "/foo/bar/lava-12345".
         # The tar archive will contain this directory as "lava-12345"
         lava_path = os.path.abspath("%s/%s" % (location, lava_test_results_dir))
-        lava_dir_name = os.path.relpath(lava_path, location)
         try:
             with tarfile.open(output, "w:gz") as tar:
-                tar.add(lava_path, lava_dir_name)
+                tar.add(lava_path, os.path.basename(lava_path))
                 # ssh authorization support
                 root_dir = os.path.join(location, "root")
                 if os.path.exists(root_dir):