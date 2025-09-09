Linaro/lava

commit be726f94af3f13ca1a8b4c5983aceac812c89724

Use null separated filenames for cpio
Otherwise a file with a new line in the name will break the cpio
archiving.

lava_dispatcher/utils/compression.py
@@ -136,13 +136,21 @@ def cpio(directory: str, filename: str) -> str:
 
     try:
         find = subprocess.Popen(
-            args=("find", "."),
+            args=("find", ".", "-depth", "-print0"),
             cwd=directory,
             stdout=subprocess.PIPE,
         )
         with find:
             cpio = subprocess.run(
-                args=("cpio", "--create", "--format", "newc", "--file", filename),
+                args=(
+                    "cpio",
+                    "--create",
+                    "--null",
+                    "--format",
+                    "newc",
+                    "--file",
+                    filename,
+                ),
                 cwd=directory,
                 check=True,
                 encoding="utf-8",

tests/lava_dispatcher/test_compression.py
@@ -238,6 +238,9 @@ class TestCpio(TestCase):
             test_file = test_dir / "bar"
             test_file.write_text("foobar")
 
+            newline_file = test_dir / "foo\nbar"
+            newline_file.write_text("one\ntwo\n")
+
             archive_file = tmp_dir_path / "foo.cpio"
 
             output = cpio(str(test_dir), str(archive_file))
@@ -251,3 +254,4 @@ class TestCpio(TestCase):
             uncpio(str(archive_file), str(unpack_dir))
 
             self.assertEqual((unpack_dir / "bar").read_text(), "foobar")
+            self.assertEqual((unpack_dir / "foo\nbar").read_text(), "one\ntwo\n")

commit 95ca242d45611ae210ba1c0ae0adc352c05f8b48

cpio: fix regression introduced by be726f94af3f13ca1a8b4c5983aceac812c89724
Remove cpio parameter `-depth` which is apparently not compatible with old
u-boot

With `-depth`, beaglebone-black and apq8016-sbc-uboot are failing to boot with
`Kernel panic - not syncing: No working init found`.

Found by lavafed.

See https://staging.validation.linaro.org/scheduler/job/397173 vs
https://staging.validation.linaro.org/scheduler/job/397157

lava_dispatcher/utils/compression.py
@@ -136,7 +136,7 @@ def cpio(directory: str, filename: str) -> str:
 
     try:
         find = subprocess.Popen(
-            args=("find", ".", "-depth", "-print0"),
+            args=("find", ".", "-print0"),
             cwd=directory,
             stdout=subprocess.PIPE,
         )