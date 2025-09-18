76de864ea295df08949572d8f53b52bc1956dad8
Igor Ponomarev <igor.ponomarev@collabora.com>
2025-09-18 14:44:07 +0100

    gitlab-ci: Use Debian 12 for django_migrations job
    Support for Debian 11 is getting removed soon.

--- .gitlab-ci/analyze/django_migrations.sh
+++ .gitlab-ci/analyze/django_migrations.sh
@@ -6,10 +6,10 @@ if [ "$1" = "setup" ]
 then
   set -x
   apt-get -q update
-  DEPS=$(./share/requires.py -p lava-server -d debian -s bullseye -n)
+  DEPS=$(./share/requires.py -p lava-server -d debian -s bookworm -n)
   apt-get install --no-install-recommends --yes $DEPS
   sudo -u postgres psql -c "CREATE DATABASE devel ;"
 else
   set -x
-  python3 ./manage.py makemigrations --no-input --check --dry-run
+  python3 ./manage.py makemigrations --verbosity 3 --no-input --check --dry-run
 fi
