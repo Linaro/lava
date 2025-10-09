Linaro/lava

commit 094570487c18434b476e5789f42d63c489bc586f

Check job view permissions for REST API test cases and test suites endpoints
Otherwise REST API will reveal job details and metadata.
This potentially can leak secrets like tokens or passwords.

lava_rest_app/v02/views.py
@@ -266,6 +266,16 @@ class TestSuiteViewSet(NestedViewSetMixin, viewsets.ReadOnlyModelViewSet):
     serializer_class = serializers.TestSuiteSerializer
     filterset_class = filters.TestSuiteFilter
 
+    def get_queryset(self):
+        job_id = self.kwargs["parent_lookup_job_id"]
+
+        job = TestJob.objects.select_related("submitter").get(id=job_id)
+
+        if not job.can_view(self.request.user):
+            raise PermissionDenied
+
+        return super().get_queryset()
+
     @detail_route(methods=["get"], suffix="csv")
     def csv(self, request, **kwargs):
         limit = request.query_params.get("limit", None)
@@ -310,6 +320,16 @@ class TestCaseViewSet(NestedViewSetMixin, viewsets.ReadOnlyModelViewSet):
     queryset = TestCase.objects
     serializer_class = serializers.TestCaseSerializer
     filterset_class = filters.TestCaseFilter
+
+    def get_queryset(self):
+        job_id = self.kwargs["parent_lookup_suite__job_id"]
+
+        job = TestJob.objects.select_related("submitter").get(id=job_id)
+
+        if not job.can_view(self.request.user):
+            raise PermissionDenied
+
+        return super().get_queryset()
 
 
 class DeviceTypeViewSet(base_views.DeviceTypeViewSet):

commit a83698224aae1c89864c6265dd88e701c1d98411

rest-api: fix crash on 404 introduced by 094570487c

lava_rest_app/v02/views.py
@@ -553,7 +553,10 @@ class TestSuiteViewSet(NestedViewSetMixin, viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         job_id = self.kwargs["parent_lookup_job_id"]
 
-        job = TestJob.objects.select_related("submitter").get(id=job_id)
+        try:
+            job = TestJob.objects.select_related("submitter").get(id=job_id)
+        except TestJob.DoesNotExist:
+            raise NotFound
 
         if not job.can_view(self.request.user):
             raise PermissionDenied
@@ -608,7 +611,10 @@ class TestCaseViewSet(NestedViewSetMixin, viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         job_id = self.kwargs["parent_lookup_suite__job_id"]
 
-        job = TestJob.objects.select_related("submitter").get(id=job_id)
+        try:
+            job = TestJob.objects.select_related("submitter").get(id=job_id)
+        except TestJob.DoesNotExist:
+            raise NotFound
 
         if not job.can_view(self.request.user):
             raise PermissionDenied