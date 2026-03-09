05b6acf8abe0829b6a4339f455f4179dbf33889d
Chase Qi <chase.qi@linaro.org>
2026-03-09 08:54:24 +0800

    lava_common: fix secondary deployment schema
    * The same as `sata.py`, add `sd.py`. `to: sd` is already supported.
    * Allow to define `root_partition` for the `image` that needs overlay.
    
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

---
+++ lava_common/schemas/deploy/sd.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2026 Linaro Limited
+#
+# Author: Chase Qi <chase.qi@linaro.org>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+from . import usb
+
+
+def schema():
+    return usb.schema()
