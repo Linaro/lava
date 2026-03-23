7097c6311bb72f99e4bb712b724498899494ec3d
Chase Qi <chase.qi@linaro.org>
2026-03-23 16:17:52 +0800

    Build and embed mkdoc documentation
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

--- .gitlab-ci/build/doc.sh
+++ .gitlab-ci/build/doc.sh
@@ -6,8 +6,8 @@ if [ "$1" = "setup" ]
 then
   set -x
   apt-get update -qq
-  apt-get install --no-install-recommends -y git make python3 python3-sphinx python3-sphinx-bootstrap-theme
+  apt-get install --no-install-recommends -y git mkdocs-material python3-pymdownx
 else
   set -x
-  make -C doc/v2 html
+  mkdocs build -f doc/mkdocs.yml
 fi
