e383479f770bc2e8816d38714f91e19d21982dfb
Igor Ponomarev <igor.ponomarev@collabora.com>
2026-05-11 15:04:44 +0100

    Normalize arguments between `DockerContainer.run` and `DockerRun.run`
    `DockerContainer` is a subclass of `DockerRun` yet it implemented
    different function signature for `run()` method.
    
    Change both of them so they take the same arguments. This means
    `DockerRun.run` now takes a list of strings as first argument
    instead of having a variable number of string arguments.
    
    Signed-off-by: Igor Ponomarev <igor.ponomarev@collabora.com>

--- tests/lava_dispatcher/utils/test_docker.py
+++ tests/lava_dispatcher/utils/test_docker.py
@@ -122,7 +122,7 @@ def test_run_architecture_check_failure(mocker):
 
     action = mocker.MagicMock()
     docker = DockerRun("myimage")
-    docker.run("date", action=action)
+    docker.run(["date"], action=action)
 
     check_output.assert_any_call(["arch"], text=True)
     check_output.assert_any_call(
@@ -144,7 +144,7 @@ def test_run_architecture_check_success(mocker):
 
     action = mocker.MagicMock()
     docker = DockerRun("myimage")
-    docker.run("echo", action=action)  # no crash = success
+    docker.run(["echo"], action=action)  # no crash = success
 
     check_output.assert_any_call(["arch"], text=True)
     check_output.assert_any_call(
@@ -167,7 +167,7 @@ def test_run_with_action(mocker):
     action = mocker.MagicMock()
 
     docker = DockerRun("myimage")
-    docker.run("date", action=action)
+    docker.run(["date"], action=action)
 
     check_arch.assert_called()
     action.run_cmd.assert_has_calls(
@@ -186,7 +186,7 @@ def test_run_with_local_image_does_not_pull(mocker):
     docker.local(True)
     action = mocker.MagicMock()
     action.run_cmd.return_value = 0
-    docker.run("date", action=action)
+    docker.run(["date"], action=action)
     action.run_cmd.assert_has_calls(
         [
             mocker.call(
@@ -213,7 +213,7 @@ def test_run_with_local_image_does_not_pull_when_missing(mocker):
     docker.local(True)
     action = mocker.MagicMock()
     action.run_cmd.return_value = 1
-    docker.run("date", action=action)
+    docker.run(["date"], action=action)
     action.run_cmd.assert_has_calls(
         [
             mocker.call(
