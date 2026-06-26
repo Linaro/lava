9f50b600b1dec867b8c638e454abee423bc8738e
Fathi Boudra <fathi.boudra@linaro.org>
2026-06-26 12:42:35 +0200

    scheduler: validate HEALTH_FREQUENCY_HOURS
    Add HEALTH_FREQUENCY_HOURS validation to AppConfig.ready() where it
    runs after all settings are fully resolved. Reject non-positive or
    non-integer values with ImproperlyConfigured.
    
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- lava_scheduler_app/apps.py
+++ lava_scheduler_app/apps.py
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 from django.apps import AppConfig
+from django.core.exceptions import ImproperlyConfigured
 
 
 class LAVASchedulerConfig(AppConfig):
@@ -12,7 +13,18 @@ class LAVASchedulerConfig(AppConfig):
     verbose_name = "lava_scheduler_app"
 
     def ready(self):
+        from django.conf import settings
+
         import lava_scheduler_app.checks  # pylint: disable=unused-import
         from lava_scheduler_app.signals import register_scheduler_app_signals
 
         register_scheduler_app_signals()
+
+        # Validate HEALTH_FREQUENCY_HOURS
+        hf = settings.HEALTH_FREQUENCY_HOURS
+        if hf is not None and (
+            isinstance(hf, bool) or not isinstance(hf, int) or hf <= 0
+        ):
+            raise ImproperlyConfigured(
+                "HEALTH_FREQUENCY_HOURS must be a positive integer, got: %r" % hf
+            )
